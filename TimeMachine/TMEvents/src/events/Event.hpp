a4e782df32831499af0ebc0957ffe70434b51007
Jake Shirley <navi@odinseye.org>
2014-04-10 19:47:26 -0700

    Removed artifact event files

--- TimeMachine/TMEvents/src/events/Event.hpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:389fb981aab56beb417a35bc0420c70e03147a9d7dd1bfb9f27922e425c83a39
-size 2295
--- TimeMachine/TMEvents/src/events/EventHandler.hpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:557f5c0b4828568134b012f94ea32f190d65af29ea1e058591d03b56a612f857
-size 391

00ec4e0b3c1922e31dba6342262a8625f6850902
Jake Shirley <navi@odinseye.org>
2014-04-10 19:47:02 -0700

    Moved event files and added System priority

---
+++ TimeMachine/TMEvents/src/Event.hpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:410fc3f6f0686048a52007cbe0cd4e81c55bcd735c95ce5375568be7c04f89a4
+size 2304
---
+++ TimeMachine/TMEvents/src/EventHandler.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6ed7b06babf9dc8176aa8ccb20529586635c2cc587369722fa25af961232ecf5
+size 831

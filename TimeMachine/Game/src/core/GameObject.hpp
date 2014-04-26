6cadf55062f65070cb39f5b3c14617b2c931aeac
Jake Shirley <navi@odinseye.org>
2014-04-25 20:07:15 -0700

    Added accessory for GameObjects' ID and if they're dead

--- TimeMachine/Game/src/core/GameObject.hpp
+++ TimeMachine/Game/src/core/GameObject.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e11231967cc0afc4cabd28cd7d95b5576e7b4d5ce7d355558068aac27830f3d6
-size 1255
+oid sha256:bac8a79bad6372693b4f335dd65a361278ce870790cca5ede4d200c724a2118a
+size 1358

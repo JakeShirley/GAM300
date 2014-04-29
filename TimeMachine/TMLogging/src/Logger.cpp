4beec4c20ea836c5cb8dbfd991898f077051c78b
Jake Shirley <navi@odinseye.org>
2014-04-29 10:33:44 -0700

    Added ability to change the log level for the Logger

--- TimeMachine/TMLogging/src/Logger.cpp
+++ TimeMachine/TMLogging/src/Logger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d4d855b30fc82dff225336ef978a21d057862d6d81995b84db136ae44c1a6d3
-size 756
+oid sha256:6b033de0e06d6dfa7545c3b498158765aef14272640f4051eaefceae17a314d9
+size 863
--- TimeMachine/TMLogging/src/Logger.hpp
+++ TimeMachine/TMLogging/src/Logger.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08eef5a400271a58db0c7d1ad8ac479444d22c48a03032f6dd7f5384e0b9b19a
-size 230
+oid sha256:00a4762341e4aca00ee2cff9538e550129eba415cfcd5640b41f1affcad07d03
+size 274

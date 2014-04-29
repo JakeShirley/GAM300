3c8c6978d53fee2832b708880f8e323721a46021
Jake Shirley <navi@odinseye.org>
2014-04-29 10:33:23 -0700

    Updated lua syntax error window to be a bit more nice

--- TimeMachine/TMApplication/src/window/CodeWindow.cpp
+++ TimeMachine/TMApplication/src/window/CodeWindow.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44c93e62e41de1fb863ecd3f34d053052418729cf0c38a0ee4ded536843672bb
-size 16421
+oid sha256:ffeabd900ec05f9c7bb403509e6c088fb3fc049f25b019e82fcfe1565a2ed1ca
+size 16494

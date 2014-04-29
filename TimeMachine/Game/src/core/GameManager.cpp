0b0b806f2ca6edf950bd690b25bcc4de0c7fad3e
Jake Shirley <navi@odinseye.org>
2014-04-29 13:37:04 -0700

    Fixed memory leak by clearing game objects when the game shuts down

--- TimeMachine/Game/src/core/GameManager.cpp
+++ TimeMachine/Game/src/core/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac152c089b27a52ddf8863c1f569e6cd1a615398f8f1b4e746ade2b20a006706
-size 2742
+oid sha256:76ab91b1149cfa0c943fc06034859bcbb9833015a1ee84b6963d73d1bddc9e03
+size 2840

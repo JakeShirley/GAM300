c88ae276253ad7b64a52effa012b64c4569be27f
Jake Shirley <navi@odinseye.org>
2014-04-29 14:14:05 -0700

    Moved output window to the bottom of the window by default

--- TimeMachine/TMApplication/src/wx/wxBaseClasses.h
+++ TimeMachine/TMApplication/src/wx/wxBaseClasses.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3555e381a15fea0630be37b8b7a2348fac0a50a97a39bb2339f3f9e3d888ce4e
+oid sha256:40eb03fca48a0d58a51f06fb5344ccb143e0b1c40ced449835986b731fe6ce8f
 size 6390

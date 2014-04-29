20f65e80435ce42a1da2f1ecb8abf62215a7c3ac
Jake Shirley <navi@odinseye.org>
2014-04-29 10:55:28 -0700

    Updated various UI elements in editor

--- TimeMachine/TMApplication/src/wx/wxBaseClasses.h
+++ TimeMachine/TMApplication/src/wx/wxBaseClasses.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7af74a7db0588502adb6ec410c57ffd5bd70570d3aa400ee120b6587b6f8810
-size 6333
+oid sha256:3555e381a15fea0630be37b8b7a2348fac0a50a97a39bb2339f3f9e3d888ce4e
+size 6390

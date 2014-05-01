2b4f20a0c3b456151d7c6f273ccdfda0d72777ca
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-04-30 20:35:33 -0700

    Stupid typo in collider component header

--- TimeMachine/Game/src/core/components/Collider.hpp
+++ TimeMachine/Game/src/core/components/Collider.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63dcd41dc65ec64101f0dc0556175132eeccbadad501f9d22f0db3dc61a85008
-size 362
+oid sha256:9137d9237ef91cf07464ebe0215134a3c3b09b7d9bcc0d2499d9680c8dc37265
+size 361

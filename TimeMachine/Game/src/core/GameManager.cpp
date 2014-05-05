148eba3226fb927cf17d56d4d981c5966fe3ad53
Jake Shirley <navi@odinseye.org>
2014-05-04 22:43:30 -0700

    Destroy GameObjects when shutting down game

--- TimeMachine/Game/src/core/GameManager.cpp
+++ TimeMachine/Game/src/core/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29e476e657faec55de4889e3624d6b72800a6f8a3d73b6c33e43b9596e3ca2f8
-size 3919
+oid sha256:9829bdce2b7054da86c23c6f2f429990ed3cab4d0610125694b342079b032404
+size 3845

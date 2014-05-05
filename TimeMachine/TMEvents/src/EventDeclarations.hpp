628d7b0259377d9a83487df9e337c0ea42e4b527
Jake Shirley <navi@odinseye.org>
2014-05-04 21:36:47 -0700

    Added a few Event declarations

--- TimeMachine/TMEvents/src/EventDeclarations.hpp
+++ TimeMachine/TMEvents/src/EventDeclarations.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3224403626b6a9afcdf5836291d536fbd3fa8f6272e617bbbcbfb5df521b5413
-size 1009
+oid sha256:87e83292fcce65bd4a72b692b5e8315bbd0382a30e71dde8f4b418787fd4ab20
+size 1066

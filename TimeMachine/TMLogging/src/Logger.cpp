e222ab94dda3b7f8e78d5735d0b82c63c9041d7c
Jake Shirley <navi@odinseye.org>
2014-05-04 22:44:03 -0700

    Pop a message box on assert level logging

--- TimeMachine/TMLogging/src/Logger.cpp
+++ TimeMachine/TMLogging/src/Logger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7746954e1442e09510dac85821cd881693bef759aa91d368f97259a3b770c7dc
-size 937
+oid sha256:7cc92524231241a5143c98539bd9dbb705b82c36a0ac30cd59ae8e056debeb27
+size 1535

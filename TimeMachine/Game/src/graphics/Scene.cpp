30ee9d082a803567dbbf7f23799556962abe624b
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-05-06 15:25:40 -0700

    Fixed crash caused by having an object with just a collider. Seemingly successful GJK detection. Added overload to cube creator to allow specific position and scale.

--- TimeMachine/Game/src/graphics/Scene.cpp
+++ TimeMachine/Game/src/graphics/Scene.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eecf37a924ccdba8701ecab5ec5126e20e26ad108e23aff0e3a531489b5233a5
-size 8874
+oid sha256:cc2086d5f07306a5c7021a557a4f4a0bb3d4aafedbdb9eac52060a28a83c574c
+size 9313
--- TimeMachine/Game/src/graphics/Scene.hpp
+++ TimeMachine/Game/src/graphics/Scene.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78b4e91a2469544ab0fe33458fa272192ecd672206afa390cafeafa730617dca
-size 671
+oid sha256:ecd8d1ef445e005f0c00148af0c14621e68e8bfede9ebfad83b0d063b4a92079
+size 768

86b2b06d065fcd1b8e304b0ac87e2eb02531f19e
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-05-05 22:49:43 -0700

    Added functions to evaluate 3 (triangle) and 4 (tetrahedron) point simplexes, which is the heavy work of GJK. All that's left is the main GJK search loop before debugging begins.

--- TimeMachine/TMPhysics/src/OopsMath.cpp
+++ TimeMachine/TMPhysics/src/OopsMath.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab07753e92fc0469c137001dea81bc8d52e1b31c7c4f9dadf4e401b43e56134f
-size 2401
+oid sha256:256e8eb4637e6d21e971cdc0ef0346ae3f774caf9be583d4824dbc784ae01435
+size 6219
--- TimeMachine/TMPhysics/src/OopsMath.hpp
+++ TimeMachine/TMPhysics/src/OopsMath.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:895aa3eaaf2aaa7cdaab7d5d808f95c9c8a76b865f9a28508ee8d7780744f0b7
-size 1593
+oid sha256:c144b65e7bf7cb03ec125453b180fac9d5c15b0ef2568c5f3781baadae7dc36f
+size 2000

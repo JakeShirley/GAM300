2b9b78114514ab95fd8c3239924e1b59e851a3f6
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-05-05 17:08:28 -0700

    Support function for convex polygons. Simplex class for GJK, computing line case, still missing triangle and tetrahedron.

--- TimeMachine/TMPhysics/src/OopsMath.hpp
+++ TimeMachine/TMPhysics/src/OopsMath.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e07e3fe6eb3cb927dd62ddc7b3f0481b9eab34122f7cff17ad9a19aadc94f178
-size 352
+oid sha256:895aa3eaaf2aaa7cdaab7d5d808f95c9c8a76b865f9a28508ee8d7780744f0b7
+size 1593

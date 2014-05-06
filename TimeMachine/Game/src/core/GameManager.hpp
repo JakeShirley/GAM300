203e74f1c93790ac0b41541accef675ff9650173
Jake Shirley <navi@odinseye.org>
2014-05-06 16:39:32 -0700

    Added GetDt to game manager

--- TimeMachine/Game/src/core/GameManager.hpp
+++ TimeMachine/Game/src/core/GameManager.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f0c158c5f51ce36c3a7a6dad582c462c6d6237c13056cf7a59773eb8dde7ca4
-size 1664
+oid sha256:fb64b2d97f95e0d3b7512ed1e2de0a6bf0e83807a93bcef149656276d6d2fd47
+size 1711

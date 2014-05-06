5ea340af1211bd22dc06a3dd71e3e150a0e04fec
Jake Shirley <navi@odinseye.org>
2014-05-06 16:00:22 -0700

    Added clean_objects()

--- TimeMachine/Game/src/core/GameManager.hpp
+++ TimeMachine/Game/src/core/GameManager.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29fca6a342869e2e76b87740bfaef78ef78c46d2670272dea8619fc8ff512401
-size 1604
+oid sha256:2f0c158c5f51ce36c3a7a6dad582c462c6d6237c13056cf7a59773eb8dde7ca4
+size 1664

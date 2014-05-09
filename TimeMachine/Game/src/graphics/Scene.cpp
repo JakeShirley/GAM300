987e5e712ffc9a30e7a1a5037629abb1f147d3f1
Jake Shirley <navi@odinseye.org>
2014-05-08 17:51:29 -0700

    Made J/K not crash game when spawning/destroying test objects

--- TimeMachine/Game/src/graphics/Scene.cpp
+++ TimeMachine/Game/src/graphics/Scene.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d726d5fc1b1a8f56065a8cd32e1caec3c317c4e160e96712cf3a2a255006dd9
-size 9405
+oid sha256:f5e23a5312dd2fd77982eb723ffc1232f15b58a731639bd25cca5cf059f59944
+size 9678

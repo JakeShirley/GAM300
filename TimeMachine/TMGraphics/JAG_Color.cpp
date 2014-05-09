602fe8ce4b5b08ee0d535d25e2d6f7b48e6646d5
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-05-08 21:07:26 -0700

    I did it

--- TimeMachine/TMGraphics/JAG_Color.cpp
+++ TimeMachine/TMGraphics/JAG_Color.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7d841b959f678b71cbcc0d052cadb106e29894fa36303ecb470083bdfa0724b
-size 3267
+oid sha256:9a2c519277c540322c7299ebc9a2326ba58b745174bdae0eb54795cb7aab097f
+size 3268

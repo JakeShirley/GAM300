b971210941b335eaf33583817a33bdef257ee22f
Jake Shirley <navi@odinseye.org>
2014-05-13 12:49:45 -0700

    Removed lots of comments from main

--- TimeMachine/Game/main.cpp
+++ TimeMachine/Game/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7740f3793358cb2212bdbbb33c6bdd797fa5446d04731f3dabbe5075cf0e938f
-size 4642
+oid sha256:5680aacf72029bd0eb6c489f4a2c0bc07c973537e80270254644cddde3dc4194
+size 763

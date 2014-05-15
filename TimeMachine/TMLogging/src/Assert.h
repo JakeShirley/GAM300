d5789b80a0ad59eae63b3629d0259828d899e356
Jake Shirley <navi@odinseye.org>
2014-05-14 17:04:30 -0700

    Disabled conditional constant warning for Asserts

--- TimeMachine/TMLogging/src/Assert.h
+++ TimeMachine/TMLogging/src/Assert.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddb15be010bff8e37460bf70f3aafe178eb241e7b33220b3042e38c91f42b421
-size 730
+oid sha256:6a625747e46ccffff1f5eafb35a5f1bc56a368def076081823dc7c5a9150a958
+size 766

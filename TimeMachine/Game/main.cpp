41dab6f87c76db0380df98419afee63cef2a6584
Jake Shirley <navi@odinseye.org>
2014-05-22 11:40:38 -0700

    Added NOTE(), TODO(), and WARN() macros to Assert.h

--- TimeMachine/Game/main.cpp
+++ TimeMachine/Game/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2d0d8021ab2a56184e419336226f2d892e48c814e3e5a0cd84d5f734cbab3e3
-size 3373
+oid sha256:a896eb0aff9d86469fa155603fd8e5720712ce61ca95d3a53caed3a039b84a85
+size 3335
--- TimeMachine/TMLogging/src/Assert.h
+++ TimeMachine/TMLogging/src/Assert.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d0e1acd952c7f3cf8bc835e99d7dac8682b3918404981421313bcf5463c960a
-size 1440
+oid sha256:06ff21ba561ddf337a299555c1503e9b9e2d62d1976679bea20aee4d434a4db9
+size 2101

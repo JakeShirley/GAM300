2abd0dc5abd3ab3a9a0d60fdab11fb6df81dfdc4
Jake Shirley <navi@odinseye.org>
2014-06-02 18:12:24 -0700

    Fixed file in Game solution

--- TimeMachine/Game/src/ComponentDeclarations.h
+++ TimeMachine/Game/src/ComponentDeclarations.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9771006e56114119dd9aa9e3d7b056eb0b251df000ef3089070fb2dbebcf204f
-size 132
+oid sha256:7085ee3750b2f86b52599b0c3789c33da8095350c95d2ead46d1fc4b19b13883
+size 133

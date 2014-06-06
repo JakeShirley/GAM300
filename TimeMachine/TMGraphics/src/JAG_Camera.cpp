b6ba9deb07d2a3a7424aeb4996240a99349bb368
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-06-05 20:20:41 -0700

    Visual Assist Rename didn't catch a reference. Fixed it.

--- TimeMachine/TMGraphics/src/JAG_Camera.cpp
+++ TimeMachine/TMGraphics/src/JAG_Camera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d76205a19414c1c34aca5a4a97b7bcd63bb47ba8b86937a89a0711893b55911c
-size 2150
+oid sha256:3c770cc5516e66b61f2758e17d49719bceb04a331361afecd3cdd5d64742f9e3
+size 2153

a76431ec7dfc940fa57013e36340aa878c5a4337
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-06-06 16:21:59 -0700

    Reduced slices on vector arrows; Removed code duplication in camera LookAt calculations; Made sphere detail scale with radius

--- TimeMachine/TMGraphics/src/JAG_Camera.h
+++ TimeMachine/TMGraphics/src/JAG_Camera.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd53b32f08cbe17859b131fc3cd260f5c2bc7e493492fad0b8c9dc111d46340b
-size 1031
+oid sha256:13fd2150bda193c3241c05f8382c36d89e3d7d334544ec9cbe74099b4d645e81
+size 994

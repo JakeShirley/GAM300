13189c22d8289b0903ac445bdc36b300e42a6ab3
Jake Shirley <navi@odinseye.org>
2014-06-07 13:11:38 -0700

    Fixed some event disconnecting issues

--- TimeMachine/Game/src/DebugMover.cpp
+++ TimeMachine/Game/src/DebugMover.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:865b011e18e900463c1e837ac638c2b58b76cd9c673abe3ab7c1e07305e8b528
-size 1492
+oid sha256:9b817e7e782c4648ffd50612e7874c02ae1419218ac82a0115c51b02d3f6db5b
+size 1564

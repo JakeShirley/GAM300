0ac6bb7749a8cbceb2d66845af4b6727a3c81321
Jake Shirley <navi@odinseye.org>
2014-06-07 13:12:13 -0700

    Added 'R' to restart the test level

--- TimeMachine/Game/src/CppLevel.cpp
+++ TimeMachine/Game/src/CppLevel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:793c3ce8be6baf153a5c166b1d872730d80073b2dc5e9c98608be5a0046c3186
-size 1684
+oid sha256:bed40f5d1cbc890d8e391a9ffff451b53ad2700cc08114e0cbbc0f2604b1c5b5
+size 1998

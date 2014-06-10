dac4cbea46cc5311b8cff8512123da5243e87874
Jake Shirley <navi@odinseye.org>
2014-06-10 14:31:49 -0700

    Removed debug spam and removed a few lines from debug vectors

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05197efa57b83cd94335a4b9352d30c1be373ecaac9ffc0c9427292db7cb1739
-size 10012
+oid sha256:9d8e59a9bd99e659066fcfbdd1fd0cab55560603772badb90e7fa5b627ce626c
+size 10014
--- TimeMachine/TMGraphics/src/JAG_DebugDraw.cpp
+++ TimeMachine/TMGraphics/src/JAG_DebugDraw.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:245374d09d8bad49ed7fe3768520503be2440fe6828dcd3944dff22bd824e1af
-size 7477
+oid sha256:a514a3ab527c23cf4f37e46c42a7442d47c7e512ff8051f6668bf9b7fa8cc706
+size 7486

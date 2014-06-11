52afb80698eff79f164129c8f85b7e21c0512b18
Jake Shirley <navi@odinseye.org>
2014-06-11 11:22:31 -0700

    Added Shift+Right click move camera

--- TimeMachine/Game/src/Camera.cpp
+++ TimeMachine/Game/src/Camera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23e358a39969296a3d9d25918633bc126fd3b6fa48ea465cb9fac84467664994
-size 2854
+oid sha256:4f1927687cc09def2c59170cf10bd4ed02e3dfcfcc9ff6aaf6e1c5e2c1aa308f
+size 2906
--- TimeMachine/TMApplication/src/wxBaseClasses.cpp
+++ TimeMachine/TMApplication/src/wxBaseClasses.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f4464fb7e655ee4cfb995bddaf34a7f662f15ecc14555d65dbc005321ed8108
-size 27376
+oid sha256:61b0dad65caab8f952bf761a1f31eeaa52ed664adebd5a8fa55df532c3adde1d
+size 27326

d044e2b20fa440d178bab94ed29deaca686c556e
Jake Shirley <navi@odinseye.org>
2014-06-12 22:59:54 -0700

    Updated EnumProperty

--- TimeMachine/TMApplication/src/PropertyEntry.h
+++ TimeMachine/TMApplication/src/PropertyEntry.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bc86afff076bd34b409e0f1ae21a350ef68b2ca8dde3efe2d6c74d388e66d37
-size 6894
+oid sha256:54f0e871e3b48c30da837dd1c0cf88401a1ec4080797174e3e7bbc9425d41057
+size 6950

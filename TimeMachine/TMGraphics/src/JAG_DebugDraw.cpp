4060bc212e7048b760dbd164132fc1a041b8973d
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-06-16 16:40:41 -0700

    Buffer usage settings, switched to com (smart) pointers. Untested functions in RenderContext may have bugs with smart pointers

--- TimeMachine/TMGraphics/src/JAG_DebugDraw.cpp
+++ TimeMachine/TMGraphics/src/JAG_DebugDraw.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40dad5a7674c1855650ff269d7b8161fb6e5a4c80ce9a8cb17d3c8afb350066c
-size 8685
+oid sha256:10bd68133a948a1f9d2b3c0f4bfc3798e1fee11178c4da9b449d251a5a63376c
+size 8707
--- TimeMachine/TMGraphics/src/JAG_RenderContext.h
+++ TimeMachine/TMGraphics/src/JAG_RenderContext.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da7478f16da73416b49bdd16452fec195ae0c1644ff3bcd9899556992a14edef
-size 7397
+oid sha256:24b1513cb1e14273bf16f39e392a6f963c3e9da1dc5fac34d8952b9e8fd34b8b
+size 7442

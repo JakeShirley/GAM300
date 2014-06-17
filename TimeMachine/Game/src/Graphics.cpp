59196e9bf4f0be7930d33793a6ffb5fce9b5f65f
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-06-16 19:24:42 -0700

    Cleaned up old comments/formatting, fixed interface thing in texture creation

--- TimeMachine/Game/src/Graphics.cpp
+++ TimeMachine/Game/src/Graphics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e416a206eeb6122b83cb4331949861bba15273e9ac8575cd872792adb6ca351
-size 7576
+oid sha256:4e2c34825a0ab320010a1bd79b4768cfecf3ab4a5980299c629b72276378d331
+size 6664
--- TimeMachine/Game/src/Graphics.h
+++ TimeMachine/Game/src/Graphics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f334a93d37b54624b6189c5daa76f7f15a2d9af25540e674c141f25082696a7f
-size 2008
+oid sha256:f76ff7545d1dcbf1b9432a92bd477e4218a194f7d44bde56ea01533c9595e2fa
+size 2058

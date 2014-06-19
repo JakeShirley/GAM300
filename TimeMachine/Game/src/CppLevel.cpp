3c1d87a55ba9ff77f951b078e51ce5d64b395275
Jake Shirley <navi@odinseye.org>
2014-06-19 12:06:03 -0700

    Actually centered at the origin

--- TimeMachine/Game/src/CppLevel.cpp
+++ TimeMachine/Game/src/CppLevel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c55308ba50e818941b9c05d7320b6b872158d87fefc98c4b74d5e746e21dbd7
-size 3772
+oid sha256:979d9da6190d29475531bd2b61e00d1c6ab75b80edb85385c527ce37daa30a6a
+size 3771

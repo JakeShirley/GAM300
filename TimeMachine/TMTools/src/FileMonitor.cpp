df11fa412af4cf07eb163dbeca807cc2ca9a00af
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-06-18 19:56:08 -0700

    Some FileMonitor implementation

--- TimeMachine/TMTools/src/FileMonitor.cpp
+++ TimeMachine/TMTools/src/FileMonitor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2888c0fc4df83d12352f3b1325c30f32a7c4effc7c3555d76157ffdeae024e0
-size 1528
+oid sha256:c27dca56e7b7a6f756601aa6d6946887d7987937b4191c532e1bd38f897083d1
+size 2640
--- TimeMachine/TMTools/src/FileMonitor.h
+++ TimeMachine/TMTools/src/FileMonitor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc70b197775fe40fd0f26023a719fbcaad336b593ea993c13036594ca80ab6e7
-size 734
+oid sha256:5ec48148d567aebdbf38eb13b9bfdd55466c44630b78a96d5e0d083a4a1906c0
+size 806

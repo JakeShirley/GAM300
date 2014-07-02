29af379881684913c0ec2c7bfef31366f6d0d7fd
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-07-01 18:40:06 -0700

    Removed some redundant calculations related to model to world transformations for models and inertia tensors.

--- TimeMachine/TMPhysics/src/OopsObject.cpp
+++ TimeMachine/TMPhysics/src/OopsObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2988d94ff0edfc9c635b0c61d00bab3d457df3db2652fdeb00a8cabb74e6f1a
-size 9402
+oid sha256:86fc8a87016299a137ab6dbff40e3d832fe35458c9761981acd937209ce9e34f
+size 9031

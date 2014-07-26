33c66573c3dcf173bfe95dcc357950add863a9a1
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-07-26 12:50:29 -0700

    Maybe actually fixed everything disappearing bug. God damn quaternions man, so needy with their normalization.

--- TimeMachine/TMPhysics/src/OopsContactConstraint.cpp
+++ TimeMachine/TMPhysics/src/OopsContactConstraint.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d067a38c1d060a154f45734b5587e122adb6bfd2687389ee1e33d65a97a5ee06
-size 7195
+oid sha256:76d0ce2728c9b6904c166a34f88a2ace0498659e34a06781bcbfcb3933bb19e5
+size 7142
--- TimeMachine/TMPhysics/src/OopsHandles.cpp
+++ TimeMachine/TMPhysics/src/OopsHandles.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c938222fcb0cf7d2b8df49336db2edfb0647ad8f219b9a26dacc768bfe1d026
-size 6123
+oid sha256:c8a6ffe298e08a96c480252a97004d7a99f640651812298d5c8198a78b0e7aae
+size 6139

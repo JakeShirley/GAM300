e82b9b0cf1d3cea31e0aa492a82652a128579307
garrett.allen <allengar22@gmail.com>
2014-07-29 20:05:44 -0700

    Updated Files for Meta Data.

---
+++ TimeMachine/TMTools/src/MetaData/Reflection.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:61b1e4436fe2a43095bf6db6d8765297192c8155a025b8527e622d8af8c396de
+size 238
---
+++ TimeMachine/TMTools/src/MetaData/TypeInfo.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4b325b3f9f770483deb00cee2e5001a24045ebeeee36fb0b7eb512d8bba07b34
+size 813

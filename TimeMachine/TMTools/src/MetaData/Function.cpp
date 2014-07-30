f5ec1c7f2556e2281f4463b3fc8f990ea8bfce09
garrett.allen <allengar22@gmail.com>
2014-07-29 20:56:39 -0700

    Method Binding through Meta Data

---
+++ TimeMachine/TMTools/src/MetaData/Function.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ac0783d264c9d181f871b0fa38ebccf37e66ca0b44bbee406c9a33f97a7a14d
+size 356
---
+++ TimeMachine/TMTools/src/MetaData/FunctionConverter.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41b00934adce4c409faa2fb6a899006ae997f8f9bae55cb489cd169e80f47a37
+size 4567

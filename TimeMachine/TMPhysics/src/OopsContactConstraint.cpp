683e6448468f371262c625f3eada4be228713342
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-08-03 13:07:56 -0700

    Sleeping no longer jeopardizes stability of stacks, although stacks still won't fall asleep.

--- TimeMachine/TMPhysics/src/OopsContactConstraint.cpp
+++ TimeMachine/TMPhysics/src/OopsContactConstraint.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76d0ce2728c9b6904c166a34f88a2ace0498659e34a06781bcbfcb3933bb19e5
-size 7142
+oid sha256:15273f44fd5e775915ee9401499e6c6a3163abcb76c57a8b893f1ab676cffad9
+size 7406
--- TimeMachine/TMPhysics/src/OopsHandles.cpp
+++ TimeMachine/TMPhysics/src/OopsHandles.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ea2f0038bb65006f5c6cff1dc882e64220306af4e10d04838a7b617b27a88ba
-size 6165
+oid sha256:bb9c33c94e366a546567fd70bdb83e61086339046490f8a6c5dfe3644224b2c8
+size 6155

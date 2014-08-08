695ecdc5450d6aa36daea1a257448f56804de2e0
Jake Shirley <navi@odinseye.org>
2014-08-08 10:24:23 -0700

    Fixed bug in DebugMover that would cause it to be a Camera

--- TimeMachine/Game/src/DebugMover.cpp
+++ TimeMachine/Game/src/DebugMover.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9389f6f46a2f6ea268e733ecfa4ce93c292a6fc29e04fcbe70e4bf3752a3716
-size 1583
+oid sha256:796e152b9afdf7743340a984a001a6eb028de000f714598c9a25f796004047f5
+size 1587

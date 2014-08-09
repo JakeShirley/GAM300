af2d03eda052c192c71e059b0d738365fbb1f479
Jake Shirley <navi@odinseye.org>
2014-08-09 13:32:53 -0700

    Hide buttons when properties are destroyed and default the property manager to select new pages when created

--- TimeMachine/TMApplication/src/PropertyGrid.cpp
+++ TimeMachine/TMApplication/src/PropertyGrid.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f23a4fe3c4ad198f62a3ea3766186c456f056a3c25505335fa9aab750f1e741a
-size 3059
+oid sha256:09af531eec4f80c4c802b8d9fd10f234e767010ece3a9fb540e46ffdae38bf77
+size 3103

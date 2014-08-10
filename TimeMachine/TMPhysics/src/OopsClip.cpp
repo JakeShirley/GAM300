1e0288c899c5d911a87ca48ae29cbc98572024da
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-08-10 13:43:05 -0700

    Fixed bug causing incorrect normals when linecasting.

--- TimeMachine/TMPhysics/src/OopsClip.cpp
+++ TimeMachine/TMPhysics/src/OopsClip.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a62d4e7de1de71a2b1f7e51851fbc97dffe6c080b2ed98f43433a1cef8873b8
-size 8437
+oid sha256:936f5c8ca51ed2bd5f097ba07a7d78598bd50280bfb236fbdecad0572ac4bf0a
+size 8483

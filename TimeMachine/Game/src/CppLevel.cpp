ec24df34ccc43626f6df852ea1570344def73a7e
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-08-10 13:04:47 -0700

    Raycasting from screen to world using mouse. The hit normal is bad, which I will fix later, but the hit point is fine. Jake, use this for picking please. All the required stuff is in CppLevel.cpp. Move it to wherever it should be and do good.

--- TimeMachine/Game/src/CppLevel.cpp
+++ TimeMachine/Game/src/CppLevel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:897c73b5eb93e63fec6da8786ab0147c4a1727a75d84a2d3c8409c3ffeaf1de0
-size 6875
+oid sha256:cac4ebfe5f753168546cd5db96a5cb3c57c6c2b1b1a71d14083114389b107abd
+size 7454
--- TimeMachine/Game/src/Physics.cpp
+++ TimeMachine/Game/src/Physics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0fea79fb2f459af5c85743e71c61a9df68d4f52aa0768c69a2ac8e93960697a
-size 5468
+oid sha256:a624ae246dc61aa575534f9341864ce2ab50e74afbb61e3003c5ba66a38dea5b
+size 5477

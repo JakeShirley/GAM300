9f1fb4e954be25fa0f26fda3d42d39c1117c7787
Jake Shirley <navi@odinseye.org>
2014-08-10 23:55:38 -0700

    Made HitInfo constructor public

--- TimeMachine/Game/src/HitInfo.h
+++ TimeMachine/Game/src/HitInfo.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b17298d894b0706c88d35a66bd6c3f757b2a193adb38049a8cf08956cc0f6119
-size 487
+oid sha256:7bcd555a41e69db2c21dd7e4602cb7e57a9c69026994c33d513e0c275de0dc13
+size 488

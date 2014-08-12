0fbf335d48c31313f3b2124932324ef0c10fb178
Jake Shirley <navi@odinseye.org>
2014-08-12 13:43:41 -0700

    Fix for property grids drawing oddly when you add things delayed

--- TimeMachine/TMApplication/src/PropertyEntry.cpp
+++ TimeMachine/TMApplication/src/PropertyEntry.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:928f144098b40feefb7e88b750fd27641e73130324de332d7bbe31c850f9c73a
-size 12788
+oid sha256:39c49d65fcfb1008f4e8137075c6b8811c087f0c674d0f7af1df18ae3cf10def
+size 12860

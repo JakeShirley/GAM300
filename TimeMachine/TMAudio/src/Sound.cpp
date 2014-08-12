03da5cbb0b82f39267360461e53d430f942838bc
Michael Chambers <m.chambers@digipen.edu>
2014-08-12 14:06:16 -0700

    3D sound functional.

--- TimeMachine/TMAudio/src/Sound.cpp
+++ TimeMachine/TMAudio/src/Sound.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3532623c3a4084c43b1450adf0c14d5f528350138619d0470191d90bb9794be8
-size 6789
+oid sha256:eb5df83f3bd52d3a23e0b51dc2579a6be64100cf58030ae40e5d376cdaf0c254
+size 6782
--- TimeMachine/TMAudio/src/SoundManager.cpp
+++ TimeMachine/TMAudio/src/SoundManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93a8e612fae3e73f09fa6e00f09d8f23a5135c97203b4e9c1e21c8071477dba9
-size 4702
+oid sha256:6aa6921e4583dac37888e97dbd9002c62d7dfa960bd54f6406d79749e6267e87
+size 4882

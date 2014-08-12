5bd1827e5b56735da82573c857363cc4db0fabd4
Jake Shirley <navi@odinseye.org>
2014-08-11 22:12:31 -0700

    Fix for Game breaking when a GameObject is selected in non-editor mode

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df5f286325f3ac6976a32c606dc73385ff750af8669b37ad3fe187a2ad53034c
-size 8239
+oid sha256:62d95121ef261cbde375c636e11f2fa0ca75a9ca0af346dddde9d6c1e4909d49
+size 8278

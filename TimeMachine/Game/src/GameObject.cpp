c50bdcdfb9561ebb31c16273e48ad5a1bc51d933
Jake Shirley <navi@odinseye.org>
2014-08-11 22:59:55 -0700

    Fix for components not showing up in property grid when selected

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62d95121ef261cbde375c636e11f2fa0ca75a9ca0af346dddde9d6c1e4909d49
-size 8278
+oid sha256:27610563a1fa875c215b9894ecc67d037cce9d411092b6009e0510cea5d602ea
+size 8340

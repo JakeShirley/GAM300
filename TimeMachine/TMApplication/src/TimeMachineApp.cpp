9603dc524d09721e225e9a310847367e5482e645
Jake Shirley <navi@odinseye.org>
2014-08-12 21:14:31 -0700

    Fixed issue with incorrect mouse events firing

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f9d2bad41dcf659df6f04d1943f55e91d4b403a90c72c2430ad048c97888e41
-size 11197
+oid sha256:8a16a95fe219fd1725a3b27f23e1530bc7e401347c8383e787342eddda19de02
+size 11184

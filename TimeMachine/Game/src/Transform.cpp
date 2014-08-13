b5f5e82af52a0a1941bef8764af62c1817aa74a4
Jake Shirley <navi@odinseye.org>
2014-08-13 13:40:05 -0700

    Removed useless callbacks for Transforms

--- TimeMachine/Game/src/Transform.cpp
+++ TimeMachine/Game/src/Transform.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a38211e45b3ee6540270ce3d96cf44d8f9fd6654928a64cf39e04dcb16108b3
-size 3858
+oid sha256:385ef39ecd209d5695a8d054f5167cbf508765c8b3c465db1ac46fc50d8704c6
+size 3609

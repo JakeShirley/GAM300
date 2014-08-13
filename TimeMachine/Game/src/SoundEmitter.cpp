6f7daab6692f6c3af049ac1d38801de69c7bb35f
Michael Chambers <m.chambers@digipen.edu>
2014-08-13 13:55:06 -0700

    Invalid labels resets the emitter sound to default.

--- TimeMachine/Game/src/SoundEmitter.cpp
+++ TimeMachine/Game/src/SoundEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2d870eb47f595ad5687ead10d88a7e9634305d5551661d70a975b2a397a6098
-size 9546
+oid sha256:b848667b4ea27eef34ef842db0c23ac2d02121ffb9acf1b0df65d06f48f22e56
+size 9668
--- TimeMachine/TMAudio/src/SoundManager.cpp
+++ TimeMachine/TMAudio/src/SoundManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06891ded93ef925e381c4e6957f0456a8d587e1e56bbc395dd30233f4c2815d6
-size 4909
+oid sha256:aed9d15c4d0eea69d6ad142dd7deca354768ccafd880f481d52caf535cafd50b
+size 4934

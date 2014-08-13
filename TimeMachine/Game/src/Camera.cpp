51dd82b199fbdbdf24683e739b7cf094baa40745
Jake Shirley <navi@odinseye.org>
2014-08-13 13:38:09 -0700

    Changed Camera movement downwards to space+ctrl

--- TimeMachine/Game/src/Camera.cpp
+++ TimeMachine/Game/src/Camera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42a084315a36b33bcea4f0dc10b4302e1e3497aabdbd5c40806661c5eba26670
-size 3746
+oid sha256:5c8fa8c267d38bb2a8753effb84cfcf0080be14263cf0a071730624c806d1545
+size 3708

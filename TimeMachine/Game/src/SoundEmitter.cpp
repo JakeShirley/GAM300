80dd9f597518b1d30f14bbf0fd4389b8edb25dda
Michael Chambers <m.chambers@digipen.edu>
2014-08-14 15:58:20 -0700

    Dropdown for valid sounds instead of "firing and hoping." Also removed the doppler effect from fast camera movement on sounds.

--- TimeMachine/Game/src/SoundEmitter.cpp
+++ TimeMachine/Game/src/SoundEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c18e3af1bb17c1fa8f142ef10b1285f43c9169d49633eb37077f8c2a9aba6b7e
-size 10017
+oid sha256:18cff5327f1286bee0ba8f866ea836905a256741b1459d3d52c16b42fe2a0e66
+size 10545
--- TimeMachine/TMAudio/src/SoundManager.cpp
+++ TimeMachine/TMAudio/src/SoundManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aed9d15c4d0eea69d6ad142dd7deca354768ccafd880f481d52caf535cafd50b
-size 4934
+oid sha256:66d411d1d9973d4cb9a95077a332e5622017fecd56a15123b73b91afb06af29a
+size 5062

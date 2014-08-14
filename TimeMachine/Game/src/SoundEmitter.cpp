e47ccb42b58aefa2b4187e299894b1274d0f12c2
Jake Shirley <navi@odinseye.org>
2014-08-14 14:34:13 -0700

    Refactor  +fix for default sounds

--- TimeMachine/Game/src/SoundEmitter.cpp
+++ TimeMachine/Game/src/SoundEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67099058a0fd5defd84d0383a558ab12595d9f9f8873f52f33eae01665634a01
-size 9683
+oid sha256:c18e3af1bb17c1fa8f142ef10b1285f43c9169d49633eb37077f8c2a9aba6b7e
+size 10017
--- TimeMachine/Game/src/SoundEmitter.h
+++ TimeMachine/Game/src/SoundEmitter.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18ac1d644ba6daec8aac17b8586d76abdeaf599e237d33ce36a9838dedc4154e
-size 1333
+oid sha256:2f1e4a4db54a23d1a2770cfa4728189bb99192e92cac5121cd940562d99b2f70
+size 1334

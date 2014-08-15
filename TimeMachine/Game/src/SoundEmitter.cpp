9ccad84e7f61db73fa21b661d8267a3836d7cded
Michael Chambers <m.chambers@digipen.edu>
2014-08-14 17:07:22 -0700

    Can now set loop beginning and ends. That's a lot of spikes spikes spikes spikes spikes spikes spikes spikes spikes spikes.

--- TimeMachine/Game/src/SoundEmitter.cpp
+++ TimeMachine/Game/src/SoundEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18cff5327f1286bee0ba8f866ea836905a256741b1459d3d52c16b42fe2a0e66
-size 10545
+oid sha256:0d0ffb4b8c979584a5d16ba2ae06050ce31b8083604dc86afb177a6c6a43d316
+size 11798
--- TimeMachine/TMAudio/src/Sound.h
+++ TimeMachine/TMAudio/src/Sound.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a29e8633c17762f9ca33fd6cec9a3badd959498fc950f1c4f89897b2a76d8e3
-size 4357
+oid sha256:60635dac8cc5d51e21fa81bdb6e865886ebf303cd2e704bdc61aca4cc7685b97
+size 5004

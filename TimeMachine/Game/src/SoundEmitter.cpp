e41a538e1a09cb4c4ea6e0d0c620e634304c5572
Michael Chambers <m.chambers@digipen.edu>
2014-08-14 18:24:19 -0700

    Formatting stuff. Because I like things sorted.

--- TimeMachine/Game/src/SoundEmitter.cpp
+++ TimeMachine/Game/src/SoundEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27866793ae125d575c3e264626c53d74d97808d5454c69c1c47e31cfdb44ce2c
-size 11883
+oid sha256:0371978df9a904ba3ad9fe7a47eeccf0e23e8f9012dd7e5fd83500984591d91f
+size 11895

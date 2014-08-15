5ba36136f33d1d2dc7f92959bd147c0910f97fa4
Jake Shirley <navi@odinseye.org>
2014-08-15 16:24:54 -0700

    Updates to Toasters!

--- TimeMachine/TMApplication/src/Toaster.cpp
+++ TimeMachine/TMApplication/src/Toaster.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61a82b84aea80f917822203eb0b47e12137f3cf25b84624f7fe06fa40edf7fd0
-size 4218
+oid sha256:6853e97186da0843c78b74e2c9d20cb3a32fc9eb0b5fad7aeaa4366e76b755f4
+size 5187
--- TimeMachine/TMApplication/src/wxBaseClasses.cpp
+++ TimeMachine/TMApplication/src/wxBaseClasses.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c3847a0ad5b2cdd8ee85bf840540609b332ee19cd1ad79504f2d27f3fe3934c
-size 27288
+oid sha256:93fe80528839a96dbcb4b3e91b11eae739ce20ac8c104196f2ddbee77cdeee3e
+size 27308

6678923f4b7e89b22c405f641a4e46a314dfd127
Michael Chambers <m.chambers@digipen.edu>
2014-08-14 17:43:04 -0700

    Tooltip clarification.

--- TimeMachine/Game/src/SoundEmitter.cpp
+++ TimeMachine/Game/src/SoundEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d0ffb4b8c979584a5d16ba2ae06050ce31b8083604dc86afb177a6c6a43d316
-size 11798
+oid sha256:27866793ae125d575c3e264626c53d74d97808d5454c69c1c47e31cfdb44ce2c
+size 11883

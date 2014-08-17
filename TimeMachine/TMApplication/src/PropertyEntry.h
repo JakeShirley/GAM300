191dbf0bba19ed72f05900f2e3cfa71c0badc68d
Jake Shirley <navi@odinseye.org>
2014-08-17 13:40:55 -0700

    Removed useless enum PropertyType

--- TimeMachine/TMApplication/src/PropertyEntry.h
+++ TimeMachine/TMApplication/src/PropertyEntry.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2747004aeb604beb57d62b43687493327d2b1e94666cfe04a4a1f3367bd3e34d
-size 9853
+oid sha256:779ab658554d5fd59def02e4489eb6ce018e0ed55e9b1a362ec7ce41d792ecd9
+size 9753

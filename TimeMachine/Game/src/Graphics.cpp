e945f426d3acd1b1e7b9c3172e305b98570fc4ce
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-08-18 16:04:48 -0700

    Lots of RenderSystem interface, camera/display stuff, edited places to use new interface

--- TimeMachine/Game/src/Graphics.cpp
+++ TimeMachine/Game/src/Graphics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be7395fafcabd6d385cd9b704f5a234071fc194690ad89266f0ce3370a6cc0a9
-size 7376
+oid sha256:d47e3536c2d4b2c51b36aafab4129050df17965d7c3638738cec9ace2bde2867
+size 3544
--- TimeMachine/TMGraphics/src/JAG_RenderSystem.h
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fef129b558920a0d2fbecef456868facd7c333901cf53e4d228d1b21993ba348
-size 1082
+oid sha256:9716e6c384f81f45b8f8ec8936f492ec4f22e57d80d7f6761ad220bc8dbab788
+size 2313

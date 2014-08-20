94feac1f7639f7ecab9de2558a52f9a50090190d
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-08-20 16:37:31 -0700

    Press 1 or 2 to switch between cameras

--- TimeMachine/Game/src/Camera.cpp
+++ TimeMachine/Game/src/Camera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95b94484a7cae1bd80541c707bc53eefc89add5fee24d8f43e4bda2e5f9107a4
-size 7383
+oid sha256:93e3e31b40f44f6870d4e185b8883dc3c34418f4862130aade3e38f5fe320951
+size 8621
--- TimeMachine/TMGraphics/src/JAG_RenderSystem.h
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9716e6c384f81f45b8f8ec8936f492ec4f22e57d80d7f6761ad220bc8dbab788
-size 2313
+oid sha256:609d94cbc981a691a832ddd9808213941cebab2ed95ff80b333877eea236ce31
+size 2325

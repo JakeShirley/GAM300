25ed7c6e9bca8558e91ad24f22f583ce77bab927
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-08-20 16:46:28 -0700

    Forgot to comment out something not fully implemented yet

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17ee2f6f5fffa2e7551bba45968efade9fd206674e8c78cc6933b2ddf908f82e
-size 7059
+oid sha256:067b66ec29a536fe197f3ef6270ff92d08b0798a27d96a258e10a850b722d4b6
+size 7061

71016aafc22818458ec67235be7dc86a459d61ca
Jake Shirley <navi@odinseye.org>
2014-08-21 20:30:02 -0700

    Ensure GameObject isn't already dead when destroying them

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b50828ae84cf8951876fc571994e9c57bf215e6c50fae921f15057af1f8ced39
-size 8692
+oid sha256:c172a6e23aa909f217d6d3bbebc4fb8446bb7c4719596ae4f35019a6a9e0c582
+size 8703

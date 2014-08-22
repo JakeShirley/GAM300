047cff4c3659675171e6a03d32a38a3d75ae0e44
Jake Shirley <navi@odinseye.org>
2014-08-21 20:29:39 -0700

    Create camera in the load of CppLevel and don't destroy it on reload

--- TimeMachine/Game/src/CppLevel.cpp
+++ TimeMachine/Game/src/CppLevel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0aec851d4fcdc410dfdd2ce699658d8ab655c2e032f1fe4a9dbb8e69519766b1
-size 10096
+oid sha256:cf4717eb7514b0ceb38588b3e968b72762ab03d3af3e5c4695303e3038ff3395
+size 10228

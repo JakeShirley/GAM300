fc84899cec14611294537cbc90ef67da480d1061
Jake Shirley <navi@odinseye.org>
2014-08-26 15:30:28 -0700

    Made a view particle editor window button

--- TimeMachine/TMApplication/src/EditorWindow.cpp
+++ TimeMachine/TMApplication/src/EditorWindow.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef0e88fc7cdb2925cc775f1ccf6b2d21c7cf63da76499572901b956e75fed5ac
-size 7016
+oid sha256:b4ce09d819c26a4d16647bd27350ff5cc89765c5cae30d1b6217ce9233f0b357
+size 7102
--- TimeMachine/TMApplication/src/wxBaseClasses.cpp
+++ TimeMachine/TMApplication/src/wxBaseClasses.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad5c6e24c252db8f10683e882f0a9563b1e503d3faf4cf23aef6f0fe52b4631f
-size 28118
+oid sha256:830c20bbaad759dd416105ad1f022cf96bbc3d08a6d3e017c97bd8e233f6a334
+size 29189

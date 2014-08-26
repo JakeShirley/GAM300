0406a094ecbf33114cfa12bf74d715260f84f0b1
Jake Shirley <navi@odinseye.org>
2014-08-26 15:06:22 -0700

    Small fixes to auto complete lists in ScriptPanel

--- TimeMachine/TMApplication/src/ScriptPanel.cpp
+++ TimeMachine/TMApplication/src/ScriptPanel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:765e37d1aa967ee0e344cea9a6652c1cdb59918a38ebe318702a32fa938262aa
-size 16298
+oid sha256:b415f8718d91a4b96ed1c0fd0111822f3aba562b3f2e37e6ff76030c21fd0a96
+size 16355

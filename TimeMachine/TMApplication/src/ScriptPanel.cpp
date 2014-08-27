f168222e1e583157726e70facfc8cb20feb63028
Jake Shirley <navi@odinseye.org>
2014-08-27 12:27:59 -0700

    Tab size of two by default

--- TimeMachine/TMApplication/src/ScriptPanel.cpp
+++ TimeMachine/TMApplication/src/ScriptPanel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be5cfc671a108ed7adf1e1be64ba9f3b4f2e1ed9aabf72cb58eac1b7797c65f3
-size 16980
+oid sha256:5d10472d725940c3cac25e384dcf68268f83578508508da0ec0a67836995fd4e
+size 16998

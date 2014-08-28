37a4561b63f1475b6441c8bfefcef64e6c8ba044
Jake Shirley <navi@odinseye.org>
2014-08-28 16:01:33 -0700

    Fixed hitting enter to auto complete moved to the next line

--- TimeMachine/TMApplication/src/ScriptPanel.cpp
+++ TimeMachine/TMApplication/src/ScriptPanel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abd8ce3e5109f9ff6e7786fcc47c9c4ae13742bcdaf50523ddda64c63d23c500
-size 17820
+oid sha256:1d20f20ae4419b9374971479bb1b1cb3ba8f93567d9d85706995fdebea61e6ef
+size 17830

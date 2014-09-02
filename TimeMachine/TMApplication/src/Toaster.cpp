a092b302c3c742a68e38bfd13ca6d4c3a41d8548
Jake Shirley <navi@odinseye.org>
2014-09-02 10:31:43 -0700

    Faster toaster pop-up.

--- TimeMachine/TMApplication/src/Toaster.cpp
+++ TimeMachine/TMApplication/src/Toaster.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a9b6786e6ba19d0ad5d34a2ad8ef3af58de3b9b42f480f866b3cde2cf7980a0
+oid sha256:7cb50e6fa97de7023dd69b03c5cec0e3cf08d0c70ef65099b24a649c500fe4ee
 size 5209

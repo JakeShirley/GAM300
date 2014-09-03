90a8e58f1adfe14dd56b62424c336028bd1e58d7
Jake Shirley <navi@odinseye.org>
2014-09-03 09:44:49 -0700

    Removed warning popups and added overload for Logger::Log for std::string

--- TimeMachine/TMTools/src/Logger.cpp
+++ TimeMachine/TMTools/src/Logger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5661f479788f4418e87004be0e30153b437996f86952133b868718449ec712c2
-size 3528
+oid sha256:4d7f4d6217df64d9fe9338e16915fefeb61bca51e79ab55bcaba2b342b1c4dd3
+size 3647
--- TimeMachine/TMTools/src/Logger.h
+++ TimeMachine/TMTools/src/Logger.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:943591a7f812dc161aaf65b6ddc7644405d8af55504fe75b8b8e99c8e86445b1
-size 768
+oid sha256:ef157e551534c4fec412bbd6ef7ab7a7b3ce3a1ff43d95c28aa15f9db981f035
+size 839

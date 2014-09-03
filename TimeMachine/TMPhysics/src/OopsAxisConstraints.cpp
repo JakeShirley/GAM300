e384af26732f367ffbef1d5b88c6bd9ff36b0731
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-09-02 20:42:39 -0700

    Hand made ragdoll without angular limits (knees and such can bend backwards). Joints only calculate their mass once per frame instead of once per iteration per frame. More convenient constructor for ball and socket joints.

--- TimeMachine/TMPhysics/src/OopsAxisConstraints.cpp
+++ TimeMachine/TMPhysics/src/OopsAxisConstraints.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4668bd6b2b06ac2b70f147e1e23b913fc9d0aaeb878cb1f7e5900544eaecbf15
-size 9556
+oid sha256:8d1f4cf3d866d4c06fcc72a1d79d0a8fd878e062da3c68ed3bb532c835dff561
+size 9559

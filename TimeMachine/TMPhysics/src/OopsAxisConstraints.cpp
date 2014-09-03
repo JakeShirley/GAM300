cea2f255f861307f75e6f64a1c493f0b27bedffe
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-09-03 13:10:13 -0700

    Fixed some edge cases in angular axis constraints that would cause them not to restrict as they're supposed to.

--- TimeMachine/TMPhysics/src/OopsAxisConstraints.cpp
+++ TimeMachine/TMPhysics/src/OopsAxisConstraints.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d1f4cf3d866d4c06fcc72a1d79d0a8fd878e062da3c68ed3bb532c835dff561
-size 9559
+oid sha256:dfbae1381d6dfea06f41afb73244cd2b506503f799ef1f27990d4edefb630c7f
+size 9568

ac228cd54832091361a2b0372e82d22047fc4d39
Jake Shirley <navi@odinseye.org>
2014-09-04 13:14:58 -0700

    Removed a few TODOs.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5bc8cf6682398213ea627b3163a8ab25e84720b97da3021396324a0e466ee55
-size 12904
+oid sha256:2f02f68ed76cd11134d5dcd4c7689373c34e39cc3d17202ad4e3ce08ffa55169
+size 12865
--- TimeMachine/Game/src/Lua.cpp
+++ TimeMachine/Game/src/Lua.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e510f0b007be854b0862582ecbcf1e86b2e672eb8e3811a40a8d51f6745aa3c
-size 2395
+oid sha256:9e0ae583d5f7e78a8070a3b3618b2ddac873a0dcb2b0471f18f4aa5b7729dfb0
+size 2354

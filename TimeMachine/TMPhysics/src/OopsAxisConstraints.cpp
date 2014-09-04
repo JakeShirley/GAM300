5a7d1bfbac0b6296d9f1d88f3e2e1a46a1fad9f9
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-09-03 20:00:37 -0700

    Angular axis constraints with limits actually work properly within the context of a revolute joint.

--- TimeMachine/TMPhysics/src/OopsAxisConstraints.cpp
+++ TimeMachine/TMPhysics/src/OopsAxisConstraints.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2d71b71bb240f9d6af785f9d58805b97e42d4e77654dcfcfb2d7bb589601b46
-size 10796
+oid sha256:aff08644fb1463b75c88ee883e8a35b25c5347326cf45962c435ea8800d4c1bd
+size 10795

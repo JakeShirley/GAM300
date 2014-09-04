ccf121c7d039f1f4a7e97a31b746dc227500fe65
Jake Shirley <navi@odinseye.org>
2014-09-03 20:38:40 -0700

    RagdollComponent class! Play with a controller.

--- TimeMachine/Game/src/EditorLevel.cpp
+++ TimeMachine/Game/src/EditorLevel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eeed96b7ad0512537678f7403dd5cfed28ef5e5afe1a0746b15d15d92b67fdef
-size 13833
+oid sha256:3b2389a5ae983145ea2208bcd14089a47355589e9fbd4b98da7a1ec9da33cee1
+size 14052
---
+++ TimeMachine/Game/src/RagdollComponent.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:45b50e11e54f4804ac8278db7dff6392fe3baa52933d9c7adbef349720e8f402
+size 5460

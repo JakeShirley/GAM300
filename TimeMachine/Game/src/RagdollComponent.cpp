24f069a524cb70586cc42d49c6e6e92af216ad90
Jake Shirley <navi@odinseye.org>
2014-09-05 10:33:50 -0700

    Made RagdollComponent stand alone with model.  God help us all.

--- TimeMachine/Game/src/RagdollComponent.cpp
+++ TimeMachine/Game/src/RagdollComponent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31dd94ffe045f70bc56862ee0dbcf4379b8b4210072825b7ff2bc2d1a21d9b6e
-size 4718
+oid sha256:bd0bbd5406ebc62112a8f36480bb09075d37e61ee493735c30452706c2731b26
+size 4935

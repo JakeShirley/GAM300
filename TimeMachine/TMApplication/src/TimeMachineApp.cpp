d84f6a7e8b48f706632dda5733eba43d0eadba79
Jake Shirley <navi@odinseye.org>
2014-09-05 13:39:14 -0700

    Max dt at .1s.

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b21d3712b5ffaf1d2c2202c4784cdbe66a61d802bf08a77824673100a5c00b8
+oid sha256:0c526fa6ebbaca53929b31b4f9a53ce31095a0973c8ca21820af327dabdb872d
 size 13657

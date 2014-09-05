068e5f77c494ca305d0482b80b53a748c8f53cb3
Cody Centers <murtag27@gmail.com>
2014-09-05 15:37:33 -0700

    Completed adding KeyFrames and editing the variables.

--- TimeMachine/TMParticles/src/ParticleEngine.h
+++ TimeMachine/TMParticles/src/ParticleEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48b1f9aa4d71d7f74f85b92d9262d263ea20a05726c2e56cd14157e3cd98e76a
+oid sha256:22defc9d1c163106add3c8b40bd7933e5066512eb79347db4820b49e868ab1fb
 size 9068

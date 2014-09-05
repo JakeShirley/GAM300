cc254169e803057465e380d204d67aa35d99ba68
Jake Shirley <navi@odinseye.org>
2014-09-05 13:34:59 -0700

    Updated FRC to take wxWidgets into account.

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de2635e29157e7e1bf542d6e5495ec70a974fe7d1e22a13429f2e69f06fcab0d
-size 13650
+oid sha256:2b21d3712b5ffaf1d2c2202c4784cdbe66a61d802bf08a77824673100a5c00b8
+size 13657

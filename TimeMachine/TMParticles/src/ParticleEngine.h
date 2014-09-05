b762bea67bda1f4792c39bda6a25ab8bbe105e95
Cody Centers <murtag27@gmail.com>
2014-09-05 14:08:04 -0700

    Minor changes to particle editor

--- TimeMachine/TMParticles/src/ParticleEngine.h
+++ TimeMachine/TMParticles/src/ParticleEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69bf97440492427219b2e33b08a70dff47ab30cbfdf47327902ebb69045727e5
-size 8994
+oid sha256:48b1f9aa4d71d7f74f85b92d9262d263ea20a05726c2e56cd14157e3cd98e76a
+size 9068

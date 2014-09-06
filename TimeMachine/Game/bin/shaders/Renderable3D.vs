b5beade4bd8f3ccc2947aa9e52cda8ef4f0de423
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-09-05 19:30:42 -0700

    Minor model interface changes, 3D object shaders, render logic in system, untested

---
+++ TimeMachine/Game/bin/shaders/Renderable3D.vs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ad774fd7e2a34e1983ceb804f7370fa425fe9a021b8ec192a36bb9609056db90
+size 1183
--- TimeMachine/TMGraphics/src/JAG_Model.cpp
+++ TimeMachine/TMGraphics/src/JAG_Model.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb65550511097c21eaa103a46b489c933be5881f9bb9d1251a973d96c7a0d604
-size 2243
+oid sha256:e59f61e4eecbb1aa552fcf11b8e96dfd0803d1d437ad2cda846309aef79eec07
+size 2226

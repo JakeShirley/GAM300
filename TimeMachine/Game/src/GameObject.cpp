8e3624d470e1041f0aa8689ae15f76d32daf354d
Jake Shirley <navi@odinseye.org>
2014-09-07 16:27:04 -0700

    Moved AddProperty to LuaComponent.  Work on serializing active property values.

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5e7bb8a71cd878c5caae41eb6ca455414f6786a0560abc74855ff8a65d6d426
-size 12875
+oid sha256:8636de78f3f35ac0a57818aa970b984a5fad59e92db5882c0cef2bfd7f3bc716
+size 12883
--- TimeMachine/Game/src/IComponent.cpp
+++ TimeMachine/Game/src/IComponent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:caff7a0b7f97662e1c64395ede6e09f511afeda74893cb4542545d7504079bf0
-size 3563
+oid sha256:cd8e078d727a442dddccf7965bac1d724fc38091a8457071e7b87f9acd3fa661
+size 801

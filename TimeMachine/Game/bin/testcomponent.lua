a0f53c542ce364f8eff0da634c829a050b37ea70
Jake Shirley <navi@odinseye.org>
2014-09-07 16:27:26 -0700

    Updated VS version and misc. changes.

--- TimeMachine/Game/bin/testcomponent.lua
+++ TimeMachine/Game/bin/testcomponent.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41d7e52b9daa1a4831a3a5cc064b9b309b87dabc5183ea13b6f305c60a240b78
-size 241
+oid sha256:02d11c125aeb68fc61ee1a2ba0ecc088462e4748d9494b8ffbb999a190bf59c1
+size 243

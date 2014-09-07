2ff055487b0fa18dab784e3a11751e65b403c0bb
Jake Shirley <navi@odinseye.org>
2014-09-07 16:20:37 -0700

    Added ability to set the base directory for the PathManager.

--- TimeMachine/TMTools/src/PathManager.cpp
+++ TimeMachine/TMTools/src/PathManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95e576c93c456c20f2e2ec5fcc68fa6b9669d68f243d35ac62649ce7d9195a76
-size 721
+oid sha256:8178806df17e3c27fb6a8396a58827c32950eb820fc972083081062ae2b4b6f5
+size 941
--- TimeMachine/TMTools/src/PathManager.h
+++ TimeMachine/TMTools/src/PathManager.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ca96f815c0417bc24c36e89c9c70e214077d8112b5e98e0febcf32bd46897e5
-size 304
+oid sha256:2c354bc58e069be8586f65c8b09128d63de87e8cb7b7efc88e48b2edb42a4d91
+size 318

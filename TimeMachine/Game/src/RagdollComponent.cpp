80ac680731a67b3e5652574dfd1041f1dd6066fd
Jake Shirley <navi@odinseye.org>
2014-09-08 17:29:33 -0700

    Added check to RagdollComponent to make them ignore the controller if it isn't plugged in.

--- TimeMachine/Game/src/RagdollComponent.cpp
+++ TimeMachine/Game/src/RagdollComponent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd0bbd5406ebc62112a8f36480bb09075d37e61ee493735c30452706c2731b26
-size 4935
+oid sha256:25dd53261044c0f0a05c50f7fcea05ff2ba1517b2a1d18f239030190185c49ad
+size 4992

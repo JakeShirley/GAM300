0ea50dd021d665c5359f999c82197f115ef1b4e2
Jake Shirley <navi@odinseye.org>
2014-09-09 11:42:47 -0700

    Fixed warning about type casting.

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:970ee07dfd00f55a6c1675fd408010bdc9640ecd4b356082edec3ac734498cfc
-size 13584
+oid sha256:b063d6fb7dbfcd74990bccaea72f8fa3e8bcc20a3ca12117b974c0e00dae84cf
+size 13609

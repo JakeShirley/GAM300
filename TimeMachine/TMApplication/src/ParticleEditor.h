0a06496d9423b706d0be4ac28b68ed6683c41069
Cody Centers <murtag27@gmail.com>
2014-09-09 15:47:54 -0700

    Added a restart option to particle editor, also fixed update file button not working

--- TimeMachine/TMApplication/src/ParticleEditor.h
+++ TimeMachine/TMApplication/src/ParticleEditor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:162d422539217c74f4c4789a9f59bb69e7bafa0718fb9adbeb6571b8abe22a1f
-size 1511
+oid sha256:37ef1ae772bafe519c300e7c89a67fb49bb0ce6f950e4d96c8a2979da5115b0f
+size 1564

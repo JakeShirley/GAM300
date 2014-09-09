df2199b34444b581c9d9463b749999480452c3fc
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-09-08 20:24:15 -0700

    Outside-out cow

--- TimeMachine/Game/src/Graphics.cpp
+++ TimeMachine/Game/src/Graphics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5adab47e44cdc668da877952cfe4b6b15f365e7440730f899f72302ed95bd363
-size 3593
+oid sha256:6194edb532100dcd9ec0a054b2a097628d988a44a1a04bd1357d8f3ddc8c1916
+size 3594
--- TimeMachine/TMGraphics/src/JAG_ModelLoader.cpp
+++ TimeMachine/TMGraphics/src/JAG_ModelLoader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:207f8ddcbcf4986fde706bea5a2aa32c726d1811184cfdcbe531cd48682af7bf
-size 4707
+oid sha256:cacedf546e0938608b753b187ca5aec57b69ccc387c0e10f7890cd3257f8028a
+size 4751

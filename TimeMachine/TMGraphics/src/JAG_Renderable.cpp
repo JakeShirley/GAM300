7ba6e0ec70a115462774ab82bdb1c00c667fac73
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-09-08 20:00:53 -0700

    Models draw. Please start using depth buffered debug draw where necessary

--- TimeMachine/TMGraphics/src/JAG_Renderable.cpp
+++ TimeMachine/TMGraphics/src/JAG_Renderable.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83b556009335e85bc48e10eafde3bb5ffb8ee471a2448f932dcf550b196a80cd
-size 578
+oid sha256:0177a0d00a340740a27806ea4196261d0ab4a7493e17dac473362644a5986050
+size 598

7e4465e4cf811b25d069e9ca03874837f27f926d
Jake Shirley <navi@odinseye.org>
2014-09-10 15:35:55 -0700

    Fix for objects teleporting to the mouse when they are selected.

--- TimeMachine/Game/src/EditorLevel.cpp
+++ TimeMachine/Game/src/EditorLevel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ec2a9c0acb18830df3ce44af0799c325fb5e269d74cf76dc21f94ad721f8b9c
-size 16397
+oid sha256:cef695befb6467fa136a669768508fdfb817a21bc46024e0d64f4aa0860bb34b
+size 17076
--- TimeMachine/Game/src/EditorLevel.h
+++ TimeMachine/Game/src/EditorLevel.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffaaa9390b08962ef443a67c54ab6440831927f53a86a2cc76dee9018ec1c5e9
-size 1004
+oid sha256:2db4bfef49ef53d224ab4b5f792d9831e418520f1b19916b404f3d5f0861ab60
+size 1086

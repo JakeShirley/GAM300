d4b40b073f795c635f8a142388fb927b878615e0
Jake Shirley <navi@odinseye.org>
2014-09-10 20:37:20 -0700

    Uncommented out screenshot button.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fb805a9a4e1e94fc1077a596f8e828db765d3dc32256a4307e997adbedf7aa8
-size 12571
+oid sha256:5ef1b15241c51a7b81b1d7cfec16ddeb1870038b1facb7cb5a7e9454bb566169
+size 12569

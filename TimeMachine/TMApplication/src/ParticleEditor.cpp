4a17d3b601b6502e0fe8e583ed1f5343bed8ccbe
Cody Centers <murtag27@gmail.com>
2014-09-10 18:40:42 -0700

    Changes to emitter type creation, as well as minor editor changes

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93a42820a3e3fef6d7acbb8b7318f7a0ef83b5c6ebc5042bb6645b67b65b8230
-size 38093
+oid sha256:2a14b050d53a82609ab049d062571659789a9afee267586a6132b7f8fff130e2
+size 40548

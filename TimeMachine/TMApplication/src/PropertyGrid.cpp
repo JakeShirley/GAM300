d4fb223071dd0883df7376341554912de6d3b09b
Jake Shirley <navi@odinseye.org>
2014-09-12 11:41:36 -0700

    Sort properties by name and not label.

--- TimeMachine/TMApplication/src/PropertyGrid.cpp
+++ TimeMachine/TMApplication/src/PropertyGrid.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:552c84f5ea57dbcac9a27dc7d9ff7053686b474ff0bfb34011c2f7376a88d009
-size 3719
+oid sha256:398c2f7a42c072517b45530762e673e6113f2f973beb43175d3d6d91d60be534
+size 3893

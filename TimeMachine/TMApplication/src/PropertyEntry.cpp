ff4345a79c742ed6ce1a9abf1bb7039ec39c8f35
Cody Centers <murtag27@gmail.com>
2014-09-11 21:34:15 -0700

    Added RemoveAllChildren to CategoryProperty.

--- TimeMachine/TMApplication/src/PropertyEntry.cpp
+++ TimeMachine/TMApplication/src/PropertyEntry.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f82b8dc25b70a17ebba419c0aa1d05f846404eddd6f2a8b26ee363fc0c952e01
-size 21939
+oid sha256:9c40e7793fd782c84f9b4166a94c635a7c6342d2396446d54c4dc16e3ac0e33e
+size 22339

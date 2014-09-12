46fe0803ef7d541240795b7eb784721da2cf369e
Cody Centers <murtag27@gmail.com>
2014-09-12 14:52:27 -0700

    Reset Emitter now properly resets timer information

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7ae2f020feeb66d290b2ddf1cf0de60bcecd4e3e14dfb195ad94e4d3873901e
-size 37038
+oid sha256:b1ef47daa47c8c736b981b1d5d6f797b7f7ffc6a2d167ba432ae98832a780f8c
+size 37044

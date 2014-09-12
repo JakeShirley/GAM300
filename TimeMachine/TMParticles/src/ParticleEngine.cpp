0f94471038999a00571c684be5c0597d1f0f77cd
Cody Centers <murtag27@gmail.com>
2014-09-12 13:50:57 -0700

    Fixed the create function on point circle type emitter

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53866b0fe9d1b15f59e69b54a587e6a1742a447b811ef36c9d4072163bcddd91
-size 37078
+oid sha256:e7ae2f020feeb66d290b2ddf1cf0de60bcecd4e3e14dfb195ad94e4d3873901e
+size 37038

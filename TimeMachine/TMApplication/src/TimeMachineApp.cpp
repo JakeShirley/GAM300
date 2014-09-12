78952ddf1255afda7faefd5ee7c19c4130330e63
Jake Shirley <navi@odinseye.org>
2014-09-11 17:48:05 -0700

    Added getter for FileWatcher on TimeMachineApp.

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:139d527be5820a1a690a5082718ce3202ffd1d19c524fa9fd1e2df46b17d1ebc
-size 13739
+oid sha256:0366a42268e8ea0762669ebc0c84fdae629d71bf9903e0ba9fc5955775449ec1
+size 13823
--- TimeMachine/TMApplication/src/TimeMachineApp.h
+++ TimeMachine/TMApplication/src/TimeMachineApp.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65b69d938516893252fd8ccad6a590c4e6f10837fe73187ed43bdc3cb33c3b99
-size 3152
+oid sha256:1c9535d47c37eaed8fbf07b611326729ecf5fce47b2d149335932e40d198bf78
+size 3189

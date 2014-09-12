06fe481c990086b2770cd04fb4ec1ca30eeafa48
Cody Centers <murtag27@gmail.com>
2014-09-12 10:29:54 -0700

    Added behaviors to the editor

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bf1ebd86901db77962dcc79630683351127396ac8bdcb420ddbdf6d628c5922
-size 44746
+oid sha256:4c72eae402d51138231ff4ef19d15c344358b0e690c58392462386dee09df863
+size 51313

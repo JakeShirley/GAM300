4b43e90839ffe43fb22fdb860de2eb3bcedb33f4
Cody Centers <murtag27@gmail.com>
2014-09-12 19:12:55 -0700

    Added a check that prevents keyframes from sometimes behaving weird

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74ee706709a3c67f08f3a26498e93c8eaf9fee22933d7b9cd0b78fc97d81fca2
-size 37270
+oid sha256:374026fda80e6a33647310bb17008ab4ead7566853180d1562e0034dcda04228
+size 37337

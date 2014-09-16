afa8f56fbb4a62bc7159d9307efbdfe3fd757911
Jake Shirley <navi@odinseye.org>
2014-09-15 23:11:23 -0700

    Fixed error in ParticleEditor.

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8415ab806f1237e3dae3f45a82ac8ef291da79b0b4547692ea7ceca23bc7e5b8
-size 56845
+oid sha256:a63734358b7270cbea78aa4818d3e543465944acfd512bcb80c0140f98c505b7
+size 56839

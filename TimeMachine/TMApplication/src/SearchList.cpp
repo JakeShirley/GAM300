27f9d462e5a5cf7cb2cfa46883a3c9e35600ddc0
Jake Shirley <navi@odinseye.org>
2014-09-15 23:12:19 -0700

    Small fix(?) for SearchLists.

--- TimeMachine/TMApplication/src/SearchList.cpp
+++ TimeMachine/TMApplication/src/SearchList.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b942ba7fa22bbc9cae3942f0562dc2e23fdca0dd8af0f767f06bdda5ec2452a
-size 4708
+oid sha256:4ba0ee41c32fd7f366f4bc192b4c31fa6b8f6ed03a64153ef03bdd39c57c4946
+size 4707

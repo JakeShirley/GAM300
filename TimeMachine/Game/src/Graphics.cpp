76e181c728c6ae75e7c5162d41fa09965cff281c
Jake Shirley <navi@odinseye.org>
2014-09-15 17:58:32 -0700

    Fixed 204 byte memory leak.

--- TimeMachine/Game/src/Graphics.cpp
+++ TimeMachine/Game/src/Graphics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fb43a852573e05332cf452d98b982749e769a065974852c2fd4c4b18b9f7447
-size 4623
+oid sha256:5cef0288d915dcf61a3cae1fbe793d9be561fcc383392ff1ca3c275629fb3b23
+size 4571

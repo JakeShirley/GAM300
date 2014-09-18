c53fae1d65494423a370a50279b0522df094a741
Jake Shirley <navi@odinseye.org>
2014-09-18 15:57:50 -0700

    Updated functionality for CategoryProperty and update the ButtonProperty button with the label of the property.

--- TimeMachine/TMApplication/src/PropertyEntry.cpp
+++ TimeMachine/TMApplication/src/PropertyEntry.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b51854ef808c4481cc59658e758c38b8a7e65c06d28968b6ea65f96bd9ac579
-size 25263
+oid sha256:9877883339daa1248fe4cb8c2e228b1fb6c559db5598091ddc060299bed68e98
+size 25380

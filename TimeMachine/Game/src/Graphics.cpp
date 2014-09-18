51a6a48fb55090ff6d4e4ea0025693ce3aca0173
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-09-18 10:47:37 -0700

    Commented out plant because it explodes my graphics card. Stop pushing these things.

--- TimeMachine/Game/src/Graphics.cpp
+++ TimeMachine/Game/src/Graphics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30fa929fae1b9fe2243538bd28e06c1e2b62b2b0eef82849fe07c5ac302be92c
-size 4793
+oid sha256:3077d20993856210f91a246853fb264ae6ba1a3c3608d147980b5a2da0883279
+size 4795

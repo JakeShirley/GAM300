c85b75a068a352bc766b4915fc088f7aea393cc1
Jake Shirley <navi@odinseye.org>
2014-09-18 22:07:18 -0700

    Updated buttons to (hopefully) always call their callbacks, even if they are selected.

--- TimeMachine/TMApplication/src/PropertyEntry.h
+++ TimeMachine/TMApplication/src/PropertyEntry.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ae14b22c2d0f748222f6ce8bccba16195cfa4b6c4647f9e282ac621cdf20a66
-size 14173
+oid sha256:57dfe605a7773671948e5663fb6ba1491d4e80c3fde45cdd97ca0ac32756b8a4
+size 14257
--- TimeMachine/TMApplication/src/PropertyGrid.cpp
+++ TimeMachine/TMApplication/src/PropertyGrid.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaacb9eb01a7b4662b8e713f53dd84f7d80ba15483f6aad7518ce392358e5871
-size 3989
+oid sha256:93033e90e3fc2a4aacb883b12ae96f81dbf3b96a92c9ed6883ae4d4c64be4c1c
+size 4215

8e6edc31aede18d2b2af8d443284b835860bc3fa
Jake Shirley <navi@odinseye.org>
2014-09-19 16:08:01 -0700

    Fix for linker errors with Vector3

--- TimeMachine/TMMath/src/Vector3.cpp
+++ TimeMachine/TMMath/src/Vector3.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64fe5268ceaa59055ec5f539b71080ca4fba7ed6baedc3436d2d94a34096329d
-size 9293
+oid sha256:c8e50b0b3735fe6659b8893e39cb8cf4d08c77157542ef96e897b195434a07c1
+size 9335

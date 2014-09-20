a18d4a4087f09a31faf4af6fed1c2db1927c035c
Jake Shirley <navi@odinseye.org>
2014-09-19 22:55:42 -0700

    Added Circle and Square to DebugDrawer for 2d shapes.

--- TimeMachine/TMGraphics/src/JAG_DebugDraw.cpp
+++ TimeMachine/TMGraphics/src/JAG_DebugDraw.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffddf10a5c516dbc1af2e6e655ed1ab6281d52be083be97939fa0417f7837644
-size 8576
+oid sha256:d3a730085e1e052f195d3078abe407ed6cee62379864e59f514a9509007f626d
+size 10047

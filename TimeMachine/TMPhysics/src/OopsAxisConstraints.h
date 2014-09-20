fac25aff6fb1dc44fa919124505871a7dbcbfcf0
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-09-19 17:36:46 -0700

    Limited twist constraint and minor bug fix to swing constraint.

--- TimeMachine/TMPhysics/src/OopsAxisConstraints.h
+++ TimeMachine/TMPhysics/src/OopsAxisConstraints.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:286aeb4ee223d484de14eb270fd66ed5cfebb98818cb26e531565b71be395182
-size 3870
+oid sha256:135104b342295eb8f172a7d7b7f2a4729af9bb89d5e681f0d40fdf3eb087f6a5
+size 4601

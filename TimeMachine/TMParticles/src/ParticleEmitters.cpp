6f2fee5b917d4457ace306ee9c0396206126b0a7
Cody Centers <murtag27@gmail.com>
2014-09-20 18:15:58 -0700

    Added Saphs DeathNova emitter

--- TimeMachine/TMParticles/src/ParticleEmitters.cpp
+++ TimeMachine/TMParticles/src/ParticleEmitters.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edb5a940225ec325e10b64beaae99ba8718b58b61ccd47ff41ddf05c84a4c32a
-size 5030
+oid sha256:dafb515bcbb0b8d6da02d1153a8599daf0560ac534826532138b67f5062bcac4
+size 8479

eca5ec712d9bc474e733e8a20a6e767ccfbc28ef
Garrett Allen <garrett.allen@digipen.edu>
2014-09-26 16:51:04 -0700

    Commiting to merge. Implemented Creator/Destroyer for Resources.(Resources currently not ported)

--- TimeMachine/Game/src/CppLevel.cpp
+++ TimeMachine/Game/src/CppLevel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69acae44785d87242cf429f6c07a7e032dd316ec4887d386729f4dc32a8a9495
-size 10807
+oid sha256:6d3f1457058427ff23d7c99bb1cdda09c81df1277949fe360527ae996c08a850
+size 10809
--- TimeMachine/TMTools/src/MetaData/Reflection.h
+++ TimeMachine/TMTools/src/MetaData/Reflection.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:373a801b19289f118c4bd762243bf2056c0a6032c0358c096762709a3f60bbc4
-size 8008
+oid sha256:8a10946daecf4569b84b82a241cb1d5f2ecd52a305df7f152cab5166526a7089
+size 8186

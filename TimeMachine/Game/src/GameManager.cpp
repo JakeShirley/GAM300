7532393c07a3cf176a2d0e6454c6db4b122f1f48
Jake Shirley <navi@odinseye.org>
2014-09-26 10:22:17 -0700

    Fixed tool menu popping up by default instead of Properties.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa5217ebe5f0f13d8a8a664f03da4bd39a3f5a9ffc0ff1b2ad4173889d90491b
+oid sha256:3956e66a78113b6b91b19fc058f67f1f9a725d343bc04753169095bcfb7df9ff
 size 18589

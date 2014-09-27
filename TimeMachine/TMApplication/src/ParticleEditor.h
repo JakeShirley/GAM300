da63d7591bfc59da3b9a84aff108573edd56eb78
Cody Centers <murtag27@gmail.com>
2014-09-26 17:34:43 -0700

    Fixed behavior indices not being updated when changing emitters in the editor. also added some stability stuff

--- TimeMachine/TMApplication/src/ParticleEditor.h
+++ TimeMachine/TMApplication/src/ParticleEditor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5306982080883f1d8005c0883a00e697af1548d0ded93574674da0ea40697bc
-size 2277
+oid sha256:af03a1b6bfd198884d5ef16bdf9c810a3f4085a39070b6895dc39d4d1b68c284
+size 2312
--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1678255c56b45edcb44e6329dd64321625a3fe8e2d955553f1941aaf160b73ad
-size 37758
+oid sha256:9089931faef32af4e0793885aaaefd7a147d826beaf58baa86ef7c351f54ad9f
+size 37930

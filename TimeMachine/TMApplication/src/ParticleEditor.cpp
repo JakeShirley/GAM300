6aff20bec66d85c0624a7e32219a2c7941f7153b
Cody Centers <murtag27@gmail.com>
2014-09-27 19:16:45 -0700

    Added hotkeys for resetting camera position, and view direction, as well as scroll wheel use

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e240b00c7621e0d9735b79280f4a19770a34f903542ed20b5eb0e90fd093f50
-size 58342
+oid sha256:d57746e557e55c04c6cb218ebc8e276cbc5ad1d14f4416e018a92f229e38ad54
+size 58898

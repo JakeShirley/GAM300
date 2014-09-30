69102bd04dfbf4d2ffa748f28947e074011e5221
Cody Centers <murtag27@gmail.com>
2014-09-29 18:39:13 -0700

    Re-worked many processes in the particle editor

--- TimeMachine/TMApplication/src/ParticleEditor.h
+++ TimeMachine/TMApplication/src/ParticleEditor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af03a1b6bfd198884d5ef16bdf9c810a3f4085a39070b6895dc39d4d1b68c284
-size 2312
+oid sha256:ebfe5f72c0c3ad2db2ddf6d54fc8632941f8f9cf6ea844407a5693349bda8fa5
+size 2604

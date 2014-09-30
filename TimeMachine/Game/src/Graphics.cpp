8ea8672a1c4ef9ef98fc3cb871c56b829893e4ac
Jake Shirley <navi@odinseye.org>
2014-09-30 10:05:23 -0700

    Made example graphics scene objects be relative to DT.

--- TimeMachine/Game/src/Graphics.cpp
+++ TimeMachine/Game/src/Graphics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c57bc656f6579b0edbb792496b19eabf09e74db171ec901663eadf54f3688e2
-size 6867
+oid sha256:3947965e920e714c853ef499d63de5224d2764a5f60d82b6a0f9863bc4d73214
+size 6912

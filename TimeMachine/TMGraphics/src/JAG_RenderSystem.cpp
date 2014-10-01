80fd84cd8870b2557005c5a857af90fe457bd591
Cody Centers <murtag27@gmail.com>
2014-09-30 23:21:17 -0700

    All effects default to be disabled

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e0bb4ad157fec1d076c3e107f426117ec868efd915b11e7eaf4f64f995a4057
+oid sha256:698442d1293701edb1415b0e54f86f0bdc49edcc4b1bcd06b1d72f8c97c9fd2e
 size 43574
--- TimeMachine/TMGraphics/src/JAG_Screen.cpp
+++ TimeMachine/TMGraphics/src/JAG_Screen.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c2b1d3e6c9ea93088fe7082e4b672d543b8292a326409ee1606f143031f7bb6
-size 1930
+oid sha256:e22d969ebf0b9b7e1ecbc49bd81032efb8eb3d5f658cd17f7b576a4ef4df6a32
+size 2022

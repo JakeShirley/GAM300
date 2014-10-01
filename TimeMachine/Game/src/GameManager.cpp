260141e2a3ac02f0f85494c5087eeb86a850d620
Jake Shirley <navi@odinseye.org>
2014-09-30 20:52:55 -0700

    Fix for stop state DT set to 0.f

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1683dac89f5ec12bcdda8888bd2715585526d66a3a49a45c95891cce856509e
-size 20272
+oid sha256:16671ced692680c23bf68ff9e8d966c800c9d9f4ba9f6abb20c52ab4205728ad
+size 20296

a084c38534faf086cb95ebb7e5cb7a12f54b8bf1
Jake Shirley <navi@odinseye.org>
2014-10-01 16:21:55 -0700

    SearchList can now use enter to select options.

--- TimeMachine/TMApplication/src/SearchList.h
+++ TimeMachine/TMApplication/src/SearchList.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25f7c3553dc027aa00414b6bf71ccf4f8e850c82615558bda69d488af64fe7ae
-size 1585
+oid sha256:53023a15812da75d048b73a214e3eea53951df49d96dfdc13617f8d88ab526df
+size 1564
--- TimeMachine/TMApplication/src/wxBaseClasses.cpp
+++ TimeMachine/TMApplication/src/wxBaseClasses.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07cd630dc461c0da91e9bf0380a6809b9ff477e63b3173ca4a5732422032fffd
-size 32593
+oid sha256:fe9911b44e7e3ad749c0232914d4fa6a1bce56999ced61c90058e8b84390ac96
+size 32607

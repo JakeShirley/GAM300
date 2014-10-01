b83316c38feb356503113767a7c2a523d0c864b9
Cody Centers <murtag27@gmail.com>
2014-10-01 12:26:54 -0700

    Added PlayerShoot, PlayerShootCollide, and PreDeathNova emitters

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:107378680e2b085b06212bed5426c0d133d696b8bbdd440120290901205efbb2
-size 56157
+oid sha256:ef5073c2c1e9bd7a57fce704fb5547f4fa38ca224e2388aa2206a137a2e7daa9
+size 56769
--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfa25995746bcc47a003338d8729e71337bf8c7618bc1cd2c6dfbda16ac843af
-size 39857
+oid sha256:6de1cf1cf1df361687a5acf36957220ab8dea85bb01922101143267b8098f6ea
+size 40805

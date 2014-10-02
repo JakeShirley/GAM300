12b9d337a5fa025a8729cb3e45f2715dac23f662
Jake Shirley <navi@odinseye.org>
2014-10-01 19:22:57 -0700

    Space initialization order changes

--- TimeMachine/Game/src/Space.cpp
+++ TimeMachine/Game/src/Space.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c50efe90029fdf64c278018c05dae61c859721017fffc671af1a1c6d28805a2b
-size 3527
+oid sha256:c8a1964ebd71d7091f6f5a1d6ec4d0f1b0385bafa4ade4ff58556944d28214fd
+size 3523

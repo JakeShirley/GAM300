b93d6cd3372536cccf9c376f9b6ae89daa3adb0a
Jake Shirley <navi@odinseye.org>
2014-10-02 16:42:46 -0700

    Stopped GameWindow from closing TimeMachine

--- TimeMachine/TMApplication/src/GameWindow.cpp
+++ TimeMachine/TMApplication/src/GameWindow.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:201d137e142b87eb87e93fbe02f4c61a470fa4e7d0f4c6acf03c39ad8c887fe9
-size 1642
+oid sha256:7a03d6558aa333209003c9ad9aaada69acbfd04e4213bdf29f4a612abeab6fa3
+size 1644

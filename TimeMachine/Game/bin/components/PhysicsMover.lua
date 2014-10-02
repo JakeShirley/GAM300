ddbe2d5a7878375bff1718bfa1c83c3a98fa2b36
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-10-02 14:00:28 -0700

    Functional player controller.

--- TimeMachine/Game/bin/components/PhysicsMover.lua
+++ TimeMachine/Game/bin/components/PhysicsMover.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:627fad76cb402287a030e87bc66e44a91433ce9b38732f2a30e0ec89da2a884c
-size 4115
+oid sha256:affe18a3c01389089440022f6c8e0bd20a5796c0756f36c9357416d1f81186b0
+size 4400

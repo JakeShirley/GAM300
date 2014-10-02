903f07a8101139e5e65f8c692f09cc5d64dbdb81
Jake Shirley <navi@odinseye.org>
2014-10-02 12:46:04 -0700

    Uncomment out TRISTAN by default

--- TimeMachine/Game/src/Graphics.cpp
+++ TimeMachine/Game/src/Graphics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6a7bf1a064e300a5a5a935a30b4219ba68c8078fb3e6b0b2fa25fee73152142
-size 8259
+oid sha256:f9fb2c2b097202cecba6fb539aff22288e68ffc1be67968c9b76341982f797f9
+size 8261

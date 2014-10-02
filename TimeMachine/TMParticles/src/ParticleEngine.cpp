ab1602dc0083294dd7681e2e8934a159612b7e37
Cody Centers <murtag27@gmail.com>
2014-10-02 16:45:03 -0700

    Created an ambient Wizard Sparkle.

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6de1cf1cf1df361687a5acf36957220ab8dea85bb01922101143267b8098f6ea
-size 40805
+oid sha256:389cb85eeb7cef85780bd50f843e9cc5be1208145477ccc1ec0ca1a4f558b677
+size 40859
--- TimeMachine/TMParticles/src/ParticleEngine.h
+++ TimeMachine/TMParticles/src/ParticleEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dffe57a10adc92f35227c9731380807dbc452ddd5ffe7bb9ec15329086922c8
-size 10762
+oid sha256:0b1b440ccc4905f0a421c809a29042bc71f1431ccf011048d527b50ba70c2339
+size 10812

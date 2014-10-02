4365146bb075aa6906cb2e778a95ae4e58e7084d
Jake Shirley <navi@odinseye.org>
2014-10-02 15:42:42 -0700

    Added getter/setter for Model scale offset.

--- TimeMachine/Game/src/Model.cpp
+++ TimeMachine/Game/src/Model.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1e444b7ee7049db7db5e853481cefe6542e45f006287f5504341a269e33aa78
-size 4225
+oid sha256:6275015f14606b0d238809b8e4f172f1c292d2c9f0a97fdbfc8d5f5f777b150f
+size 4321

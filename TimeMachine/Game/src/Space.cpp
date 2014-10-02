9fda0d6738df2e7c257f94c209ddd84533592e74
Jake Shirley <navi@odinseye.org>
2014-10-02 16:42:32 -0700

    Updated paused spaces to not hide GameObjects

--- TimeMachine/Game/src/Space.cpp
+++ TimeMachine/Game/src/Space.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e50c09720f7d1c52b74cfee1d7bc9b543388ca5491d851d003f9f16ddd2b2811
-size 3850
+oid sha256:c7f26be3c5cf2a35f1f74538c5db7f20be5df6e8aa27b78b2b5dc2d97a92bbdc
+size 3809

2d94ed524f629c5b8e016719ee11fd5528daa7bd
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-10-02 13:51:57 -0700

    Almost there character controller.

--- TimeMachine/Game/bin/components/PhysicsMover.lua
+++ TimeMachine/Game/bin/components/PhysicsMover.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50fe5f4eea605f99c23e2da158a61c2295b482682fbfb636d9c3b15314e93a5c
-size 4003
+oid sha256:627fad76cb402287a030e87bc66e44a91433ce9b38732f2a30e0ec89da2a884c
+size 4115
--- TimeMachine/Game/src/CppLevel.cpp
+++ TimeMachine/Game/src/CppLevel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65ab52f1a05a1505fbed523eb80a5a618573a96c69e79ab73daad6cbaff5c712
-size 12148
+oid sha256:7510d2c3d047ac468e486474870e70f9404ef9f57388adb7714906829c166c1e
+size 12757

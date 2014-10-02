b3f9332c0acdfedb8ba9c005893dde39cdd8b420
Garrett Allen <garrett.allen@digipen.edu>
2014-10-02 13:15:43 -0700

    Quick fix to Vector3's new method for serialization. Tested to confirm reading and writing correctly.

--- TimeMachine/Game/src/Model.cpp
+++ TimeMachine/Game/src/Model.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1e444b7ee7049db7db5e853481cefe6542e45f006287f5504341a269e33aa78
-size 4225
+oid sha256:638b8aa8bde4cb72806122e20a5f9337db767c35d0dbb317c5e307e59b5a76f3
+size 4223
--- TimeMachine/TMTools/src/JsonSerializer.cpp
+++ TimeMachine/TMTools/src/JsonSerializer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91dac6080ba70d124aabb1eb0c0004915ddb2f83e89027d17df12d9b3adb0380
+oid sha256:8c0908e45a16f574347f87c8ba473a447516ffcf14c02d05a96fedbb90c30924
 size 14351

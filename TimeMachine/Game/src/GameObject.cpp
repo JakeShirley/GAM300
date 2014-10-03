2decc57ddc62ed05cf9e2e9934143d31cf9fced6
Jake Shirley <navi@odinseye.org>
2014-10-02 21:27:18 -0700

    GameObjects clear them selves if they are selected on destruction

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48e9b1c0bb89f337803681e36f41e0ef28b00aa025335de431df9e30b720e65e
-size 19280
+oid sha256:95c570d1dabd7e0b32b97e3225d9d05203f6d1fc874be3f6d086d76b7a90f183
+size 19410

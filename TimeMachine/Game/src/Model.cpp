d3faddcdf61e327f422570a55f96034e8bd1553f
Jake Shirley <navi@odinseye.org>
2014-10-02 17:00:57 -0700

    Serialize model scale offset

--- TimeMachine/Game/src/Model.cpp
+++ TimeMachine/Game/src/Model.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c1509392faf591062b722dd6c33911dea5b516c6f3845a60f41f8aa894dd2af
-size 4323
+oid sha256:6275015f14606b0d238809b8e4f172f1c292d2c9f0a97fdbfc8d5f5f777b150f
+size 4321

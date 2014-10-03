01357ffb93819ea4fc5485b226dd3aefddf2e3d9
Jake Shirley <navi@odinseye.org>
2014-10-02 17:00:45 -0700

    Fix player scale

--- TimeMachine/Game/src/CppLevel.cpp
+++ TimeMachine/Game/src/CppLevel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7225b609d9c4c938c04c5a5284c5a1f1d3f84e4810152c24dedc1e5433a9b10f
-size 12062
+oid sha256:c356f5fd32680e622ed456c60929e07e9c5afa1fc327a24596329f6312d20fbe
+size 12061

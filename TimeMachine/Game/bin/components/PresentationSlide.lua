357047b40a454015c50f176f62349011fdffb5aa
Jake Shirley <navi@odinseye.org>
2014-10-02 21:27:45 -0700

    Load presentation slide component

---
+++ TimeMachine/Game/bin/components/PresentationSlide.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:751a52b3aa892ac6573616db8cb4c70ded74514632f3df7126f0b2c1a5a7fb2d
+size 746
--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d3d8580f1320aa4fc8be54e2a3f69933638af627632176b54ed421c251dedee
-size 23403
+oid sha256:2ee5b63ef0fb0a7362309a4e8ea4a7bd8eaafc0c78442225d0fc56862029c4c2
+size 23490

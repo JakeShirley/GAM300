563f3caf0000a213df34f28b0ee701342d4b4010
Jake Shirley <navi@odinseye.org>
2014-10-02 17:01:11 -0700

    Fix pause spaces from hiding

--- TimeMachine/Game/src/Space.cpp
+++ TimeMachine/Game/src/Space.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7f26be3c5cf2a35f1f74538c5db7f20be5df6e8aa27b78b2b5dc2d97a92bbdc
-size 3809
+oid sha256:489814c79234dd39782224fe59705afb31209362221ef25d64c5a62788f4937a
+size 3796

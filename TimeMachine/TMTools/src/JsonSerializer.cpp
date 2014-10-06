0aef3166c53428061ef63b594164786d53c7b933
Jake Shirley <navi@odinseye.org>
2014-10-05 19:52:33 -0700

    Fix for loading Vec3s not as a member of a class

--- TimeMachine/TMTools/src/JsonSerializer.cpp
+++ TimeMachine/TMTools/src/JsonSerializer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cbd4a954e3916eb122ae8b0b7e32386c585ca7cf813e323e164daabd129d17a
-size 14349
+oid sha256:d05f3e316c835942fbd52b72906603cbca0d26638ab83b37fb28d5aa5a22ba1c
+size 14583

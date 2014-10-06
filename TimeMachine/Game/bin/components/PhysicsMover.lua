058b1c1f2e9732e0a0810f89863886516666bf2a
Jake Shirley <navi@odinseye.org>
2014-10-06 09:49:57 -0700

    Made Engine:Connect not take event priority and created Engine:ConnectP if you want to specify your event priority for your listener.

--- TimeMachine/Game/bin/components/PhysicsMover.lua
+++ TimeMachine/Game/bin/components/PhysicsMover.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61c1be097c2158fae50ea59cedab9c13ff0fe39834c58849fe6c079253ebb277
-size 4562
+oid sha256:b5fe9971b2cf77927e829254ef6a9bfad6baca3f78e82683ec6381f1a70e8b7e
+size 4540
--- TimeMachine/Game/bin/components/PresentationSlide.lua
+++ TimeMachine/Game/bin/components/PresentationSlide.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c16eb1c9cfc3a4fca93b460a9216f465809c22f8c259b8e5bab740e605f80e4
-size 1759
+oid sha256:fedbba18ec569e4f7d36134c0166d48411945dc0a41c64e289a9623749cc3477
+size 1738

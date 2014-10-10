c44de69bb5569ecc7444a56b48b0d3626b263f7b
Cody Centers <murtag27@gmail.com>
2014-10-10 16:38:35 -0700

    Fixed my warnings.

--- TimeMachine/TMParticles/src/ParticleBehaviorMods.h
+++ TimeMachine/TMParticles/src/ParticleBehaviorMods.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cee69ba10dd2587f3108d355b8776731503eb979bb5b36d14ed5cac81dc5199
-size 2115
+oid sha256:5097d6431e40ab985b3827df4ee58cdb0dca4cb2957ff9ca1e3f191b0ce68417
+size 2135
--- TimeMachine/TMParticles/src/ParticleEngine.h
+++ TimeMachine/TMParticles/src/ParticleEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb27b38ebc883e3f8cb2458ff868f8eb4d9825c29e3e4ff91ea6389bdc06f0a9
-size 11432
+oid sha256:2f2965af161c267de55fea80f35f2d91b5d69677794771218e77969c76ecad97
+size 11474

734fb7ffd3354a22377075efdf324c6d4cd70e8e
Jake Shirley <navi@odinseye.org>
2014-10-09 21:09:10 -0700

    Default Collider materials and warn logging if a material doesn't exist.

--- TimeMachine/TMPhysics/src/OopsMan.cpp
+++ TimeMachine/TMPhysics/src/OopsMan.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afc3d4106dbfc50915335562a75e2524b99f144e2637fe96a289829e3c9bd3d3
-size 4574
+oid sha256:c16f32e7fff860a9367f8d42f07901e466ae91e7587b0bf7bd7f7f0043d3f86a
+size 4797
--- TimeMachine/TMPhysics/src/OopsMan.h
+++ TimeMachine/TMPhysics/src/OopsMan.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70ed05f2cfb7d839929bb11c00071ceead858ba4c5a3ed223a6b3e357ea90cad
-size 2341
+oid sha256:8be311d1d13ba13fa5b21d8ae47eb2f1c5a8cac72571185061b63246d0a94ecc
+size 2386

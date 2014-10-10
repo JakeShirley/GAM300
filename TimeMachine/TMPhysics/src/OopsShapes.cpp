be82569f264bdc080768ce692b365060b8da095b
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-10-09 22:43:11 -0700

    Beginnings of optimized SAT for cube to cube collision detection.

--- TimeMachine/TMPhysics/src/OopsShapes.cpp
+++ TimeMachine/TMPhysics/src/OopsShapes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd48f295d3dad0043f68058f097a7dfff58d009655bc52658ad698a2b0778917
-size 5435
+oid sha256:0a18c840704b0f5a2a1f9a19d35bcfa9b49c81ccf1320ba19e6d8be0d9ce30f7
+size 11035
--- TimeMachine/TMPhysics/src/OopsShapes.h
+++ TimeMachine/TMPhysics/src/OopsShapes.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e4e490f710a617dcddc2837125499a4a16b539b5dfa9702c8e9a413b959b171
-size 1286
+oid sha256:f09d8a5b2033af2e109fd07d4dcff726bbd86062fba5f79ca09c5365b6523b2d
+size 1479

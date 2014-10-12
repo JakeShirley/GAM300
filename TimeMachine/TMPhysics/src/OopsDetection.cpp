c3629c40a7b6f71b70bf28a4bd000d30bc12db69
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-10-12 13:46:52 -0700

    Commented out use of SAT for box-box collisions because it's not quite done but I want to push.

--- TimeMachine/TMPhysics/src/OopsDetection.cpp
+++ TimeMachine/TMPhysics/src/OopsDetection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:029e73c042ded06a4ada6e3a79a3043d3979d3e41abd032e9bbf1f8c3ab9e7c2
-size 18551
+oid sha256:eef5b8fcaade1b54694024f439f2e353fd7db142bd20c522e5bc276df955e33e
+size 18642

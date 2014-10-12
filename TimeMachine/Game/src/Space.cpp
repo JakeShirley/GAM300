1e55d76ba0043b73b0c8b939de2181f488d9bd96
Jake Shirley <navi@odinseye.org>
2014-10-12 14:34:56 -0700

    Spaces call Destroy on GameObjects.

--- TimeMachine/Game/src/Space.cpp
+++ TimeMachine/Game/src/Space.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a79464491b9df641cd17141705c00396e8ba9886209ab8d8d3f90e95f07a5ed
-size 6237
+oid sha256:26e0341bcafb9cb235ea178217777e0ec6cf1f363121e5e34ef14168b17db2e2
+size 6232

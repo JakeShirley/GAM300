9b482f76d7162e53ff4e16f6bde127a00c279a60
Jake Shirley <navi@odinseye.org>
2014-10-12 18:54:56 -0700

    Fixed -width and -height arguments to the application.

--- TimeMachine/TMApplication/src/TimeMachineApp.h
+++ TimeMachine/TMApplication/src/TimeMachineApp.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62b222efcdec757da445f54789c0b3cdf9e679b53c90cc0db9d71421bdac65cb
-size 3388
+oid sha256:f2e0671bbb851a55aabb11bb7da9014e495ab7330d12fed9de92ad3827b70435
+size 3428

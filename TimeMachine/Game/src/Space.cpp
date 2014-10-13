6a0c346120c2401e45ce40390ec35f2762c2d785
Jake Shirley <navi@odinseye.org>
2014-10-12 18:56:34 -0700

    Spaces with EventDispatcher and DestroyObject file

--- TimeMachine/Game/src/Space.cpp
+++ TimeMachine/Game/src/Space.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26e0341bcafb9cb235ea178217777e0ec6cf1f363121e5e34ef14168b17db2e2
-size 6232
+oid sha256:6b75a9fee26489c7f5163656058d6ce5f3add22b9500ff7be4c5ac9ba5fcf51f
+size 6759
--- TimeMachine/Game/src/Space.h
+++ TimeMachine/Game/src/Space.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47a09b85ddb730e3b56ff1672cce6bda4affad36eb36bc81d5c95e13db40d3d1
-size 1927
+oid sha256:243b90c1518bdb43210cdd2db271a759f063b28cd4a9238015091e8e384f943a
+size 1975

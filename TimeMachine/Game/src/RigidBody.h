d81daf3018de86c0cf7c04d2056dea2dda6e583d
Jake Shirley <navi@odinseye.org>
2014-10-12 19:33:51 -0700

    PostLoad

--- TimeMachine/Game/src/RigidBody.h
+++ TimeMachine/Game/src/RigidBody.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71acd837fccccbf3e704bc948d223844bdbf8c5d5b365e54e80827130dac6981
-size 2135
+oid sha256:d4c238ac6763d49a650a0d2597a22f6543a55064ab47e9c4a80a82aec2286f91
+size 2171

e4b1e9638a0f727a7214282051024952c4116ba6
Cody Centers <murtag27@gmail.com>
2014-10-14 15:28:40 -0700

    Particle editor now saves expressions, and added a sprint in the editor

--- TimeMachine/TMTools/src/ExpressionHandler.h
+++ TimeMachine/TMTools/src/ExpressionHandler.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39f91d1c45b08c247fb3d4bd24a130417bd9fe3249858718c7d69ac0eb48ec1e
-size 1997
+oid sha256:7bc8a3e61160e19b968fd41eb3b6c63f1eb9856ff7bbafe663d2af356344502c
+size 2033

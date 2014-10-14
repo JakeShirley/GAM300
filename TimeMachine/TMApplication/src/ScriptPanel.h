646e539a5ca79548bdeb4b370aee48043ab2eebb
Jake Shirley <navi@odinseye.org>
2014-10-13 18:21:45 -0700

    Added OnSave callbacks for LuaScriptPanels

--- TimeMachine/TMApplication/src/ScriptPanel.h
+++ TimeMachine/TMApplication/src/ScriptPanel.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fff9a500136e84c241c8ba5a40915454fe3fbb570762311c5637c90011100fc
-size 2040
+oid sha256:dba572e5a057eb43ee1c79cd7258c41fd798dcaf6a3805940c7ff6b44069f169
+size 2192

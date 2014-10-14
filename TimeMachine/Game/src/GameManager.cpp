67280e5840bbc4f76b9d975abbfd956861799323
Jake Shirley <navi@odinseye.org>
2014-10-14 14:27:44 -0700

    Added menu option to dump the lua stack

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23fac1c38a35a29b66eff2d2e66fd9303d8de3620a1066b612c118ec04d971b2
-size 31288
+oid sha256:291a981e443692ba86c2738e9dd42e79c26fb01ea7e37ef02eee92190d9bc8a6
+size 31287

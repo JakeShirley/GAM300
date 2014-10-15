beac152bb561c27771afd9391d2334ce263746fc
Jake Shirley <navi@odinseye.org>
2014-10-14 17:06:06 -0700

    Clean up Lua stack after stopping the game.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:291a981e443692ba86c2738e9dd42e79c26fb01ea7e37ef02eee92190d9bc8a6
-size 31287
+oid sha256:2bc58d62539f843878d233207bc94a8abc9c29e6a3518c0340fe492d97db0ef8
+size 31328

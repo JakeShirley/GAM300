ca30423ecf7890146dc45a40c046f3018967d15b
Jake Shirley <navi@odinseye.org>
2014-10-14 19:01:30 -0700

    Fix for LuaScripts crashing when closing their script file views.

--- TimeMachine/Game/src/LuaComponent.cpp
+++ TimeMachine/Game/src/LuaComponent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d20af095975fb7446a62e6c75608ae9b7cdc01cf9effea6742a71d26f193a543
-size 8759
+oid sha256:4b31ed856b5030f2ec587ecd49f91bf4205bcbf76a40bcee0c5655e0deafe7b6
+size 8845
--- TimeMachine/TMApplication/src/ScriptPanel.cpp
+++ TimeMachine/TMApplication/src/ScriptPanel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08f85071a42f27894630bdf4f4e5b2edb0a47c10fe45ba5f064976c9e18ec977
-size 19925
+oid sha256:7c74996951bc1745f45a33afed90afe28610637294a0c260e3d53e2f1610388c
+size 20858

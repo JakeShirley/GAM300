65fb5d399bce6a70840b96895d703353e24b8e75
Jake Shirley <navi@odinseye.org>
2014-10-14 17:05:41 -0700

    Fix GameStateManager to set Initialize state before calling initialize

--- TimeMachine/Game/src/GameStateManager.cpp
+++ TimeMachine/Game/src/GameStateManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7606913f649c9345fda32a2026293cbbb9647a5a818c75574e6aec07794a1fdd
+oid sha256:83c4c97e1c4953386c672f78f9b04d9347aca75c5a6e36447843edcb7027acca
 size 8478

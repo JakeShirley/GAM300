c6232456b3044da69e836780256fcd15f6b13e65
Cody Centers <murtag27@gmail.com>
2014-10-15 15:57:28 -0700

    Added the ability to pass dt to expressions

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f053d81fc12f1bce53863d42d34e539a5d78399694ee029e665c74d9da9a8ea
-size 59779
+oid sha256:5ec434413703eec0ad1d01f92a0d459f17cbe9ab5d6cfb35f9bb51b05cca0c69
+size 59805
--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:826100b423dfa77581f2617da55b4753fa6a8df0ab0ab236fa800a281266299b
-size 42033
+oid sha256:b465a04d5a581e6a0f7b47db19725f24e583ab09de52aaedb5e5ac969f669ccf
+size 41902

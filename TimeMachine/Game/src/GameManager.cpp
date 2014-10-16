38dd8dfda5610750547cbed75dc71cf74acf0d75
Jake Shirley <navi@odinseye.org>
2014-10-15 23:19:22 -0700

    Added level settings icon

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bc58d62539f843878d233207bc94a8abc9c29e6a3518c0340fe492d97db0ef8
-size 31328
+oid sha256:ec2d00e04c39defe344bbac3632bee20750f1837e6ff7f2150ce4ea0e7cc9213
+size 31371

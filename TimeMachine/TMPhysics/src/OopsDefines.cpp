844ad1bf026535143f4b9d24714b85e96f9a936f
Jake Shirley <navi@odinseye.org>
2014-10-16 13:24:31 -0700

    Stop drawing primitives by default in physics

--- TimeMachine/TMPhysics/src/OopsDefines.cpp
+++ TimeMachine/TMPhysics/src/OopsDefines.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dd73c39d6c67ddee9fe7f80209ff1abe68f28df8134a99d4dd6de5715914b9a
-size 1285
+oid sha256:789736cf366b6aa2051189f7e5f268c754c8671d95152a7379e1b2933718f699
+size 1286

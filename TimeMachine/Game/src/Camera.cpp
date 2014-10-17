e014607a8d0dd2c7be67137a9cdbaf6533eac4dc
Garrett Allen <garrett.allen@digipen.edu>
2014-10-16 22:45:44 -0700

    Added Commenting to Methods in Meta Data through AddMethodComment after the AddMethod Macro Call. Added Camera functionality for the middle mouse button.

--- TimeMachine/Game/src/Camera.cpp
+++ TimeMachine/Game/src/Camera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:593ba120a62d17ad00a9cf4aea78b84d542716b40762d5ff940313d6c034a1aa
-size 12354
+oid sha256:d1b518671cfcb61d7d8ef8a093adc8d61cb8dd3a80b6910c80dae5c2f2da8a97
+size 12861

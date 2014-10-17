1ed84d27552308d8d01ca63737455fcb5f2655bd
Jake Shirley <navi@odinseye.org>
2014-10-16 17:46:02 -0700

    Added component-wise Vector3 multiplication.

--- TimeMachine/TMPhysics/src/OopsModelAdjInfo.cpp
+++ TimeMachine/TMPhysics/src/OopsModelAdjInfo.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ce6c404e3c57e579c2f79ce70a08a0563c1814e97d4e8ab8ebb4d6a93b6782e
-size 4040
+oid sha256:433459b007f8a952e41880c779f1fe41e78c34c44e93e0b32a46570c7bf6d981
+size 4046

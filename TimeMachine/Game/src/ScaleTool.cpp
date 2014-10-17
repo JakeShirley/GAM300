12f2b7e04c36c0a57f1074014695d48964c1647a
Jake Shirley <navi@odinseye.org>
2014-10-16 17:46:44 -0700

    Added scale offset to Model and Collider.

--- TimeMachine/Game/src/ScaleTool.cpp
+++ TimeMachine/Game/src/ScaleTool.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:331198232d6d4d547a508948adec8b4e4dadae7d335fa5a81d002010a72b6160
-size 12485
+oid sha256:901a4febf89c2e0efd04de6de8b8e63efd25397357dad6582017bc1981b6701d
+size 12539

961117ecd3e42f417cd712806b5bd0471b2d3b94
Jake Shirley <navi@odinseye.org>
2014-10-18 14:45:52 -0700

    Added extra assertions to EventManager.

--- TimeMachine/TMEvents/include/EventManager.cpp
+++ TimeMachine/TMEvents/include/EventManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a38288408bfbb9904163164cfe3ffd39fa02d2b9239ff2a1acc7919d1ad7524
-size 2259
+oid sha256:219405a711160c9c6d4134166aa01fdeff066a2de8a150c9437232efe92c3090
+size 2428

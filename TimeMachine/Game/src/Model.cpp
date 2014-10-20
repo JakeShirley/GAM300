2c0aa4fea75d17494898db5b461ff08a9071729f
Jake Shirley <navi@odinseye.org>
2014-10-19 19:56:34 -0700

    Added zero-scale check for Model components.

--- TimeMachine/Game/src/Model.cpp
+++ TimeMachine/Game/src/Model.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab8e56d03e763e5b9e5a965d2e6d786225cab6de065775a5ee339a17c29d4092
-size 5454
+oid sha256:358f3bcfac99272a6fc0d843505ec8dd40be2409714e0b48e19d94d12f35b257
+size 5628

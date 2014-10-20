3d654a1b2e96f77d9575bcd6f8d85206aca431fc
Jake Shirley <navi@odinseye.org>
2014-10-20 16:10:05 -0700

    Test comments for RigidBody component.

--- TimeMachine/Game/src/RigidBody.cpp
+++ TimeMachine/Game/src/RigidBody.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:946353644887cd12a72a453d4548d809121ffaf15a52cc2c72fd1be1757c72f7
-size 16710
+oid sha256:d939e41d0d2331a1bd238c47eef1f7b23b17f1398ef0cea31ac83a6d436a8401
+size 16848

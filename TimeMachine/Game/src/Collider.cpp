476d3f4128c94af00e568748943f4faff697d97f
Jake Shirley <navi@odinseye.org>
2014-10-19 19:56:10 -0700

    Added extra checks for materials and models in Physics.

--- TimeMachine/Game/src/Collider.cpp
+++ TimeMachine/Game/src/Collider.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8e41c45116affacbb6c3ee1351f04b020c10575fc797622435bdb830cb32796
-size 6436
+oid sha256:d66f197f194f65b8161c7a15a307369f931ef8daf7ac96faf8e95cf839311cd9
+size 6637

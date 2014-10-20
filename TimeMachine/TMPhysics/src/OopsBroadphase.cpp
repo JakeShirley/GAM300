1232976d0cdfb613319572dc37987bc0c0f49f93
Jake Shirley <navi@odinseye.org>
2014-10-19 17:13:59 -0700

    Skip over expired objects when raycasting

--- TimeMachine/TMPhysics/src/OopsBroadphase.cpp
+++ TimeMachine/TMPhysics/src/OopsBroadphase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bde45743cf7e104794168a68fd497e42c62ed0f62967507e4d31afdd2ae542d
-size 16658
+oid sha256:1e2909f0450e41a1947deb18323d368b52cc42e7a52fe43afc11e51d4d09058e
+size 16764

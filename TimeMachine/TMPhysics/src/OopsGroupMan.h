2d3fcb1d20b2e3ab77fa09e0c0dc2389ee089e6d
Jake Shirley <navi@odinseye.org>
2014-10-21 11:47:01 -0700

    Expose number of collision groups.

--- TimeMachine/TMPhysics/src/OopsGroupMan.h
+++ TimeMachine/TMPhysics/src/OopsGroupMan.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55ab0bb871396b00fdcbcc23d5effd94fa6a5b80a43d2b1d2c911de68017cca9
-size 1489
+oid sha256:1379c8af1d3fe12350aa7fc9f51aa7b00017d50a7fa5def38e397b1a661e5825
+size 1561

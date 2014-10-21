4c1dddccc54cd4d6333c360d0ba42015ceec4ce9
Jake Shirley <navi@odinseye.org>
2014-10-20 21:49:26 -0700

    Updated auto-registration of meta types.

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78b8b50a915e7c3cd4740ad38a768f242f4ef65124ffdf23bb22ac30b8720e5f
-size 14647
+oid sha256:2b01f0f4008bbb1ca99cc822d9143821c297acc1dc1e9599d2e9446bd9dcd9bc
+size 14375

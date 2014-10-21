58ec98c0a237b466a587438c60cf9f4a65ee4a73
Justin Merrill-Boyet <ecureuil21@gmail.com>
2014-10-20 23:08:14 -0700

    Jake added extra check for objects

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03e7dd1e09752d3f26361fe6ddf1e073998b6b398e7cccd9366288faba2722be
-size 24599
+oid sha256:7ab937936ae0619ce8cf3b9b1f0a5b36492fa8acf47d658c5365e2b5b6c6b62f
+size 24616

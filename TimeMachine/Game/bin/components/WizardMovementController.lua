b413f54e8987e065a032d41c1fdd859ac38399cc
Brian Inkrot <binkrot@gmail.com>
2014-10-20 21:32:58 -0700

    Almost fixed turning...

--- TimeMachine/Game/bin/components/WizardMovementController.lua
+++ TimeMachine/Game/bin/components/WizardMovementController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e12f89408b3911a5ed7f165519eecad106ea4934f5db84b0ab277ac6d4a384b4
-size 3884
+oid sha256:fb07032adaf7c725e182f5958a96123574c521065922b90a65df5b79fe0bb4ab
+size 5561

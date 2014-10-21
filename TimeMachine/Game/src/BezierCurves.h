2bf0326ef08ba2205a4bbae0977e5fded95a0f4b
Cody Centers <murtag27@gmail.com>
2014-10-20 20:50:58 -0700

    Cinematic camera can now pause at a node for a given amount of time

--- TimeMachine/Game/src/BezierCurves.h
+++ TimeMachine/Game/src/BezierCurves.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d5c43abc2697c52124af3b23d699846f93a4110ef89d08548f42408946bd013
-size 5653
+oid sha256:b27649bc8992763be3ddb6f0d515667374491d3edbbc64ff2a67d6eed47e3a58
+size 6174
--- TimeMachine/Game/src/CinematicCamera.cpp
+++ TimeMachine/Game/src/CinematicCamera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12829e86a6470aa312aa5cb01d2f129f1a811ed8da11849296dd466915e4ae17
-size 15798
+oid sha256:e7eb0506dfa7dcc42cb5f2bf868e9b55f370d3efa7a67b09f52250e3ca1f6fdb
+size 16534

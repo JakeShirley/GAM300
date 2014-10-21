2075b12208bc3653788c850afc6bc55296d2643f
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-10-21 01:10:15 -0700

    Optimized Sphere-Sphere collision detection. Several optimizations to manifolds and their usage in constraints.

--- TimeMachine/TMPhysics/src/OopsDetection.cpp
+++ TimeMachine/TMPhysics/src/OopsDetection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9120e526b140d2c29a65e6889e9db3a71e93f931a28d4fbccec47f538877f5e
-size 19437
+oid sha256:1434ff6c7d4654277c59a653615e62e84b28521a58055dfdec87d0cd16e628c5
+size 19765

da79f9e573ea13588c9a8596e34389e97a5bc3cd
Jake Shirley <navi@odinseye.org>
2014-10-20 17:59:48 -0700

    LuaComponents now order their properties in the PropertyGrid by order they were added in. TM-14 #resolve #time 15m

--- TimeMachine/Game/src/LuaComponent.cpp
+++ TimeMachine/Game/src/LuaComponent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:776ea06275512b98c9882b55caff0a1d52a4c8d77fbce8f17995e721ff2d1759
-size 12554
+oid sha256:7fcc6af3fdf3c210b19cbb4fc2c46b28224ea2bb84b1f6f4bf1ea60542d13eee
+size 12656

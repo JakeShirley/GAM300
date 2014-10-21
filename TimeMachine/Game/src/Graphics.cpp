03a4e02c41788340bddab93ffbe390892673f9f5
Jake Shirley <navi@odinseye.org>
2014-10-20 17:41:36 -0700

    Graphics auto-loads asset folders.

--- TimeMachine/Game/src/Graphics.cpp
+++ TimeMachine/Game/src/Graphics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76cf20e98fd52250922a85e69809c92fa73624cd1a4aaa965ff71a64ecb47dab
-size 12706
+oid sha256:d7c9f65769db970887f870bf7f600de1a2e77309a26ab928371ddd3d3f670aa3
+size 12448

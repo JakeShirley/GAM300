9186e4a55c3bf68865009f819d3288d166282565
Jake Shirley <navi@odinseye.org>
2014-10-20 21:51:31 -0700

    Fix infinite loop with self-parenting objects.

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03e7dd1e09752d3f26361fe6ddf1e073998b6b398e7cccd9366288faba2722be
-size 24599
+oid sha256:246f090ac19f1f3a6b27b95b9caa8247f206660d20f21f8385158d2b8aff160d
+size 24643

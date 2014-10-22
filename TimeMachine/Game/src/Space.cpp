8d45b22544af19c6d64fb29e3e8c4a353bf739fb
Jake Shirley <navi@odinseye.org>
2014-10-21 23:38:44 -0700

    Added Create and CreateAt on Space.

--- TimeMachine/Game/src/Space.cpp
+++ TimeMachine/Game/src/Space.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3081090fdf4f22520b6887521fb4b5afcbfbdb701cdb91732f0fb02c2eaf576f
-size 8119
+oid sha256:6d01f00d902c809d6edf2a4b495ae79052b1f64e688b183b3954cc99ffcf6ce5
+size 8591
--- TimeMachine/Game/src/Space.h
+++ TimeMachine/Game/src/Space.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:654f747082a8b5b61bcacfbfe397c5d4c2debe49f7f962218e3e561c90517b36
-size 2087
+oid sha256:2554df47e011518add4502a7a3330758724aa694d29f6adce429cd6a370c76e5
+size 2259

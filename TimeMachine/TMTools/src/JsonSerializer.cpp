8d90b21074eaab815be06b56b82327d3d7017c47
Jake Shirley <navi@odinseye.org>
2014-10-21 22:44:26 -0700

    Made JsonSerializer only add .json if there isn't already a file extension.

--- TimeMachine/TMTools/src/JsonSerializer.cpp
+++ TimeMachine/TMTools/src/JsonSerializer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e0ca25ec7fea05b56796403cbffdf92a36ab5d827749073fef246256c970a6d
-size 16472
+oid sha256:5ddf6c16a1c78bfec096c587383a8c72c34b2d20fca799257403a7b027846c8b
+size 16576

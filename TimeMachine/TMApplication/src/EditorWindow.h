2e2b0a500cd2a6c570b118dd23da363a2109d988
Jake Shirley <navi@odinseye.org>
2014-10-22 15:29:26 -0700

    Added view->Collision Group Editor

--- TimeMachine/TMApplication/src/EditorWindow.h
+++ TimeMachine/TMApplication/src/EditorWindow.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9deee957395df845cd9fa377cc9774a6746d46ce3da7e725aaff8bc3383a9ad7
-size 2444
+oid sha256:d018734355ab0ee9db13bec4657dba7ae750291508f3f76177b07cba4d32cec4
+size 2524

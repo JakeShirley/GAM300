632ce25a578e8f005a469e83b427c3cc14f5bc28
Michael Chambers <m.chambers@digipen.edu>
2014-10-22 23:45:56 -0700

    Makin' it so you don't need to push the button twice to dash.

--- TimeMachine/Game/bin/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4024d32d185317cf8ce5da8d03189f4630140983badbada1da702bcc66effd9b
-size 11891
+oid sha256:5f25611fa1b8fbb7da95d806bd25bc2a61c8935bfde13cf5ffa098f77c546889
+size 11958

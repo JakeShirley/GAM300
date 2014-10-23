71eb4dd83633ee4edbaf1a19cf310b42367da22b
Jake Shirley <navi@odinseye.org>
2014-10-23 09:57:57 -0700

    Set/GetVisisble on Model components.  Note: For existing models this defaults to false for some reason.

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7127bcde9b88d89d1df5d701cfd1ebccd3eb62be15d415d39bac0ff058d2f268
-size 25425
+oid sha256:2095bf2c2700e92ed1734ace9a20651af9076f684fd136126da9c913c02943c7
+size 25475

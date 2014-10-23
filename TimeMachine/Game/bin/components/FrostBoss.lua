c0c76c6b02271cc94239f4185eb000d21d88c1ed
Brian Inkrot <binkrot@gmail.com>
2014-10-23 16:35:22 -0700

    Breath Attack and really primitive feedback is a thing.

--- TimeMachine/Game/bin/components/FrostBoss.lua
+++ TimeMachine/Game/bin/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41a4d29d05810edcbaae301a3b55acec49c8a0d5cae3e7a0ba2203b649e6b185
-size 1834
+oid sha256:2e88bc6cfe67b7d7b2bf8678135988eaaee5029c8e1b9299eab353bf60ffbd6a
+size 3566

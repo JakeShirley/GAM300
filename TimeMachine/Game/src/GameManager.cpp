1f7f4e937a7bc9184c5982fbe60e7671a2d1081d
Garrett Allen <garrett.allen@digipen.edu>
2014-10-22 20:28:48 -0700

    Updating linked archetypes when uploading changes.  unlinking all archetypes of removed type when removing archetypes.  Changed CreateObjectAsArchetype to not load or initialize

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a186d481b35d3e1beb2aaa3f4357043ee1cd237af0bb81704697a4cb7586a98
-size 35028
+oid sha256:c7a40c617758ddfa6e0e7c71412a8c2f12481213223a86c1eb19aa7470b8e50c
+size 35322
--- TimeMachine/Game/src/Level.cpp
+++ TimeMachine/Game/src/Level.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e68a2a4785272d44d34242fb1ac555f1104f1f4c445c5907f6a64c43e0fcfe54
-size 4164
+oid sha256:ef0f92ae97397ba038fb04438a589190c9f941c67a04569de66ac3d6719af7a6
+size 4158

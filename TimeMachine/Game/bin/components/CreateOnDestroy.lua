584242500a3463ab26d00dd8ba188eb5e6dd75f6
Michael Chambers <m.chambers@digipen.edu>
2014-10-23 15:20:17 -0700

    Housekeeping.

--- TimeMachine/Game/bin/components/CreateOnDestroy.lua
+++ TimeMachine/Game/bin/components/CreateOnDestroy.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65cb410b2db70659296179b9ad45f8f0976f16cc37d690f24a140e9ca9a84d35
-size 1051
+oid sha256:41b7b5e49d88c7977f6168f6fbe581872ec2b9bf132d16eb3f706e6de0e4a570
+size 1049

eda9e43ffb5613daf2779155224554f8cddca5ec
Jake Shirley <navi@odinseye.org>
2014-10-23 11:29:12 -0700

    Added Owner as self.Owner to components.

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2095bf2c2700e92ed1734ace9a20651af9076f684fd136126da9c913c02943c7
-size 25475
+oid sha256:5c5e661abc86750fae9db44226c58c8e10db643801a3b9bc7bf5f290624560cf
+size 24558

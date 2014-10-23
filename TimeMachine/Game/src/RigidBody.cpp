bac5537ed38bfae9c91777f0e8556774271f656a
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-10-22 19:42:48 -0700

    RotateBasisTowards2. RotateBasisTowards confirmed working.

--- TimeMachine/Game/src/RigidBody.cpp
+++ TimeMachine/Game/src/RigidBody.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c945783198c4ea1157dbff07be10aecb0b57edd3167a56fadad66f625909b0b3
-size 21773
+oid sha256:a65c629c15e672143ef2c5d45d135b253055697cba6329539bfd98a4d1bfa61d
+size 22194
--- TimeMachine/Game/src/RigidBody.h
+++ TimeMachine/Game/src/RigidBody.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57b2d9e3310e3f9838dff2b0e4773755e900e0bd8cbd0f0980da9928616efcbc
-size 3935
+oid sha256:d1ae1b71e0467c5541dd53b847b8d462535f2edd31167d0bd5501984c73e94d2
+size 4055

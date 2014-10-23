45910e621fc0d4e5efd437364cdc82ed2ad62a43
Jake Shirley <navi@odinseye.org>
2014-10-22 23:16:06 -0700

    Fixed autocomplete for Lua components

--- TimeMachine/Game/src/LuaComponent.cpp
+++ TimeMachine/Game/src/LuaComponent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd0655a2489f29a57d38b59bef94dec8b33a9a8d99d624c5bee84c8a7f0fe90e
-size 19947
+oid sha256:e26bec055545ea814f474f4e9317f1044e09db6a2e0c9046e3b71033bf2b3b65
+size 20235

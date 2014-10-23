cff19e22a55c8d2a444ca9135e19728c075751fc
Michael Chambers <m.chambers@digipen.edu>
2014-10-23 14:38:49 -0700

    Updated Damage for my own purposes and so that in the future we don't have that "x10" somewhere.

--- TimeMachine/Game/bin/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f25611fa1b8fbb7da95d806bd25bc2a61c8935bfde13cf5ffa098f77c546889
-size 11958
+oid sha256:5fbee2bc4fc35f2eed46884f7dc625bf429665fec20c171a03a52c2e8949e6fb
+size 11959

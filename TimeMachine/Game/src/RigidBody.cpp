a4ca385ff588282a0844b7d95151cbe0f4016eb1
Jake Shirley <navi@odinseye.org>
2014-10-22 17:48:41 -0700

    Serializing RigidBody gravity now.

--- TimeMachine/Game/src/RigidBody.cpp
+++ TimeMachine/Game/src/RigidBody.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e3cb6cb38111b8fc230bd7cecca280c94c1b1882e2a91389ed79d071ff2525e
-size 20636
+oid sha256:c2a87c3235ac35c8ce2838b4f789925e7f84edc4ce7564318e5c23962026cc01
+size 20212
--- TimeMachine/Game/src/RigidBody.h
+++ TimeMachine/Game/src/RigidBody.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cb612d300d8eae453bee39b573a3da555368cb8340789fc1d799587ff388534
-size 3751
+oid sha256:ca6a9bbc70991fc77a01a677fe85a4f8aedbdac5d0536e3e9c6387d975cce43a
+size 3770

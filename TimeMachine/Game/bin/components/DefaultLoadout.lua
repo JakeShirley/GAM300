3b2c9bb43306978dc2c04fc241c9b63b9fc067ed
Michael Chambers <m.chambers@digipen.edu>
2014-10-22 22:52:56 -0700

    Fixed undirected dashing.

--- TimeMachine/Game/bin/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b45d93fac059cc26aeb7aadd26fa68a0d0863692e86e95d93247a29c0f3d08a
-size 12121
+oid sha256:ddc5511d669e09d7f30decba5abf57fbeb4f4d316333f9471a5c54a1038177dd
+size 12162

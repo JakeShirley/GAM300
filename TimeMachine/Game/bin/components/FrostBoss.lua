eff93e9e7808f8909712139ee5d9359619597543
Brian Inkrot <binkrot@gmail.com>
2014-10-23 22:39:36 -0700

    more progress...

--- TimeMachine/Game/bin/components/FrostBoss.lua
+++ TimeMachine/Game/bin/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae743e06b2dbec8042b0fef7f380838ea4b1c739953c43250931952d6e1bb6f6
-size 7294
+oid sha256:66635ecca9ac114e8e8daec4eb07eab9013ba3d89c54b2d6153a334fd54402b2
+size 7497

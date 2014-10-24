f58502b10596728fe473d38fdf45c01d024153e0
Jake Shirley <navi@odinseye.org>
2014-10-24 09:47:39 -0700

    Made Component properties remember  if they were collapsed or not. TM-13 #resolve #time 1h

--- TimeMachine/TMApplication/src/PropertyGrid.cpp
+++ TimeMachine/TMApplication/src/PropertyGrid.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fbb21e9a8ea9b0478e1d8a13d0807f016770dec9c40d1c6bb1aa8c553727e61
-size 5607
+oid sha256:9738f5ef1c34019fad9064dc688f4b64d77a11dc3f0afdd57c6be52e57c638fc
+size 5850

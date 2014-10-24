4d21bbbd3fdc77475189e80be40c13c9e5d45340
Jake Shirley <navi@odinseye.org>
2014-10-23 20:47:45 -0700

    Fullscreen view menu toggle

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7329712c4d7201d9bfddddaca9967db3bdf587b0f53bbf3dac4d33ce47e8e2f9
-size 35472
+oid sha256:3a01c04f5cc163d76a5840b3855a9a379ae999a9e57e4b92d646588996c02358
+size 36189

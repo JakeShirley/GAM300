a76ce0bdcfea22407af4bcf85f2b16ec8b09cec5
Brian Inkrot <binkrot@gmail.com>
2014-10-23 20:57:26 -0700

    Frost Boss is starting to come alive.

--- TimeMachine/Game/bin/components/FrostBoss.lua
+++ TimeMachine/Game/bin/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e88bc6cfe67b7d7b2bf8678135988eaaee5029c8e1b9299eab353bf60ffbd6a
-size 3566
+oid sha256:ae743e06b2dbec8042b0fef7f380838ea4b1c739953c43250931952d6e1bb6f6
+size 7294

c4f3d65bbb6f0e0c08fcd08b84de3c380e03a500
Jake Shirley <navi@odinseye.org>
2014-10-23 17:15:01 -0700

    Bind light component methods to Lua

--- TimeMachine/Game/src/DirectionalLight.cpp
+++ TimeMachine/Game/src/DirectionalLight.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16635cedc26459edae92be011bd481852c5a37471f7130ad2533c302cc82c9f0
-size 2765
+oid sha256:e9689ce51100989a912f68840fa0b693da1b3e9a69a8db0078860fbb03ecab52
+size 2813
--- TimeMachine/Game/src/PointLight.cpp
+++ TimeMachine/Game/src/PointLight.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc744a7468dcf3a52eb456660f55b04b5ae0f9d20fc735bedc846e5033d424e7
-size 3434
+oid sha256:094be01204209c5db8e1acdea70f018845bd8f9e488515e5d6e377903520af2a
+size 3588

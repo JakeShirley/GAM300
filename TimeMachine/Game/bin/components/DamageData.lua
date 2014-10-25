5b7c929155432d065bddca7a2a01da642dafcbc8
Joe <joeybonaccorsi@yahoo.com>
2014-10-24 18:41:37 -0700

    Made progress toward Knockback and stun working

--- TimeMachine/Game/bin/components/DamageData.lua
+++ TimeMachine/Game/bin/components/DamageData.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff72c2d24caa8c0854b17056fe206d2b22cc76e7644fa0aebc4f589ba5ce4563
-size 2646
+oid sha256:38980051b5ea6258671de116432d82861901cbf24db85860d58169f22e15672e
+size 3002

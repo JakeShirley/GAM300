f6da14433f58c80f5aa02d26efdc156fd2e7ffb4
Jake Shirley <navi@odinseye.org>
2014-10-27 11:46:56 -0700

    Fix for bad search list logic.

--- TimeMachine/TMApplication/src/SearchList.cpp
+++ TimeMachine/TMApplication/src/SearchList.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3b02d5a2d7882e25e9f1208c1eb089b6e210178db07b91cf06a2d955b938153
-size 4621
+oid sha256:71374a6faea036a6a4d5c954dd158a8b1e52fe94263b82479dfc109a0a96d3c7
+size 4679

6a7680bc17bfc6b64bbc8c0f2bdd6303df2b096b
Cody Centers <murtag27@gmail.com>
2014-10-27 16:54:41 -0700

    Added support for multiple textures for emitters

--- TimeMachine/TMParticles/src/ParticleEngine.h
+++ TimeMachine/TMParticles/src/ParticleEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:107b28f139cb2aa731fbce5f11ce088f556f3a19d5a63dbb2299bba06d6075e5
-size 12171
+oid sha256:ef29eb08a32301c82dde64355f9e8871e1259d03cd4c532be6e6e96cc7e65c66
+size 12397
--- TimeMachine/TMParticles/src/ParticleEngineHelpers.cpp
+++ TimeMachine/TMParticles/src/ParticleEngineHelpers.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3dc624fa64583e85959e832ac4b6f35467b0adf2b5c4c7d99bad1016a9d3e81
-size 9803
+oid sha256:e471f7fd8efd2b04621a7935b01901afe16d74a6a03437e13fda8ca0f69c356d
+size 11192

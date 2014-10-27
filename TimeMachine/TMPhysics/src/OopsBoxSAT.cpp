ee3595985b97140f16708094f2d194ba58434972
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-10-27 15:31:43 -0700

    Deepest contact is always first for box-box collisions to increase solution convergence and stack stability.

--- TimeMachine/TMPhysics/src/OopsBoxSAT.cpp
+++ TimeMachine/TMPhysics/src/OopsBoxSAT.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6a0068b115a1ddfcc602f29abafa8917cdb5d1e08518343949de4061dd1370a
-size 19813
+oid sha256:33fd13094696bf56cd07dbc4b9c231d54b2468367759a67aeff09e33e92d0ae0
+size 20348

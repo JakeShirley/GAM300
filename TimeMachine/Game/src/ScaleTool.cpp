4319d5be8fd7ab45aff95893aa31151585697df5
Jake Shirley <navi@odinseye.org>
2014-10-28 11:30:39 -0700

    Removed plane drawing for Scale gizmo

--- TimeMachine/Game/src/ScaleTool.cpp
+++ TimeMachine/Game/src/ScaleTool.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:901a4febf89c2e0efd04de6de8b8e63efd25397357dad6582017bc1981b6701d
-size 12539
+oid sha256:eaef3b99b7da53d302875cb245caf53138b61f49c48245c20baf5b6348bd29a9
+size 12596

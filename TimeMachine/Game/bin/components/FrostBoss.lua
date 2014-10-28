e3de1c008ecca3bb0bfcf132d43bf125251976f4
Brian Inkrot <binkrot@gmail.com>
2014-10-28 12:09:51 -0700

    Start of visual polish on Frost Boss.

--- TimeMachine/Game/bin/components/FrostBoss.lua
+++ TimeMachine/Game/bin/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd743d6e6953dc020f8e2fff8775fed46f05f72d2ac75d35d147c4092a9f2607
-size 11293
+oid sha256:dc44f153fc3809953050a6674c9b3b2eab9cfad1f03e4f8cb5c56b69223ef36d
+size 11438
--- TimeMachine/TMParticles/src/ParticleEmitters.cpp
+++ TimeMachine/TMParticles/src/ParticleEmitters.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6ecbcec28bede7ce2ea333a7d4895f8c312f8b55de9fa0e023e7a73c4b4a903
-size 24429
+oid sha256:597145f3d65ea41178d31aedd9297d9d449a1968603a59859d924a4eee3fe2bb
+size 24589

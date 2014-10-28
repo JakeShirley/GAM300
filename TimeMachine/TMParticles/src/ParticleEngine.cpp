c7e3c6f8c06ff0b19f0a4a3a20cebbbaf84df055
Cody Centers <murtag27@gmail.com>
2014-10-28 11:45:48 -0700

    Fixed dragon breath emitter

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18654d0ef04ae90492b59995a4ed3fe368893aaa392222df1edb02219812b62e
-size 39607
+oid sha256:7fcdde1ba683f27432d8f2d0a8612b8bed57dc69f5eb888378c94f494a116188
+size 40078

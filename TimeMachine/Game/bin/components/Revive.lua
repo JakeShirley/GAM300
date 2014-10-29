aee2c2b91e00ea2cb5f56d16eb2314e9b5808763
Michael Chambers <m.chambers@digipen.edu>
2014-10-29 14:42:42 -0700

    GlobalDeath and GlobalRevive events moved out of health and into revive component.

--- TimeMachine/Game/bin/components/Revive.lua
+++ TimeMachine/Game/bin/components/Revive.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41da948f425c49fda11f8f1f16acd8a6095958f866a084e302b2fa5f6b2c3132
-size 6775
+oid sha256:f8e3df56e68b9d0184675a810d6fd8c5933f3c63fb95cb83f8aa639f6dc91283
+size 7146

7def1f5588bc47f5b9ea80e4997bc46750497e28
Jake Shirley <navi@odinseye.org>
2014-10-28 22:35:51 -0700

    Fix issue with not post-loading components when adding from the editor.

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d0605852da8adfc955f13cafdeaa792f0e35273849dac6fa3ae1eb217e9c188
-size 27799
+oid sha256:db9fd3f63edab2d0f6a19850c5c8e296410558dd04c9c70188239881e02a283d
+size 27839

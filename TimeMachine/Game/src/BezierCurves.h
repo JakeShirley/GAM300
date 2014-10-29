235ad13923bd16b0244c24b5351de15d17f9d41e
Cody Centers <murtag27@gmail.com>
2014-10-28 23:36:29 -0700

    Cinematic Camera component now handles banking.

--- TimeMachine/Game/src/BezierCurves.h
+++ TimeMachine/Game/src/BezierCurves.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a982e384510f7261bbfed1cc5647a38c4342a330439703a06af785c44ad90969
-size 6487
+oid sha256:a04d0fd9ffa001b200ca6ae4aef8bc73d34dea4085f7bc32b6e739c9c5191d8b
+size 7032
--- TimeMachine/Game/src/CinematicCamera.cpp
+++ TimeMachine/Game/src/CinematicCamera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddf962dbec8994d91d22f23ef2b77b02088779dfb1d8805d6d069ae5779b0334
-size 17213
+oid sha256:a731574ace5ad681ef8d2604911fac810e21a4b91563d9e302bdfec5aefcd415
+size 18820

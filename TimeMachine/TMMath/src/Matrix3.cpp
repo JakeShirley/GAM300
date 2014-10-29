54f65ee62f97de37820b728f5054d8ae8d33270f
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-10-29 15:40:28 -0700

    Euler angles confirmed working in editor. Fixed backwards icicle shooting in the process of verifying that transform's rotations are right.

--- TimeMachine/TMMath/src/Matrix3.cpp
+++ TimeMachine/TMMath/src/Matrix3.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:733f7839adb4c7b1d4819aa4fa3886f5bb792f9ed6da7a2b7bf2dc08da298eb0
-size 5185
+oid sha256:29cfd9600a31484f20c4d12a973b6651af67bdcd7fb2d2d061150e7124401b3a
+size 5315

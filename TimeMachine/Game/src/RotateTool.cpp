2174f9d272a393fa2e4dafe4111111f5beb2e977
Jake Shirley <navi@odinseye.org>
2014-10-29 16:06:10 -0700

    Rotate Tool

---
+++ TimeMachine/Game/src/RotateTool.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2cedf11f0b1b07e289d7d28e41999fabf4cacedc36b92fdbbbf8c93811c01d71
+size 7603
---
+++ TimeMachine/TMApplication/src/LibraryNotebook.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7320da7e8d090b55a633a5db3591972db618683b66bcf33da6745dc4926ed227
+size 364

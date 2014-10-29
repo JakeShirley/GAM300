7bcf49755cfa6b19b5085cd8ee2a0bb9e9a730ba
Michael Chambers <m.chambers@digipen.edu>
2014-10-29 16:25:50 -0700

    Dying changes the player's material to white, reviving changes it back to what it was.

--- TimeMachine/Game/bin/components/Revive.lua
+++ TimeMachine/Game/bin/components/Revive.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8e3df56e68b9d0184675a810d6fd8c5933f3c63fb95cb83f8aa639f6dc91283
-size 7146
+oid sha256:5d5ae0358efc48504d0d8d7e47b7e8218f83d6133be6b4e58c0cb93cff31b61a
+size 7806

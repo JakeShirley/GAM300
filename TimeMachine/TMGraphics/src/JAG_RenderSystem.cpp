e71f3f6e2f900a065b4768eae500595d11e73299
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-10-28 18:41:53 -0700

    Non-working shadow map filtering

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d20875b20c544e353fdf6c3ecb9c18bfaf9c57c8eca183759b1290fbabe41365
-size 61227
+oid sha256:31ad5213c939c915e02287568fcd0460abb07374ebe71325e3d31a409ad3308b
+size 61655
--- TimeMachine/TMGraphics/src/JAG_RenderSystem.h
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67dafcbc3ff73740debe839961c706be2094db529d32880a8e305604c0747fef
-size 10000
+oid sha256:0baa1f557b744777ab657a0cca1de413501bce85b5d8fe61b889648729f25064
+size 10057

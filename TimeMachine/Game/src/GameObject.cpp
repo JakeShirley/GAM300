46b778c8dfb422e7207c028c384eaa7e43546bad
Jake Shirley <navi@odinseye.org>
2014-10-28 23:23:46 -0700

    Ensured PostLoad was called on all objects created from Space

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20fc34b7f61854d899f494c89fe6bb10aa47bb2e644187ffdbbaf1ff960beba4
-size 27865
+oid sha256:f2785b6d5ed954d555a7c34a4f1170921cf29550bd484c9f7e8db3324ce10f9a
+size 27878

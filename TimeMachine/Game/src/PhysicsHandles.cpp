9fa63e4a72493f77e7b81e67f899d580518f9dcf
Jake Shirley <navi@odinseye.org>
2014-10-30 14:15:49 -0700

    Fix for initializing Kinematics.

--- TimeMachine/Game/src/PhysicsHandles.cpp
+++ TimeMachine/Game/src/PhysicsHandles.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10fab8b4678bae7376775c9cc81cb50f85732316b627929417c26c3450a7a92a
-size 16202
+oid sha256:30d942dcf09c6fe40617f1516b9abab5dddfcb5d571b0f848f88c1e03d849863
+size 16159
--- TimeMachine/Game/src/RigidBody.cpp
+++ TimeMachine/Game/src/RigidBody.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfb494eb0afcf5e82a1848e60696bc49ae24464210e211ef6c1449ff8cc1c217
+oid sha256:fffdb88851b32a1e33a9b019263043c4bf9df0a06c71bf9d15eabf4b1279fef9
 size 22310

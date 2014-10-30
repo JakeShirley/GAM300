38e75aa2084c26d189933ef58cd71f5e898b934e
Michael Chambers <m.chambers@digipen.edu>
2014-10-30 13:22:55 -0700

    Missed some more.

--- TimeMachine/Game/bin/components/DestroyOnCollide.lua
+++ TimeMachine/Game/bin/components/DestroyOnCollide.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f468155e5f9fbac7cd75d451eb725382aece91d6b4cdd36c60329bf3c2f2fc6d
-size 3170
+oid sha256:7480973cc5bbb352c5ec6d3bfae5a73b0d30ae1ffca7ffeb5962897b5db66251
+size 3176

a16c0e51843349cb60e0006caedcd050559ba9f5
Michael Chambers <m.chambers@digipen.edu>
2014-10-30 13:21:20 -0700

    Removed a horribly spammy debug print.

--- TimeMachine/Game/bin/components/DestroyOnCollide.lua
+++ TimeMachine/Game/bin/components/DestroyOnCollide.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eee07e3e778928c49701e1e0429e63bf3e26040670aae2eccdcc44bc0fff9cfe
-size 3168
+oid sha256:f468155e5f9fbac7cd75d451eb725382aece91d6b4cdd36c60329bf3c2f2fc6d
+size 3170

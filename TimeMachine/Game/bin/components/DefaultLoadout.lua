3de4b163d3d863df6cadc89437389826645adff3
Michael Chambers <m.chambers@digipen.edu>
2014-10-29 21:14:31 -0700

    Fixed some death oriented edge cases.

--- TimeMachine/Game/bin/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbb173b9da5cb6b58d474929cc96d8d9c6dc0fc18486523701aba2f0142a5384
-size 13510
+oid sha256:edb6db6142f57d9b1e64d66f0ce6d39ec764093907b487da78c3d8341c6e6575
+size 13543

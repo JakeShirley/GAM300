2c886eee707a507d22c03d8c3c5e6d2b92055347
Garrett Allen <garrett.allen@digipen.edu>
2014-10-30 11:42:31 -0700

    fixed a bug with runtime cloning

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96cdf3c31de7fe51bbb43cd600124ccc8955e5bb8d224cb3cfa68509db5564ea
-size 31738
+oid sha256:31230b7d3c5ebb9cb5945f58935699e181e1b5d9ead823b46be47dbad677f9eb
+size 31795

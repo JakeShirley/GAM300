0835e5962c403b013bad4581b01d5ceac4850542
Jake Shirley <navi@odinseye.org>
2014-10-29 19:30:51 -0700

    Updated DispatchToRegion to get objects by reference.

--- TimeMachine/Game/src/Collider.cpp
+++ TimeMachine/Game/src/Collider.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7bdbc025594892fc2b1069cfa416f68565b91685ffbba8ec3ceea54f4cc54f9
-size 10712
+oid sha256:e62247b3273e979900bd3ebb42bbb85e51584e7705029fa2f727838638c4a313
+size 10713

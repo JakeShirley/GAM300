3b2b34c2e74653c98e4ca72b270e8ba81e6e8c32
Jake Shirley <navi@odinseye.org>
2014-10-31 05:23:31 -0700

    Fixed issue with Translate tool in full screen

--- TimeMachine/Game/src/TranslateTool.cpp
+++ TimeMachine/Game/src/TranslateTool.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f0a1409e136a043376ad3cfcb0558185d2f5cb69785cba734d603a0b2bd1b83
-size 13765
+oid sha256:e3f1ed4e1c8538e432f1ed987dd4729c32feebbdefd14c47f6ab3f7c9d35b9ef
+size 13802

28025bf1b564b498b3544e8e509934bd9b2c51e2
Jake Shirley <navi@odinseye.org>
2014-10-31 04:18:47 -0700

    Ensure TreeItems are created when selecting GameObjects.

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d8a8ccee8e575fb24d254e8afdf47d55b57d97ac958722811f0c0dc6b9331a3
-size 28051
+oid sha256:48e4e6110296689d085fbee1e3a74673c02b7935b430032c762dec1fb5c58b78
+size 28096

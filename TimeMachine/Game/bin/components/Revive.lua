ca26c1a8cff1940e806240b519367690b763161f
Michael Chambers <m.chambers@digipen.edu>
2014-10-31 08:41:38 -0700

    Revive and default loadout changes for demo.

--- TimeMachine/Game/bin/components/Revive.lua
+++ TimeMachine/Game/bin/components/Revive.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbf5787284944a497468c2e40496a277edf8ce1eca836a436a9c2a48d54f4f79
-size 8004
+oid sha256:eb5a680176cac8b13ad637e945cc4a430a67fa63ef6c9b040207d3613d1641b7
+size 8006

66a538fd52d5312cfbd143dcbc384a27dfd07808
Brian Inkrot <binkrot@gmail.com>
2014-10-31 09:38:01 -0700

    done, or not, maybe, who knows

--- TimeMachine/Game/bin/components/SegCinCam.lua
+++ TimeMachine/Game/bin/components/SegCinCam.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7df96cc634d9f39759b600cc0d5a67c139dd9f97f3c0aa11228cd759d6e700ba
-size 3377
+oid sha256:b04d95004d72d991d3312c749359119c3d24dfdd3acdbe9b4d8d6f8be07822d4
+size 3439

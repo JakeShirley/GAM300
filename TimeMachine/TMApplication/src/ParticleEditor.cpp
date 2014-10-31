1f85b1a4f30bfdeb2e28aff5556e261ebb7dba49
Cody Centers <murtag27@gmail.com>
2014-10-30 21:28:27 -0700

    Fixed display issue with editor, and emitter components now interpret emitter positions as offsets from owners (when locked)

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72d65373b47312c3784ab2410d1f2372324b758da53e00aaf458614d77eff1e6
-size 60357
+oid sha256:0a7fe925471e36fb8e6dbeede65f41abe2dbaa26ccbc13748491e41232a794c0
+size 60857

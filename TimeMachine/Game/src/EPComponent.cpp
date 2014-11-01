55416e3f626f435319824761981736e2ab626980
Jake Shirley <navi@odinseye.org>
2014-11-01 15:30:19 -0700

    Segment Prototype slides

--- TimeMachine/Game/src/EPComponent.cpp
+++ TimeMachine/Game/src/EPComponent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3855edf283d32f8fefdbe5103c1f5113d2b39e26b46033f737e2e1a989225bc9
-size 12864
+oid sha256:9e147cc289801598b0f22aa753b406f630c86689692d5fe22db95ce591d39ca9
+size 12870

a3b3d3e1af705b35ceea2d72425dad0383821ee2
Cody Centers <murtag27@gmail.com>
2014-11-03 18:07:34 -0800

    Smoothed functionality of watching the camera track, with the cinematic camera controller.

--- TimeMachine/Game/src/BezierCurves.h
+++ TimeMachine/Game/src/BezierCurves.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5883f5b5ea4c30797d2ca7fc8f5ea16ebbb10d0e94ff906dd34a37d5e13db7a4
-size 7281
+oid sha256:4621b7196a8a9526cee269eec00da63d2e1d1925ff79e0689f1481be037dbd3c
+size 7906

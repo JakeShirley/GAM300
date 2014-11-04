4cbf5161da8ff1563f93d0edc77195d9298ed414
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-11-03 16:16:06 -0800

    Fixed reload model memory leak

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:658a632f981d104be67821effdfe6a92139cd6ceabc67a9ca80903ee48298a90
-size 61889
+oid sha256:437043e3ee58aacb482b927917dba1581a3399710390079cea83cd6239e7ac39
+size 61613

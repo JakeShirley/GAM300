c899ee24952041c85831b2b175f07ccd455b01d5
Garrett Allen <garrett.allen@digipen.edu>
2014-11-04 21:26:19 -0800

    multiple object cloning. Yay!

--- TimeMachine/Game/src/GameManager.h
+++ TimeMachine/Game/src/GameManager.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d5a29d636945928d00afe457ec929cedfb21f6dfa15de29c887b79e20150d82
-size 3408
+oid sha256:a0ea1261659768bf77f6231cfc22e6090b85fc3b8ee3c8d0b6f5299923513b38
+size 3490

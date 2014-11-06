fd46f589a07b9cc5196132e851683d18132205c3
Cody Centers <murtag27@gmail.com>
2014-11-05 21:03:39 -0800

    Added functionality to let emitters scale up and down. Not implemented with the component Yet.

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0ff1d1c80ea84f7f4f37e467286c6ede88863bb25303d596a6f0c5dfbb1eeaf
-size 41015
+oid sha256:a78fbf95999942546d14249e56b8c6901e2243b6bed693d32bed75a6786de18c
+size 41493
--- TimeMachine/TMParticles/src/ParticleEngine.h
+++ TimeMachine/TMParticles/src/ParticleEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b22a73525ff91df5f62a897dda38ba97409603c81ae63db34a41487bf38042dc
-size 12594
+oid sha256:c357168c0793db89d0f20f87d05780b2ccd47e42b76ae63498f7e0b08a17ebbd
+size 12757

e6cb86f2719fe55b46143f705bd90d8f1f5e427a
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-05 20:48:47 -0800

    Scale gizmo feels good.

--- TimeMachine/Game/src/ScaleTool.h
+++ TimeMachine/Game/src/ScaleTool.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2e36789493f19bca8814e5363c58febe6d327efc7ae1c4696fe2cafaf77114c
-size 657
+oid sha256:c82178a09461777ef21ca00567dc3de749f96a80973339b00b4e39a05b25882d
+size 962

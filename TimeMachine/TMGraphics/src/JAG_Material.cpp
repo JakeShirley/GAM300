389f8c67ad29afbec69003c0b9e50f69d6925719
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-11-05 22:14:58 -0800

    Material name defaults to filename + extension

--- TimeMachine/TMGraphics/src/JAG_Material.cpp
+++ TimeMachine/TMGraphics/src/JAG_Material.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d5bc5aa63d5396da086b8de6d124952d85a83463667a9ef38b1c0bdee8e3064
-size 4485
+oid sha256:c31831c7e9600b5ff884df7ebfca0176ddcbaae3e7658cea12575fd20d8262f1
+size 4577

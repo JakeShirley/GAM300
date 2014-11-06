b9b78fc9c8cfc649ae442a1c845e1fa3829c2d25
Cody Centers <murtag27@gmail.com>
2014-11-05 21:18:18 -0800

    Expressions (for the most part), scale with emitter

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a78fbf95999942546d14249e56b8c6901e2243b6bed693d32bed75a6786de18c
-size 41493
+oid sha256:5b814f3e315e208ab7c441e9d021e5c33775bc9c0058aa631b15d11a30df34d0
+size 41652

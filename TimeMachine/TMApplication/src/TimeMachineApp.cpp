c4d4d325d1084e6202c3231ccf6c5cea7776d9a6
Jake Shirley <navi@odinseye.org>
2014-11-06 10:55:23 -0800

    Clear logging stuff on deletion of TimeMachine.

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1fd40e0324e0e6812450d1ca254173dfb46175eeac5c7a56c1a588f252f66ab
-size 15287
+oid sha256:ccaf7250364c557fc379ee0a443446ff70beabf0f994a45e6a7fcbc8e7a68f5c
+size 15361

cc108305c9f1ee020113db6463b1b0708865ca69
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-06 21:56:48 -0800

    Minor potential improvements to box-box collision detection

--- TimeMachine/TMPhysics/src/OopsBoxSAT.cpp
+++ TimeMachine/TMPhysics/src/OopsBoxSAT.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5e4a3c90c933f323e8cfa64388237e427d3092249dce8cd70dcfe141909fffe
-size 21151
+oid sha256:f6170bfaa1f9d71428841f3c2bb8bd1d877a9cf5d0b42dd2e70c2cf4767ae776
+size 21132

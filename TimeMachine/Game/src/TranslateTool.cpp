d68df89efeb6c01245f87f79a7cbf6edc1f7874d
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-07 13:20:47 -0800

    Axes aren't as hard to select anymore on translate and scale gizmos.

--- TimeMachine/Game/src/TranslateTool.cpp
+++ TimeMachine/Game/src/TranslateTool.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:445d9d1ce5f23a4bb117aac9e7a51fafcd0c160a79147401f0ddddd5d4eb56f2
+oid sha256:449c00e4372289da1ffb1db1262df394efdf2b95ef8018c57de49d4142ed1780
 size 10050

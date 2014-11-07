1ae435d3fa10a6fab668db9bad1b62da486ad98f
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-06 18:01:41 -0800

    Removed convex hull with bonkers iteration count.

--- TimeMachine/Game/src/EPComponent.cpp
+++ TimeMachine/Game/src/EPComponent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdcb1328c88a09eabe1303bc8c444fe41b6793abf940dad9e810e7315686338d
-size 12946
+oid sha256:3b95242da78854d13886de9a097faa61148db08a1a6340a063c19b8fa5d02fce
+size 12950

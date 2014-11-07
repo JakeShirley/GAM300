7cf323b5cdf57b9d5808cf5ac8fee3cee1d72cc2
Cody Centers <murtag27@gmail.com>
2014-11-07 13:59:44 -0800

    Particles render again.

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0de2be998e2cae7e42dc156ea0a52b92a58b4e2bf1d15392a4a9ed9dfe61e408
-size 63102
+oid sha256:9bfb9409834a9ae7fa39a4cab9992e4deff1e1f417fe09bf338eed45bd2beb59
+size 63100

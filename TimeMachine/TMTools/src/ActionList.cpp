7c95c2976ccd5ee7f0b35c642906f26d9a17d727
Michael Chambers <m.chambers@digipen.edu>
2014-11-07 16:46:32 -0800

    Work On Elastic ease type.

--- TimeMachine/TMTools/src/ActionList.cpp
+++ TimeMachine/TMTools/src/ActionList.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c7cd2deae1d55bdcbdf7e71aee484e877df8df56e14741e7336359e3b03eb99
-size 15628
+oid sha256:408450e79c0fb34c413a75b4230bdc6340ccd93fd8157719fb1ced96d868f92f
+size 16829
--- TimeMachine/TMTools/src/EaseTypeDeclarations.h
+++ TimeMachine/TMTools/src/EaseTypeDeclarations.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:978e38a51ef77e190b943a57c36987a817a6b6685db0d0df8370c269570875ef
-size 198
+oid sha256:bdb79ac91c2da9d5c7122130d7afd99761c65eea660b86da8832627d4c742d24
+size 218

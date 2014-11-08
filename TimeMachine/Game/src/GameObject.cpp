e9aa2a336a2da3210a43fcccf8bb25afd00d130e
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-07 16:09:23 -0800

    Fix for hiding/showing children

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07e68bd859831020ca9e75f1bc3ad5b283af9f977bcc3160906f5b3f9a9f67d5
-size 37981
+oid sha256:7f1f3c3a4d028f339954fd14be65e3989500c35e6d5a6a42515384b762c94bd3
+size 37982

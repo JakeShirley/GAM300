f109e0b6817137b3364a625b0f609adce4140684
Michael Chambers <m.chambers@digipen.edu>
2014-11-09 15:25:43 -0800

    Added getters for turn/move speed just for autocomplete cleanliness.

--- TimeMachine/Game/bin/components/StatusManager.lua
+++ TimeMachine/Game/bin/components/StatusManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7694b8edd8c0cf92bc346c4a4de19dbc204f88e40e9cffc4d66ffbcf6ecc58c
-size 4177
+oid sha256:9180e9048552d061c6135f593e499e0f5556b8d3752ad084fa1acaadd9fca365
+size 4363

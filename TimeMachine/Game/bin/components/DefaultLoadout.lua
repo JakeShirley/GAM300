31ecafda7c2474b05ada0234f3918b5fd3ec84ac
Michael Chambers <m.chambers@digipen.edu>
2014-11-09 15:29:14 -0800

    My test level for resource file's sake.

--- TimeMachine/Game/bin/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29f8aaa8f294e3593b8a6b95f09049494aac8f2f632d39809134c8dbd5d52ef4
-size 14954
+oid sha256:d6983516c6ce3efc21ebc1a8721e4f2f951f778fa403f98bfe87ff338da87edd
+size 14938

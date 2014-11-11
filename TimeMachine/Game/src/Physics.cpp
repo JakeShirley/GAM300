f76365d46a77acbe2d04ea5bf7ecb63ea2f62175
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-10 17:04:38 -0800

    Positioning works properly from editor to game. Fixed a bug resulting in bad contact info for composite bodies. Rotation via gizmo still doesn't work nicely.

--- TimeMachine/Game/src/Physics.cpp
+++ TimeMachine/Game/src/Physics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85a33d57a90c7e22de995477ad5f34ba9195fef957858c3e9e4cbd5c94cd3cd7
-size 13808
+oid sha256:82a3524d141051212afe01dcd8e0eb3f8a89d63d012cbe1900dbbe70069e45c8
+size 13818
--- TimeMachine/TMPhysics/src/OopsManifold.h
+++ TimeMachine/TMPhysics/src/OopsManifold.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7f3826983e993aab532f25779429c1b8288cd954b7c9a333263a3910db718b4
-size 2912
+oid sha256:2afd4bca3de6bde997f62e6451fc93794a5a75614e75abdb8da214ec3ca0b274
+size 2933

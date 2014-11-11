02246ae934569e6317d3dc4c9cc057f6370b97a3
Cody Centers <murtag27@gmail.com>
2014-11-10 17:02:59 -0800

    Emitters now only show in editor if the object is selected

--- TimeMachine/Game/src/ParticleEmitter.cpp
+++ TimeMachine/Game/src/ParticleEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a22ddd068b4e1c8a5dec32f1de7db63685dd3a800cc0523b07792f335769cca0
-size 9335
+oid sha256:f3c07839572ff4117132c8f583efd4935c73cd8d21b03c3e50479bb81b353cdf
+size 9582

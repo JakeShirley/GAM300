568d399e09b983ef3668d14034a198f9ba4cb5e3
Cody Centers <murtag27@gmail.com>
2014-11-10 17:53:48 -0800

    Emitters can now have their particles share movement with their emitter

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b814f3e315e208ab7c441e9d021e5c33775bc9c0058aa631b15d11a30df34d0
-size 41652
+oid sha256:2a8bed3ce04ad88ef1c80513bc5bd3baf786f4f53c8712cc292d5ffb3eb5616d
+size 41911

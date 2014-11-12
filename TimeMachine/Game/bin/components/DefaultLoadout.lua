17717fdde7c2263387e600b234e6c725513b4f79
Michael Chambers <m.chambers@digipen.edu>
2014-11-11 16:29:50 -0800

    Made objects that were created at runtime parent to their respective objects.

--- TimeMachine/Game/bin/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:002694d1e903992103789b004c628ec5cf4a3bb2d35aecf72500f16c15adf038
-size 15463
+oid sha256:db63a7cf62b3c418609486ce3851cf35254aaadbfef80c2362ae7c4596247edb
+size 15465

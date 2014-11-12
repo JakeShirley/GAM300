67e3e6d4b7bf5321c45bea6e7cf426f11600ebe8
Michael Chambers <m.chambers@digipen.edu>
2014-11-11 16:06:37 -0800

    Stopped calling Initialize when linking back to an archetype.

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a25211723a516daec1518ed97e1efa7482a3bab0919896031700c707f3cbb15b
-size 38465
+oid sha256:20707530fa637983d4d55f32ccdca2647bc851c5d04f41db3bf2df3e468a1035
+size 38467

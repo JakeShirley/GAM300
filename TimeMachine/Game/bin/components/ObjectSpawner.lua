2950d4a837e6ac77f1dcf401446f7de3a12818c3
Michael Chambers <m.chambers@digipen.edu>
2014-11-12 01:03:24 -0800

    Fixed an InheritedColor bug in default loadout when firing super.

--- TimeMachine/Game/bin/components/ObjectSpawner.lua
+++ TimeMachine/Game/bin/components/ObjectSpawner.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20128a8d7535d836814b9e6c5e6432fb701c1e16aac355cdce750ebb5163940d
-size 2007
+oid sha256:5f6abca499a8f2cec7147df70f0ad7a6f385cd69c41b2ea91b41eda1a1239e2d
+size 2105

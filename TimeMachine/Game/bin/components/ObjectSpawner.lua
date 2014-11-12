2ce25665103496139c05df1ff613d2e44c07d509
Michael Chambers <m.chambers@digipen.edu>
2014-11-11 23:28:45 -0800

    ObjectSpawner correctly doesn't duplicate objects now.

--- TimeMachine/Game/bin/components/ObjectSpawner.lua
+++ TimeMachine/Game/bin/components/ObjectSpawner.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b08ebc4ddbbd7dba82b34168a491bee66bda90815009585f0d45b1471f54d0d
-size 1994
+oid sha256:20128a8d7535d836814b9e6c5e6432fb701c1e16aac355cdce750ebb5163940d
+size 2007

f958f12ae9c9ef0164b14aaffbb717216b0d59de
Cody Centers <murtag27@gmail.com>
2014-11-12 12:57:03 -0800

    Added some safety checks for the cinematic camera, and the framework for a rez casting effect

--- TimeMachine/TMParticles/src/ParticleEngine.h
+++ TimeMachine/TMParticles/src/ParticleEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:668d8be8d60cd120338d9440c13cd87c15d177d30dbe4d9a00b8fce47bbff1f2
-size 12810
+oid sha256:3c46caaa096c9ea24f9524595f2929a247c0e00f1abc675937b368a0bc1f613d
+size 12850

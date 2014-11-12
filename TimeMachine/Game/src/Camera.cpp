d9d4b43c59292f5c912e8a3604566a4c549851a5
Michael Chambers <m.chambers@digipen.edu>
2014-11-11 19:15:45 -0800

    Removed the pivoting on F key because it appears to be horribly busted.

--- TimeMachine/Game/src/Camera.cpp
+++ TimeMachine/Game/src/Camera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afed7565727b184ad665ac122cb7ce7034e10bc6f92641755b76b0abfc4c19cf
-size 11586
+oid sha256:06dbe15874e9f6dc0dcb55877590ad83962048a7ea5623a9681e501877e482ba
+size 11590

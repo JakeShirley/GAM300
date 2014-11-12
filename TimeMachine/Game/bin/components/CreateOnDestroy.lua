c24ec4e72d7f3fbbe721e7779c2208a906298fdd
Michael Chambers <m.chambers@digipen.edu>
2014-11-11 16:55:30 -0800

    Made CreateOnDestroy check to inherit particle color from Inherited Color. So now bullets explode with the right color.

--- TimeMachine/Game/bin/components/CreateOnDestroy.lua
+++ TimeMachine/Game/bin/components/CreateOnDestroy.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41b7b5e49d88c7977f6168f6fbe581872ec2b9bf132d16eb3f706e6de0e4a570
-size 1049
+oid sha256:5cba9ffef7d1a7f3c7e14eb2e1075409c00026c0ae184bcc0044efdf8abaf475
+size 1343
--- TimeMachine/Game/bin/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db63a7cf62b3c418609486ce3851cf35254aaadbfef80c2362ae7c4596247edb
-size 15465
+oid sha256:ab7673cadf2002e9624895832caf1d59ad98f5d1b1bfa244c7a6d4d4804a4812
+size 15690

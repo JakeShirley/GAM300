a4c9c6929831cfb862705f0137089990026f9de0
Michael Chambers <m.chambers@digipen.edu>
2014-11-12 01:07:26 -0800

    Fixed a bunch of lowercase Actives because of self.Properties.Active == false compares always being true and to fit overall convention.

--- TimeMachine/Game/bin/components/EnergyMechanic.lua
+++ TimeMachine/Game/bin/components/EnergyMechanic.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ececa643e3c7a371c804218c533c02fe4b99b5c07cc7a9a94cb0fcdbbd0a2c5
+oid sha256:1422d21dcce8d72fb6b91b9e9a3ae7c7ee55c309cca363e9c2854aa8937057b9
 size 3478
--- TimeMachine/Game/bin/components/GamepadInput.lua
+++ TimeMachine/Game/bin/components/GamepadInput.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bb996c28cd7b0c03ae12b8549208bea5b51aebc6353147328ac4f39e974aa3b
+oid sha256:d65706af8120a6127a53fb2ae68daf13fe22918b3736cb2b9545151dbcb31680
 size 6319

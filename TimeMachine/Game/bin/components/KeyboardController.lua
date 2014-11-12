033de0cffd2c05b0fe438b2c401c4f91ab503f0d
Michael Chambers <m.chambers@digipen.edu>
2014-11-12 01:25:00 -0800

    Entire player controller interactable with keyboard now.

--- TimeMachine/Game/bin/components/KeyboardController.lua
+++ TimeMachine/Game/bin/components/KeyboardController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39437ed8355c35579d7a9aba345491a8f5bd8079c948e59a3e59e1c0f6b9b2af
-size 1560
+oid sha256:c1c44d7229c8f1e251cdd3b5940f6ccf04015b065e94cf856b4870c98ec693da
+size 4037
--- TimeMachine/Game/bin/components/Revive.lua
+++ TimeMachine/Game/bin/components/Revive.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9783e84551dcd1288ec0f8efc87b725fad2b86767737573692b72c769a69542e
-size 9161
+oid sha256:489e0cda1d56c1a6c71fa42ae1046b49f59934f5e8698fecafbcccd5c348af73
+size 9503

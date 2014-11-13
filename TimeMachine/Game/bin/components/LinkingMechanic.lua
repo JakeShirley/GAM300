8b377136f323f9d242dd947759a6cd66bcb77b26
Vasuthant <mike@friendpeople.org>
2014-11-13 15:42:16 -0800

    Movement aspect of Linking working in a way.

--- TimeMachine/Game/bin/components/LinkingMechanic.lua
+++ TimeMachine/Game/bin/components/LinkingMechanic.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc027a4cf4a39927c19b732d0273bde06fb31d616b1723fceb282d204842a954
-size 1237
+oid sha256:d90e64a9b47074d9fd1ff23721bef1fa00ce050b4e11de47c9fd85de908b5b21
+size 3348

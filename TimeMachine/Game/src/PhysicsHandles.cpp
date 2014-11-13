d649095532c5e6e856141c18ae014c1e189dad1e
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-12 16:11:27 -0800

    Fixed matrix to euler conversion, fixing issues of rotations flipping from editor to game.

--- TimeMachine/Game/src/PhysicsHandles.cpp
+++ TimeMachine/Game/src/PhysicsHandles.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4f1e76dcfcf56966b56b4902321f638377e183d4e0971d89eb38b88173a9023
-size 12866
+oid sha256:12b522d949febb93a3102d1ecaa21c7dd1e61510d14ee8423974caaffbdf7e17
+size 12829

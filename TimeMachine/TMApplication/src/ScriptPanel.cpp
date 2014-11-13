b546e2062ceaf79081600d1f0a8d2309014a7d8b
Jake Shirley <navi@odinseye.org>
2014-11-12 18:08:57 -0800

    Fixed alt invalidating scripting tab.

--- TimeMachine/TMApplication/src/ScriptPanel.cpp
+++ TimeMachine/TMApplication/src/ScriptPanel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:033b9707d29c177ecff565408cecc2fd4492170727d06189ab0c97035d63ea7f
-size 25694
+oid sha256:a51a4dbb2050bf6ebbe9720acfb2538fe0ca810cce173ccdbf50b8e96c4e6ec5
+size 25731

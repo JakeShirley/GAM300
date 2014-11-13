29dbd094706ba79fc75e89464ae1a9469398eec3
Vasuthant <mike@friendpeople.org>
2014-11-13 15:42:30 -0800

    Don't link with dead people.

--- TimeMachine/Game/bin/components/LinkingMechanic.lua
+++ TimeMachine/Game/bin/components/LinkingMechanic.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d90e64a9b47074d9fd1ff23721bef1fa00ce050b4e11de47c9fd85de908b5b21
-size 3348
+oid sha256:a76cf05ccb0288137a31981a95844d39adeb2194d4dfc2f68d64fee5b416c65e
+size 3380

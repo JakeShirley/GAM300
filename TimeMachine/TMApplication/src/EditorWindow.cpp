c2ec1c27b5b93dc7bdbb5cf10bcff79d6a730730
Jake Shirley <navi@odinseye.org>
2014-11-13 11:12:22 -0800

    Fixed memory leaks in regards to ObjectTrees.

--- TimeMachine/TMApplication/src/EditorWindow.cpp
+++ TimeMachine/TMApplication/src/EditorWindow.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfc6809ca9151ccc21d686f461e0acd15e45691fde54e8564a7348261a2ef9f1
-size 8375
+oid sha256:695346a6db13f9265eaf3e3d537f9c1ca2894568ab30f02441b1dc02c7e61151
+size 8485

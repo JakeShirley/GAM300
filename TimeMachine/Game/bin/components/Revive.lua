e386b583786eb86f4fd1f3b74275419775e92b16
Michael Chambers <m.chambers@digipen.edu>
2014-11-12 22:39:43 -0800

    Revive bugfix.

--- TimeMachine/Game/bin/components/Revive.lua
+++ TimeMachine/Game/bin/components/Revive.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e67136b1f428790d431a6e93e2709b832900bd4b507b47d85ed6689c3aa83e08
-size 10545
+oid sha256:578507b6bc3eb43b6d74735767cdad5f7a50376c3aa7c2396426d9dd6d279edd
+size 10567

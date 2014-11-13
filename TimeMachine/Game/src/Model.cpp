20e33ce0e45dd36e8d792693e5e4e3049f80cc00
Jake Shirley <navi@odinseye.org>
2014-11-12 16:46:36 -0800

    Added UV scaling and color modulation to Model component.

--- TimeMachine/Game/src/Model.cpp
+++ TimeMachine/Game/src/Model.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42df5b04e63cacde282b7bcb63ada329365239bd49f3315f4f391ee99e908ba7
-size 7156
+oid sha256:6c759bdf8c438187431d6a7ce40e799eb9da0c051d34e9da924dd9d9a358c385
+size 8512

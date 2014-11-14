0307c1489e1f29e6d9a733e77e2dd01fe3e1f032
Jake Shirley <navi@odinseye.org>
2014-11-13 16:01:27 -0800

    Fix crash bug when closing with selected GameObject.

--- TimeMachine/TMApplication/src/ObjectTree.cpp
+++ TimeMachine/TMApplication/src/ObjectTree.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5523a5b3d146008c50fb593182aeecdbf916378fc66036d5a2fe1e75d019bead
-size 6305
+oid sha256:bd8592af80f9f8f49c06b78777c26f430377b4ccf7d13780b69f0cb5c4360bd2
+size 6341

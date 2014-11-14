10a544c5ca0cc6d6aafae4b5b05fdfff14210058
Michael Chambers <mike@friendpeople.org>
2014-11-13 16:20:40 -0800

    Testing damage and destroy on collide without persist events as suggested.

--- TimeMachine/Game/bin/components/Damage.lua
+++ TimeMachine/Game/bin/components/Damage.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b710c37d2ed42fb22f6a2036925ba7815f6136fd534ab3c6b1747c6b34f101d9
-size 4072
+oid sha256:c6efddd5e1c7a7e71f8d0424bcbbe3012df64cdbfd62a0f62e9dd7214f744bce
+size 4074

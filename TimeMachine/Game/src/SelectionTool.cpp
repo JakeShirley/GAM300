548338b46fc52d28bb3b10eca7cf992a2d4e2375
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-19 15:07:29 -0800

    Fixed bug preventing de-selection of objects selected with the object sidebar.

--- TimeMachine/Game/src/SelectionTool.cpp
+++ TimeMachine/Game/src/SelectionTool.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8bc1c1cd3140eff25b0081128e3e6ddb9a7212e38b3d548a208e7b8a4fa48c1
-size 7373
+oid sha256:a624c6877d2c7b81eb3f8b755c233bfe6fd616a45f3c0ad652f8479874ffb661
+size 7219

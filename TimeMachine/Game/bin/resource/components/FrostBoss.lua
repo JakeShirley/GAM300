2723d06ed7ddec7918b4308ae6935ec88c8cd5f7
Brian Inkrot <binkrot@gmail.com>
2014-11-20 22:18:20 -0800

    Fixed rotation on Ice Spike (but discarded change in Transform, Tristan's update hopefully fixed it anyway). Created ScalingBar component in preparation of hacky health bar implementation.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a8cce18b069a2c7a712f4ee3399c7748d4c7ff9ceb99b52e853618a85f3e783
-size 30185
+oid sha256:50c8c22218686e7047f59aa1213ea33fbaf75064dbb1e0529c1367f878cf98a3
+size 30491
--- TimeMachine/Game/bin/resource/components/IceSpikeScript.lua
+++ TimeMachine/Game/bin/resource/components/IceSpikeScript.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8af08b4ec8f16d08b9275d8445f20710b84f706d4598b98f3507b4d90aea3ffe
-size 3428
+oid sha256:d7f4e832d54ebccfe27bcd5ee80a6306d98458fedc778e48719124db5420594c
+size 3430

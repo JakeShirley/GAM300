07713c32be4fd9b5a7208bfe7ce7bf039f19aedc
Jake Shirley <navi@odinseye.org>
2014-11-20 22:11:06 -0800

    Changed if to an Assert in ObjectTree.

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fe91c9037b76d6f526427d3c05e0b976d398fe70fcfc7afbdd1e7d18c9f4aeb
-size 40349
+oid sha256:4aa714b8450bfa5504e7df6922f1c4a49eef74f7442e8b5d0e28a0f6d5c5fe43
+size 40350
--- TimeMachine/TMApplication/src/ObjectTree.cpp
+++ TimeMachine/TMApplication/src/ObjectTree.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f92d60388c487dc27083118a8f09729625aa7fa348acea93836390f3f0e45340
-size 6533
+oid sha256:8bef33fa1899251a19e0d79b7235f0d0c5bfbe71a411ffa2b6ba5a0367006629
+size 6535

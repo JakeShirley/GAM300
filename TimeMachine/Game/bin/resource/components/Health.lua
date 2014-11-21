8ac5b55421f27b209f091a9b4392a90aecd2de1d
Brian Inkrot <binkrot@gmail.com>
2014-11-20 22:57:01 -0800

    Health and Energy Bars functional, working on creating a basic generic HUD element once parenting comes back!

--- TimeMachine/Game/bin/resource/components/Health.lua
+++ TimeMachine/Game/bin/resource/components/Health.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b98353beac37c37810d120a9d4951bb797fd08a299469fe1e57b465fa1d61e7
-size 5310
+oid sha256:48772a8cdef8a5a84a1c2841dfb31aa3cc2892133b826ef726c7ae0885d5369e
+size 5418
--- TimeMachine/Game/bin/resource/components/ScalingBar.lua
+++ TimeMachine/Game/bin/resource/components/ScalingBar.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74ee51f93d9ed621d1ef1e831e912b89577769b4510a10809ffd7b63eda35e63
-size 4754
+oid sha256:3285a49d1d1ea2d94df9681288edbad2c2ff819b38ad126fb9c5e9f27973ff13
+size 4859

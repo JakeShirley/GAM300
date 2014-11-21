23a98d527feedd413bb6fb5e1e803b1063ac1462
Jake Shirley <navi@odinseye.org>
2014-11-20 21:21:50 -0800

    Fixed issue(s) with editor window exit crashes. TM-26 #time 1h #resolve

--- TimeMachine/TMApplication/src/ObjectTree.h
+++ TimeMachine/TMApplication/src/ObjectTree.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10ed3e7392a243f17647857ace46aa13e6cb062015129be318b31fe8b72e2920
-size 2639
+oid sha256:64c58c85a086b5e5081e848b1f5f0daae709d5ce48ca75da4b665eaea18405be
+size 2656

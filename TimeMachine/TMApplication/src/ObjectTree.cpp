63a04d442980e7e9b203d3dc3007c46debad6bfd
Jake Shirley <navi@odinseye.org>
2014-11-20 16:41:38 -0800

    First pass at ordering object tree items.

--- TimeMachine/TMApplication/src/ObjectTree.cpp
+++ TimeMachine/TMApplication/src/ObjectTree.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51f7a7b9e392b9aba37d943b1779dc5a622d5dd083c6f087303b424845e53958
-size 6408
+oid sha256:c39d15ddf657e3978997203425f662a0485ad670d81fc4de7bd82c66bd6b6ce2
+size 6396

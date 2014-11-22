713fb7de33c337e6a4181e8bcada5c4fb9a5466e
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-21 16:20:58 -0800

    Scale gizmo graphic doesn't go negative past 0. Scale gizmo now has relevant properties in the settings section of the editor.

--- TimeMachine/Game/src/ScaleTool.cpp
+++ TimeMachine/Game/src/ScaleTool.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5de854b19ee50e3236e7a77d7626f3578a42dc5454daf55b88cba8bdf9f215a9
-size 7713
+oid sha256:0c57feee10450df8552cea6f410cafc746f532756a18a40e9eb48bd47dc03bf5
+size 8486

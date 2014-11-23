a54b372954b6e9924f3e568922033c493eddffca
Michael Chambers <m.chambers@digipen.edu>
2014-11-23 03:11:24 -0800

    More complete menu experience.

--- TimeMachine/Game/bin/resource/components/Menu.lua
+++ TimeMachine/Game/bin/resource/components/Menu.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b5bc6e439559e437217323a0fbd49ae72b0053c4604ab1d5e3848ac9fecdf31
-size 9447
+oid sha256:b4e25dbfdb95dc87e100c301271773c2f76fb3a954bc87a085668cd2a3300420
+size 14092

4470b744bfa281926f1012016918b6d96e2383d4
Michael Chambers <m.chambers@digipen.edu>
2014-11-22 21:19:29 -0800

    Commented out floating point exceptions in main.cpp

--- TimeMachine/Game/main.cpp
+++ TimeMachine/Game/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a67e8eea41c19af745ca4426204da2be422036a2c2db37ba2def5f60086df1c
-size 1450
+oid sha256:036ca6a4d568571a998c0192598ea555c335f48359412d00d139b70bdb2f0beb
+size 1452

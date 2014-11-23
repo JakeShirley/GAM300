5dd17feddf33202618d7d989f28d89a1e89a3e70
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-22 23:16:42 -0800

    Fixed Michael's parented camera issue.

--- TimeMachine/Game/src/Camera.cpp
+++ TimeMachine/Game/src/Camera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cccd4fbe7915b90fa917a3597321fda222e77d009a183aa5b5afa6a2a93b10d3
-size 12907
+oid sha256:9d87f5b930e64dcf7d8e0bbd49e87bee9ccd9ad0584c4416de16c4ff2dc0ba27
+size 12708
--- TimeMachine/Game/src/Transform.cpp
+++ TimeMachine/Game/src/Transform.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b79499567e9cac49f1ada6a19fcc5e90da3a0256170d9a7761a2c6d11c223b1d
-size 20285
+oid sha256:8e160604849c08086c994a526488efeffae6a1a4142d3db60125a66c5d12d55d
+size 20309

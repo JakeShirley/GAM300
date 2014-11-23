98ca0275ce79301940d1fd303e3300da218aed2b
Jake Shirley <navi@odinseye.org>
2014-11-22 20:42:49 -0800

    Spiffed up ActionSequence::Cancel()

--- TimeMachine/TMTools/src/ActionList.cpp
+++ TimeMachine/TMTools/src/ActionList.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01900763ad829ae0d353bc8dec027638af13b48a6e7e6d11dc1408696cee568b
-size 26027
+oid sha256:ebd22439d2f58bd55a379b032349a257c1a0a416362bab4a0a4bab128f553090
+size 26061

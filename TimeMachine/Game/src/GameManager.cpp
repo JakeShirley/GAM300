c0012fa26c6fd40bcab536832f0edabe7388d3d9
Jake Shirley <navi@odinseye.org>
2014-11-23 17:17:04 -0800

    Extracted 'play' logic and updated the game to start a level if the editor is not enabled.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41573c48e562b3e506c010f87ee09924e87bcd0fbbacf2d9ef8a27290d4c87de
-size 44704
+oid sha256:983aef1cf8423b9c501dc97271644f9bf4d467666639fab97b202fe667578ec2
+size 44895
--- TimeMachine/Game/src/GameManager.h
+++ TimeMachine/Game/src/GameManager.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65532a6b1e372b2ba7895f6866fb0f702fa040d4b4b5c293103ad18ef22b0bef
-size 3776
+oid sha256:6ff2dbcaa38d2236fdfba80cb816e4fc97db912d2829f323f0c2c2f8b4e62aa7
+size 3843

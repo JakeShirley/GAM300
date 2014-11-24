26d0dae62f44853e1fcb6543c01e7898401dd543
Jake Shirley <navi@odinseye.org>
2014-11-24 11:41:23 -0800

    Updated Lua table serialization to be alphabetical for more deterministic results.

--- TimeMachine/TMTools/src/LuaLib.cpp
+++ TimeMachine/TMTools/src/LuaLib.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7da13d4036bc2abacb24600a4e844b0af4e9ed75895d8c1dead2cd18c0078783
-size 15243
+oid sha256:1c7914e52cbb5ea00bd0a3a94946a18ad98685f5e1ed4821362ba7467d68c2df
+size 15198

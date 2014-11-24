0cb374cc546b009ac57f13ed8a31969d2fe3a6c4
Jake Shirley <navi@odinseye.org>
2014-11-24 15:28:51 -0800

    Refactored GetIsPersistant to GetPersistant.

--- TimeMachine/Game/src/Space.h
+++ TimeMachine/Game/src/Space.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8eb2ee1b89f5ef4cf592e0816798e7e3d0b09ea0b72f70cff7e19b35091d3f6
-size 2642
+oid sha256:55626802951f581d34620a6f65c76c9a760f8f769435fb412bb0b49c189e88b6
+size 2640

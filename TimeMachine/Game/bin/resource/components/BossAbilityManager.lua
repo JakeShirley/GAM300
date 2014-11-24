b9ec15c2d6059724c390230e22cda0a91c1194f5
Joe <joeybonaccorsi@yahoo.com>
2014-11-24 12:38:51 -0800

    Added functionality to chain abilities with BossAbilityManager

--- TimeMachine/Game/bin/resource/components/BossAbilityManager.lua
+++ TimeMachine/Game/bin/resource/components/BossAbilityManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2025ea5fb3957e815b96107c9146b0d7b73c8d2dbb3bf54cb01b932fa5407878
-size 5051
+oid sha256:327bb469ee078e6e6d4d0a3132698919c04ea119bb0b8792cc6a1895f135d50b
+size 6244

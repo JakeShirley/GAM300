ec80f43e47545218dbc0ee4d1a4c1b9c514aafef
Brian Inkrot <binkrot@gmail.com>
2014-11-24 00:02:06 -0800

    Boss working entirely on BossAbilityManager now, just a matter of cleaning a lot of it up. Fixed a couple bugs in PlayerList that were messing up NumberOfPlayersDead. Also added DebugText booleans / functionality to a few components, and generally cleaned up debug text a bit.

--- TimeMachine/Game/bin/resource/components/BossAbilityManager.lua
+++ TimeMachine/Game/bin/resource/components/BossAbilityManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2025ea5fb3957e815b96107c9146b0d7b73c8d2dbb3bf54cb01b932fa5407878
-size 5051
+oid sha256:06bd692783de0eef379357c468d84a13550b783cee8edc8ebc5602d537f2a415
+size 5282
--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:322a1f20d3ae19639b1355a67bfd7f2396849b2d54f63be03b3c8ecfe71e4980
-size 31851
+oid sha256:9ab12234c32602c606938bfec58b177832a561f25a86390d22de2a9dc3061e20
+size 32963

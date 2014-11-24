7c6fb82371aba0a8a9e1113fb99322a356e2bf79
Jake Shirley <navi@odinseye.org>
2014-11-23 17:12:58 -0800

    Comments and added TMConfig to TimeMachineApp.

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dcad5e133998c57da96510d7979c4d61408f91cba9a1a2990716de834e6385b
-size 15549
+oid sha256:5d6c7a410ab2f33a5cbcd53c6dee242e5f608fcdc43f2c83ec9336cdeb869cb6
+size 15650

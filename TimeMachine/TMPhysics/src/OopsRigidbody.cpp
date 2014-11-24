eea539f84865aa80928ea6316789df89ec87a7aa
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-24 15:14:28 -0800

    Fixed wizard rotation bugs.

--- TimeMachine/TMPhysics/src/OopsRigidbody.cpp
+++ TimeMachine/TMPhysics/src/OopsRigidbody.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:843fa1adfa4b53b579a1fb675b329c40f6140ac07c668d480500e09b60276cd8
-size 8175
+oid sha256:dbd7fcda49a12d3834f3c26cc35acc2d9e058bfb35738e45629aa768b85dbf10
+size 8796

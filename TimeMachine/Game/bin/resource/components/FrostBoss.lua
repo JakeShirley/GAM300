4f23f320425c6aa6015b8649804e5929b2ba98e4
Brian Inkrot <binkrot@gmail.com>
2014-11-23 21:06:29 -0800

    Almost have the phases in, saving progress just in case when i pull to fix other issues.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50c8c22218686e7047f59aa1213ea33fbaf75064dbb1e0529c1367f878cf98a3
-size 30491
+oid sha256:322a1f20d3ae19639b1355a67bfd7f2396849b2d54f63be03b3c8ecfe71e4980
+size 31851

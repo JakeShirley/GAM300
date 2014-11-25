8864df8d09c2bb02f7f9a90b3ae64ee1c8b3e527
Jake Shirley <navi@odinseye.org>
2014-11-25 11:53:07 -0800

    Fixed resource paths when it grabs from file.

--- TimeMachine/TMTools/src/IResource.cpp
+++ TimeMachine/TMTools/src/IResource.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdd7b1194e8ee2342cae426423d7ef96b3063ec37da402ddfcedb9dfe9970df8
-size 9114
+oid sha256:084140e4c29fdb5ef53c04398c752e8092a23a074f2af825afc2aa967c87039f
+size 9141

fb256711d6c30600bda3fb8adc019931e4e431a5
Jake Shirley <navi@odinseye.org>
2014-11-24 21:32:37 -0800

    Call destructor for ScriptEvents.

--- TimeMachine/TMEvents/src/Event.cpp
+++ TimeMachine/TMEvents/src/Event.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77c9b7c80c6bbc10eec9d6027f3886eed8190848b93ecbfde0a9fd8fed178a81
-size 2114
+oid sha256:dd4edb2fb0016f01c098ca79530a7da5137e04d8ae732d221444b79fc1b1337e
+size 2145

c9aaafb43d1954fed613a816cc715d08f584951c
Cody Centers <murtag27@gmail.com>
2014-11-24 16:22:04 -0800

    Emitter components no longer activate against peoples wills.

--- TimeMachine/Game/src/ParticleEmitter.cpp
+++ TimeMachine/Game/src/ParticleEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e10e9eac641fbd915e92d5e549759aa4357647cd312119bd3dc3b02b4f561538
-size 9697
+oid sha256:67da4d9535d47b5190a9e70f77a7c954bdfb874f502c518b962355851a319b22
+size 9726

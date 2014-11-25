5c966d3d556e56c112798db9923474ff8235d6ae
Michael Chambers <m.chambers@digipen.edu>
2014-11-24 19:40:15 -0800

    Localed some things that weren't local.

--- TimeMachine/Game/bin/resource/components/Health.lua
+++ TimeMachine/Game/bin/resource/components/Health.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bae0500315edc2e3503d230d24a8358296215fc0a9b825bfb5155a006e940c5
-size 5627
+oid sha256:9ceb0ba9a5e08957cde6027c95aa9e84b95e44b64e014685541a2c2d15ba45e0
+size 5636
--- TimeMachine/Game/bin/resource/components/LinkingMechanic.lua
+++ TimeMachine/Game/bin/resource/components/LinkingMechanic.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a76cf05ccb0288137a31981a95844d39adeb2194d4dfc2f68d64fee5b416c65e
-size 3380
+oid sha256:9b176344b2cc5cad8b1f1602a9adf9f332cdfa40f2fb0431881dfd4cb7b45ba1
+size 3388

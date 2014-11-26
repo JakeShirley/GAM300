2e5f4203d97aa3d8c03714a4a6de2cea0dc62d15
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-11-25 20:57:19 -0800

    Image based lighting without importance sampling

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c22b8eac57f4746d78bb6c672620389156bd505c85babd6f12a88a7c45d78496
-size 67870
+oid sha256:98f15613a7e9f212febf82035c4c83a9e564c58fffe8f10265177ecee95c9360
+size 68601
--- TimeMachine/TMGraphics/src/JAG_RenderSystem.h
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f031fa3c516924f284ac59c6dd768fcd3aecc444a29371475258d4f0fafde900
-size 11334
+oid sha256:3b6ce7bacfaf18514acfd9eb97fe097e074ca7d198ac5081ccd38962553ff6df
+size 11353

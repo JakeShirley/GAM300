f1bd6666253b9d70c1f1ea00cd7aedc7724dbc02
Jake Shirley <navi@odinseye.org>
2014-11-26 21:58:33 -0800

    Removed old, hardcoded logic and auto-load the last level opened in the editor.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cac5df11ce527c902bb4fc343580c1d3c788cb817b8e48ef199df3d958e1bc7
-size 45417
+oid sha256:8ed20c485f19dc60f3ee562da43414c30415fdb65747f0e2b259e22fb19fd277
+size 44967

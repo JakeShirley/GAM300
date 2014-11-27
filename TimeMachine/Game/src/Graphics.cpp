bfb20c898301431c62a6135d280c9c88975b7e2c
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-11-26 21:04:33 -0800

    Starting proper IBL stuff

--- TimeMachine/Game/src/Graphics.cpp
+++ TimeMachine/Game/src/Graphics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51cfcf5c2560ce0a68af855894b472b959e8528637ccb3ff60a4a7916dcd7c76
-size 15393
+oid sha256:f338510e67a68f3066b52a532bb64e0583f30f94b0e896294abd8b64e19a9346
+size 15391
--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1174efe4ca114658be9c5f72c0038a61c495cdb33d397f316c81a5d5d6b0e59c
-size 68657
+oid sha256:3b1414cc7276f8bbaa67e362ada79f2a393edd8022f55bac9a1c4343e20235f9
+size 68659

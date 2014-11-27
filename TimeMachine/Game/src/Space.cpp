81874eb648ea28774b2b3c612846a4839ff1e700
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-11-26 16:51:02 -0800

    First draft master exploder.

--- TimeMachine/Game/src/Space.cpp
+++ TimeMachine/Game/src/Space.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:731a14115549511ae016feb7ae286e5835dc1118cfe6a270725fcb972feb753f
-size 13117
+oid sha256:2884c87be7ee26f509151806fac53b77f90313ed264c943efcbcc937bd3d0f6c
+size 13204

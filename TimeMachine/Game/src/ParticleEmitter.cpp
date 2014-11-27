9369e2123e480c1e9af3c251689538b3d114d71d
Jake Shirley <navi@odinseye.org>
2014-11-26 21:58:09 -0800

    Added Active property to ParticleEmitters.

--- TimeMachine/Game/src/ParticleEmitter.cpp
+++ TimeMachine/Game/src/ParticleEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cf5d9f7e7306f62de0add3668f9c6ae133945463da366158a32eaba9aba73e3
-size 9749
+oid sha256:ed01afc7e450d2035f2a602dc896844a338cfea572416871c1349087db1e4e1f
+size 10011
--- TimeMachine/Game/src/ParticleEmitter.h
+++ TimeMachine/Game/src/ParticleEmitter.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab4f4e4ea6cc922a209515162660ab107379c3f3cdb87590e9b7cf27d6c8e86f
-size 2328
+oid sha256:35dc77a5e71885980cc6d9e715fd33a79f758b0185b96dc2ac96cbd021cb5dfb
+size 2343

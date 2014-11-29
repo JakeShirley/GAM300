a05578b24c426836885226d070265bae72f9bb8d
Jake Shirley <navi@odinseye.org>
2014-11-29 15:50:05 -0800

    Remove default value for default level.

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3541d7377aa8414d5fd2a6f6c6850438cc2af99e876608fe65b63b18a4e95808
-size 15653
+oid sha256:afd815a5494496aa2ef2519354a60ef8bd13031bc78bac49487ac0d86adb7560
+size 15587

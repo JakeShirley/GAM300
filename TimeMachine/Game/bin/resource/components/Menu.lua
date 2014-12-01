80c626b837b97daaf57f6009e61fc104956b7650
Michael Chambers <m.chambers@digipen.edu>
2014-11-30 18:05:40 -0800

    Escape pausing works.

--- TimeMachine/Game/bin/resource/components/Menu.lua
+++ TimeMachine/Game/bin/resource/components/Menu.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5f947d1d3a916625f138c63f49fac15dd13b6c5236056dcd2c1226c8039a18a
-size 23891
+oid sha256:07a10122fd22110034fb537a6d35edf91f6900eda806ab3a78ff8526639b9b30
+size 24059

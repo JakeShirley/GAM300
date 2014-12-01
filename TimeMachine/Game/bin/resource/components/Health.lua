d436fef02505a6546003f62fb74384321f92cd24
Brian Inkrot <binkrot@gmail.com>
2014-12-01 13:56:31 -0800

    More improvements to arena, eggs can't be destroyed before they're active, and added some player HUDs that need to be positioned.

--- TimeMachine/Game/bin/resource/components/Health.lua
+++ TimeMachine/Game/bin/resource/components/Health.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dc4a9939271e85439a8cfb1e703a668d586bc6a166f6b1cdc39df2cec0e6609
-size 5729
+oid sha256:0587bcd56ef55ac24b1d0152efe391a1506e4ccba0ca8226b34461778c8122b8
+size 5784

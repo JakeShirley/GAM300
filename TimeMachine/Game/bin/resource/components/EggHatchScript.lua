b353e2400977a2dc6690e37b40019f82e00290fd
Brian Inkrot <binkrot@gmail.com>
2014-11-30 23:36:56 -0800

    Hooked up Eggs put around Frost Arena.

---
+++ TimeMachine/Game/bin/resource/components/EggHatchScript.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b929a4d848569e02144ea30f8dbb9dc7eb498c7aad8292a4b7dd1b7d69417bb5
+size 1067
--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab2e351b67b74a5526c49ffda97e31c08598ae0eeb98cb5f07d47c31c4244497
-size 36486
+oid sha256:e2f121d959ca11b7cacc193cb0c42d02d6c66c6fc8cb8f9f073b023efac3d3eb
+size 36623

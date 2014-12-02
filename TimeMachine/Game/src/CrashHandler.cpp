6da7438d90b0d316bd7833cc49dc733d390ef22a
Jake Shirley <navi@odinseye.org>
2014-12-01 19:31:15 -0800

    Added CrashRPT crash handling.

--- TimeMachine/Game/src/CrashHandler.cpp
+++ TimeMachine/Game/src/CrashHandler.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dadfcab82201311f9315775070f8e077e39ab406bdbae1fcd7cf85f530e6233
-size 1717
+oid sha256:fe9bc9cf660227ad9b9165b451f879b5c19e9d5dca16a5831da941b76474e31c
+size 4153
--- TimeMachine/Game/src/CrashHandler.h
+++ TimeMachine/Game/src/CrashHandler.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:298af6fcf21b64f81133efb85024bdae0b8903c62e99dce549f3f061d6f903ec
-size 281
+oid sha256:f4d38314fa103929b8c9bf79f466875ed3824d5c1a917b9c8facc626e5822ead
+size 670

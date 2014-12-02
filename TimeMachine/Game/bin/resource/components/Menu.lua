0bf3633bd1bed52c5c60d49dbca73defe291606e
Michael Chambers <m.chambers@digipen.edu>
2014-12-01 22:18:13 -0800

    Confirmation of Destructive Action done for main menu.

--- TimeMachine/Game/bin/resource/components/Menu.lua
+++ TimeMachine/Game/bin/resource/components/Menu.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:490ac34fe456fa7d14aba4d7ca09d529547d3a34be38dfa6682aaac7c1a96208
-size 27618
+oid sha256:1a4bf2ad62b6e84628444a4857deef3ac849bb0ba44bf5d442cbfc98e1dec05b
+size 28817

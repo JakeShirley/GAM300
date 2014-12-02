3b4bb5290c3df55c652af41d07eb57247fbfa92f
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-12-01 21:12:30 -0800

    Jake linker thing.

--- TimeMachine/TMApplication/src/AppRevision.h
+++ TimeMachine/TMApplication/src/AppRevision.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1600490a07d9b15c7ca6faeb25bb105b0782580e603a2c35647917061ee47f1
-size 118
+oid sha256:188606c2affa8b3274bf2883a9007da9a0a538f3f8e06dcc921a911085829d4e
+size 119

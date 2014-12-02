ad8f0595f5d4ee7928c773944c48d5799bbf9b69
Garrett Allen <garrett.allen@digipen.edu>
2014-12-01 22:44:05 -0800

    using Exit at runtime with multiple spaces should now exit cleanly.

--- TimeMachine/Game/src/GameManager.h
+++ TimeMachine/Game/src/GameManager.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb14a2a4d04460c955d606b45e25e0ca3747cbb14eb1de6a998b5ea782577dc5
-size 4118
+oid sha256:53490be7fba2eafbd27529a98503fe0ec9f3a068a12a32de6908ea2ef1a28efd
+size 4130
--- TimeMachine/TMTools/src/IResource.cpp
+++ TimeMachine/TMTools/src/IResource.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:084140e4c29fdb5ef53c04398c752e8092a23a074f2af825afc2aa967c87039f
-size 9141
+oid sha256:5c8f0f93f25c18f717d82e2f465a5e47a670d6deeaf40cb4518568165a072df4
+size 8842

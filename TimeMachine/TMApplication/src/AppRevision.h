e1897da567954be7a7fd2418ff2f2191eb16c79b
Brian Inkrot <binkrot@gmail.com>
2014-12-02 11:57:14 -0800

    Fixed Frost Nova, improved add ai to actually chase, and re-added gitignore to backups.

--- TimeMachine/TMApplication/src/AppRevision.h
+++ TimeMachine/TMApplication/src/AppRevision.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1600490a07d9b15c7ca6faeb25bb105b0782580e603a2c35647917061ee47f1
-size 118
+oid sha256:4e920ffc3b6d31af2ce9d652ffa348459968c46e395fe85a134c2830e7c9ec40
+size 119

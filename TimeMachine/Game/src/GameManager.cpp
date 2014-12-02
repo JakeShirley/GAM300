ed9164afe2e38637bf2a19f463eb213b387f36f3
Jake Shirley <navi@odinseye.org>
2014-12-02 00:04:56 -0800

    Added assert for error checking.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e15d192d8ca1067ec7d71c3fe58c7824c2a79b626513ce2c0252d50d71c96dcc
-size 45339
+oid sha256:8bcc59910862e6adc0fe30f762fcc86a7d4b95a0bb68bc410468a97199f1ad45
+size 45372

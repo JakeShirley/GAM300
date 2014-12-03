e9c897b5e6b966a448b22cd844819101d3a9cb30
Cody Centers <murtag27@gmail.com>
2014-12-03 12:24:29 -0800

    Made files for emitters specific to each wizard

--- TimeMachine/TMParticles/src/FrostWizardEmitters.cpp
+++ TimeMachine/TMParticles/src/FrostWizardEmitters.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b3cd18b7533b00703cdd3a9aff3ef4c29f3e4570402019aae15ff504c5302f2
-size 8861
+oid sha256:95ded521fa92d39025649247a503501afa1bac70279aa54331afcbe7f6554d85
+size 8849
--- TimeMachine/TMParticles/src/ParticleManager.cpp
+++ TimeMachine/TMParticles/src/ParticleManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61d125e9155e52a5c3f798fcd871004d747e0ad24e11d7be5198b3b27cc2238d
-size 18017
+oid sha256:ccb60745b4cb95602c99f31ee1040298f6eace2bb69b0c17fff2d87359948b6e
+size 18067

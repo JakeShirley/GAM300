1279d625b8c139fe7cef6604428a8a22c4be257b
Garrett Allen <garrett.allen@digipen.edu>
2014-12-03 15:12:11 -0800

    crash handler defaulted to off

--- TimeMachine/Game/src/CrashHandler.h
+++ TimeMachine/Game/src/CrashHandler.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4d38314fa103929b8c9bf79f466875ed3824d5c1a917b9c8facc626e5822ead
-size 670
+oid sha256:8fb654967b8bb8074d29b8f8600d4850842d7550a29c5f5bfaba2a33f4581a64
+size 672

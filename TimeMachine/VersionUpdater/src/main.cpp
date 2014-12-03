7e8f93b494656f18f53868472ad418f8ec24603b
Jake Shirley <navi@odinseye.org>
2014-12-03 09:59:28 -0800

    Updated VersionUpdater to create the entire AppRevision.h

--- TimeMachine/VersionUpdater/src/main.cpp
+++ TimeMachine/VersionUpdater/src/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7171393e6eb3c12ec2b2d27ee8e0dd52601b8baada6cf0e231e27be8f32e3d37
-size 1921
+oid sha256:1a6873e0d393ac49c2d017c592b9db8d650bc88f589ed829d0a0ca79576bdd46
+size 960

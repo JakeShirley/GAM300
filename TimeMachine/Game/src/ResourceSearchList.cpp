bec2bdd61ff789d57605d1fea895bc34cf425556
Jake Shirley <navi@odinseye.org>
2014-12-03 10:12:22 -0800

    Changed RemoveResource button.

--- TimeMachine/Game/src/ResourceSearchList.cpp
+++ TimeMachine/Game/src/ResourceSearchList.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37fcfccd2eaaac5e49af2c41a92b2e4a0c3ee8c92827e67ec35a34c24b189e60
-size 6112
+oid sha256:f1b53cb2a444f93225e0e37f55809b1cc42b0843a3ec5ee77e0dfa2b87415863
+size 6103

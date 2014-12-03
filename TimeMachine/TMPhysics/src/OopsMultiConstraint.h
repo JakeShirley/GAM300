93b3ae717c8557186986367efd46ba2b38b8fcba
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-12-03 11:37:14 -0800

    Removed hard coded limits and defaulted breakability to false.

--- TimeMachine/TMPhysics/src/OopsMultiConstraint.h
+++ TimeMachine/TMPhysics/src/OopsMultiConstraint.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9bcf77667463b1416bebf020965c8b9304eb8cef4c7a8623876777b9f0c1b42
-size 2571
+oid sha256:c0940193e1b54e0d62651dcc8cfcc8a489e57927b5fc2c19a70b8fed9ff54932
+size 2591

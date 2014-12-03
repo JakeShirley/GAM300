29ffbb57d6a38857cb18c22e787554c6f4f83c9c
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-12-02 16:32:46 -0800

    Ghosts won't contribute to the manifold of a non-ghost, resulting in more stable collisions with ghost/non-ghost families (like players).

--- TimeMachine/TMPhysics/src/OopsDetection.cpp
+++ TimeMachine/TMPhysics/src/OopsDetection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69e96d3865c415308017695a4cf252a6a0bdc6be5c1189d4e2f29dbe5788593c
-size 21861
+oid sha256:e9e1df80037d5ced656164a21da0bb1b3cf452e52aabcb1338e6fb36ffd57ff9
+size 22209

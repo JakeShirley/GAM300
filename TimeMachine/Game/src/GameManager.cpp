82e3bc9453b8a1f4877529310930bcd735814f32
Jake Shirley <navi@odinseye.org>
2014-12-03 09:58:15 -0800

    Fixed default level loading and fixed CrashRpt including the correct output.log file.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce9558a73fe2f1f9b9aacb7bc553b16e40651bc0081bc7955777c1902736f35b
-size 45510
+oid sha256:4592dd250761e0f2e97e981e117bc93aa0581ffc444773e6dda5d50a81f035cb
+size 45637

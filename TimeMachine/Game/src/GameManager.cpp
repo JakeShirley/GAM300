4e1737b8412d3bc21e6cbd9fb24e1263b7fee2f9
Jake Shirley <navi@odinseye.org>
2014-12-04 15:55:38 -0800

    Removed fake menu option.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0aba20338ca498737e5e5d9ee23b4c11730be354acb894285387b27d06bacac5
-size 46697
+oid sha256:ac8f9284537d0ea948bbfd433885623a66d26342128f6231234c7d65b9c919f1
+size 46561

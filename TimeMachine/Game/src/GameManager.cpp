142ce3ccbbb362196716100ad003711937d94393
Jake Shirley <navi@odinseye.org>
2014-12-03 17:36:08 -0800

    Fixed broken dialog things related to saving in editor/non editor modes.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77e7ed9bb4c0b1fd1363a4c96ddcd40304f215cbc72c554afe5102140bc710e4
-size 45753
+oid sha256:eaa1102c3ab9cc38787e19a79ac88ed222ee588de405484a8dbec357e52c3bb7
+size 45900

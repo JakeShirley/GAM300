456be9b640458cf61c095ef3a57b357c82273e40
Jake Shirley <navi@odinseye.org>
2014-12-04 15:52:17 -0800

    Moved Quit to the bottom of the File menu.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be47f15313ae10491123629229bae38c2c77b8315ef3b7e3063c2ff1560f9aa1
-size 46676
+oid sha256:0aba20338ca498737e5e5d9ee23b4c11730be354acb894285387b27d06bacac5
+size 46697

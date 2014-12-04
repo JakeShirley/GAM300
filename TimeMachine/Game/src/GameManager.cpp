d8ffa9bb3c190d1b7ec2916c9f02f3d1fb94b284
Jake Shirley <navi@odinseye.org>
2014-12-03 18:01:00 -0800

    Only clear ResourceSearchList if it exists.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3824dff33cbe6f6d4d649a751cadf727f3a0e7982bb9037cafe380080b37e40b
-size 45734
+oid sha256:6576c6bf990d04fa4ea6366f83b16881f1ff8237c17da4cf9ddc3823ee1385eb
+size 45784

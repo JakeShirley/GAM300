531583ca73e00aabfca6e041abff518662eed988
Jake Shirley <navi@odinseye.org>
2014-12-04 13:27:02 -0800

    Added right click events to SearchLists.

--- TimeMachine/TMApplication/src/SearchList.cpp
+++ TimeMachine/TMApplication/src/SearchList.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbbd9cb60429d6d4be2060efada8a6fe5bdcbf2a5a0cefa77f32e09282ddd1ba
-size 5104
+oid sha256:88b0a766744ef0e510884f0d5b9693400ffe09bfe947a8786e684f776afbeb6a
+size 5839

2644c6024806696c0bfd30f7e4f19bd7f6930f2d
Michael Chambers <m.chambers@digipen.edu>
2014-12-05 14:13:39 -0800

    Cheat Codes update and charge particle looks better.

--- TimeMachine/Game/bin/resource/components/CheatCodes.lua
+++ TimeMachine/Game/bin/resource/components/CheatCodes.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e588f35d2044f6a2632085f12b12fc57e80d9b84e9ed0c3a9460ab342f6399b
-size 3576
+oid sha256:9a2fcd80b3e1000fe767939c3bda0f11fa5b5c5274be57be9c83716a88680712
+size 5368

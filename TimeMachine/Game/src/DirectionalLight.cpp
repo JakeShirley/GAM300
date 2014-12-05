c48c2619840defbac7b7fea7365b4d13cee0306a
Jake Shirley <navi@odinseye.org>
2014-12-04 22:50:25 -0800

    Fixed light components leaking.  Zero memory leaks!

--- TimeMachine/Game/src/DirectionalLight.cpp
+++ TimeMachine/Game/src/DirectionalLight.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:440677aee98e1c2c6056ab8398848789d8c621ba940842567fddd8ead222bb52
-size 2787
+oid sha256:5fdc4ad33fa068c5b5bc598360ed52e1030474f6fb572b66ce53d356600c048b
+size 2836
--- TimeMachine/Game/src/SpotLight.cpp
+++ TimeMachine/Game/src/SpotLight.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0c4fc4021dbc19a4d7264b2102e519c4232c7a6d1b2e35c177b165dff9e0ca3
-size 4302
+oid sha256:c574724d9127af588f36c43fe44a2e424592eb4ca236b38c391b557afa2dd7ec
+size 4351

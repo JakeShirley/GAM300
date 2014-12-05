b5d06a1babede2b7ca233396e4f18863bb56dbb3
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-12-04 19:58:36 -0800

    Edited WM_ACTIVATE callback for alt-tab fullscreen stuff. Ready for testing

--- TimeMachine/TMGraphics/src/JAG_RenderContext.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderContext.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6da6c5711642beaee6195ce511e2ef84203546b78219bcaf717a7d0c3a30eb51
-size 99652
+oid sha256:7ab0a7a2f4d6cdc4a55b2c6b0c66a1d0358cc9f8fa8d862d61786ab1126f47d6
+size 99654

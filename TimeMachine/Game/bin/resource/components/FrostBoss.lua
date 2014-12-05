a6852271060dab8787cc95480d9126999d1e0b6a
Brian Inkrot <binkrot@gmail.com>
2014-12-04 23:37:02 -0800

    New sky dome, fixed some icicle aiming, and went insane with weird rotations.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2e7a6a609c43fd69aebdf585378916d1778ca8230e1660908a5f15a57ef5e5c
-size 38085
+oid sha256:844e46ac1017a9fa4145f79af49a6178cbb6db05c1c91d6e53fd9059d14915a3
+size 38287

aadb2bf0b609605e25eba2d196a92589187c0f0f
Jake Shirley <navi@odinseye.org>
2014-12-04 22:44:15 -0800

    Added SpotLight component!

--- TimeMachine/Game/src/ComponentDeclarations.h
+++ TimeMachine/Game/src/ComponentDeclarations.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b7925be3b5d90e127752ac5fa948325d68992efa1a70c265920522c6bd0901d
-size 632
+oid sha256:c51bae4b31d19d40dc25a7141d4c650038d801131601be6d5b81df89246911a2
+size 653
---
+++ TimeMachine/Game/src/SpotLight.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0c4fc4021dbc19a4d7264b2102e519c4232c7a6d1b2e35c177b165dff9e0ca3
+size 4302

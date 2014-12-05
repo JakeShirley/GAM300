f2e9061d0c4026dde1bf0aa2f205bab9790352d5
Cody Centers <murtag27@gmail.com>
2014-12-04 22:09:57 -0800

    added a behavior to particles

--- TimeMachine/TMParticles/src/ParticleEmitters.cpp
+++ TimeMachine/TMParticles/src/ParticleEmitters.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77522d0d62bc4f44ab411779b23888803fe006caeedb8cf630a5ee313d979dee
-size 41460
+oid sha256:066993ebed27f2c44976bc1829bb66af49488c72752bf6547809bfe6e6eee3c0
+size 41467

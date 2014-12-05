0867627a83a69c109665f94267bdd3717dd58d09
Garrett Allen <garrett.allen@digipen.edu>
2014-12-04 16:54:14 -0800

    unparenting through right clicking on the object should be working. Also you can unparent by setting the parent to a nullptr

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6cb8ecf4f28011508f6d9e270db08a9dc62f79b4c824d2f440cfd48468f15ff
-size 42169
+oid sha256:4e47635d5426edd0b819028305320354695a7dbdb854ba9fb628df01f9ffe499
+size 42354

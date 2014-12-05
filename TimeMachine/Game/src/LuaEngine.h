683778add8b16b2b641ff813cbda65a9daa689a5
Jake Shirley <navi@odinseye.org>
2014-12-05 13:58:14 -0800

    Added checks to ensure all ScriptEvents are cleaned up.

--- TimeMachine/Game/src/LuaEngine.h
+++ TimeMachine/Game/src/LuaEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75e6bd39c40f684b3026ddf4413e8ef81c8561e4b4ff52aa0bce837caf92cc3c
-size 1673
+oid sha256:275538ad80d39296cd0c529fffaa54e938440c1f820d39596b36d9701835947a
+size 1701

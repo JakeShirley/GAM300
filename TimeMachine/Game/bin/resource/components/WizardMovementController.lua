bc1c1ba10b5cd5940d351e7307fbd3d618c2a9b8
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-12-05 15:04:15 -0800

    New player movement responds properly to death.

--- TimeMachine/Game/bin/resource/components/WizardMovementController.lua
+++ TimeMachine/Game/bin/resource/components/WizardMovementController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1d2f9279f8280c90f6780238d5c4c282e1d4fc4982ad77cdc54750c01f371d7
-size 6495
+oid sha256:bec3c551d9a350799a6581a1d35d75e9dec6fba030b8028a60206064cfeb00e6
+size 6496

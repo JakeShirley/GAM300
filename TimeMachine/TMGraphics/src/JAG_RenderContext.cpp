df1a6e844b1d2d84ddf8a2e2f924614a69cffdc8
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-12-05 14:41:32 -0800

    Fixed DirectX resource leaks

--- TimeMachine/TMGraphics/src/JAG_RenderContext.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderContext.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:081fde849337dc2cede9d8326db87b717d5c3a3d927bc87e2d67ccdc849a0cfb
-size 100551
+oid sha256:9cb409b716b24354615c936536a1223564b9f85e3002b0da9dd6dd77783d63b3
+size 100445
--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1f9cae78ff159e7a61d86ba42a92aa1d02a3e3e1bc5cb2070ab8958769b235d
-size 69729
+oid sha256:11c1cd3106670ac6059c876bef6d62f657aca5903becc866d22524973f77a181
+size 69730

b762410a1bf67212502493b23f29403ec20c9665
Jake Shirley <navi@odinseye.org>
2014-12-05 14:25:20 -0800

    Audio now pauses if we lose focus.

--- TimeMachine/TMAudio/src/SoundManager.h
+++ TimeMachine/TMAudio/src/SoundManager.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:628cffcd68bed16660544aee3e744f8617d5e0768e742c07a0306d60a800deeb
-size 1638
+oid sha256:6d6af23798f8036cac28fbb551342c02c5fd55bbf579af7f3b1bdc9cacc0b93d
+size 1724

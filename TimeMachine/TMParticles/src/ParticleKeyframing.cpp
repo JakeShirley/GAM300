7712faacf5a618d203e354754f29d3408252a695
Cody Centers <murtag27@gmail.com>
2014-12-04 17:25:04 -0800

    Many changes and additions to the monolith orb emitters

--- TimeMachine/TMParticles/src/ParticleKeyframing.cpp
+++ TimeMachine/TMParticles/src/ParticleKeyframing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfa192013cf42aefcd9e61a0a24be757bb63af368b101486829e299b7bd32824
-size 2502
+oid sha256:e2a4f2000ff786d50a98fba417cae8249f1f4616684b8698d78d6c456797d664
+size 2528

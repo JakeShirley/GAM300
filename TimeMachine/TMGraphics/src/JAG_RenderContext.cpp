b7ca1b11a36b30aa5407971163772a0c29c61962
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-12-05 12:25:44 -0800

    Fixed macros, added resource delete loops on RenderSystem destructor

--- TimeMachine/TMGraphics/src/JAG_RenderContext.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderContext.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ab0a7a2f4d6cdc4a55b2c6b0c66a1d0358cc9f8fa8d862d61786ab1126f47d6
-size 99654
+oid sha256:081fde849337dc2cede9d8326db87b717d5c3a3d927bc87e2d67ccdc849a0cfb
+size 100551

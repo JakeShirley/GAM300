20414ce05ffcd43e540731c0b24b0a79ef51e18c
Jake Shirley <navi@odinseye.org>
2014-12-04 16:19:56 -0800

    Made the GameManager pause all active spaces when focus is lost and re-activate them when focus is gained.

--- TimeMachine/TMApplication/src/EditorWindow.h
+++ TimeMachine/TMApplication/src/EditorWindow.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b800c531bcef201477af3391c369814a1302f8ec3a0d5b884f4f332d7946b0da
-size 2755
+oid sha256:4561958c164d22f3dfd7967276eddcdace96bf45809c4bd22b05dc85a425d353
+size 2813
--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f996a212317b5b5ecb3eca13eb758a8b1b2b8d3ccb78a67f299058b6f387bf6f
-size 16102
+oid sha256:e1de8856244a0ef0791f6bee4d749e710383a4a02d6fa1c242fb1f49737a6e74
+size 16938

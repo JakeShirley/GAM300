155ce0d0fb884c44da5e8b10e23b2c803875a46f
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-12-05 15:48:45 -0800

    Cleaned up new movement in player controller and tweaked its values a bit.

--- TimeMachine/Game/bin/resource/components/WizardMovementController.lua
+++ TimeMachine/Game/bin/resource/components/WizardMovementController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bec3c551d9a350799a6581a1d35d75e9dec6fba030b8028a60206064cfeb00e6
-size 6496
+oid sha256:c1451de90cd13d1100268f9da4bc2f36eb6d786b070b0fc275b901a27d33b581
+size 6784

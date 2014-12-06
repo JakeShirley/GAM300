1e920b9149d468fba218cd4ba7505c12748b7031
Brian Inkrot <binkrot@gmail.com>
2014-12-06 01:38:02 -0800

    Fixed Eggs spawning, and turned off some of the debug text.

--- TimeMachine/Game/bin/resource/components/Boss_Wander_Charge.lua
+++ TimeMachine/Game/bin/resource/components/Boss_Wander_Charge.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60d1162624bc64397aac69c0ec84aa5d8fedc3d5f94366c33152b71b7e4b033d
-size 15653
+oid sha256:adf534c66c77faeadbfbe7a80ff3c4889416218102d08970b1929fffd5c14a04
+size 15655
--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bbafbd569da51af72d404b224b74bfc2d0336637c7d5a652c084efeb0dad7d2
-size 39977
+oid sha256:e29ff4038dce40568392643f426f94ae8d74d619d1ab738f2c330d4c50a24fc2
+size 39979

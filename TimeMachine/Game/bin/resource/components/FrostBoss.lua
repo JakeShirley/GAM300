e557c69533f935819ca6e5d42067065555630664
Michael Chambers <m.chambers@digipen.edu>
2014-12-05 20:59:46 -0800

    Fixed an uncommented ability on the frost boss.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:805dcf994a8c09a3566ca03a8903cba47c20ac29557d95a1ead38ed4cdf304cd
-size 39582
+oid sha256:4fb0f7ff790d47c378218a1789077c3f50eab5958ab52df9b4e9bf7807c20721
+size 39584

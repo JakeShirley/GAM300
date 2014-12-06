f8c098a95c6b37d81f2a270c7e4bcb7905a37144
Brian Inkrot <binkrot@gmail.com>
2014-12-05 20:40:04 -0800

    Hopefully fixed a bug that could leave the player controller off.

--- TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:027da033bf5122b1a7d07f76460def864eed4566964ff232fd95824da014fa5d
-size 19268
+oid sha256:eb3dc2be2815ec6f69b2ac5f9582872f195a715267807ce88c01e0568a41c1ea
+size 19392

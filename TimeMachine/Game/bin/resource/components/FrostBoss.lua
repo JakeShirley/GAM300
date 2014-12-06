69c5f1805ed4e5b78b2ae0717ec19c36d89691df
Brian Inkrot <binkrot@gmail.com>
2014-12-05 21:55:18 -0800

    Maybe last setting of the boss and level?

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4acdbc7abbe809a1184ef69ad0f051b06568b1fdc1b3a9772c3e4ec15a270aa3
-size 39597
+oid sha256:b69741619fb8ed5251fb6ef29e42e235bfb676a6012241db20f80d36ced284ef
+size 39584

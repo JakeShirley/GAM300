96e08ca2bbdcf391d10f7b0e382a072643285a97
Tristan Schneider <forgottenplanet22@yahoo.com>
2014-12-05 19:32:47 -0800

    Frost babies have physics deaths. TimedLifetime uses action lists. Added more graceful disappearing options to TimedLifetime.

--- TimeMachine/Game/bin/resource/components/PhysicsDeath.lua
+++ TimeMachine/Game/bin/resource/components/PhysicsDeath.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfba877ae68ddede07d5d011ce9263223daac32cf171a1b44937dfe5ade2d213
-size 9804
+oid sha256:0da80e8701b7d7469e1d41d149ca29bef2ad0484102b3af3a38a4bcee727065d
+size 10008

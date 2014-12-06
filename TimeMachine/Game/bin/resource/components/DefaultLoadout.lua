450f2f4c7e7b3e6e9f5d9934e42f86ce9f2e56ec
Joe <joeybonaccorsi@yahoo.com>
2014-12-05 18:57:57 -0800

    Added sound effects to all the things Fixed timing of frost nova attack

--- TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46f2ce8c55b35bfa073b1be6d2ec7497c172b8986d2ffd43db71d1e057ad46a4
-size 17358
+oid sha256:af5a4a6d1c0b60d9e69900fc778ad3fa5a108efb9a6cca5352b43397aa3cf56d
+size 17390

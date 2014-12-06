81ad37e27293a61486c6dfff55d89a7477ead63d
Brian Inkrot <binkrot@gmail.com>
2014-12-05 17:53:35 -0800

    Touching up stuff more and more.  Bigger bullet explosion when hits something.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:deacdca3bd0a8f21b69fee5b5ad0cc06b7b55bb471d1927ceccaa36b88bc3009
-size 38614
+oid sha256:d2895d1def2662e490b644c174230b3648c0cd7a49cd7092c729ac0f069752fb
+size 38637

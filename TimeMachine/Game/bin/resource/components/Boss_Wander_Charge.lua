4c168e97c9b8f8e2d7b65709aa8753e4c8ad066a
Brian Inkrot <binkrot@gmail.com>
2014-12-05 21:50:47 -0800

    Babi body tweaked, stunned from ice spike more, fixed Egg health bar.

--- TimeMachine/Game/bin/resource/components/Boss_Wander_Charge.lua
+++ TimeMachine/Game/bin/resource/components/Boss_Wander_Charge.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:166e1c538d780fe3b1e962307a930fd60097ef5ee203eb838911b5a590e97ee8
-size 15599
+oid sha256:60d1162624bc64397aac69c0ec84aa5d8fedc3d5f94366c33152b71b7e4b033d
+size 15653
--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3635ab4a77a29cb83b917a85dc5169e3f9dd811abeca32f2915c9b7871471db5
-size 39308
+oid sha256:1badda04f4f5667f6643c0a1f9d6a9246748f29311a748c74cf6552a1db9fd99
+size 39321

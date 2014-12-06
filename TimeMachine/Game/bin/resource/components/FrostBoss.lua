356bd571b190886ae7b20524bcb8e52b969748de
Michael Chambers <m.chambers@digipen.edu>
2014-12-05 20:51:35 -0800

    Pushing the listener behavior for crates becoming mobile and getting a timed lifetime.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e18bd9fe367eef1e4876a5127287a25c39ddd2810ee9598a06097929ab03102
-size 39218
+oid sha256:19ae36f07c5e1173e915eeae4acb4211b2f68ae962b717f4080f953de65b1f5a
+size 39506

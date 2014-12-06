8f87f76d3a16bdf9ce7dc946a89efa024069c5df
Cody Centers <murtag27@gmail.com>
2014-12-05 17:57:01 -0800

    I hope that i correctly re-worked the cinematics

--- TimeMachine/Game/src/GenericCurve.cpp
+++ TimeMachine/Game/src/GenericCurve.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03e2fc1d2e98297c6781502f4365b9a70f421f52b318a4d49a48cff7e211da34
-size 16759
+oid sha256:91c190ec706abaabba22a33b01e64fa2aa4e9a1e26070e0093be66a315e5d1a0
+size 16769

85243a1ce0fc3123e1d50eed4b38c60524ec73e1
Joe <joeybonaccorsi@yahoo.com>
2014-12-05 22:51:41 -0800

    Important things

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23cc92939ada175e1adab8b49a33484fd7d3d6ec5fd992817dd66b4086535a18
-size 39467
+oid sha256:ca604d6ec61de2787433b9eaf16d29b27f50d878815f190f270706285437b27a
+size 39421

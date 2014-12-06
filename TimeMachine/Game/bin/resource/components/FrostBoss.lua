f85c024014902bac0dc37a756a6d5461bfd96876
Brian Inkrot <binkrot@gmail.com>
2014-12-05 23:03:19 -0800

    Merge FrostBoss

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:206e18111b8fb577951e249a118bfbe702f42dd9f4c12d5d539aef56338eb357
-size 39769
+oid sha256:9bbafbd569da51af72d404b224b74bfc2d0336637c7d5a652c084efeb0dad7d2
+size 39977

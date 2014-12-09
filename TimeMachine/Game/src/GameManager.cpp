47f5e3ff1165e601c1f5ef9015cf15dd55fe37aa
Jake Shirley <navi@odinseye.org>
2014-12-09 12:47:22 -0800

    Fixed path loading for resources.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0e2205763434271c31478eba0e8663f7d10b4c5d68cfedaa975560d605a0681
-size 48229
+oid sha256:00e986b27d37b69a5685369ecdb04de047ff161f14cd191b67d4019decf329a9
+size 48255
--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2764ab5b8ff1c364c1f53356d414ed960a62cd9da6bd45395cee7fd9c54142aa
-size 43176
+oid sha256:f9b3813f9c843fcfe59ede4d76e5eb635182499897aaebf8ace292e7f85bdb52
+size 43289

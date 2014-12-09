55fb9d3f7c7b89fcea1ffd75061fa6a5baf5c6e8
Jake Shirley <navi@odinseye.org>
2014-12-09 13:01:25 -0800

    Spiffed up keyboard controller.

--- TimeMachine/Game/bin/resource/components/KeyboardController.lua
+++ TimeMachine/Game/bin/resource/components/KeyboardController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2883ece0946418046798a7bf83ed1729e76cb2605302dc54dc7e29693a6ad047
-size 4263
+oid sha256:e79b8a10076a8a83cd0491cf93466e7c0c54cd3bda9f66a53c60d042b46fa1e0
+size 4853

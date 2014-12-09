528abdfcddf1d5e2fb16291aa3cddffb30211700
Jake Shirley <navi@odinseye.org>
2014-12-09 15:49:27 -0800

    Tweaked values in FrostArena.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e29ff4038dce40568392643f426f94ae8d74d619d1ab738f2c330d4c50a24fc2
-size 39979
+oid sha256:5916a7010c911deeb89183e6f4ceeaf093979508103eead4272e7266aeb06c29
+size 39980

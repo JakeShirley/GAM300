f2a457f3c6c214b514516cdf124c2de9641530ce
Kai-Hwa Yao <kaihwayao@gmail.com>
2014-12-10 12:00:00 -0800

    Edited lighting final pass lighting accumulation. Undid all the graphics logic-side stuff I broke last night doing my project

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ebe60de84cbcdcc16850cb90c0a458b85c636a6efe58413a2ff0c043369950e
-size 71248
+oid sha256:3f66cda973b3c4c4dd53e2c22b99bc5274446a8fa1dc7fea72a2970cffb04f7f
+size 71057

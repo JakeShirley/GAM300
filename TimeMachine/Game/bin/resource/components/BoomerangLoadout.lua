0b957943205280870599ac7daeb92e8c71c38e37
Justin Merrill-Boyet <ecureuil21@gmail.com>
2014-12-14 21:23:48 -0800

    Pseudocoded loadout scripts

---
+++ TimeMachine/Game/bin/resource/components/BoomerangLoadout.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:985e39e0d343a6b3a573c9bfbc75289958a3628f4d55a4ffee4dbe1dd12c3ac5
+size 5397
--- TimeMachine/Game/bin/resource/components/LoadoutManager.lua
+++ TimeMachine/Game/bin/resource/components/LoadoutManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af2e3fb689388a5c825e822c10d242c570fd7a82000c93f47d81f18d2566ba82
-size 7548
+oid sha256:c4bb725444bbb30c006338816f312eab7b13ab33ef4ce9e488f068d1806e87ab
+size 7662

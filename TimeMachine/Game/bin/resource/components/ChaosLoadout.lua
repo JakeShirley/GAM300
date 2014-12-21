b913cbabaa5367f05a8db8d30dc875181bd61c3b
Justin Merrill-Boyet <ecureuil21@gmail.com>
2014-12-21 15:16:57 -0800

    The attack of the chaos loadout works (think Shadow Fiend!)

--- TimeMachine/Game/bin/resource/components/ChaosLoadout.lua
+++ TimeMachine/Game/bin/resource/components/ChaosLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27db6c35163a8495479635bbbc1c8fa9b92ad06c9243d7c880f2682e808910d6
-size 5585
+oid sha256:1c166326c42e71eec952b10e0c9bbc2c0678cfa56a75c67a4ee390bcd03fceda
+size 6792

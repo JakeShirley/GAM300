b07856350e4c1a3724aea9f8b9b4fd38c034f60d
Justin Merrill-Boyet <ecureuil21@gmail.com>
2014-12-23 15:31:45 -0800

    Merge stuff

--- TimeMachine/Game/bin/resource/components/LoadoutManager.lua
+++ TimeMachine/Game/bin/resource/components/LoadoutManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4bb725444bbb30c006338816f312eab7b13ab33ef4ce9e488f068d1806e87ab
-size 7662
+oid sha256:38649da069069838be400bf512fa2cce566a73a394b3b66d2411b9a20397a28d
+size 7660

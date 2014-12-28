42bd6aa408609b9e1743257e48a701d66836833f
Justin Merrill-Boyet <ecureuil21@gmail.com>
2014-12-28 15:44:32 -0800

    Increased the collision scale of the Chaos bullet, Chaos Hook drag position has an offset based on the target's scale, Chaos super ability is working, Boomerang super is mostly working

--- TimeMachine/Game/bin/resource/components/BoomerangLoadout.lua
+++ TimeMachine/Game/bin/resource/components/BoomerangLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84418ab9f719a873c471d39515e2a4d721689cf80edd10d6818a234d08ba592f
-size 7203
+oid sha256:17cc6592c1a67bbd049267849825a26e1c1060deead2f6334b4d31179cbec87a
+size 9306
--- TimeMachine/Game/bin/resource/components/LoadoutManager.lua
+++ TimeMachine/Game/bin/resource/components/LoadoutManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df401776b42ad723aa8107873f01f83ba8d08a5c92248bc097f0d51b94b16d8f
-size 7700
+oid sha256:0bd995702bd190f418122422ac7e674a7925b34a29fa48cb847099d00ebe9bf5
+size 7744

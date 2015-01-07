3fdea476a100bb4823634e14341649a95a35e491
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-01-06 19:08:32 -0800

    Melee loadout now has a sword "projectile" that's the same color as the player

--- TimeMachine/Game/bin/resource/components/MeleeLoadout.lua
+++ TimeMachine/Game/bin/resource/components/MeleeLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5a302a500fa459eb0dae8084541fc9c385dff1133a3bb92ee63d612a20ac008
-size 8462
+oid sha256:e4549d9d7ec081369e96bc9fc00990b77c4d5d34b06edaa83ad50877de48061f
+size 10377

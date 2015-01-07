147b4d7dabc603fe2b674a77f30c42569eda4507
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-01-06 20:50:55 -0800

    Added the super to Melee loadout

--- TimeMachine/Game/bin/resource/components/MeleeLoadout.lua
+++ TimeMachine/Game/bin/resource/components/MeleeLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4549d9d7ec081369e96bc9fc00990b77c4d5d34b06edaa83ad50877de48061f
-size 10377
+oid sha256:6099896f454412c88bdfb4e494ec876296235b6a35f0d4c3630de770d25dbd71
+size 12324

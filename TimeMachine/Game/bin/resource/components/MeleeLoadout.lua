bad44172681c58a5811263966f376aca27e255bc
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-01-06 17:42:49 -0800

    The attack in Melee loadout is a 3 hit combo (hits further into the combo do more damage; combo resets after last hit or if the player doesn't attack soon enough)

--- TimeMachine/Game/bin/resource/components/MeleeLoadout.lua
+++ TimeMachine/Game/bin/resource/components/MeleeLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18fdf224e41a5d5a4ed7058bc25847fa82ba0b40eba36ad85c46ad962f26c17b
-size 7115
+oid sha256:f5a302a500fa459eb0dae8084541fc9c385dff1133a3bb92ee63d612a20ac008
+size 8462

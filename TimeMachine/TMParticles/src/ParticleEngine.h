1b0109a93c61c852899dd844d9e1332339638a30
Cody Centers <murtag27@gmail.com>
2015-01-08 21:55:20 -0800

    ParticleEngine.h clean up

--- TimeMachine/TMParticles/src/ParticleEngine.h
+++ TimeMachine/TMParticles/src/ParticleEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f0266f94e61e1b12b05596a84f7ff67d568e7f567c81449baf0e0020f343c36
-size 9344
+oid sha256:ca3efc61d2f5421cc03c45ad5ad3194cd0b656291579bc4ecd821207c394ffec
+size 11029

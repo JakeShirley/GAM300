e11416d99152a6a5f79ca0d9450846136d6d3fcf
Jake Shirley <navi@odinseye.org>
2015-01-10 16:00:41 -0800

    Made SetLevelInEditor return if it succeeds or not.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:698368467ebadc32224fab81005c80fca0b708ff175a7c9e52a2414dc88476f5
-size 49856
+oid sha256:702774d5ca02d2273f174b7484bb67c71e3f9471ddde57cff0232d114162e933
+size 49886

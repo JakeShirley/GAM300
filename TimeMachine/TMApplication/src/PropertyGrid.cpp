2db96e1360b65bd01e3347ece2f38d572fb3d320
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-01-10 18:28:23 -0800

    Tried to fix a memory leak in particle editor.

--- TimeMachine/TMApplication/src/PropertyGrid.cpp
+++ TimeMachine/TMApplication/src/PropertyGrid.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e64bc76e7e411765f03e3b803fed59626dc84fcbcf3e897ae3b218646305d51a
-size 5921
+oid sha256:1c5cedd9856ea643a0bf1140052c874312ca8b5afae302845d4e3421dc24639c
+size 6514

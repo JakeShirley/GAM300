55db742ebc0ce69bc954bf0e951b17ecf4ea3537
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-01-13 13:29:22 -0800

    Moved all constraint allocations to PhysicsSpace where I can later make them contiguous in memory.

--- TimeMachine/Game/src/ConstraintHandle.h
+++ TimeMachine/Game/src/ConstraintHandle.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:266604b9d34633d62eb2d8fd077048be86a7b5d96ed544faa20dba024ad583d4
-size 5607
+oid sha256:94d82a1a3e5eb468d3d13bfc458d3087376fd1a270ee6628147b4b1308f137ad
+size 5513

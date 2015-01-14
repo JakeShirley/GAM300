20fcabd855820f4e3a76f8fdf2d33b6166873a22
Jake Shirley <navi@odinseye.org>
2015-01-13 16:08:02 -0800

    Updated RodConstraint to use GameObject picker.

--- TimeMachine/Game/src/IConstraint.h
+++ TimeMachine/Game/src/IConstraint.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11663e3faa77895beed2424f51f256593537025e220454a24af797950ed2fa91
-size 503
+oid sha256:cf20482a0b0ec09f3ddd3e0b7a5d3a37d6255f327652d875515b419b34d8028f
+size 535

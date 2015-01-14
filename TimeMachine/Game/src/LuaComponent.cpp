e9b44cf7ed83361849aa0cb6aeb81f5e7ef62a67
Jake Shirley <navi@odinseye.org>
2015-01-13 23:35:47 -0800

    Fixed warning in LuaComponent.

--- TimeMachine/Game/src/LuaComponent.cpp
+++ TimeMachine/Game/src/LuaComponent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e48591fa4d548f82be6e1c01398c2a486782df045a59feaebbcb2d8f57d00b70
-size 22034
+oid sha256:5669d11ef2c40ba6b9963fd91f077482c5e54b5b922b694a13ed5ef1ac391a9b
+size 22033

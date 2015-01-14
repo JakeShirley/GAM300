dad659cdadcfa85d957b6123a40a3067b3f3dfa0
Brian Inkrot <binkrot@gmail.com>
2015-01-13 22:04:53 -0800

    Added proper error handling for Lua script loading/reloading.

--- TimeMachine/Game/src/LuaComponent.cpp
+++ TimeMachine/Game/src/LuaComponent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c62b52996c4b2d951146f3d452aea62ae802a72b52f3a5ef2c5b88626628526
-size 21887
+oid sha256:e48591fa4d548f82be6e1c01398c2a486782df045a59feaebbcb2d8f57d00b70
+size 22034

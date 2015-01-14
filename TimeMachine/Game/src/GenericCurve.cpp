8d07e77af12d31d0d75cf854ae34612c76af906a
Cody Centers <murtag27@gmail.com>
2015-01-13 22:06:24 -0800

    Cinematic Camera and the Generic Curve track components now work fully.

--- TimeMachine/Game/src/GenericCurve.cpp
+++ TimeMachine/Game/src/GenericCurve.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91c190ec706abaabba22a33b01e64fa2aa4e9a1e26070e0093be66a315e5d1a0
-size 16769
+oid sha256:3bcebafe6f7f4bc3ae8c12e714c13b5e42f4494ff76d296c4dedee9f0665cf96
+size 17200
--- TimeMachine/Game/src/GenericCurve.h
+++ TimeMachine/Game/src/GenericCurve.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfc5145ce15a2c46109f076890b64a9a137273be9ed9f83ad2658d7756512187
-size 3010
+oid sha256:592eacbe139b62ce5ea0753e4f36390249fcde5fa9272a18cc333a93807eb8e8
+size 2984

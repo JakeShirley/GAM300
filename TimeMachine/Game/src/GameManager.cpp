79046c966d0e9488a40d7f6b45d54352dfc9a632
Brian Inkrot <binkrot@gmail.com>
2015-01-13 22:14:06 -0800

    Added check to only load .lua files in the script folder.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef29f5669fa3d5419a26d4fff0223ccf37d799535a3c212dc3df48733c5a74c1
-size 50086
+oid sha256:5ee42dccaf1fd18415f39bd87882e7f61e0175bd1186ee7ac650474155964426
+size 50165

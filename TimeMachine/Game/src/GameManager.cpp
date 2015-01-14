5c5e77f909ed2a9a3dbae966ca6ac2643095c9a8
Jake Shirley <navi@odinseye.org>
2015-01-14 14:29:14 -0800

    Made script creation automatically open the script files.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ee42dccaf1fd18415f39bd87882e7f61e0175bd1186ee7ac650474155964426
-size 50165
+oid sha256:168e2f3342846c3d8ee73ec48142e0ac8ed4c59891da0444e2ab6eef7e04a1de
+size 50270

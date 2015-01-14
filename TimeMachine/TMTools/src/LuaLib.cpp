65454c15ede8d27d5f9679a423f64839534358d1
Brian Inkrot <binkrot@gmail.com>
2015-01-13 22:15:08 -0800

    Removed Lua memory output.

--- TimeMachine/TMTools/src/LuaLib.cpp
+++ TimeMachine/TMTools/src/LuaLib.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8604416fb6c0e376783d3eaf95ebb6d172b3a46134f97a7c1941a8375b9c1ba
-size 19217
+oid sha256:88a5fd7856806f956371afc5c3bae2b27fdcb009472a2c58ee9a0e300ab72603
+size 19246

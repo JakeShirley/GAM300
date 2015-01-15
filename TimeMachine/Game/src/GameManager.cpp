45808e08971a6c214d60db2af412bb0351553b22
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-01-15 12:37:09 -0800

    Fixed an event connection leak in GameManager.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a21b3bd77ea19a6db46cb6f113de45dc7faac810b55e764b0018f15c112e7676
+oid sha256:c6b59bb87a9ad91f0328d0f63ff15d0344825e2ccdb735902fac4efb226c8fe0
 size 50324

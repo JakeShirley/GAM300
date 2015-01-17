15fa22d634837c4cf66a5c4b80cf68fb8f768d0f
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-01-17 13:13:57 -0800

    Made the player slower while holding the attack in the chaos loadout

--- TimeMachine/Game/bin/resource/components/ChaosLoadout.lua
+++ TimeMachine/Game/bin/resource/components/ChaosLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f51024acd9f410cc4027427eec0f93d76044d940fcfba23faed7116264798e18
-size 12742
+oid sha256:0afa2a011901f82c5ce92b5f22ab8957482120380188362b182b12564dcdccf3
+size 12738

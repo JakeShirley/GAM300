682a7831569e0bbb7977517b69d4f915bc03da5c
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-01-16 20:41:44 -0800

    Fixed header file name

--- TimeMachine/TMGraphics/src/JAG_Instancer.h
+++ TimeMachine/TMGraphics/src/JAG_Instancer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6e9674ef6f8845cd8688c01dd466d6db5e41e00c63b863f4e806452c1ec6790
-size 6753
+oid sha256:f2a43cbb68d9e748ad29cb60c608ba20129537b8fed1f2618a8870454abf710c
+size 6752

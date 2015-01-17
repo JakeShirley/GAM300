a1de0224d6df1fe0a7988938c21dd9381ddbc457
Jake Shirley <navi@odinseye.org>
2015-01-17 14:26:32 -0800

    Fix for Joey's screwed up Oops::Math

--- TimeMachine/TMPhysics/src/OopsMath.cpp
+++ TimeMachine/TMPhysics/src/OopsMath.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d04266456548de64a6f57fc9e202576049ed64350842a76baa72d4f4743c5dea
-size 21402
+oid sha256:3f7d77333373bdefbc122459438c60b0fd5bf21a3e593b523bf8c0536939b0f2
+size 20519

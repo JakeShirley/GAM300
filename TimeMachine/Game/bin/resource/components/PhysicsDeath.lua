3d2c3b6e58d36f06ef03eb1bcd29dea70edf0cad
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-01-17 22:23:43 -0800

    Expanded physics death to accommodate breaking frost nova projectiles.

--- TimeMachine/Game/bin/resource/components/PhysicsDeath.lua
+++ TimeMachine/Game/bin/resource/components/PhysicsDeath.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3bf13f5e8b394f7ccadc6eedbc6c33248c30571f0186430a6404005ed0d4aef
-size 10364
+oid sha256:5a7b4dd153680ffc66c0ee58e85b14270c15e0e5af3ce60f9f21e0839e69740d
+size 10968

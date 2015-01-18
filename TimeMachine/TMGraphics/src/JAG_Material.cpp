61e1fb7b4fdc87b404c7e5b75fd9d78c16fca9d0
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-01-17 19:41:38 -0800

    Commented out test code, Added explicit function for enabling a Material property

--- TimeMachine/TMGraphics/src/JAG_Material.cpp
+++ TimeMachine/TMGraphics/src/JAG_Material.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86e24388ff802dffc75852bf09818bb3c8216b08ebeb6f60b3fc5c4eaf348965
-size 10191
+oid sha256:7bbcdaa517bb989f95c2c60c118e390a29f60f0a10ce93667234611f016b003f
+size 10368
--- TimeMachine/TMGraphics/src/JAG_Material.h
+++ TimeMachine/TMGraphics/src/JAG_Material.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:361ebd1ac534f5b4f2d848939eaad1e31750ed82513fca03aa46f510f1dd2b7f
-size 2554
+oid sha256:d59335dbb8cb53e1339385ce6cf3656114035f98f281808a7135da32e8e672cd
+size 2678

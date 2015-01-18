501c49a14cefe1162a4cad5944fb02531cf55c69
Michael Chambers <m.chambers@digipen.edu>
2015-01-17 17:49:32 -0800

    Made cheat codes not crash when players 3 and 4 weren't there.

--- TimeMachine/Game/bin/resource/components/CheatCodes.lua
+++ TimeMachine/Game/bin/resource/components/CheatCodes.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:249b0fcdf42cd5712d2bac95c65ab8ce5d2c6a68cec6052440c93cb6ab3c20f8
-size 6010
+oid sha256:04c988a876f01adae08e1aa0aafbca3511198d987edec70a5714d318d3e99168
+size 6077

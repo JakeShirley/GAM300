3012de5c21a3e4088820020d0656ae63c5916c0b
Jake Shirley <navi@odinseye.org>
2015-01-17 16:35:34 -0800

    Fixed erroneous behavior with JAG::Text if the first text object is hidden.

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27759ed31a38acb12a27c62f7083a3e6fc90b6460056fe69bfc825378df0b977
-size 72998
+oid sha256:e276807aecbdd369f95d6e39a0ce3d4c487a3ed5babd14c67bc805fe7f2bdb92
+size 73086

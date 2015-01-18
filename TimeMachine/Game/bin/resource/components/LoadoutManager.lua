664f44c7fcfbde35f515f47ba3831ca16f9d123f
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-01-17 16:58:16 -0800

    Basic button press (X button) loadout swapping. Created an archetype for each loadout type for pickups

--- TimeMachine/Game/bin/resource/components/LoadoutManager.lua
+++ TimeMachine/Game/bin/resource/components/LoadoutManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12ac30ec93b4bc65c80843061d77d64ce11a3bcf60d04629d5ee31dcceb34504
-size 7837
+oid sha256:0cee26bca6538b72862b3511b37de40da6d1996ff8fd5e245fae7619a11694d5
+size 9167

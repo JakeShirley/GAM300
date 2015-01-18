c8f3d9a065df2c7806d5e5d656c4f04e2280242c
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-01-18 01:36:45 -0800

    Ice block breaks.

--- TimeMachine/Game/bin/resource/components/IceBlock.lua
+++ TimeMachine/Game/bin/resource/components/IceBlock.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6d9a00a2a583b16409e3f55fea1eb3b4034f8638899428a05ff100810e2a4d6
-size 3046
+oid sha256:79742230ae16b43b43e2bdedebea344c6bcfa556cd781ca4d31c3dee30e48bf2
+size 3166

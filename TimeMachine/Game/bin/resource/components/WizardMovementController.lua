7bcff9c461c260169cd9b10249d72ed086a67363
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-01-17 22:19:56 -0800

    Changed everything in player controller that would cause him to ignore gravity.

--- TimeMachine/Game/bin/resource/components/WizardMovementController.lua
+++ TimeMachine/Game/bin/resource/components/WizardMovementController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f0985e7eee1946a6d0e15d0d3391ea5e5ca6ae15b757fbde29a8617b264cd36
-size 8398
+oid sha256:b0c7dea3eb321c133b869d5b56fd3a9c1bdaf70310ec9fea28fceb027bd569ab
+size 8547

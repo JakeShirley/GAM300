4b442218fd3a1d7535cb8fb37213af3c80124b2b
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-01-17 22:21:43 -0800

    Actually fixed the negative scale from Collider::GetExtents that I thought I did before. Made gravity serialize properly. Fixed a bug causing setting ghost to true on a convex hull to make its mass infinite.

--- TimeMachine/Game/src/RigidBody.cpp
+++ TimeMachine/Game/src/RigidBody.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4490ac749cbd2cfb6f5af760958e7afdfab74a663cc6c851421eca5bd405bbe
-size 15222
+oid sha256:1ae3856f138678b7f837c475faf90977808ec2dc82cd9cd5708ced58aa1e07a5
+size 15257

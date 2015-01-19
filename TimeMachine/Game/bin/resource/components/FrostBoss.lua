9561f8d810989cbc7621d7cbe82a05c82109fe1d
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-01-18 19:03:23 -0800

    Put timed lifetime on Blizzard v2.0 and corrected some weird bug making it spawn counterclockwise

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04f7e8f3c5b1d0c2b297dd1629a44776b52479530c5ed51bebf75e0511f4c1b5
-size 42071
+oid sha256:e7d9ea0230df75cb4f6ec33a8f9f12093b778ed93f059a4171df0a62888e6a7f
+size 42075

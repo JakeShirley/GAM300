a155842213f9440db2373edbb0e2ef34230c109a
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-01-19 18:00:53 -0800

    Fixed revive bubble positioning bug

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cc4351bc0058e4c37455b9fdbd1565a177bb3bc7fc414857e1039f1c2b63e52
-size 43450
+oid sha256:fb056977d87bd1fb73d0c7826d575186b4113f43f7626d2b51df972f6137c8ba
+size 43445

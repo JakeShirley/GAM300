cb4d15596759eeaeb85a27bc301a38bcaff6edad
Jake Shirley <navi@odinseye.org>
2015-01-20 17:45:52 -0800

    Remove logging on start up loading graphics files.

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff63266909f5ca9b91c52e753f5d627da708df6d8ac0ae1061aaa12c63773dbf
-size 73495
+oid sha256:178d2bdb1c80ab16ab911a620f9662f1b1ea848b0a3c3977c729ba61e8d9e647
+size 73371

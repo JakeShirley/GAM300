ea0179fed09b110f0dcbe93501f2784f64fd09a8
Garrett Allen <garrett.allen@digipen.edu>
2015-01-20 17:54:57 -0800

    quick update to materials. We now generate .jmat only for Graphics Materials. Also a quick fix to me saving when I shouldn't on load.

--- TimeMachine/TMGraphics/src/JAG_Material.cpp
+++ TimeMachine/TMGraphics/src/JAG_Material.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc1e2aee08418eeb3a440bcd7190a700f31837af86b20a66fd6f618e055b716b
-size 10362
+oid sha256:03f3c8380e0e229ce32f4c6c6d339bdc8b49b75d60b7e49d57c19623a116a162
+size 10372

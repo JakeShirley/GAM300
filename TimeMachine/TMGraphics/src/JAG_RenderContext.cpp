de30cfd92a79545666affd13f6a7bd3f8fff8931
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-01-20 17:55:44 -0800

    Fixed conditional expression constant warning

--- TimeMachine/TMGraphics/src/JAG_RenderContext.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderContext.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5429cc9f49f6bece83ed410f3cedee9844839211b92a8024202de5380d848ae4
-size 100685
+oid sha256:ff5b95457feaa97a4d9d69b5c132b97056fc580f5457cd43226eb38d4f3301ee
+size 100674

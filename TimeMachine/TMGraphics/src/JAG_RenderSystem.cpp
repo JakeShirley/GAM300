1b98e475031b5fec97dcf1711c70725d63c5bee7
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-01-21 20:28:14 -0800

    Fixed shadow toggle. Instancing works for non-shadow pass. Modified threshold pass to calculate threshold in gamma space

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:516fe763943f09edc5a7dd86ca0ff6d5f603d5b2e681ac3d219f5395efa6e1db
-size 75994
+oid sha256:82d4360abf1e144e10eff6f469db449dd3e26e2ceb757bd04b4dc05d3d73f5b5
+size 76341

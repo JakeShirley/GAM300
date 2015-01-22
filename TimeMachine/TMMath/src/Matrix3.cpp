d5050262f7558ceb05dcecac54b358bf32a4fd40
Cody Centers <murtag27@gmail.com>
2015-01-22 14:24:44 -0800

    Fixed a behavior, and fixed a function in mat3

--- TimeMachine/TMMath/src/Matrix3.cpp
+++ TimeMachine/TMMath/src/Matrix3.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9002a6a762b4ca65a6f77dcf0b69e61cf1722282040c49d1b0b3871e5445b7ff
-size 10809
+oid sha256:a1e97205c6bfe46824a732485dbac1653bf41306ad90a7f3c5cdcb59b6ad118b
+size 11020

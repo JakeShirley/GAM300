058619025803f532204d4a9bb75c9887d1349cfc
Jake Shirley <navi@odinseye.org>
2015-01-22 18:35:56 -0800

    Fixed crash bug with Documentation window.

--- TimeMachine/TMApplication/src/DocumentationWindow.cpp
+++ TimeMachine/TMApplication/src/DocumentationWindow.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f1c847256118c4117f3dc932dd5181e08baf1cfedf6ef6c4bf405919b4f1da4
-size 3764
+oid sha256:ca954ad2b2ba5a677466fc731e968fcc2dfd31fdd87fe6c4bba9f49e605db04f
+size 3480
--- TimeMachine/TMApplication/src/EditorWindow.h
+++ TimeMachine/TMApplication/src/EditorWindow.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30b6db6e2bf113d0650ae721284195084e1a74ded423ccb02430da31a9232a83
-size 2898
+oid sha256:67447b2e62ba31682fa80472044748a3e1b912ca250aefd53e3b928ab243ab90
+size 3070

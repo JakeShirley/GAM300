9d2292c1dcc18c56c84581f80d0685ce59b0f666
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-01-22 23:15:37 -0800

    Working on changes for boomerang loadout, had to commit because of pull conflicts

--- TimeMachine/Game/bin/resource/components/BoomerangLoadout.lua
+++ TimeMachine/Game/bin/resource/components/BoomerangLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94bcf0b8f918332a6acbf9d2ca5900097d58c9d8f5ac4b049f970a02464ef684
-size 11423
+oid sha256:5fe922a917346126e1227127c35299b7a32b50e2717c7af8113df98b009ffcc7
+size 12389

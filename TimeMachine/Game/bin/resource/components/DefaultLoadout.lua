23e2735429887cb0524c4ef2ec10c731280229fd
Michael Chambers <m.chambers@digipen.edu>
2015-01-22 23:39:34 -0800

    Made it even better.

--- TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d86cd8e98c7e4ff56c9adbd2e18c0ef34ace07d5d75a21a923c20a8834202bad
-size 17751
+oid sha256:e083b99bd6970e9dbcc5ea7766c5be3ad3c10a646a6416da755bdf45a6d2f12e
+size 17173

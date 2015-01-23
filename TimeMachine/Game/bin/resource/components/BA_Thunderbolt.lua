085e5334c6cdd41250e6b33b009e034488c9b0b1
Brian Inkrot <binkrot@gmail.com>
2015-01-23 13:11:47 -0800

    Working on Avalanche

--- TimeMachine/Game/bin/resource/components/BA_Thunderbolt.lua
+++ TimeMachine/Game/bin/resource/components/BA_Thunderbolt.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfd7dd489e7bdd05ead84ea0606dd14363673d605a02427729f755ca24877022
-size 4054
+oid sha256:1580adc755330fd4e16cd874c8f44dac76d93af0652ce303454a63a52f09ce29
+size 4060

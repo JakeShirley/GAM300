3673100dbe99c670792cad8ee0e3d87fc3a69805
Cody Centers <murtag27@gmail.com>
2015-01-22 16:05:28 -0800

    Added the ability to turn on/off debug draw on curves/cinematics

--- TimeMachine/Game/src/CinematicCamera.cpp
+++ TimeMachine/Game/src/CinematicCamera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be4cc1ec56601d0b9ede8e03648fea35d706fa9064a901323a041dd28661ccac
-size 26764
+oid sha256:4ff74852acd8d5edaf6d21766ade26f337cb8b44b620032c5889c91e87915d02
+size 26778

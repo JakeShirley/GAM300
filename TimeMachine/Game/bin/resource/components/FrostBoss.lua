c02828cfde56b548d2b130b542258b6e6fe47d21
Brian Inkrot <binkrot@gmail.com>
2015-01-22 19:45:57 -0800

    Tweak to super, added a roar to Notsa Phiron's wakeup.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c751acc9e06c53546bfa014387e445b13e298d5d68de66f6984dca6c4c9f5f5e
-size 42529
+oid sha256:0000b9d5dc2a858578c0e03e491e69dd02209ceb25d5b77ccab518588127eadd
+size 42624

0cbb541ab6bc7affb1e36683809c191d32472aa9
Jake Shirley <navi@odinseye.org>
2015-01-22 17:42:33 -0800

    Make fade values go from 0 to 1.

--- TimeMachine/Game/src/LuaEngine.cpp
+++ TimeMachine/Game/src/LuaEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e3789fa50da837c2bd4937de1e4506d058d2b55355e5f5a0c36a3fce1839f6e
-size 9158
+oid sha256:0b8a337ba2d89ab8a4d00af83a39e3d8eef87c9319302624ee6e2c8d5f8f3077
+size 9170

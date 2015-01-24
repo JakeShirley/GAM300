f18dc694e6c61811f47293e6c755cf6cccc91b9d
Jake Shirley <navi@odinseye.org>
2015-01-23 21:30:09 -0800

    Default SpriteText to white.

--- TimeMachine/Game/src/SpriteText.cpp
+++ TimeMachine/Game/src/SpriteText.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f67cd450078ddc4b77da1e6aa1c31408be58150578d869b07ef3848cd29a7a6
-size 5502
+oid sha256:2c26dc9348305dd5e64730b00c979adb57ac6d1010d6071a1cc1e96dffb870c6
+size 5528

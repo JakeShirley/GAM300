8156f873a749329ee4d63c0f942ce7cb65fa28b0
Jake Shirley <navi@odinseye.org>
2015-01-23 20:52:27 -0800

    Added debug break for allocations of a certain size in main.

--- TimeMachine/Game/main.cpp
+++ TimeMachine/Game/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e44e5cb439c7b88b0aa45d2e3367a1bc0f5c721f4e51ea43d0a2004e5114ea43
-size 1512
+oid sha256:4f478125b152969fa2b2642dd43c5e8887bf0f4ad10c9a6d6409d2e93d7457a1
+size 1731

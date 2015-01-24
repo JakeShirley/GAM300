2ee3d75a4646f3cc7ccdbbf39099d9910f3794f7
Jake Shirley <navi@odinseye.org>
2015-01-23 20:55:37 -0800

    Updated defaults for SpriteText component.

--- TimeMachine/Game/src/SpriteText.cpp
+++ TimeMachine/Game/src/SpriteText.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc296ed770c0bb27fcd297596c359f81ebdfe27a04c14c98bf93e90997832981
-size 5495
+oid sha256:6f67cd450078ddc4b77da1e6aa1c31408be58150578d869b07ef3848cd29a7a6
+size 5502

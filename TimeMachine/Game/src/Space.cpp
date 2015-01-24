201fd6e330dd01fad62b28490630fe2339c216c5
Cody Centers <murtag27@gmail.com>
2015-01-24 15:04:58 -0800

    Fix for crash on exit from editor space.

--- TimeMachine/Game/src/Space.cpp
+++ TimeMachine/Game/src/Space.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b5dc727df285deb18559b62c7fe800a0d3636c78288242a3665bf768593abff
-size 14711
+oid sha256:71d2b5ea2ec9b5190982bc3387e9dc898aae0b648fef5b9f617953ba157bf9d0
+size 14712

45a1e4882ba3bcad950a8301f85fcfb26b4e1f01
Jake Shirley <navi@odinseye.org>
2015-01-23 16:23:59 -0800

    Made Level uninitialize be called before GameObjects are cleared.

--- TimeMachine/Game/src/Space.cpp
+++ TimeMachine/Game/src/Space.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e1f8d438c6e59684dcf142cb1db7e642557c75b4cc6e1aea122f87127ea2ad4
+oid sha256:7b409fa00f2d43f5ec4b799385f985ab60118db7584731ba6db1176d1402981b
 size 14625

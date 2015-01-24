1aa64a83e7fd31f97cc875758ffeb39d89227a7d
Michael Chambers <m.chambers@digipen.edu>
2015-01-23 16:14:42 -0800

    Made the music loop instead of not.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25193bb112179cb92e271cb81fc674090b14c12e380c0aa183222eb616aadbe5
-size 42884
+oid sha256:8662b9b661771502c350df72e54133d96efb0b2875ff8e10b00e9539c48cd2a1
+size 42974

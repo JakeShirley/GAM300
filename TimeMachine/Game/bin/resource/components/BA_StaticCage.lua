ca444b0a55cf62687a5571beeb316d3f561e8fee
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-01-23 18:36:01 -0800

    Created a BossTemplate script for new bosses that will register phases and chains of abilities with the BossAbilityManager

--- TimeMachine/Game/bin/resource/components/BA_StaticCage.lua
+++ TimeMachine/Game/bin/resource/components/BA_StaticCage.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5a142149c55c49c9ff23f4cb447743ec481cdd0f33b17bd6915cf10cb157612
-size 4779
+oid sha256:ddad9d601721b0f593f9517f37607a358a7c423a3ee55575c71eaae5b9b2afce
+size 4720

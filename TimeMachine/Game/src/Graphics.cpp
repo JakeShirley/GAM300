b8af3b801fb8514b70eeec6216efb7af2762dd15
Jake Shirley <navi@odinseye.org>
2015-01-23 16:23:36 -0800

    Removed a few focus game calls to hopefully make the editor responses less janky.

--- TimeMachine/Game/src/Graphics.cpp
+++ TimeMachine/Game/src/Graphics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c265743c843fcc50acf2fab2bf17f33d055ded9134ad29cc7304fc6689085b93
-size 13037
+oid sha256:2364964d61546ab493d0d9c8b28370f40348b969e1a5a8e6c1b53d5ce88a6c07
+size 12737

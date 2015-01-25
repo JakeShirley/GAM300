abb2bc984de4e3bcf8458d555061baccdb6798a9
Michael Chambers <m.chambers@digipen.edu>
2015-01-24 18:36:35 -0800

    AIs dash if theyre far from the safety zone.

--- TimeMachine/Game/bin/resource/components/AI_WizardMover.lua
+++ TimeMachine/Game/bin/resource/components/AI_WizardMover.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:981c0583f0cfbbbf4bb2933876cd639a70f96fcf44a19c12dc1a941e4e093f59
-size 8539
+oid sha256:85b5a375a0f05bbede53315d3b4020b628166b6f0d412007d6872867b2f07d38
+size 8857

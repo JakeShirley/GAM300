998890e0a9a406db137904749e1e9399710c743c
garrett.allen <allengar22@gmail.com>
2015-01-24 17:57:52 -0800

    fixed a bug I caused with script opening

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96773dcebfd2b6be9a9c2ca29549779c57093c98d0232161f4b7d41203f10e5c
-size 52535
+oid sha256:e183976fd2e9f92e7fa6c06dbff352d0bc8178fc15cd76094364d6b63d913d1a
+size 52564
--- TimeMachine/Game/src/ScriptResource.cpp
+++ TimeMachine/Game/src/ScriptResource.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d47c117d45511693d091e3804aab3a965a96ad6d63487b7a716dd1e181c8a829
-size 672
+oid sha256:2fdf7900872e744e8d96eecee58085545f4f206e1d8cbbfdaf6d291734d51aad
+size 699

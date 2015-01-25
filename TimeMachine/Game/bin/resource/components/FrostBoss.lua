f1ee1216f493858a4bccba46dd30d227fb0e625f
Cody Centers <murtag27@gmail.com>
2015-01-24 17:15:34 -0800

    Fix to cinematic camera too

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00936f1dc85e633f09e5747ca217b2cd224a64e53cb291a51bd745e4245878f7
-size 44098
+oid sha256:cb1083a993aa44060f93471ad6b85f391e9a124ccda5b49268a7c4a7a5f2ea61
+size 44102

900901e69b24730c426a38149a5d5f30b9d66e75
Cody Centers <murtag27@gmail.com>
2015-01-24 20:44:32 -0800

    Fixed already existing bezier curve using components

--- TimeMachine/Game/src/CinematicCamera.cpp
+++ TimeMachine/Game/src/CinematicCamera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acc982d193acb3c6f49260f0d594a922eddedb7e06d2acdac8ce1a0c681cc0fe
-size 28242
+oid sha256:2d69570c979bb26bc13c88c5f7599e3294d2a6c9a0dc6d1baa407d1ba7699e71
+size 28287

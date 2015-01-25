bad3880b654726049330b4eba4a26dce31c4ba9e
Jake Shirley <navi@odinseye.org>
2015-01-25 14:15:30 -0800

    Fixed LuaLib random seeding on startup.

--- TimeMachine/TMTools/src/LuaLib.cpp
+++ TimeMachine/TMTools/src/LuaLib.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:419943dacc3e8baa25958ad7ae5364cc41bdf5aa5e836b4e845e4d5d1bec574d
+oid sha256:d927710536a4e9b111f2aa752436c51ba02a9fbd9c4bbecb844109ae8da76d0f
 size 20358

374ceb111921bd2981a939578777be8b5f83ea35
Jake Shirley <navi@odinseye.org>
2015-01-24 18:37:29 -0800

    Added awesome seaguls!

--- TimeMachine/Game/bin/resource/components/SeaguleMover.lua
+++ TimeMachine/Game/bin/resource/components/SeaguleMover.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5aaeb2365713ebe86e8a07004aeb9e1679fde75665c44670a7bbc2797c270924
-size 1812
+oid sha256:a55f797766117684199c32e249a4dfd12dbd0453f26987425a7f2d7e78a4ea8f
+size 2075

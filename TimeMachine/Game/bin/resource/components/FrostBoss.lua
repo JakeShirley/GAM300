b45bf370243f0a9e9cf71772b8f1a2faf071af65
Michael Chambers <m.chambers@digipen.edu>
2015-01-25 14:06:38 -0800

    Casued a bug with Assassinate to not crash. Origin of bug to be investigated.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e96bd1219eb28c18ae83a1571d42137f8cb390a691199039b8736038a6ea567
-size 44613
+oid sha256:f69cd2b15da417b3ab70246b5a310ed0f50537d44eb795c286edad9ce381ceb0
+size 44766

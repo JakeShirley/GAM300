e277cfa08d47585127e6f41338716936d5087804
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-01-24 16:10:26 -0800

    Loadouts come out of the boss when she dies

--- TimeMachine/Game/bin/resource/components/BoomerangLoadout.lua
+++ TimeMachine/Game/bin/resource/components/BoomerangLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba415eeadd15b71cc76bf54f04d29b4c1a5692e9fe9fabd516d71576f7dee48e
-size 12312
+oid sha256:662efb3046b5a60d128859aa7181f1214c0ffe542c01ffb73996870e3bc9275f
+size 12767

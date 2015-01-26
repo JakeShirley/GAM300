4e51d963e0206b0d5a8fee3f0a678281e1cfa632
Michael Chambers <m.chambers@digipen.edu>
2015-01-25 17:31:46 -0800

    Made healing not kill you.  Yeah that happened.

--- TimeMachine/Game/bin/resource/components/FloatingCombatText.lua
+++ TimeMachine/Game/bin/resource/components/FloatingCombatText.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:105c1b7d7f5e13ade1a69a97ced287c56c52efebfa6309a80c75ffec987496f4
-size 3979
+oid sha256:6d8a58fc4dce40c4a263df603a03d6303996f77c0584a6a509f6874d74b08789
+size 3943

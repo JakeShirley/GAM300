46826a5c218268da190f20da1bdbd524dd7882e2
Michael Chambers <m.chambers@digipen.edu>
2015-01-26 14:25:29 -0800

    Correctly cleaning up my allocated SoundEvents.

--- TimeMachine/TMAudio/src/SoundManager.cpp
+++ TimeMachine/TMAudio/src/SoundManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7814f3007412becc34b3c5cd850fc98c2dd6a00dd54798e3b409cc9d12852232
-size 7904
+oid sha256:96ae0b577b6b4e4185d8b4dd19d0e6ee139587ae8afbc36e5be93d750d256ea9
+size 7986

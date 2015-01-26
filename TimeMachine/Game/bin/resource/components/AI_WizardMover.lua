c90320fafb9370ec8145483a26891490449e0aa2
Michael Chambers <m.chambers@digipen.edu>
2015-01-25 17:57:45 -0800

    Made it so Frost_AI stropped using the deprecated Boss_GenericBehaviors targetting abilities.

--- TimeMachine/Game/bin/resource/components/AI_WizardMover.lua
+++ TimeMachine/Game/bin/resource/components/AI_WizardMover.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:287dcbb1078dc24b235ffecc69b792fd91e364dbbcfff9a2cb65cf54fb514340
-size 9941
+oid sha256:01ff118b56ef7c4fa0264b2cd13e43db09231bf26dbeab8c0a5dc9415265ee58
+size 9944

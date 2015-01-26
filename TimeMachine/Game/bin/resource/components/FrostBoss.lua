1cab96a3be6f0719254b3cdbb36689348da17b10
Cody Centers <murtag27@gmail.com>
2015-01-26 14:45:27 -0800

    Fixed boss breath logic.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e549ba2a5ac43d3f4d821012a6f89dee80edcf6014ae6d63630ade5aa91e348
-size 48037
+oid sha256:34bd709ef3c4e0d37d274f51ba15d0be0213f0d64d13afb0f322331d221b761d
+size 47453

6a21c1ff16cdbf6236e6f06ed57c9af8ad122f28
Jake Shirley <navi@odinseye.org>
2015-01-27 14:51:52 -0800

    Updated seeding of Lua random.

--- TimeMachine/TMTools/src/LuaLib.cpp
+++ TimeMachine/TMTools/src/LuaLib.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d927710536a4e9b111f2aa752436c51ba02a9fbd9c4bbecb844109ae8da76d0f
-size 20358
+oid sha256:112e4cf48539b160f8c023f5ffc6f188e5580200413877b2d1e60d59a9220e69
+size 20342

9205dc7f9afa21847805b2d620771498d90760b2
Jake Shirley <navi@odinseye.org>
2015-01-27 20:17:41 -0800

    Changed documentation to produce files for primitives that have methods defined.

--- TimeMachine/TMTools/src/TypeInfo.cpp
+++ TimeMachine/TMTools/src/TypeInfo.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71b54b1681bcaeb9f341a3910e1f01547caf3092ce4540e507fd5985aa62c05b
-size 773
+oid sha256:96d90c081f95e32415bfd3018e010402d4c44fb94dffc8691b6e5eeaad0e8cf1
+size 872

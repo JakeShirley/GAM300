1d672c74e806fa2d305728202959ec3e3978c557
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-01-28 15:43:48 -0800

    Shortened the opening cinematic. Started working on being able to catch boomerangs

--- TimeMachine/Game/bin/resource/components/BoomerangLoadout.lua
+++ TimeMachine/Game/bin/resource/components/BoomerangLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d0bcfc319735a325f84b22926a1fce0f4904bba3b2dc75f060933b6a063dd1f
-size 12848
+oid sha256:7fd5aa095c0196d02fa893b4dabeda707fef48a13da7f301d56519ead678595d
+size 13345

6f5d3fd7d2dd3af73aa1273031c8488c423b303e
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-01-28 22:19:01 -0800

    Inline constraint component stuff.

--- TimeMachine/Game/BallAndSocketConstraint.h
+++ TimeMachine/Game/BallAndSocketConstraint.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89a4999e2f7ad35107a1a410def498f6b0334d64726541769fe55d15c7879779
-size 1144
+oid sha256:a26ea1e3fd2dc525af8af2018cec7696dba4613d5c1479ceb83a1ecf983ec9d1
+size 1172

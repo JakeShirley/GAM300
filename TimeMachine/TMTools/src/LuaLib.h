02a515806d9ed9989fba1dc569c492253493bce0
Jake Shirley <navi@odinseye.org>
2015-01-29 18:45:23 -0800

    Removed the useless storage of the return value from loaded Lua libraries.

--- TimeMachine/TMTools/src/LuaLib.h
+++ TimeMachine/TMTools/src/LuaLib.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc0c83b10b8290c01c3d629dea1b0f057a084bf88e89f007669314d208c7a701
-size 5548
+oid sha256:f4360aa8d7e5860e9bb2204d0e2e92fecfab138de0f0a3662d0b994c5b82ace0
+size 5526

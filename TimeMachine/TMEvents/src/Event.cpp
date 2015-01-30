3dcbc784c70278649fa5056634a277a571a1996f
Jake Shirley <navi@odinseye.org>
2015-01-30 10:44:56 -0800

    Updated Event documentation.

--- TimeMachine/TMEvents/src/Event.cpp
+++ TimeMachine/TMEvents/src/Event.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c383c94b7d2ea0637b482b741aa8aa0604c4fb3d9465cff1ebc60a2315fcd879
-size 3365
+oid sha256:c6a743a36bce3438b1f6d0fa6c8675a1d0c774cd6d07081d5d1de60caff96519
+size 3301

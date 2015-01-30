24fe9bd3c89b21333c1e87422c3ae41d548f786a
Jake Shirley <navi@odinseye.org>
2015-01-29 23:37:30 -0800

    Fixed typo in LuaMeta.lua.

--- TimeMachine/Game/bin/LuaMeta.lua
+++ TimeMachine/Game/bin/LuaMeta.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79a764bff420296254aa4a8c812beca9ce74ff1b9b7b389f76e97d45790ba898
-size 3342
+oid sha256:12699fa678b77e6fb55b93d46a3b2edb747e2d2670bc97a05f33ae9c116ec461
+size 3347

b512caf122a63ac0bdc34233c2bba00f13f33401
Jake Shirley <navi@odinseye.org>
2015-01-29 21:07:24 -0800

    Added a divide by 0 check in __div.

--- TimeMachine/Game/bin/LuaMeta.lua
+++ TimeMachine/Game/bin/LuaMeta.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37b95cc9717f93694cd4af0c19f2f57a3ad7db49c5026c4d3b7131de39ae591a
-size 3280
+oid sha256:79a764bff420296254aa4a8c812beca9ce74ff1b9b7b389f76e97d45790ba898
+size 3342

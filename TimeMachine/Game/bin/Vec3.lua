31b78a9826c179e6ccf08995c46a13e298d337e7
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-01-31 18:23:05 -0800

    Frost Breath effect

--- TimeMachine/Game/bin/Vec3.lua
+++ TimeMachine/Game/bin/Vec3.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:beb5b8b77d5e6dd42b7a8d8536c04491ac75a965e00896c9a846ce7ac0c5d4ff
-size 4480
+oid sha256:02e9c6a41cfb172ef246c532650be3a38e1477751f85eca002bbc29f568bb7c2
+size 4708
--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64a6e906f9b0069f7ef0d7cf15552dad42df3dbb71c220ecc7e3977b55d00f5d
-size 48988
+oid sha256:1904a726bed27ef65410be66a4dfe301378259ef6a1781eb6f1bcf84feaaed7d
+size 50071

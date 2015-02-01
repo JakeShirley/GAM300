ba0ee06e9b8601863d528507f58a547e5f3c669f
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-01-31 19:21:25 -0800

    Fix for michael

--- TimeMachine/Game/bin/resource/components/FloatingCombatText.lua
+++ TimeMachine/Game/bin/resource/components/FloatingCombatText.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1325e3989bfea2976ae5ced4a16f33bd0cb9679e504079f52f8c130f205477d
+oid sha256:00d9331eb1b5620817b6f127ec2e44b4efb694b9871b22bd7cbb0ac89c36a412
 size 4013

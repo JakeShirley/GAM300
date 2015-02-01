c32826552513d5192377b494f1a1b33d2c261612
Jake Shirley <navi@odinseye.org>
2015-01-31 16:19:34 -0800

    Updated FrostArena to use SoundEventEmitter.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64a6e906f9b0069f7ef0d7cf15552dad42df3dbb71c220ecc7e3977b55d00f5d
-size 48988
+oid sha256:3cb8cbd24f4c1b2b1b5d53c383ed4c8ead2a9579134b250ecd69b0bf08c86eed
+size 48924

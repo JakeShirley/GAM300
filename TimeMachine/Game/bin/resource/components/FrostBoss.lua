061a7954caae7abb921bb4e5f74000b213e84180
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-01-31 22:02:49 -0800

    Fixed bug with new breath effect

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0b076813d12202263c0760d2ab6c319bdd292cb35907d28f640f0076fa9e4f1
+oid sha256:fe5294a4b83f9f0abaec4ab39dfe76a4cb8877512b1bf9227b7d60f0beb756ce
 size 50733

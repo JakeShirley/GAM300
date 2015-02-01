0d5a83b2d5ee3c09dd7c8b91df225f3949dac92f
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-01 02:14:02 -0800

    Physics fix that makes Unparenter work that was supposed to be part of last commit.

--- TimeMachine/Game/src/PhysicsHandles.cpp
+++ TimeMachine/Game/src/PhysicsHandles.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8947bd93ceaf2deae74a27ecc05d374e68cb98d20c7f73fc05ea0b52e0d8d1df
-size 16574
+oid sha256:544265e2cde5b38ac1724432b8ef4a53f3d725313e3f6ed10e1c09a4c48881da
+size 16605

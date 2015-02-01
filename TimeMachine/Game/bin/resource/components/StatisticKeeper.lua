7b341bee8658c82d257b92cba121c2e9c39ad261
Michael Chambers <m.chambers@digipen.edu>
2015-01-31 19:23:37 -0800

    Pretty bland looking stat screen.

--- TimeMachine/Game/bin/resource/components/StatisticKeeper.lua
+++ TimeMachine/Game/bin/resource/components/StatisticKeeper.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d004a1c0694dcb4258fb93773ba614c45313353c92ae4191a23cbc3f4f5c497
-size 4015
+oid sha256:18e9b9c07ccc1a053126df462384ea32171986e2cfcf6e54510fdcfdf4717931
+size 4335

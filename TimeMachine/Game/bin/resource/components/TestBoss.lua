abcd0e8a31937c3329fe57a2abe6f080bcb86bcb
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-01 02:14:33 -0800

    I don't know what this is, but it was missing a local.

--- TimeMachine/Game/bin/resource/components/TestBoss.lua
+++ TimeMachine/Game/bin/resource/components/TestBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:697e66e5b20cbd0c2f434393753f05acfc5449671e751f59e992a6b5123e0df3
-size 3545
+oid sha256:aa197a5be53774bffc5adf501f9aa80b0a54a31d2a5678a655796396889fb830
+size 3551

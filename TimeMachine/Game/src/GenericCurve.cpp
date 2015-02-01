372cd1dff27f7ca681e3a2c1be247e451ff54cae
Cody Centers <murtag27@gmail.com>
2015-01-31 17:31:58 -0800

    Added default up value for curves

--- TimeMachine/Game/src/GenericCurve.cpp
+++ TimeMachine/Game/src/GenericCurve.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e345c2884da124df86da2042506fe90b514a6fb6af4749aa5647d5b0beabef45
-size 20476
+oid sha256:ad76193510141505b94c4978cf79b7acaa2904fd52beca8f3fb2a9919de69564
+size 20523

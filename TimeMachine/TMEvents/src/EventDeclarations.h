52d031d3d0870206c4926ac0d1688c8193a480ec
Jake Shirley <navi@odinseye.org>
2015-02-02 14:27:20 -0800

    Replaced SpaceInitilizedEvent with GameObjectInitializedEvent in most scripts.

--- TimeMachine/TMEvents/src/EventDeclarations.h
+++ TimeMachine/TMEvents/src/EventDeclarations.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e5ba90aba30bff5e9973224b497c6aa04dc735c346b8ed0a122380dff768f75
-size 1528
+oid sha256:9cbbca797caffddab3c2dd12d54aabb48524bdcdf7e7686ba726b745733b383f
+size 1562

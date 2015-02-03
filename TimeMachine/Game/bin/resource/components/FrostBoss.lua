dbb4809f2d758da5ffd587d05362648bb4d9848a
Brian Inkrot <binkrot@gmail.com>
2015-02-02 22:51:12 -0800

    Improvements to Frost Boss Phase Change attacks.  Lighting on Blizzard, Wander on Babies.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50f119c698c9d6cb9e7a4d560b28987ee5af7e1bafbf9f21863466dc02043573
-size 51020
+oid sha256:2adbbbff6a8744d22d987fc6636412e20eb688cb0a2498075fe877b48f7ce882
+size 53238

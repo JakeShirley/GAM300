2de09b456a5ec6525496b4ef60ca00cee46c1499
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-02-02 22:50:32 -0800

    Fixed volumetric lights, tweaked default values.

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a26c988e4775b1c6016a2f1144f72d56cdb6892b5401e7ccea773591fd70c05
-size 78040
+oid sha256:39e0592891db33f5e1259dc3abca25d1c9387772b44ab6d4bc1596e2e09d020d
+size 78299

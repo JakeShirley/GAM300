87aeb500366b5e9712142179ca33ffa0ec00a99f
Michael Chambers <m.chambers@digipen.edu>
2015-02-02 22:16:39 -0800

    Made health pickup a little more poppy.

--- TimeMachine/Game/bin/resource/components/Behavior_Float.lua
+++ TimeMachine/Game/bin/resource/components/Behavior_Float.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15ffb9f52ac146a0e2fdf09c98eeabd51049622c095857ec784925242cf69560
-size 1990
+oid sha256:05197b9c7cf6c05b410bff0fced9426258b372600288abe8ae4ceb1e21767b6f
+size 2972

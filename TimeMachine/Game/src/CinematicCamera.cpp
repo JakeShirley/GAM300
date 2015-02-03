59bd68028f7559e57cc18672df0f6bbe2a77c0cd
Cody Centers <murtag27@gmail.com>
2015-02-03 15:27:47 -0800

    Fixed a bug where waiting at the end of a camera track while focusing the track kept setting the basis of the camera, direction would always be 0 in that case.

--- TimeMachine/Game/src/CinematicCamera.cpp
+++ TimeMachine/Game/src/CinematicCamera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9431e46eddebdfe2efa6dc2b7717bc49b2c5aef260ff0f7a264e690ab5faf0e1
-size 25752
+oid sha256:a18c038674127e6412e5e3417b663d24047b1c705a8c4d1a6f6eaf9d89b3c2b3
+size 25785

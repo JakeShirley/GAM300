9b2c6799a0feb03da2beca8844279c09ccb51a49
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-02-02 17:39:02 -0800

    Desaturation test was not uncommented

--- TimeMachine/TMGraphics/src/JAG_Screen.cpp
+++ TimeMachine/TMGraphics/src/JAG_Screen.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8966b15f136574925e6160ee0e599b286909ee7bcce0ecd6dcd6def9c7be09e
-size 2306
+oid sha256:769db3a4983f23d259fb9651ef60ed90b4795a37bd80ddb5cc2581d9abcf183d
+size 2329

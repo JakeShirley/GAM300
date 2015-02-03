9d676c8cfb8da01918e9411632ccfa437903eb4c
Garrett Allen <garrett.allen@digipen.edu>
2015-02-02 17:35:07 -0800

    updated linking to take linked parents into account. You shouldn't be able to do anything to a child if any parent up the heirarchy is linked.

--- TimeMachine/Game/src/GameObject.h
+++ TimeMachine/Game/src/GameObject.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8af617c30b2eb1602896f3f5b865db2831235e708a19d872a77c4ffc313f31d
-size 7744
+oid sha256:3eff745a6bb3234499d71ca90fdf6410ae7b3f62dfab3213c889492df7172086
+size 7926

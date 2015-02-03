fdff576e4bbe77540b05e2ce1efb3de2caef73e1
Michael Chambers <m.chambers@digipen.edu>
2015-02-03 13:55:51 -0800

    Made "Pause Locking" a thing, disabling the player's ability to pause.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:819eb59525de5feb8b22defbb92f8db6347eede9ee1bfc48a5a54fd65220ae86
-size 54010
+oid sha256:76d7cb43491a673d221267e612215d26bd7ddab3d353ee793afd53e895d28abd
+size 54460

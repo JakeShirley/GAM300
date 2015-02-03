fc35508f34b9a633ea987ca2e475a5ecd2efe155
Cody Centers <murtag27@gmail.com>
2015-02-02 23:32:29 -0800

    Fixed an issue with pointers not being initialized in cinematic camera

--- TimeMachine/Game/src/CinematicCamera.cpp
+++ TimeMachine/Game/src/CinematicCamera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2578dcdf6c4c4fc977a30e41b42df81afb8511c8719bc2177132f53d0e05cbac
-size 25709
+oid sha256:9431e46eddebdfe2efa6dc2b7717bc49b2c5aef260ff0f7a264e690ab5faf0e1
+size 25752

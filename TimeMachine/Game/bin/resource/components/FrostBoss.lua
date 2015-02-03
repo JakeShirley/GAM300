34a766f03d43314ec3e277b5e88adf2f7c731e2f
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-02-02 22:31:59 -0800

    Name intro for Saph

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a32495fc17d5d5a03cac88cdb3a8ee000e5da30a8cf58a9ecd3e867a57490fe6
-size 51019
+oid sha256:86f3f9941c511d588dc68e0d36742e32d30ca22e79cc9ac9a8d747d82225214f
+size 51727

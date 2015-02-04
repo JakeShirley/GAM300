6863863beb74a9bca2719743b9ac2fa3e93fdced
Cody Centers <murtag27@gmail.com>
2015-02-04 13:24:39 -0800

    Fixed brians weird snow bug.

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af55fb55e1ec673d5bfc28fe3c1796dab769ccfdc5a0f9d4be9560a8ebd124a8
-size 43881
+oid sha256:1b3b6c5d10a5fc58413ce0394aeb400e0c3f8681f2a02a60877c3bc2dd5d6704
+size 43880

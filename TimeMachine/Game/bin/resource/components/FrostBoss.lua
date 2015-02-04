0a0aaa2d5e6b7f2d9425d97c559473cbc79be194
Brian Inkrot <binkrot@gmail.com>
2015-02-04 09:33:18 -0800

    Worked on Volumetric lighting for Lightning Arena.  Turns out adding huge black models solves problems. Prototyped one chain bridge for clarity testing. Added spotlights to players in Lightning Arena. Added a TargetMostSeparatePlayer to PlayerList, and implemented it for frost nova phase 3.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0b769f0f1c055db363c2416eab28eaf064332129f3325f4e4abd47dc32f1fe5
-size 55248
+oid sha256:c912b1f124fed00e342ce94ea7da7e33149a440ff2014e0b3e6c0c605923aee0
+size 55362
--- TimeMachine/Game/bin/resource/components/PlayerList.lua
+++ TimeMachine/Game/bin/resource/components/PlayerList.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3a2294868cc7b3e684fbd54a0945de495d91abdf9170e05ac151e17002974fb
-size 6451
+oid sha256:8cea06a416c7e2088ce04b2141d64d4884ef50653e0c6ee3b8d5470d3eb10730
+size 7411

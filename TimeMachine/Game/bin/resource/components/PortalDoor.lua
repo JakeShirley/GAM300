070b4bbf8f13c2273c59f0b7811130251ccbaee8
Garrett Allen <garrett.allen@digipen.edu>
2015-02-03 18:27:42 -0800

    quick changes to portal door

--- TimeMachine/Game/bin/resource/components/PortalDoor.lua
+++ TimeMachine/Game/bin/resource/components/PortalDoor.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7156b3212abfeecf476bafe36b42c093a9398c912d8e87ced9ac6ac41fdfed9b
-size 2900
+oid sha256:246c8c3b43ed3e741bb1059f84a3f177fa0d8ca7a75b61b1ef76f2b825993425
+size 3089

7f1a662814c86caa67e200e7727c2cb05ff9ca96
Michael Chambers <m.chambers@digipen.edu>
2015-02-03 18:26:26 -0800

    Stopped allowing Nil loadouts.

--- TimeMachine/Game/bin/resource/components/LoadoutManager.lua
+++ TimeMachine/Game/bin/resource/components/LoadoutManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cba3a8c275aa6747cce67379041b4f286fefb97928b4361197e93ea1d2c0f849
-size 10360
+oid sha256:4702d9076cffe051f30fedd04847e33fdf49b9d4674277f03d614f1093b89fd3
+size 10415

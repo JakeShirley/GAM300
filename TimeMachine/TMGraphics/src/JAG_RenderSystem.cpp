1b97dad0d085ec0207a256c5103ec3abeee43ed4
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-02-04 10:31:45 -0800

    Removed separate cleanup for volpointlights container because it is now used for sorting output only. Swapped position of renderable2d function layer arguments so they make more sense

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0337fffe95256fb0db3da64e8f22b36eb40bd6e6c7665dec9dd4290ad104fe5
-size 79508
+oid sha256:a7d640165ce1426499f3783363106c44aa57a87cb07297892d4111e9b5a2b173
+size 79324

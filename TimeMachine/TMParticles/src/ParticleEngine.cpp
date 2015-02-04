d4256dc30f7e4e813e2c69c55540a717bc07fb80
Cody Centers <murtag27@gmail.com>
2015-02-03 16:00:49 -0800

    Added a rock Impact emitter, as well as made the Blizzard2 emitters not poopy.

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0fa6d0c18e7f5c974f8ece8fe7e210881c4a6482f8ecf6ba9119744928c121d
-size 43529
+oid sha256:881ab07df2f9e6f1efe3678853959c37d051299ee4ec575e950fd7ff7bb6e496
+size 43748

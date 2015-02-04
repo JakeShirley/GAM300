e3f37d18da53bb645a6b62651280f38562b70e81
Cody Centers <murtag27@gmail.com>
2015-02-03 17:39:06 -0800

    Supported the deletion of nodes on cinematics and generic curves.

--- TimeMachine/Game/src/GenericCurve.cpp
+++ TimeMachine/Game/src/GenericCurve.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b11d0ebe969585c8064acdc2a61f070f417c17c666b09965f865400c6762e7a3
-size 18229
+oid sha256:f836bb3a41b7aa2e65bdceddeaa8ce18b72bc47150594e2dd297f1e819ae0d0b
+size 17632

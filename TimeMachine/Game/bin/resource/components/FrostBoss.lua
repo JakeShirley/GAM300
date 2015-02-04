2fec8d3d0de308bd069fbf2df0999d39b3c810d9
garrett.allen <allengar22@gmail.com>
2015-02-03 23:53:52 -0800

    added ice spike shard to frost breath attack. It's still on it because I want some test feedback. its a line of code to take it out or change it. also added a emitter to the assi_bullet archetype for it to show better

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92a45bddaa03099565eec7b800f183eba04f33e40b90e3970f3ada7dbfd45ba0
-size 54882
+oid sha256:d0b76d8e5cee4138881d9908fba695f33b06bc656263da864bac2f9ce3b7ecdd
+size 54996
--- TimeMachine/Game/bin/resource/components/ShardExplosion.lua
+++ TimeMachine/Game/bin/resource/components/ShardExplosion.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b4f74e3c71150e488c436176a3094b61cca2b94490f8b4a04008a1679d9388d
-size 2342
+oid sha256:c80415df44c9e7f24298e5c76e81fd6ea343770e87bacdd8bf0ed5d406623be6
+size 3472

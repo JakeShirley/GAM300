b73b7b81e52e9e828cbf85105798a4da6a444d08
Cody Centers <murtag27@gmail.com>
2015-02-04 19:16:29 -0800

    Minor bug fix/optimization to cinematic camera.

--- TimeMachine/Game/src/CinematicCamera.cpp
+++ TimeMachine/Game/src/CinematicCamera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76fcc7f997e190fdadbafabe19acd04f0bf46a491a8f0918d353be100bb82f80
-size 26350
+oid sha256:d925899411708405554d953b2668ec1287f9c0997d760d23604d3d4819adc309
+size 26352

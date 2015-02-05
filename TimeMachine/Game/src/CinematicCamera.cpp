a1a0801009c5b8009d42a93948d61edaa5207a25
Cody Centers <murtag27@gmail.com>
2015-02-04 23:49:22 -0800

    Ensured booleans get flipped correctly when calling functions to change focus logic.

--- TimeMachine/Game/src/CinematicCamera.cpp
+++ TimeMachine/Game/src/CinematicCamera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4cced2cf4576403bb1809878b0b45552516b9f05fe075aa224f8a912b70a13a
-size 27088
+oid sha256:0392e1b9614aab42f4bb62d6da457b41b081014d26973f1cba7c11061c41e355
+size 27142

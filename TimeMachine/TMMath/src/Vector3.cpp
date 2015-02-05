90481aeb9a52667bb08f273f3ae573293f8b56e5
Cody Centers <murtag27@gmail.com>
2015-02-05 14:24:30 -0800

    Added a static LERP function to Vector3

--- TimeMachine/TMMath/src/Vector3.cpp
+++ TimeMachine/TMMath/src/Vector3.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa1d9921d9b2a4b091d9f890dcc92e26d6f4bc0bc346b8fa084e268a433fbef7
-size 11800
+oid sha256:12dd5045d3f884c6cdb7169fd80dd7ca3705c108c44fbc9ab8353c04df531c04
+size 11970

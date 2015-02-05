da69d4bdb4df7dfd1f0147984e15c5b2c514ada1
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-02-04 21:39:23 -0800

    Implemented Add/GetGraphicsSpace in RenderSystem, and GetName inside GraphicsSpace

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c1357794015c3e0a6832a7c3a053d35938ac916bfc38eece783b68424100a5d
-size 76465
+oid sha256:798665a1e22db403e072ba3c5b7b547aee00772f0bcce229d3383804045c9cae
+size 76985

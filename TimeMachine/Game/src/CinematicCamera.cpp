bf3f92f5d4c2c9d5cfff83d2c0b276c3535e1b12
Cody Centers <murtag27@gmail.com>
2015-02-05 12:05:42 -0800

    Added a handle recall button and increased the scale of handles, for cinematic tracks.

--- TimeMachine/Game/src/CinematicCamera.cpp
+++ TimeMachine/Game/src/CinematicCamera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d4e088909296a7fa14bc35620a0cefe7f3c0ea5afe5cc2be3a590bcd57b3252
-size 27294
+oid sha256:17782c6a5ae3a98da06824ed9aba5f58450b114968871055b90a0660d4b1d772
+size 27658

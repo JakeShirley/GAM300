f8405c967f48838cdeed2135422adefb3924f69b
garrett.allen <allengar22@gmail.com>
2015-02-06 05:59:01 -0800

    made it so if you cancel the cinematic the camera doesnt weird out from shaking

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e411f4ac6ee12e2c9a8d297116c1b50a170acd84fa7988fd04b55607cacdb991
-size 61203
+oid sha256:6e0f0c37c31ab04d5b7e0d605e44c8a930eb49726b71b1e1ca5ee936b449dfca
+size 61364

8beae4576a018395663a4d42fa7a8e794b1374ef
Michael Chambers <m.chambers@digipen.edu>
2015-02-05 18:12:59 -0800

    Fix AI level becoming saturated forever.

--- TimeMachine/Game/bin/resource/components/BossGameController.lua
+++ TimeMachine/Game/bin/resource/components/BossGameController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdd821995842dddbf2a9f892a67c73d51ed23b16a5a371e37ff6d7f30b3fe7a6
-size 2460
+oid sha256:02b72db81346c8e585af4866a7781ed47d0c5661fb97d08b5d03899bcbc23dad
+size 2239
--- TimeMachine/Game/bin/resource/components/StatBoard.lua
+++ TimeMachine/Game/bin/resource/components/StatBoard.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9907e57c869f2eec2a9a442a40d60d789e7594ca71b357189320a3342c6929c
-size 5178
+oid sha256:8f5f8cee056abcb564b23f7c84200734cc199541bdbe180a284fa31be4d145a5
+size 5433

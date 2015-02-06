5181b3f19fc6852f5f4f0f19f6b1ee52d8a56ac9
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-02-05 20:24:17 -0800

    Script fixes

--- TimeMachine/Game/bin/resource/components/StatBoard.lua
+++ TimeMachine/Game/bin/resource/components/StatBoard.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09becf8d64d8e4833fff13d5a6fbefb6d2f5658e3bb46f540b90bec0c227374b
-size 5410
+oid sha256:4cbce78b56299f9ab7a3a7173f3e297375d6307a92c6f1171f35aaca3cd8e872
+size 5377
--- TimeMachine/Game/src/GameStateManager.cpp
+++ TimeMachine/Game/src/GameStateManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bf1a5bd4969b39850496251899b73baf0e0837f91602a58f2e98aec06c7c1fb
-size 8989
+oid sha256:83f4e11e65711b663fd487c2043008f13cda5a40208099e9176cebd6d1b39132
+size 8934

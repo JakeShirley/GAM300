7f34e9b147f78932baafbb3412f09edb614345da
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-05 23:47:16 -0800

    Colliders in thunder arena and small change to player controller grounding calculation.

--- TimeMachine/Game/bin/resource/components/WizardMovementController.lua
+++ TimeMachine/Game/bin/resource/components/WizardMovementController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d307ab9d2ab4b4a59b68478d1ef560a2114d56794b847d45e3c1c161f26a9ebc
-size 10845
+oid sha256:95642b5329d9b844a63368461a48ef7e0216c9fcb23d59ed6b0eb9a1dd9d1180
+size 11044

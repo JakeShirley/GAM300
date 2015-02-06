8a0c78450c15a57262bbb652e0a6a777a67d4da3
Jake Shirley <navi@odinseye.org>
2015-02-06 03:01:36 -0800

    Added aspect ration override for Cameras.

--- TimeMachine/Game/src/Camera.h
+++ TimeMachine/Game/src/Camera.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73a81b44b9e3b218c3b899ca52fe27f3cc019c67f1b20c8a74a7b4e21bab1a6a
-size 1355
+oid sha256:abff23458d08763a278857ca4ce060d587459997b603c9a5275838e28b6bb650
+size 1367

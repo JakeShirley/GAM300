ad723406ba80b5c4500de2873d1afd22859ece6d
Michael Chambers <m.chambers@digipen.edu>
2015-02-06 10:06:46 -0800

    Some cross space health watcher behavior improvements.

--- TimeMachine/Game/bin/resource/components/HUDCreator.lua
+++ TimeMachine/Game/bin/resource/components/HUDCreator.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf55587bfa06005515849de7f54a533d8e47eaefece8d0cf14fb488c9d99e111
-size 1479
+oid sha256:fe1ec2ca8c7c2e70b66e61ae851dc7eb29c716f791de570732af700c49e350a7
+size 1684

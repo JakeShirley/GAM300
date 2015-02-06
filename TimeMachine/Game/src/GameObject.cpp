6f138f541488514aadcad4eb8459d323024ccfff
Jake Shirley <navi@odinseye.org>
2015-02-05 22:56:47 -0800

    Updated RemoveComponent to only unitilialize if the object has been initialized.

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87d5198e8073ef2f8d1673af3f0bbf144a721f0c6771681cd19de4548ced3bd5
-size 44056
+oid sha256:1ffa9e84f7677a0ba66de09da9696265a443e208bec76d91d23824e394351cc9
+size 44035

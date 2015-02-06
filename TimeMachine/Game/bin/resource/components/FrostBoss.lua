79a7eb5c7d2f9bd6c967602760c1494e4465e99d
Jake Shirley <navi@odinseye.org>
2015-02-06 06:58:34 -0800

    Boss intro text scaling out.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19bb043c56e665fb713961d45b95c1cf8e29882428cd56d9d3a6f104ac5bd3ad
-size 64391
+oid sha256:4261daddeb4ef5178ec24b47ac08768fbceac497fe2decccfd6c5b745894fe26
+size 64594

69538cb1f5041835fc4171914e4606bbb084bcc4
Jake Shirley <navi@odinseye.org>
2015-02-06 01:37:17 -0800

    Added Engine:Set/GetGlobalFade

--- TimeMachine/Game/src/Graphics.cpp
+++ TimeMachine/Game/src/Graphics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2ca7edc92d496ded1420b9fcd5750eadf5cd1feab6410896cbf7d81bb8022c7
-size 13011
+oid sha256:94bba6411e91c303584c127bb902715fcac549f11a3859396181c04e97ccec89
+size 13393

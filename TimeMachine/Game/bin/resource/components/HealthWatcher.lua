59e88a78a81447b97a91f8d5263283ceebcdd3f2
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-02-06 02:11:11 -0800

    Working boss and player hud on frost arena

--- TimeMachine/Game/bin/resource/components/HealthWatcher.lua
+++ TimeMachine/Game/bin/resource/components/HealthWatcher.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e815cc07375b4e2c4fb0650bda7cf6f8cfbf8f5b748c02fb86b1bbca9a6adecf
-size 1736
+oid sha256:6ae0f612fcadb5a5f7f1f0b09562be7014adc90890cbdd8319bfc289781224b5
+size 1786

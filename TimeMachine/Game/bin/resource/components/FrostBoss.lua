a212369fcf9d45ec2c5cfa7494e6d1da7562184a
Jake Shirley <navi@odinseye.org>
2015-02-06 06:47:53 -0800

    More Frost Boss intro.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a121ba147e31c31c97039b80c95c2ec49f55db526083b6cbfcee888283d4ae8
-size 61005
+oid sha256:51e59b5a948d0b829900de095bad4d6b21c3d2f92cdaad322ae400e5d77a0fa7
+size 60994

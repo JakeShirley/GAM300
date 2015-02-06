e4dfd4ffc68f1c84474251b521d55b920f3f73b8
Michael Chambers <m.chambers@digipen.edu>
2015-02-05 23:46:31 -0800

    Statboard update.

--- TimeMachine/Game/bin/resource/components/StatBoard.lua
+++ TimeMachine/Game/bin/resource/components/StatBoard.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:106ce4064daebb0801e5691b516488e4e02c6a36da6751a3e24a104d781bc73f
-size 6142
+oid sha256:fd1090b489820601bcf6e80d0f07daf89565a4cd9dd0d9b36530be009c910d70
+size 6550

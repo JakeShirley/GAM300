16759af1eea39c775418a5fcd48ccd5db6d72616
Brian Inkrot <binkrot@gmail.com>
2015-02-06 05:21:03 -0800

    Start of anim.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed829272d5579b4d916040c9448a80250eb15ddca61fa181bf9446503193f2b3
-size 60365
+oid sha256:758c9b7c514b73a9e1f9f43ef4fc1edfa3bdf90876825c11bb4a1e400f54e7e1
+size 60845

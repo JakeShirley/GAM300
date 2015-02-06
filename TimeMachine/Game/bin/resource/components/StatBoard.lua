f02deb0769e2894ec094ea980451ab992ed8ec25
Michael Chambers <m.chambers@digipen.edu>
2015-02-06 10:05:01 -0800

    Added some more stuff for the medals.

--- TimeMachine/Game/bin/resource/components/StatBoard.lua
+++ TimeMachine/Game/bin/resource/components/StatBoard.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14fe3f240b6ce616e6cd3c953017096c1081f17bf629248b21f2be9838741850
-size 8481
+oid sha256:721eb9a2e8ae6b12d5878d29a216168642dea68f6cb66ad90c51dff633797baa
+size 8712

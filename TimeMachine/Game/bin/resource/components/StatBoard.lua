36ece47896a43caa98455acb120aae1d0ec2a9a8
Michael Chambers <m.chambers@digipen.edu>
2015-02-05 20:40:07 -0800

    Changes to the way the statboard currently looks.

--- TimeMachine/Game/bin/resource/components/StatBoard.lua
+++ TimeMachine/Game/bin/resource/components/StatBoard.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e64faf2368e526bc35a67035f3518f5818235d49bd049a4d6f285e14fecdc25
-size 5850
+oid sha256:1bed196051ad4c74104e36cc7190e6f59231ab1c0e240e9b0f1081f32173e296
+size 5849

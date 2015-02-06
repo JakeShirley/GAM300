8d008049b660c98fd43c9756b78a261ee3d6ad23
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-02-05 22:20:51 -0800

    HUD Stuff

--- TimeMachine/Game/bin/resource/components/MenuTrackBehavior.lua
+++ TimeMachine/Game/bin/resource/components/MenuTrackBehavior.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ce9b65f174b6a0df868905740f9c212692bfbe05d31c2c4eb2e2ddb5af7e654
-size 1631
+oid sha256:35aecc6a338f0ec2fab8a11e0644f20f72eb52911f4d2f77b239ef7a1b7eee1c
+size 1419

dbc8a3d1c00fc340a9d855378ee2e16ab1cb02c4
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-02-06 03:55:41 -0800

    Finally have a working slerp, added Get/SetCurrentFrame in AnimationController

--- TimeMachine/Game/src/SkinnedModel.cpp
+++ TimeMachine/Game/src/SkinnedModel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17aadad28b8daff1a1f2ae5e21b03cbdd0334fb8274e91b8508a5d846fb2e4ff
-size 7336
+oid sha256:80e2743c6d52a7e9ad7f2cd183c1603a230a282e3d29b412d0262a01fc92d42b
+size 7344

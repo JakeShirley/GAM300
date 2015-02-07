2ff0417105f6ae8287c36fbe2a0d3462bf26bbca
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-06 23:11:37 -0800

    Duplicate dot products are now avoided while traversing the half edge graph for support points, so collision detection for convex hulls is slightly more efficient.

--- TimeMachine/TMPhysics/src/OopsModelAdjInfo.cpp
+++ TimeMachine/TMPhysics/src/OopsModelAdjInfo.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d1c31b5d2d4ef2c75514f38aa9ad5fda7197b85ee479592f3fb102145ee0a71
-size 3993
+oid sha256:f57736bfa0173172dd94bcd12e77a2c600d5830023410d4de30000c296cda8a2
+size 4885

512f9ba9638a877b0c1d80534c02980a2b014110
Jake Shirley <navi@odinseye.org>
2015-02-07 15:49:39 -0800

    Fixed button callbacks crashing the game and re-enabled SetParent.

--- TimeMachine/Game/src/GamePropertyEntries.cpp
+++ TimeMachine/Game/src/GamePropertyEntries.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5edf59214aba9033e18416ed8292d80d395a5cbcfdd05a9768ec5e1b2306573
-size 3068
+oid sha256:6b2f050715e2ab81de62b250424f43837303ba70f701f1efc848a53b009c8f72
+size 3110

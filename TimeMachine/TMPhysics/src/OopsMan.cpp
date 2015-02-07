564052112601cac8392e9b88b014db07d63da214
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-07 00:03:38 -0800

    Convex hulls now store their extents so updating their aabbs is now constant time instead of linear.

--- TimeMachine/TMPhysics/src/OopsMan.cpp
+++ TimeMachine/TMPhysics/src/OopsMan.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:354da828de0f636c36f2428edaf74fda1315b0ca169bcd4ff24b3311756b3624
-size 6785
+oid sha256:587c1ba552dcffa4ba1c6d79071a606986b20146d6ac1b0b0a17196a0dde2028
+size 6713
--- TimeMachine/TMPhysics/src/OopsObject.cpp
+++ TimeMachine/TMPhysics/src/OopsObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5e92afe34c67070d12b21d43793dc0f06c915aeb8cea013448920d33e0cd6dd
-size 16898
+oid sha256:783d40d5a5bc850ea11f0f5152dbc2d8c39248877794dacb8ae77334d27bb314
+size 17001

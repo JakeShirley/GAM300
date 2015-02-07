ae163a105aae86200a5e1358c9e8348103d9fb5c
Jake Shirley <navi@odinseye.org>
2015-02-07 13:28:38 -0800

    Presentation level.

--- TimeMachine/Game/bin/resource/components/PresentationSlides.lua
+++ TimeMachine/Game/bin/resource/components/PresentationSlides.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ec2393d9cd72e5fe510dc00dab9f619d5ea749844ec3635ef5a9bb2baf720bd
-size 2221
+oid sha256:985cfb5815a5cd966b1299bb4119142e24ab826706fc0372b4cca3d13aa01a2f
+size 2468

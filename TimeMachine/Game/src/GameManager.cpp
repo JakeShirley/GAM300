1a507c43bac01af7a71871602ca1fab5103f309f
Jake Shirley <navi@odinseye.org>
2015-02-07 21:52:10 -0800

    Add menu option for enabling editor grid.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e973ec65dd4de8a406391d87c5e7df65ea2cc9e8123e046ba09eced1aef39734
-size 52810
+oid sha256:0d101467567902e1eb671df43c6df2ad321221e83876ce6e73070e0b560bb5a8
+size 53423
--- TimeMachine/TMApplication/src/MenuBarMenu.cpp
+++ TimeMachine/TMApplication/src/MenuBarMenu.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a68c8d91682d4d157ae0609c2c804dbab66d099076e221e3db37018d0beb4a9a
-size 921
+oid sha256:b734e3d49608064666d584840d0d717931e21d0eb9b6b257b9054c2314c65e74
+size 1289

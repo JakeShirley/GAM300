9494368c988502952c0fa0cb5b2aad93d2e5c4ec
Michael Chambers <m.chambers@digipen.edu>
2015-02-09 14:33:24 -0800

    Menu functionality fixes.

--- TimeMachine/Game/bin/resource/components/Menu.lua
+++ TimeMachine/Game/bin/resource/components/Menu.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e801b866b336ecd0b1eb8a17ba1580ec9b46f2fc127f154b7aafe610ff8aa36
-size 34353
+oid sha256:a8f4d1d495714c5385ebe84771aaf940a34b3fde9b46069a04fcd0f7ebf04450
+size 34407

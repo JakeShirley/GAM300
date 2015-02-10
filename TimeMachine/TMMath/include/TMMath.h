47aeb063f88d2707154c81b49659a8dee81c5e4d
Jake Shirley <navi@odinseye.org>
2015-02-10 15:09:12 -0800

    Removed declaration for glm::vec3.

--- TimeMachine/TMMath/include/TMMath.h
+++ TimeMachine/TMMath/include/TMMath.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a37c3103f363d7ecd9b99e9d48f6f543b1f921f731a7cb913396b34b026eabd
-size 6117
+oid sha256:36acf6fe72835ef5da2b745e0e9c921cd02f54143feafc39004d5f84f5f58b5e
+size 6088

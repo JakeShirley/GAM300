b1b8beb0796c5267dcca70232b47e12af5cee043
Jake Shirley <navi@odinseye.org>
2015-02-09 17:19:55 -0800

    Removed std::string -> string typedef.

--- TimeMachine/TMTools/src/TypeInfo.h
+++ TimeMachine/TMTools/src/TypeInfo.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59ae2dc0a4f668fcd2a4a2af2166b9dbd070ffc09301f69ed8768bb24dd6a040
-size 2783
+oid sha256:0e81d633c281f40791433b42251c5efdb9aef7538d13b3bdad97246b378810e0
+size 2764

643e5d97327d4464d168e39ca3d2fb1dc42168d4
Cody Centers <murtag27@gmail.com>
2015-02-09 22:48:56 -0800

    Added a button to open the emitter files with notepad++. will next look into errors due to it not being installed, and default to notepad

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4eb07124df85523c4c21d650e830e80031040518776411bd028a2d8bcfeec910
-size 68591
+oid sha256:7e01dc6abeae2aeab352ff391bf46e2fbed48fcfb5682c65272aa3701f3152fa
+size 68756

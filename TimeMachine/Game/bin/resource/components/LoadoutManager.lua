dd50b0cc2fc9b83d733cc59308fdf23306eeda48
Brian Inkrot <binkrot@gmail.com>
2015-02-10 11:10:05 -0800

    Fixed Super Finally.

--- TimeMachine/Game/bin/resource/components/LoadoutManager.lua
+++ TimeMachine/Game/bin/resource/components/LoadoutManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a21de12d6fae4c7973d1c69a137a06261703a8b0029fb50342dfe0005caf35d6
-size 10561
+oid sha256:e10e9a06bb208c17cdeee6a6a345557b5255048cd28dbad2eb71e88ec73a60f8
+size 10884

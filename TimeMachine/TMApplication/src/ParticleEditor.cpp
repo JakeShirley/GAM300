d86b23fb15994159260082aa4de438f60091df2f
Cody Centers <murtag27@gmail.com>
2015-02-09 23:10:37 -0800

    Added backup text editor opening. Still not aware of a way to suppress errors from wxExecute.

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e01dc6abeae2aeab352ff391bf46e2fbed48fcfb5682c65272aa3701f3152fa
-size 68756
+oid sha256:493fab831894e781d3406fbc5e946386aca75799087a94c03bf83fd61f8d6181
+size 69101

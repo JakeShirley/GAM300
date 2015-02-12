954728db959470b32d528ed65fb3b2d1ab50f805
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-11 22:24:50 -0800

    Specialized solving for contact blocks. Turned division in PGS to a multiplication.

--- TimeMachine/TMPhysics/src/OopsMultiContactConstraint.cpp
+++ TimeMachine/TMPhysics/src/OopsMultiContactConstraint.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d6a59c5d6d0c2cf1ef41d651888446df675645cb53bc75226ca69fb3e4369c5
-size 8324
+oid sha256:f041e00720ab51805ef6f7be804e0a771370c1cf56cc59b76600fe59b71292b8
+size 11521

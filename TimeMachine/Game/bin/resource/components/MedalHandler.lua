4a442e7a3adc61ea635e9ed4cc182040fec908d3
Brian Inkrot <binkrot@gmail.com>
2015-02-13 14:07:46 -0800

    Changed some text for accolades.

--- TimeMachine/Game/bin/resource/components/MedalHandler.lua
+++ TimeMachine/Game/bin/resource/components/MedalHandler.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e07b595d88cb17b8256fb573821cc0a5234c44567ca8d7828870607f7a4d291b
-size 4043
+oid sha256:8c10dd92b09b8aa6a218affdcead81c5127ab01e2a87e7558410be6acdb0ddc3
+size 4046

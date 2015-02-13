f1c3a3a3d35ad28b4f5b5a2e2d0f738fdb8d00ea
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-13 12:20:47 -0800

    Fixed a crash caused by broadphase when restarting a level when there were collisions in the last frame.

--- TimeMachine/TMPhysics/src/OopsBroadphase.cpp
+++ TimeMachine/TMPhysics/src/OopsBroadphase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72ce41ac3a758d8c2677b9423854275c629aeb24c10d23b3cf6f5511e256250d
-size 19947
+oid sha256:6e91920c0d56d17aeeac6d5022b72c107cfea1448b4ea6d7f7a777778782c3ca
+size 19948

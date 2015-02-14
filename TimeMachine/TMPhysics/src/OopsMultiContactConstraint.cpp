14698a58b33cb06457759fab0e68bf38bb034be8
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-13 19:31:14 -0800

    Switched back to a Lagrangian solver for contacts, decreasing time spent in physics by around 10% #getCatto'd

--- TimeMachine/TMPhysics/src/OopsMultiContactConstraint.cpp
+++ TimeMachine/TMPhysics/src/OopsMultiContactConstraint.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17dd6bda3fb1acc581d724d010509b7f93473d43895323ee31a769ec52c0c8b8
-size 11791
+oid sha256:c5546c26c781e95be656cdb4694347099f2301995edfe2d7f27ff98e2b2c5aee
+size 11506

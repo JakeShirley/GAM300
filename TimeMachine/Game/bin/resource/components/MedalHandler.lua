e0237f4669e0f052a15d40b0de9f3ca25b09fae1
Brian Inkrot <binkrot@gmail.com>
2015-02-13 16:06:48 -0800

    Now they're all past tense!

--- TimeMachine/Game/bin/resource/components/MedalHandler.lua
+++ TimeMachine/Game/bin/resource/components/MedalHandler.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c10dd92b09b8aa6a218affdcead81c5127ab01e2a87e7558410be6acdb0ddc3
-size 4046
+oid sha256:9764a6906d5f9cd9c97f8b421123dabcfa3df8af77cfa7ca301b3da6008687b1
+size 4047

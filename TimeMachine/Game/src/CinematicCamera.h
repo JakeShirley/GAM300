9da969efe71b4599f9ce5ff3916958e544732282
Jake Shirley <navi@odinseye.org>
2015-02-14 16:37:11 -0800

    Remove camera node colliders

--- TimeMachine/Game/src/CinematicCamera.h
+++ TimeMachine/Game/src/CinematicCamera.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c10f0854e4a992627af6add9d8468f95c5644e20bf9410a8eb88762163d12901
-size 4016
+oid sha256:c874f21c288d89e87a9ba195660a043decace9246295edccbe2b5b15a051bb3b
+size 4061

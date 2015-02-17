a893bb5a2509c622705620dc1c70096058598899
Brian Inkrot <binkrot@gmail.com>
2015-02-16 18:29:49 -0800

    Update to a couple Lightning Boss Attacks.  More feedback / more reasonable Static Cage, though still far from perfect...

--- TimeMachine/Game/bin/resource/components/BA_StaticCage.lua
+++ TimeMachine/Game/bin/resource/components/BA_StaticCage.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:060719ea94295ba63e879810cc3e18bdcd736f6c1086ab6874502a8cb741009e
-size 5468
+oid sha256:41b19474f1d1a506e8529fbdabf869cd70ba0d9200b0c8160863e18b9d90728d
+size 5546
--- TimeMachine/Game/bin/resource/components/StaticCageScript.lua
+++ TimeMachine/Game/bin/resource/components/StaticCageScript.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d5c29f7bc8bb9f752621eae76e174df420c7e601b894e8fc83c24868cc42347
-size 3328
+oid sha256:8f43796fdc320cd4a7ddaacabca5bf76014b2b19d8132ad5e815c8ae9d5f5b8c
+size 4572

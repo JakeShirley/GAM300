cd742a9b80de5ce05d61c83edff86f09efd4e8c2
Jake Shirley <navi@odinseye.org>
2015-02-17 15:58:57 -0800

    Added extra check for Sprite::SetTexture.

--- TimeMachine/Game/src/Sprite.cpp
+++ TimeMachine/Game/src/Sprite.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:877198c8d388fbb1dda16f3d521217f1f947f50febd3cf697a9858fe90adef13
-size 8722
+oid sha256:a519b9f4bac5c24d9303a7b4edc9f40d78323e822d5da4607c8f6f34c121a727
+size 8753

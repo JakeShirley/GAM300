5b53a3c1a6ded50c09fc5c98ba197cceec5533f5
garrett.allen <allengar22@gmail.com>
2015-02-16 16:34:01 -0800

    cleanup and started updating old button code. Starting to centralize handling resources through ResourceSearchList. But if we want we can abstract it another level if that doesnt make sense to anyone else.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad628bde6ca71a77f71c4f21e84cd59a5abc27fbd5d1c624848b4a278429fb91
-size 54445
+oid sha256:7916d696a43fcbebff31049eb28c9633dc87ded5001608332bbcf7ec308074ae
+size 54009
--- TimeMachine/Game/src/ResourceSearchList.cpp
+++ TimeMachine/Game/src/ResourceSearchList.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3168ff95bbcabef1294954c8f7ef15b07ed5096b7ba4338990dc6abbf3830daf
-size 16571
+oid sha256:3233f2eb82fc0c741b090b6c62cf1363c7d0f052bae5149def87329a1032553c
+size 16703

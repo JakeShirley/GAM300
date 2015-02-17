a64dc694526ed2e6f80a1a750ea84f313aebb595
Michael Chambers <m.chambers@digipen.edu>
2015-02-16 18:07:26 -0800

    Added a few more stats to the tracker.

--- TimeMachine/Game/bin/resource/components/StatBoard.lua
+++ TimeMachine/Game/bin/resource/components/StatBoard.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e60e7ca6dba2399b1162f0618d9235812db28298c6350d73a12a4c9577aa80bc
-size 8778
+oid sha256:f41e56ad4030376d475fdddfadd3a35af54ca3685d5a48ead1e6d691e25187c2
+size 9807

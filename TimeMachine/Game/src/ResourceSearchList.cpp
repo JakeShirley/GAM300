8fd7e083072d34f102e6025155c5281a4a331f29
garrett.allen <allengar22@gmail.com>
2015-02-16 16:04:52 -0800

    unnecessary and incorrect operation. The Space already gets cleaned up on shutdown.

--- TimeMachine/Game/src/ResourceSearchList.cpp
+++ TimeMachine/Game/src/ResourceSearchList.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99854f50b8a8e85c248940e1c2e3a5a5d7aa33cf8176f46eea4fe04354479edc
-size 16597
+oid sha256:3168ff95bbcabef1294954c8f7ef15b07ed5096b7ba4338990dc6abbf3830daf
+size 16571

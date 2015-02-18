97b891b2ee81f2067fbc151407e036424527a3a4
Brian Inkrot <binkrot@gmail.com>
2015-02-17 22:13:14 -0800

    Worked a little on a couple emitters, if you notice them in game give me feedback. Also put basic combat text on gaining Energy

--- TimeMachine/Game/bin/resource/components/EnergyMechanic.lua
+++ TimeMachine/Game/bin/resource/components/EnergyMechanic.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7aefe2c860f5cfc1d22844028897b11efcc9be17c78c6f566c7cec55d5d3d8d4
-size 3595
+oid sha256:da212fbb9277535ce14be888766ac58404606e38133e2c6856bc1a98eaa1aa28
+size 3843

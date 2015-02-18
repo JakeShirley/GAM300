794f5df57f5cba123a33a4e7709fd234aa5d0573
Cody Centers <murtag27@gmail.com>
2015-02-18 12:34:25 -0800

    Enabled emitter keyframes to be able to use any particle/emitter value in expressions as well.

--- TimeMachine/TMParticles/src/ParticleEngine.h
+++ TimeMachine/TMParticles/src/ParticleEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecfab99f0dfff39613f75dbcee5e68456af122991f8070114a69fb5e62c63e10
-size 11198
+oid sha256:af4ca47c6ff1fd2c495dcfb34d9a74eab5ff0984154669ebcd08c21e377d46a2
+size 11138

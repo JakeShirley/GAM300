7ad8aa8bbd7af9e69bad53ab2d8248a24363bc74
Cody Centers <murtag27@gmail.com>
2015-02-20 13:19:53 -0800

    Added getters and setters for arrive/depart times on nodes.

--- TimeMachine/Game/src/CinematicCamera.cpp
+++ TimeMachine/Game/src/CinematicCamera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5a5af5c0a3c1584e8fb1f8097e8db14d811d8dc217309c5f1799db0dedb8508
-size 29841
+oid sha256:e6fbc8db73b9176c57ab60acd60009cca86a6547fdbf675c179cffccfef44eb0
+size 30307

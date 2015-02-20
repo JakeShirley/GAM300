757406d272731455fc7d22817249172e75a89525
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-02-19 16:51:50 -0800

    Updated ModelConverter to not store meshes separately. Changed animation file naming

--- TimeMachine/TMGraphics/src/JAG_Model.cpp
+++ TimeMachine/TMGraphics/src/JAG_Model.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dce493d750baef296352de39658a20d0c63da6feaa9966b8c9ab22f31d059908
-size 4734
+oid sha256:0edc27d4900d10c17a7849e38b1bbc653ff6b11beca70ea4cb9212348eb397b2
+size 5016

555104fd144d106f170dbb899e6e8dc9b431e7d9
Brian Inkrot <binkrot@gmail.com>
2015-02-20 15:26:45 -0800

    Slight tweaks to lightning boss stuff

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fed951547426359af59fb29e2a4e34b28eb543a9b392c75f21e619caad4c92f
-size 77214
+oid sha256:5a4a8237c384f706cb94022ebff9b6d30d714b369fc774bc3c92976bd3ecf3e9
+size 77324

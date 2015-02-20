8a9491d82d2a81d3082a53d6f106facac75c778e
Brian Inkrot <binkrot@gmail.com>
2015-02-19 17:17:56 -0800

    Fixed continuous spawning for emitters. -Cody

--- TimeMachine/TMParticles/src/ParticleEngine.h
+++ TimeMachine/TMParticles/src/ParticleEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af4ca47c6ff1fd2c495dcfb34d9a74eab5ff0984154669ebcd08c21e377d46a2
-size 11138
+oid sha256:462ec10ee1369386e9ccb99c2134aa0158ca073a5b7714883273ea7753973877
+size 11169

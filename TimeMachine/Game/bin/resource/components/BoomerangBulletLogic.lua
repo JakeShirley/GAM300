ebffb4ffe8a958f9c7bcd737b83ef022fe9207fe
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-02-21 15:23:18 -0800

    Cleaned up some stuff in loadout scripts, changed melee loadout a bit, made changes to the loadout exploder script

--- TimeMachine/Game/bin/resource/components/BoomerangBulletLogic.lua
+++ TimeMachine/Game/bin/resource/components/BoomerangBulletLogic.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:283eea8a1999df565bc38640ac8c5f652b563d720cacb20b3cfc7680f5066503
-size 3533
+oid sha256:a61162a5785ced6b5efed727a523d2e7295b1fa2305884a8f86ef0cdf937fc37
+size 3451
--- TimeMachine/Game/bin/resource/components/BoomerangLoadout.lua
+++ TimeMachine/Game/bin/resource/components/BoomerangLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9847fdd35c158770b84ff17af67241714286fe3041efeb74118a9f446083ff8c
-size 13425
+oid sha256:5b9ae9075b7af8dffaa355241ebbf4fc7b0288fc25dd3a5105366052ca2c5d49
+size 12618

ff2bb73963c4b9822e3acd1702cd10c79e1ae68c
Cody Centers <murtag27@gmail.com>
2015-02-20 18:24:31 -0800

    Emitters are now within spaces!!! boom roasted.

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b3eed7654fc5cfcfabf22aa82d4bbfd3c6015622723dcc82ff279adfc46b448
-size 79551
+oid sha256:f901201889e35af6b03b1b90cea5e8a523738cafeaeaddb076b07eb1052b0ef5
+size 79619

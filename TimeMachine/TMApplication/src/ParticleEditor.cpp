fee3e7ab406b82781cfa9a93ebb97206f4079d0a
Cody Centers <murtag27@gmail.com>
2015-02-21 15:37:52 -0800

    re-writing expressions to PEF format no longer adds additional spaces.

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c12581780cf5af2b68fbc8d9eb8c0280765c8960e0cf5f48e618aea811da7dcc
-size 70373
+oid sha256:b2bd4167ea43bba3fba516efdb12314d24788f04e566bb08d8ebc14a55530c25
+size 70372

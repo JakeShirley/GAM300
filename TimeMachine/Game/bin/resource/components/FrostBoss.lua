20c93f986cab33456c07daa7af206aa9db746efa
Michael Chambers <m.chambers@digipen.edu>
2015-02-20 16:21:08 -0800

    Revert "New bank file and new tweak."
    This reverts commit 6f466f9ae4d83f237925c1e62a21b8ab87b4f811.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0008eb00fdafa1f6ee0c557c38ddfedddb2630b9319ca406cddbd33cadecdb00
-size 77738
+oid sha256:9c5ec2d2a905f9852efbce4c5e30e226baff8d3aa671a04382ba4e95f24eba99
+size 77610

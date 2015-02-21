d42e1d37ea1f4f1b6bc0700546198c43d0510c08
Cody Centers <murtag27@gmail.com>
2015-02-20 18:23:58 -0800

    Added boss explode, needs to be polished but its in.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c5ec2d2a905f9852efbce4c5e30e226baff8d3aa671a04382ba4e95f24eba99
+oid sha256:a696333b5564c727cf0774b6c87a2b3085ded8a8bd5b5ffe551eb47fb5a686e5
 size 77610

618c50b90817211fb114fc5e41ae87363fb42a82
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-02-21 15:17:02 -0800

    Random curves for owl tracks

--- TimeMachine/Game/bin/resource/components/WaypointMovement.lua
+++ TimeMachine/Game/bin/resource/components/WaypointMovement.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f905756a8bfc026273d929c7b268400aaa540468c97001f4faf90c4a6cf7794
-size 4337
+oid sha256:c592544ff4cee891edac5c35a137f3c07fe812e8e5bf2d258debedf198f33fb9
+size 4753

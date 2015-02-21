801d7b0ee421a01c600625a835c3853d03e5d534
Cody Centers <murtag27@gmail.com>
2015-02-21 15:29:30 -0800

    Particle editor emitter shows up again now.

--- TimeMachine/Game/bin/resource/components/Test_snowsplode.lua
+++ TimeMachine/Game/bin/resource/components/Test_snowsplode.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f66eebcacfd31cf5d5ba3284f5bc42e411559973d0d06d9f16f9d8d2226bb3f
-size 1662
+oid sha256:f9b18da7e7cdd3557260a90978109c9914ca7064d5c2efafa4781b340f2587c7
+size 1663

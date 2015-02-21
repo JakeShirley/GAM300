f18f1b93ecc46973933f494fb8bfb8f4682e8b28
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-02-21 15:41:28 -0800

    Fix on music transition

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:114df9e6d566963107e54ebebc1f4dbca2ee4fd636adcc23bd97a679e3debe7b
-size 77830
+oid sha256:94ac2bb9cd4f140ec37cb062b9e7a7f10b095a1154c9d5bdcdbd5dfe25d627d3
+size 77824

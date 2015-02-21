9619ca76dc20b8ee88ce413ba999b4c30575cff8
Michael Chambers <m.chambers@digipen.edu>
2015-02-20 16:41:04 -0800

    Found the issue with sound and pushing the new musical phase transition.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c5ec2d2a905f9852efbce4c5e30e226baff8d3aa671a04382ba4e95f24eba99
-size 77610
+oid sha256:7293b5fd6fb425a047ff5285939ad8fc589ff019dd0f650f941d11c1f0eb00a9
+size 77716

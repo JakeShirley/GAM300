7a8788073d70474927a9d15688188db5bd432322
Cody Centers <murtag27@gmail.com>
2015-02-21 18:37:55 -0800

    Fixed the static cage tell emitter.

--- TimeMachine/Game/bin/resource/components/BA_StaticCage.lua
+++ TimeMachine/Game/bin/resource/components/BA_StaticCage.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:addac030481574821ce29418ab668fdac4e9cc20934f7a2490c44ecb571218ce
-size 5907
+oid sha256:9062c5ce0a2161f5f408a79d0b5cf05e045cbcc93817e28a320313d1b36856f3
+size 5911

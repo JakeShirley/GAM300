fa2149d9838a6b73d57cf7aa6ca8336b1e7a2ddb
Jake Shirley <navi@odinseye.org>
2015-02-21 19:43:25 -0800

    Added initial splash screen.

--- TimeMachine/Game/bin/resource/components/LevelSwitchBehavior.lua
+++ TimeMachine/Game/bin/resource/components/LevelSwitchBehavior.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad953d7270f994f94ca26c8766469c2bbb9e4231bfc020ed18f0818c6a4790ef
-size 6126
+oid sha256:790f53c3a11e2c99bc944db2634fc03dfa38ae90fd3f078a5e2aab5a99a7d207
+size 6015
---
+++ TimeMachine/Game/bin/resource/components/SplashScreenHandler.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ea79e0c4864f5405807cb8fe4abdcfcc0caf279b65e99084f8950c254e090b7c
+size 2765

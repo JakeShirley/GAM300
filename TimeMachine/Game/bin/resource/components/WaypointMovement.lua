f8d0cfa35d211f36f246d9152952b5deb8922a78
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-02-22 00:43:33 -0800

    Cleaning up generated owl paths

--- TimeMachine/Game/bin/resource/components/WaypointMovement.lua
+++ TimeMachine/Game/bin/resource/components/WaypointMovement.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d15476051243a09f2ba4b24ac0b96a67eabafe961bccc1a2fc71acececc89f8
-size 5068
+oid sha256:c38ba0e2be4e55ff63351de3ac8a8a6518ac68f0d9022ea6f5256586aa817cc3
+size 5226

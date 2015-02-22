49aff9acabff51b249b8a0fb2ecb9c7d974031f6
Jake Shirley <navi@odinseye.org>
2015-02-22 13:26:15 -0800

    Added toggle for camera movement, turning it off in HUD spaces.

--- TimeMachine/Game/src/Camera.h
+++ TimeMachine/Game/src/Camera.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abff23458d08763a278857ca4ce060d587459997b603c9a5275838e28b6bb650
-size 1367
+oid sha256:8a91f9f63ee28c899008703eda2cc016a0c9239d3fc94392ec11ee6ae14c0be3
+size 1527

e96575d7cd6e9328cf5f85d048ca994a5d5f7a69
Cody Centers <murtag27@gmail.com>
2015-02-21 16:33:36 -0800

    Revert "Revert "Made all the lightningarena round platforms the same archetype, designers are fucking lazy.""
    This reverts commit 206ac5e3a0fe5b778f73f1192334f2d239b33a3c.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:114df9e6d566963107e54ebebc1f4dbca2ee4fd636adcc23bd97a679e3debe7b
-size 77830
+oid sha256:0ed4f1c87642a60539d14765fc78c9200ec1605676d2d3b757bd4fd2b762b23a
+size 77826

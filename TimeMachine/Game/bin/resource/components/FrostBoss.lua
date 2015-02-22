104a1f2bc68043f3d942e3df5f3e437634da6bd2
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-02-21 16:27:39 -0800

    Added chain abilities for phase 2 and 3 for frost boss

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:114df9e6d566963107e54ebebc1f4dbca2ee4fd636adcc23bd97a679e3debe7b
-size 77830
+oid sha256:a3ebede5969e415fd199c09d73fe9df4e2e659cae7c1979e7f491617eb33da46
+size 81103

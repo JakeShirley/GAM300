2c8b3ab11c133e6488fa24c1e0ba9587a0c68c81
garrett.allen <allengar22@gmail.com>
2015-02-21 17:57:23 -0800

    attempt at micro commiting. Boss Camera and Movement tracks about 80% done.

--- TimeMachine/Game/bin/resource/components/LightningBoss.lua
+++ TimeMachine/Game/bin/resource/components/LightningBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a87334f8712dc2c302892ac9763a310be4edd6569ce813e41ff093edf2c5817f
-size 6270
+oid sha256:e759c232a3454b8165afb32f2233839ca8f6987d389144ffea9b4831960ea087
+size 7243

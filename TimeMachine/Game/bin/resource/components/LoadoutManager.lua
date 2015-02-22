35d34a7d38192f3ae8fc8e4deaae304cf426c3d2
Michael Chambers <m.chambers@digipen.edu>
2015-02-21 18:51:28 -0800

    Beginning of the Shock Rifle changes.

--- TimeMachine/Game/bin/resource/components/LoadoutManager.lua
+++ TimeMachine/Game/bin/resource/components/LoadoutManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e10e9a06bb208c17cdeee6a6a345557b5255048cd28dbad2eb71e88ec73a60f8
-size 10884
+oid sha256:96c95faec1855f64c2803b78136a9aa3dd67b950dda22094ab57bf578b4d0c7b
+size 11078

59f5555b43698f33298eb03aef8e7bcea9df3c3a
Cody Centers <murtag27@gmail.com>
2015-02-22 23:20:52 -0800

    fixed a texture bug.

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:988d50ac28e82e97c598b65536f57226fa7016d56f0e0dcb554e1f6435875332
-size 52819
+oid sha256:015c65a3d5b00525b574bcb4b27a0e81353ae743bf8d8fc12f7f4fb62cd6cdf9
+size 52845

afa6db38737957b36e582bae5a44a647ee631452
Michael Chambers <m.chambers@digipen.edu>
2015-02-23 15:05:45 -0800

    Debug text cleanup.

--- TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d08ed2f66c503aa52607f88ffc044aefa18efd5cff1df311b8eb7a1d74154183
-size 19643
+oid sha256:b736bb32919313b2576a1b8dcd4554e06c3aebabc26857a78279cd5b264012cc
+size 19614

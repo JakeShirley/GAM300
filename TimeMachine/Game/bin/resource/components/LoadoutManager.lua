bf3473b807607542904f826ee20b627816d6c61c
Michael Chambers <m.chambers@digipen.edu>
2015-02-23 10:46:44 -0800

    Checking for persistent space before using it.

--- TimeMachine/Game/bin/resource/components/LoadoutManager.lua
+++ TimeMachine/Game/bin/resource/components/LoadoutManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:109f50501bb6afc0305d8132d15f5c0f0d51ac275fc0270df2924b79a9489a21
-size 10762
+oid sha256:d22cfedfc301b6904ea8d4fad2716ef89e0129c6de7dc7afc9bd9d668457ee82
+size 10816

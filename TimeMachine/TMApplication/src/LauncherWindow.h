7fc5963c5e22b5ce06dd6ede84e386d0c32a3258
Jake Shirley <navi@odinseye.org>
2015-02-22 22:06:43 -0800

    Added some startup feedback.

--- TimeMachine/TMApplication/src/LauncherWindow.h
+++ TimeMachine/TMApplication/src/LauncherWindow.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:744bcb1f78c6b56f1dbf0a84bd1a7fb24aee8edb4c9ee069b0146f41c6e6565e
-size 228
+oid sha256:2fbabee213f4171e48821087bbdc427c063d13baa923ae10c556d97366c8c79b
+size 253
--- TimeMachine/TMApplication/src/wxBaseClasses.cpp
+++ TimeMachine/TMApplication/src/wxBaseClasses.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfc023854fdc016de374d3af972f87881e64d94f2cc0bef8998dbf664d70957f
-size 31562
+oid sha256:51e31849ac1dc797693c3a40de7c66d61b41966cc84c3693985cd1e091114dfa
+size 32518

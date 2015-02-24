505b0a62b2237a5edca033dad0d7cc066a07bca9
Michael Chambers <m.chambers@digipen.edu>
2015-02-24 14:26:52 -0800

    Hack fix for frost breath moving.

--- TimeMachine/Game/bin/resource/components/Revive.lua
+++ TimeMachine/Game/bin/resource/components/Revive.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65a535b6013d2f3c10016a1ee474ace3161e7ce8cd24b1009352088aca424ade
-size 12839
+oid sha256:253c80b07e37a740ef2a4edae0b517854c10619081671cfa4ed0e530da171c54
+size 12939

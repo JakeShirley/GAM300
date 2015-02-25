f0fd1ad902eb19436dc7cf33be9993a3fc9c9421
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-25 14:02:16 -0800

    Local normals are calculated when using GJK, so convex hulls will use NGS properly, which should improve their stability.

--- TimeMachine/TMPhysics/src/OopsManifold.cpp
+++ TimeMachine/TMPhysics/src/OopsManifold.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0954fedf7b12af0fcd15fb059c1497bc12286d4ee6e84a93b94bc90f00cda9e
-size 16454
+oid sha256:6cb51f5fce73385a1a42343028387571b864ecb10088a4d52119407f3d47b2a5
+size 16425

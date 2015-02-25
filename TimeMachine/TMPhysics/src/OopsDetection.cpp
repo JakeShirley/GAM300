4f90ff696c18347a7f967a4fb15701df38a8b0d8
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-25 14:44:06 -0800

    Fix to me totally blowing it with collision groups.

--- TimeMachine/TMPhysics/src/OopsDetection.cpp
+++ TimeMachine/TMPhysics/src/OopsDetection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8358ae836a6d7485795fdc459fa649e38d788061c91d8ebf485b034c85c7e2a4
-size 25000
+oid sha256:6c97ae7611b2dd9a039da444e3e89d128bffe1622012af5695485acb6226f833
+size 25028

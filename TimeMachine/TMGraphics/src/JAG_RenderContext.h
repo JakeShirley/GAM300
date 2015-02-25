5b466011ab9549b06bd71f0578197d76c09c7a7d
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-02-25 13:44:42 -0800

    Animation dirty flag for SetCurrentFrame, Added Map/UnMap functionality in RenderContext

--- TimeMachine/TMGraphics/src/JAG_RenderContext.h
+++ TimeMachine/TMGraphics/src/JAG_RenderContext.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4a50b33b80d4eb9f72b967d7819cd5d4166d77dbb06813b23be7bc154df5dd4
-size 9456
+oid sha256:e36d40fc2afb3833fb6516305a4da87d74bc446f43d694c4e891734dd7a8dd66
+size 9532

d55d98151e868fc16a8312bca0aafd3ece22f7c6
Brian Inkrot <binkrot@gmail.com>
2015-02-26 14:06:52 -0800

    AIs don't have ghosts. Fixed FrostBoss IceSpike Spamming. Added Lightning Rock Models to the engine, haven't used them yet.

--- TimeMachine/Game/bin/resource/components/Revive.lua
+++ TimeMachine/Game/bin/resource/components/Revive.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:532aef76ce7e712ca8a715393ebd7fe1ea85def3011403ea026412ef862b50e7
-size 14167
+oid sha256:5ef00007e17bd1cde106f02b2d444a5138153529a0885ff85d238354673f82af
+size 14363

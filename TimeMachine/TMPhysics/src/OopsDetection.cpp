c39f0b36e417140e75a7bb80212f054973f2e7fb
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-25 23:11:29 -0800

    Fixed crash in physics between two objects that shouldn't resolve. Sorry about that.

--- TimeMachine/TMPhysics/src/OopsDetection.cpp
+++ TimeMachine/TMPhysics/src/OopsDetection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c97ae7611b2dd9a039da444e3e89d128bffe1622012af5695485acb6226f833
-size 25028
+oid sha256:66e91b1540656e17a5253271136ff4057095d39dcee296ccd583dc0ebfc11ccc
+size 24949

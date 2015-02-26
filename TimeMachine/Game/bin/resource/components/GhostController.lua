88cfc299002c886adf0a34132fcfeb1a813d46a4
Brian Inkrot <binkrot@gmail.com>
2015-02-26 10:25:34 -0800

    old stuff

---
+++ TimeMachine/Game/bin/resource/components/GhostController.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a4381d86ef7d608504999d35678b52030fbf94f6e9d3527ad2ad6947c8b1439
+size 13970
--- TimeMachine/Game/bin/resource/components/Revive.lua
+++ TimeMachine/Game/bin/resource/components/Revive.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65a535b6013d2f3c10016a1ee474ace3161e7ce8cd24b1009352088aca424ade
-size 12839
+oid sha256:5771e64f2f58a10a87a2e5dc452b46c84b0b339331a45cb1a8f234b582c6344c
+size 14105

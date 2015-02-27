43f5744eca4d8632edbb1ed8e7f2828f1d5ab381
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-27 13:05:09 -0800

    Angular drag exposed to editor. Fixed crash bug when creating archetypes. Added additional information to duplicate assert message. Physics sphere archetype.

--- TimeMachine/Game/src/RigidBody.cpp
+++ TimeMachine/Game/src/RigidBody.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33ef7f9430f7b4a6599566f7a65adf7c447dccb5574eb617069dfc4fd6673a86
-size 15231
+oid sha256:47a4858f4f6eb58eac8b1d8d67f07feac445b1716af504b5c009142a7601816f
+size 15941

a6668c2831509f73de0a97d1c6b8a92950f432d8
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-02-26 18:02:45 -0800

    Fixed a crash I introduced by fixing transform crash.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c552a2615c790ac931d8cf6a8438fc85ffe30dd1b3a3ff1c972ea2881517137
-size 53199
+oid sha256:088dc2e262e65ca994ba1c646cc49e3940c724b04cb7be91f5ee1681f79c5742
+size 53293

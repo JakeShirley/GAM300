3aa44a5d57b1f2afcaade450db5cb1db920f21ae
Brian Inkrot <binkrot@gmail.com>
2015-02-26 22:55:30 -0800

    Fixed new ice spike collider going to (0, 0, 0). Fixed Players being able to walk through the initial icicle pile. Justin tweaked DefaultLoadout bullet sizes.

--- TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0e7739c48558d67b84bfac151ba4d38bed007f12a0f38797c38ab8b87a29692
-size 19856
+oid sha256:9a661c688972dc52a043027f4e6c647a47e8d5b393b61c7150aac3b94564c9a2
+size 19862

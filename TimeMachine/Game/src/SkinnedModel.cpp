31cf8e935fafe22d151f0066fec9db28eb1dae1d
Jake Shirley <navi@odinseye.org>
2015-02-28 15:53:30 -0800

    Additional error handling with SkinnedModel.

--- TimeMachine/Game/src/SkinnedModel.cpp
+++ TimeMachine/Game/src/SkinnedModel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de01c56848e6fc1931f6fc22a858b8a644ed8af832eb4d980d9dde0b44f00e2e
-size 11187
+oid sha256:9c015acb723e9dd9fae5db78d0199c5450f50b0b8357cd100a70df11140ba9fb
+size 11319

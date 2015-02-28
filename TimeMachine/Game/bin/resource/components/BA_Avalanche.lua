0de03528b4465aaae343be91de353b6e6d93700e
Brian Inkrot <binkrot@gmail.com>
2015-02-28 14:33:38 -0800

    Updates to playtest build Friday 2/28

--- TimeMachine/Game/bin/resource/components/BA_Avalanche.lua
+++ TimeMachine/Game/bin/resource/components/BA_Avalanche.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1501d07e225a32b508bb8c3492ab1e9ce1746f58e166b7957b83b030b2c4b71
-size 6485
+oid sha256:a646596c6cea1511e54958de83b7a3032654c35bf52215b6bc9660841a65d6bd
+size 6595

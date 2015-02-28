59f172324e901b3aa7acc316814db32452805282
Jake Shirley <navi@odinseye.org>
2015-02-27 22:23:44 -0800

    Made a Transform archetype that is used with Create Transform.  Don't screw this up guys.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:692bcebaa77652ba99d936a5bf3519c2ff37420f12af408dc5a15094db4e2381
-size 52930
+oid sha256:7775a31bc19f79638153d0d9acdf6e669b5da666580cc25f9ded4d330e024130
+size 52948

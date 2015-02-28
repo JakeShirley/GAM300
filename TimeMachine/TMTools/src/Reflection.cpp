4203841d2474b09d1389a350c8178ec11ddb93ec
Jake Shirley <navi@odinseye.org>
2015-02-27 22:15:33 -0800

    Added type tagging and an operator section to documentation.

--- TimeMachine/TMTools/src/Reflection.cpp
+++ TimeMachine/TMTools/src/Reflection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61596979663456c82eeb0483f857138627fb2bba9eb8e764030e56d581f74aae
-size 7286
+oid sha256:72fd84956c26fabaa7fece58fd078d83514b64acd9a0d22ac273d2101316cc0f
+size 8710

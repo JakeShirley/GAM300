26ec725d3b578887b057a9995991c284e1443b90
Cody Centers <murtag27@gmail.com>
2015-02-28 14:08:54 -0800

    This has been in my changed files for a while, i cant quite remember what it changed...looks like breath stuff.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afe39421395b783b45e1efd474c320310a52acfbca44da6ad07ef84dc6e1976e
+oid sha256:42ef287307d7ba0a8c62dbf62e88b8975b507e51d699d6e3a4602c5b464b6a03
 size 82714

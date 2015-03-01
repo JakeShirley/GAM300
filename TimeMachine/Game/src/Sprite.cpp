3a5731d4c6b2b0a22ff3b52bc6699f51b93eca3b
Jake Shirley <navi@odinseye.org>
2015-02-28 16:26:02 -0800

    Added Get/SetBillboard to Sprite.

--- TimeMachine/Game/src/Sprite.cpp
+++ TimeMachine/Game/src/Sprite.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a30b671176b6209800d759413bd6ada0d9e4483f1b088e14a20311e8c839e4d
-size 8755
+oid sha256:9f7a3988ba525eccfe36c34b5708c6fb6e010f22ec195271316396e6b94d39fa
+size 9314

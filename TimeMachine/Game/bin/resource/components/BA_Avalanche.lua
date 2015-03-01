9e3b334917159428b9285e8558e61613ed6299a9
Brian Inkrot <binkrot@gmail.com>
2015-03-01 01:09:49 -0800

    Fixed Avalanche attack use imbalance

--- TimeMachine/Game/bin/resource/components/BA_Avalanche.lua
+++ TimeMachine/Game/bin/resource/components/BA_Avalanche.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22a32251b9414fbd11ffd514b40b0bae78fa20be1e22010ea605e3bf2093894a
-size 7501
+oid sha256:0cd20f8d58f5e6120b8ba55c3756ef1420d895d7c551802f941e85c78a564e3e
+size 7495

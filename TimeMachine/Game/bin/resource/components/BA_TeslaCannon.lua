aa4fa4ed745d1f5eef357c51d7323b3903b501bb
garrett.allen <allengar22@gmail.com>
2015-03-01 00:06:25 -0800

    sorry pushed testing thing.

--- TimeMachine/Game/bin/resource/components/BA_TeslaCannon.lua
+++ TimeMachine/Game/bin/resource/components/BA_TeslaCannon.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33a008283b77ec1b07f6024fbe33bdae4b6bfd2551aa12db22cc4486a4ef3bbd
-size 12695
+oid sha256:4e12aae337eacf776b6c47ba34173c4ad8847c3971306f1722e9beca7c51c59d
+size 12693

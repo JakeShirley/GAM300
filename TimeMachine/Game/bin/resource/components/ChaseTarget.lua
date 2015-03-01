74713e0a34cabbd9a448721511bf1a74d3b518a4
Brian Inkrot <binkrot@gmail.com>
2015-03-01 01:08:21 -0800

    Ghost Owl chases backwards now, so it looks right.

--- TimeMachine/Game/bin/resource/components/ChaseTarget.lua
+++ TimeMachine/Game/bin/resource/components/ChaseTarget.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5722fbbb7e349608a73db80147f7df4f6ca2afcfdeb2e4f4d806ab7888620d87
-size 4053
+oid sha256:269da338d3e686f3bc25fe22c35998d0bbe51f3859cc52770898f4edd25c4a46
+size 4560

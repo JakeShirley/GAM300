8cf3889d54e07350cfb19e075161f5e011570bf4
Jake Shirley <navi@odinseye.org>
2015-02-28 17:21:27 -0800

    Fixed label for aspect ratio for Camera.

--- TimeMachine/Game/src/Camera.cpp
+++ TimeMachine/Game/src/Camera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70a238726902a8a42a06445c4a672ed6d07873d1706af13331a3f98098629dfa
+oid sha256:afb41b322ddf8fcbbbe405557543ca9bf88933b5ff1d7fcb7d9dc27ea61fa718
 size 11974

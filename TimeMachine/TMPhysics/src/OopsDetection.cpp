32a38624d8b4f95f2b93f88b7e913092c3a7cddc
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-03-02 17:50:19 -0800

    Fixed bugs for static meshes when colliding with ghosts.

--- TimeMachine/TMPhysics/src/OopsDetection.cpp
+++ TimeMachine/TMPhysics/src/OopsDetection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9383572dd17b9c99fca7a31a68079f2d52e38b10e487accebf3000e29a9cd1f0
-size 27543
+oid sha256:83dd73a8944a9bc1eb224fa76519d32303a2f97976a8dc629cb1ff4669a052fb
+size 27905

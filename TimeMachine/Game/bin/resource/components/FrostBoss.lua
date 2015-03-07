63e55df7cf1aef289170c8bc0416426143b69844
garrett.allen <allengar22@gmail.com>
2015-03-07 14:57:36 -0800

    quick fix to boss titles. now use SetBossMaterial function on BossTitleLevel after changing levels to get the correct title

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e22cab3d2c42e7c8cb7f8fca046c88b26e45e58b2bfa9ed0624eae41577c8daf
-size 83238
+oid sha256:2c5f63a72f6c885cb4fc35c09c1031f0f047360937a78682422a4a01dd3eb22b
+size 83232

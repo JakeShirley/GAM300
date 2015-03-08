b671f20fd4713be05fb4596accb521b6489aa6b0
Michael Chambers <m.chambers@digipen.edu>
2015-03-07 18:54:03 -0800

    Loadout color stuff.

--- TimeMachine/Game/bin/resource/components/Loadout_ShockRifle.lua
+++ TimeMachine/Game/bin/resource/components/Loadout_ShockRifle.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c356c071c87b5099d823f8dbcc187fcb5187589ce49717934956ed58c3a4eba3
-size 11908
+oid sha256:ad7cda0e5a3cbcd995895b6914ed811f11bda17b2016bd24b4a3ba53fcc69197
+size 12101

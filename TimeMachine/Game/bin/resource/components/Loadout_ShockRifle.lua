387ecf8cc3c929f385f2f2fc820ff718554dd49a
Michael Chambers <m.chambers@digipen.edu>
2015-03-07 17:44:39 -0800

    ShockPulse can't maintain super while dead/stunned.

--- TimeMachine/Game/bin/resource/components/Loadout_ShockRifle.lua
+++ TimeMachine/Game/bin/resource/components/Loadout_ShockRifle.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ef7f3fd938764349c2b61c664e46854e6e680ca81ad14a943c689b1f8af4e0d
-size 11423
+oid sha256:d30b42b72630fb88acd05b7d7ca029148d2f0baee1514ea0d4acae31ac600166
+size 11544

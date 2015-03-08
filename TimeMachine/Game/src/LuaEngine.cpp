40c1eab90427fb58971d300b88bb0f24c6c2dda0
Jake Shirley <navi@odinseye.org>
2015-03-08 16:02:14 -0700

    Added script breaking for event leaks.

--- TimeMachine/Game/src/LuaEngine.cpp
+++ TimeMachine/Game/src/LuaEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:087f4b0972eb70a7029ac63427422de7b12ad6d779b131e8c00838fe30ea912b
-size 9016
+oid sha256:157049c4b3a83ffe7a14651abcca2011175164c3cafcd891966f8eb976cc7c8f
+size 9743
--- TimeMachine/TMTools/src/LuaLib.h
+++ TimeMachine/TMTools/src/LuaLib.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f37aa117e61204cbd4b37fa83d77295ff0b0892f895d85a3f90d4a1b56f27c27
-size 5769
+oid sha256:c654506fb9b47550e4b99275934f2e4269e01bd71357c18f2ec4ca7b9fd79fc5
+size 5821

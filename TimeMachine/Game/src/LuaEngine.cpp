e79249b4ea76d46a8d10f1568080bbb8dc9e856b
Jake Shirley <navi@odinseye.org>
2015-03-07 18:50:05 -0800

    Added Engine:Get/SetDtScalar.

--- TimeMachine/Game/src/LuaEngine.cpp
+++ TimeMachine/Game/src/LuaEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5677fbe03c1d1ebc58ce8b046bcac76bc6dd18099cf48c1cf993dc63268bfaf0
-size 8638
+oid sha256:087f4b0972eb70a7029ac63427422de7b12ad6d779b131e8c00838fe30ea912b
+size 9016
--- TimeMachine/Game/src/LuaEngine.h
+++ TimeMachine/Game/src/LuaEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a473338d3a93ea4aa24af51c5c885ced518bf6ea09dd65fd1e233f245a14977
-size 1732
+oid sha256:224969bb3c362c3343385964de6f47046043665c7bc14e44472cc887f9fd5729
+size 1796

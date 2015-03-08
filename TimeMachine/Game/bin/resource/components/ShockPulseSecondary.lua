2616354ac3a6c0db2ccfa898fe780350e80fe44b
Michael Chambers <m.chambers@digipen.edu>
2015-03-07 16:43:43 -0800

    Made the ShockPulse secondary not cause chain reaction on simple collision.

--- TimeMachine/Game/bin/resource/components/ShockPulseSecondary.lua
+++ TimeMachine/Game/bin/resource/components/ShockPulseSecondary.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf70c6a0d224a97f7ced18a6fd34faed8d97340b5338ba78d73c78bfe7485071
-size 3937
+oid sha256:f5221e020cf59b3a9abd534130dd18f2f5abd21f7b342caf8a56004eb12df876
+size 3981

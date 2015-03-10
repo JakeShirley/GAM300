c5464de1de044590de6de2e7f16e3fef3d449be6
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-03-10 16:42:49 -0700

    Commented out emitter debug text

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da0032e540fd1ed7fb42f8235aa3ddc18b5bfa6b86053bac5bb93aef600aacd5
-size 85183
+oid sha256:0ad17bdd5d336ab188a67d9b860c6ac5b7e28137a6e6b7791dc925a891ea33a0
+size 85185

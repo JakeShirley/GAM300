3cc08865179de8bfae3a4f3c208b76daf1272a3c
Jake Shirley <navi@odinseye.org>
2015-03-11 12:24:12 -0700

    Added texture reloading at runtime.

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80ff478fed8c8cc5e336154c3fce112171629eaeb92f439034a5ae71088b9010
-size 84963
+oid sha256:20a7544e8cbb0947307e5b13a3d59b901d430413738985a6a4fa2f0ab0adfd12
+size 84987

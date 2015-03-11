5e5ad0e26338f56d6cf5147d4b4cadf091e2a619
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-03-10 22:42:42 -0700

    Trying new tone mapping operator. Fixed particle editor.

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bd19c6ac055bda8c25bab57a3ec137d8e5fb12fd2b1c770334127d441e42148
-size 85221
+oid sha256:80ff478fed8c8cc5e336154c3fce112171629eaeb92f439034a5ae71088b9010
+size 84963

63df68dd4ae1a3c030ed5d9d9797242cd4199a06
Brian Inkrot <binkrot@gmail.com>
2015-03-10 19:05:47 -0700

    Upped max particle instance count.

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ad17bdd5d336ab188a67d9b860c6ac5b7e28137a6e6b7791dc925a891ea33a0
+oid sha256:7f48f29f2d581b95e1f4a34b3e36977b3483c23e89eea1735fbd91197ae8ddb5
 size 85185

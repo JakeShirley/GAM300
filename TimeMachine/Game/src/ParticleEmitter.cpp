24470623f7362842117d996324d4257c98ae9203
Brian Inkrot <binkrot@gmail.com>
2015-03-10 20:20:14 -0700

    Updated particle emitters to check more emitter pointers.

--- TimeMachine/Game/src/ParticleEmitter.cpp
+++ TimeMachine/Game/src/ParticleEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40cf1dd8960d474fbf8a90b69fe99aaee2fd8ca3b506d576ba9dd62dc0500cb3
-size 18369
+oid sha256:bfa58dcda736a063b11ec08fc47313daa7a736b62605628f91b98344db099ece
+size 18871

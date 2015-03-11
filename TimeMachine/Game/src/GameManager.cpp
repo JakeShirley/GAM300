dfd333495374b7628c66943160c0e76acd203b9e
Jake Shirley <navi@odinseye.org>
2015-03-11 12:22:38 -0700

    Extra error handling on destroy for GameObjects.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3791759067820406aa57e7ca547645193b70afd10111afa3378422becb651fce
-size 53821
+oid sha256:92c5478489d6c9766cbde5190f4d93c482bc2f61f984aad1bb8dcf1ee1924cd5
+size 53853
--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d6e32ddc87b1bf1cf9bc10d2677f9fb57c528db2e35218a6fb32c8a10592616
-size 45354
+oid sha256:6f69883f4a160fc2d11c76debdf3707a9832569ae5209d11b9a1920510fb4428
+size 45456

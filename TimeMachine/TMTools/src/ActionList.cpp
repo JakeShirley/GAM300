0adb7ff0c52c2a928423edf50dea767e75ca249c
Michael Chambers <m.chambers@digipen.edu>
2015-03-12 16:07:19 -0700

    Went macro slaphappy with easetype bounds checking.

--- TimeMachine/TMTools/src/ActionList.cpp
+++ TimeMachine/TMTools/src/ActionList.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59051b3125b7cca983cae2b87c48dbdc3a64448be7395162634b43379df596dc
-size 30423
+oid sha256:ef846c4c94d1beeda0f61140ad06f17bd1a4820c5e664151f5e0983adc6a21a6
+size 29524

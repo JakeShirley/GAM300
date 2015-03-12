106aa4cfd3751a0a5fd857ac1817d72f440af2b6
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-03-11 17:42:13 -0700

    Moved thread deletion to unload instead of destructor of static objects in the hopes that it helps thread freezing problem.

--- TimeMachine/TMPhysics/src/OopsThreadPool.cpp
+++ TimeMachine/TMPhysics/src/OopsThreadPool.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc4a0e3d5346ce8a92966cba07d423e20cd4b86c56b50499607b832e42ada436
-size 4697
+oid sha256:782bba2abce9e3d3a5859abb3a942e047292e70232a28e262656b424e84ae14f
+size 4749

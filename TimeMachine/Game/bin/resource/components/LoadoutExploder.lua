c52aeb4913a374589f11d4338e1818f131a09975
Michael Chambers <m.chambers@digipen.edu>
2015-03-12 18:21:12 -0700

    Got loadout exploder to explode again. Seems to have been an issue with initial collisions on creation, so I delay their velocity being set.

--- TimeMachine/Game/bin/resource/components/LoadoutExploder.lua
+++ TimeMachine/Game/bin/resource/components/LoadoutExploder.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:432cb2a07371a49ae44b36ca1fce05081488de3f0f27819b88d3039ccca84d4e
-size 2696
+oid sha256:127118a723e1c5954f0abb02dc25afb1c5d17223cc6a935ca824b4c7f884cf69
+size 2766

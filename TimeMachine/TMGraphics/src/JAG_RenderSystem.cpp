832e379938244a6a91bf6225684be78efd786f10
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-03-12 23:03:43 -0700

    Depth sort non-bbq sprites

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b3bbe5c9d92dff10e7323b0cff0a76640f28ddbfce5a8a948fbed93e6767d21
-size 84413
+oid sha256:7bd43d1b92de5f28016195ae7402da18e5705e37560580f40c034862c9372f9b
+size 84682

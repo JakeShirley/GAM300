bb29810cd947a5a9158cd554c9e1707b6b3773a6
Michael Chambers <m.chambers@digipen.edu>
2015-03-12 23:17:39 -0700

    MainMenu stuff. Organization and space reset.

--- TimeMachine/Game/bin/resource/components/MenuTrackBehavior.lua
+++ TimeMachine/Game/bin/resource/components/MenuTrackBehavior.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9ccc9ae403992f37cca95d1f295b90e459e04c0573047155ab94997c37dc13c
-size 1700
+oid sha256:8b98bcd374ff692c2535264267f43fbe997ef98ac2ccb67223921cc60e98e8ce
+size 1771

ddb330b89ecfaadbaceba98d9ea5adf5a50454b2
Jake Shirley <navi@odinseye.org>
2015-03-12 18:22:38 -0700

    Removed unused variable.

--- TimeMachine/Game/src/GameManager.h
+++ TimeMachine/Game/src/GameManager.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dc22cf350624e0a99425f99ec8bd2ac100543f392b596fa983855cdde0d2144
-size 5259
+oid sha256:603c377fb5ae4a2cadbb588b01087648b9415ed7d686be5e2d7422cabe54e7ed
+size 5144

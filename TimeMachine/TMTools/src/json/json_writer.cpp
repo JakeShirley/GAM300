be0e55ee931ad756689e2429fe0916498b5c93ad
Jake Shirley <navi@odinseye.org>
2015-03-13 12:59:55 -0700

    Removed float rounding in Json for now, caused more issues than it solved.

--- TimeMachine/TMTools/src/json/json_writer.cpp
+++ TimeMachine/TMTools/src/json/json_writer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ecbd2ef415f8f5a6781f94a5705d4aa1f03360f9a4e57327ec6dcd42317aa4d
-size 33190
+oid sha256:21f26ceec22955b66457d78008e03cb2fc4c54be84ff98e82f66c83d7fa5806d
+size 33460

b1ff538109ba0897adfa3a5944a6c07e6f251e70
Jake Shirley <navi@odinseye.org>
2015-03-13 13:02:58 -0700

    Removed useless check in json.

--- TimeMachine/TMTools/src/json/json_writer.cpp
+++ TimeMachine/TMTools/src/json/json_writer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21f26ceec22955b66457d78008e03cb2fc4c54be84ff98e82f66c83d7fa5806d
-size 33460
+oid sha256:b8e342ce1d2c0e2ba998046bc5708ebe6d6c5796c9b4adbc34ffbaf2b83ba9ea
+size 33423

5e18582d12e31d63b5de52c410d97fb1c731d44e
Jake Shirley <navi@odinseye.org>
2015-03-13 16:59:21 -0700

    Updated sounds and moved them to BossAbilityManager.

--- TimeMachine/Game/bin/resource/components/BossAbilityManager.lua
+++ TimeMachine/Game/bin/resource/components/BossAbilityManager.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c0d76ea40e3dd7160e1fc601308374fea1f6864d25eadc57124e16a79fc11c6
-size 7902
+oid sha256:f1731bbffe7f12eab456652bcc0717aa87097f80abaf70f77230c22f928cc145
+size 9180
--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfb6c92c74b493c4ab6b98bedb779ac86912950bb9c8971b752607bcb1538614
-size 83286
+oid sha256:8de06a0ea611700dc029417fbda3d4ba5f7434a342e3e6bc4b46adf6c8a18d88
+size 82814

9a935211f386693a42e6c33eefe85c0810738ba5
garrett.allen <allengar22@gmail.com>
2015-03-14 16:31:17 -0700

    small fix to linking. Almost fixed things not rotating correctly when you update an archetype on the other linked archetypes.

--- TimeMachine/Game/src/GameObject.cpp
+++ TimeMachine/Game/src/GameObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee6109bb1c6d86ed2fb6f3eb3932420170053cb4555950a18648004c5d422ab2
-size 45041
+oid sha256:d3fe5adca76e25885c7110f5926d75682b8fafb5f4871ce0b1aebb50e4c61c9b
+size 45067

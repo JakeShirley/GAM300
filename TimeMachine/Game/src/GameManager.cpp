be9ac417fe415f1dd08e3dbcc87162be152aa036
Jake Shirley <navi@odinseye.org>
2015-03-13 20:03:10 -0700

    Fixed exception handling with scripting events and for input callbacks.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68df40fc624115a4447bb85a9ee24c100318f7e4005ef57bcf8a39641a1d296d
-size 57369
+oid sha256:8976f25d099a096c261d55997c5112a26f6cd17e9dcb01eaab66430abd84052a
+size 57981

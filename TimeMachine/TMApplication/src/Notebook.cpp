4ac716ba79a28ea32401e8502831624bcc29c4b7
Jake Shirley <navi@odinseye.org>
2015-03-13 20:24:49 -0700

    Fixed tab serialization.

--- TimeMachine/TMApplication/src/Notebook.cpp
+++ TimeMachine/TMApplication/src/Notebook.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af3a9d43a87f4d7d50c60ac04883d0c8382619f3a22a2cf289656d9c6e3d0e50
-size 7921
+oid sha256:da71892c95799d0aebe526345e09b1acbebb2e6e9b8f5c32f6cf8227f9a2e7b1
+size 7957
--- TimeMachine/TMApplication/src/ScriptPanel.cpp
+++ TimeMachine/TMApplication/src/ScriptPanel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee6518a8c5053fc3618b4d0bc8f1bc007f74ca09f30c14a5398a7f1e3dd4ff3b
-size 27618
+oid sha256:bcc74cc231ee2a9d1cdb07564d764f5a0f40c53e90600b6de9a1b0497f5d6cb7
+size 27644

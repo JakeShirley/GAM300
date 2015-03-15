ea5337cad4ac45efd764f66811774c07111f4889
Jake Shirley <navi@odinseye.org>
2015-03-14 23:52:21 -0700

    Fixes for stat boards.

--- TimeMachine/Game/bin/resource/components/StatBoard.lua
+++ TimeMachine/Game/bin/resource/components/StatBoard.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ccb3ab0fa6e8c2e0990e5a5834a99875abaa3fce4ea660471f39d0e059a6851
-size 9641
+oid sha256:32eefe474bf1b374b592907daaba7254d2c2e7569a93a5a9e1557a95cc913c02
+size 9101

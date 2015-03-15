31072332d830a045052b0b2df94fcb19603a3e48
garrett.allen <allengar22@gmail.com>
2015-03-14 23:13:33 -0700

    mikes doof

--- TimeMachine/Game/bin/resource/components/BA_GhostOwl.lua
+++ TimeMachine/Game/bin/resource/components/BA_GhostOwl.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79603265c31ca814ff7b6f815630ffe3065427a854d1ecd8ab46f8ffec331012
-size 4067
+oid sha256:df3225a603550b2872faa5e2653aea81083cfb7cc1aa072ca759110dc67d3c8d
+size 4069

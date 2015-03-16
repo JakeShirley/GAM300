b0c48e6e21952a3a4edcde2a2f5490be8ebbf5e0
Jake Shirley <navi@odinseye.org>
2015-03-16 11:58:57 -0700

    Updated Saph to use AnimationController.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed68bade49565ee8e1c7142f103d2f73b89e5ed53e3faf080529d7877e948c92
-size 83518
+oid sha256:6a49971fcee21295312219adf53f33f78e832f8a97820e1e618a47b0c291d2e7
+size 78182
--- TimeMachine/Game/bin/resource/script_libraries/AnimationData.lua
+++ TimeMachine/Game/bin/resource/script_libraries/AnimationData.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b0cc5f653b7c53533515ea920765843424669dce16401fbf6ae1c2a55bb854d
-size 5655
+oid sha256:c329d6b7e329a0b5df2677184039cb618613369a9df6696bec0f4b51262ce60c
+size 14209

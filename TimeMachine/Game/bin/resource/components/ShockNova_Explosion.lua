4d89b59ca777d683132f1da2493455f6eb1c21aa
Garrett Allen <garrett.allen@digipen.edu>
2015-03-19 14:39:48 -0700

    some SFX and another shock nova ability visual change.

--- TimeMachine/Game/bin/resource/components/ShockNova_Explosion.lua
+++ TimeMachine/Game/bin/resource/components/ShockNova_Explosion.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bfc49e61f77bf83a61c2b2720d1c4df973e788c113f233de3161987bec99534
-size 2100
+oid sha256:088e2aaeda53ed6356d4640af408526fd4a3ca0e65570f6230686c77ae5898bf
+size 1372

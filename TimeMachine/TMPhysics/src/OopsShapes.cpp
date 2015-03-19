baef91d55f93270794c61ecd3602ed6b676bd783
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-03-18 17:46:32 -0700

    Nicer debug draw for meshes when selected.

--- TimeMachine/TMPhysics/src/OopsShapes.cpp
+++ TimeMachine/TMPhysics/src/OopsShapes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95fd4d6dd73ae6fc9e2da6c5f8fa1d449c035c2d7679082565a1c97c30f52502
-size 7607
+oid sha256:a929fcf46ba0cac1168322a03d789023f25b9e52759bd4195dacc6ed8d73a611
+size 7570

0bd0f19ec4319de30458965a50d4c4bcabfffaca
Brian Inkrot <binkrot@gmail.com>
2015-03-18 22:39:39 -0700

    Quick fix to Orb Pong

--- TimeMachine/Game/bin/resource/components/BA_OrbPong.lua
+++ TimeMachine/Game/bin/resource/components/BA_OrbPong.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13c78e5da240d15eeb793f45adaae54abf3f3a15dbd8907fa7e82c6da1310844
-size 5712
+oid sha256:5c7eb56c723e066a07dcd1a8d1dbb2d868ea84a69796f4b300d98cbfca821f4a
+size 5719

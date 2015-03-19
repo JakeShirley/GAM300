227482a630035ebf18233c5b30a7667cc29f21f4
Brian Inkrot <binkrot@gmail.com>
2015-03-18 22:12:50 -0700

    First pass of new lightning level, with updated owl locatons, respawn locations, etc.

--- TimeMachine/Game/bin/resource/components/WaypointMovement.lua
+++ TimeMachine/Game/bin/resource/components/WaypointMovement.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a0e66ad090c5bd34247a0667130f95d1bdc2918c20d3d28349fa3d587e09be1
-size 7735
+oid sha256:be6ab5a70a193c9efdd2b6e0a2c0ece207dd3c8696609b8daf1c337d2d2a3a0d
+size 8069

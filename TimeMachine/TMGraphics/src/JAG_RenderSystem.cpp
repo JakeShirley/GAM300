03fc431e8161e75f32bd88f545af02bc9d83155e
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-03-20 05:19:31 -0700

    Removed volumetric lights from lighting/final pass, now draws after 2D straight onto light-combined scene

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c09993a1707d015b511e01f10ebdfbc0fd36359fece7956f00dc43a68292e6d2
-size 85297
+oid sha256:d52f73cad3cd1ccc8059c9c4b242d6b268fb8cfe16d79fcfa37835ae26622118
+size 85202

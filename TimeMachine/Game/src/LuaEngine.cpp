ea7b7e08fc1af988d1f0a48d58b84438f6286058
Jake Shirley <navi@odinseye.org>
2015-03-19 22:27:19 -0700

    Exposed GraphicsQuality in Lua Engine.

--- TimeMachine/Game/src/LuaEngine.cpp
+++ TimeMachine/Game/src/LuaEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d10033c4f4fe9aa0cb8ec888538797f5050a80e6f41d4f39f6aa06559035963
-size 9690
+oid sha256:866bccb965a590b66410eddb816e405c58a3f8aa78f226d34ee81f00861f01c8
+size 9905

51319cd82f390a1ac993466fde6c07a37b80ff1f
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-03-20 07:36:59 -0700

    Platform Drop for bottom center

--- TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
+++ TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f140ab2c4fc89eca60868a044440fa3f3629c82fe4ef151bf9389735a95bb1e8
-size 5507
+oid sha256:ea5dda97f64e73ea20e13a616492dd7083657afef66cc269c3c5ce56bcb3deb5
+size 6501

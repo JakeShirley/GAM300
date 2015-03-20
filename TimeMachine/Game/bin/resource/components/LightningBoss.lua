8ec1b3f22f102fd541716ec4e510302bba483e35
Brian Inkrot <binkrot@gmail.com>
2015-03-20 09:23:46 -0700

    More tweaks to Boss 2

--- TimeMachine/Game/bin/resource/components/LightningBoss.lua
+++ TimeMachine/Game/bin/resource/components/LightningBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bd15ab4c35ae00867bc222411f07cfcaefcfc5a4ee512932e0f2842b51ef990
-size 44460
+oid sha256:4441b9711537f29b2e1c068e3451a0f036dbb1a03f998dbb67c58e56b8d9a29d
+size 50761

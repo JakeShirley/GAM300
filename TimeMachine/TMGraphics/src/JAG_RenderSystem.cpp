f3ee158a0dc0936af5a0ce3d7201b1cc34b95eb2
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-03-20 01:59:01 -0700

    Graphics space shadow map generation untested

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aaf76628b4e55fae6cea7b73a700a96077341880ac56bf9f07c1b4d863a2d04c
-size 84811
+oid sha256:9a963679a231ffa3080c9ee7fb8bf9a531bb100a787281760583fee09b9e14d5
+size 84893
--- TimeMachine/TMGraphics/src/JAG_RenderSystem.h
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec9820b2ed25394499d896d63e15556eef22b3868de956279d2f0ab9155fa73b
-size 11738
+oid sha256:04266f800200da8add5eb17c5f9248b60df5b33663c20d68da81e8485ea8b7c7
+size 11499

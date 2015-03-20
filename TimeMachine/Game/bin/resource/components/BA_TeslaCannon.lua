5afd9e1217f52192c98b53cf1f3e6b8e6005ade5
Jake Shirley <navi@odinseye.org>
2015-03-20 13:07:05 -0700

    Boomerang Buff, Tesla Nerf

--- TimeMachine/Game/bin/resource/components/BA_TeslaCannon.lua
+++ TimeMachine/Game/bin/resource/components/BA_TeslaCannon.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13ed9710ea79bdadf93e6216deda7b8c5bb5906952207a4d3dd078baa3297f37
-size 17938
+oid sha256:49253dfd6b742599552482bba6de8c24a5b3cacf763e76c96721727a6594591e
+size 18138

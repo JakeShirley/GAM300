2fe9a35b9d56646255af38d1b73eca48e1fee717
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-03-20 05:59:13 -0700

    PlatformDrop awaiting Brian approval

--- TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
+++ TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4e379b53000121e4ebda56c0ad23bbd86b8aa0aa08da2a065b1c1c6a3a2bdf5
-size 5376
+oid sha256:f140ab2c4fc89eca60868a044440fa3f3629c82fe4ef151bf9389735a95bb1e8
+size 5507

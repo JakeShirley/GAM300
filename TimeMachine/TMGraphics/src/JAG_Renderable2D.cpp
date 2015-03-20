6a06abe87ac6bea9f411b1afe9a24c3ecb04c0f1
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-03-19 23:44:05 -0700

    Added sprite alpha, tweaked layer alpha blending equation so it looks righter

--- TimeMachine/TMGraphics/src/JAG_Renderable2D.cpp
+++ TimeMachine/TMGraphics/src/JAG_Renderable2D.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f89ecee7ee732fc5769f80db9431dd97c208e9dc572200d1d8419a49c838cf9f
-size 3649
+oid sha256:978f5d912970b488ad3c969398dd8acad51235500f0203a5e7c3bf840b7852fc
+size 4137

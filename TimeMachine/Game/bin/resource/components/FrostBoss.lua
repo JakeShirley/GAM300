eb50b5ed93237c0ddb358fdaf2c8f933d3b1a8de
Michael Chambers <m.chambers@digipen.edu>
2015-03-20 06:33:15 -0700

    Made it so frost boss doesn't die multiple times and make multiple portals.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0260d5cd187cd96c65c594d41bffaa2dff40f404c53b6a9652ee85797a5ee965
-size 79120
+oid sha256:be50cba526219ecd53044e0ec70df9888f8001abe6785908077a1ea5ed3ba9a4
+size 79170

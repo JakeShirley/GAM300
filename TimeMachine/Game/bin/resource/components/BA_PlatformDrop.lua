e3030d969f2b3e5a07e58d1dd3bf700f2799da89
Brian Inkrot <binkrot@gmail.com>
2015-03-20 09:49:26 -0700

    Cleaning up a couple attack sequences, tweaks to later phase sequences, and trying to create a terrain material with lower restitution, but it literally made everything explode.

--- TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
+++ TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea5dda97f64e73ea20e13a616492dd7083657afef66cc269c3c5ce56bcb3deb5
-size 6501
+oid sha256:80393cec28f5ad3460f1dbe7b02a816e153ede45edd8f0ef8063ee3a3c5efe0d
+size 6503
--- TimeMachine/Game/bin/resource/components/LightningBoss.lua
+++ TimeMachine/Game/bin/resource/components/LightningBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4441b9711537f29b2e1c068e3451a0f036dbb1a03f998dbb67c58e56b8d9a29d
-size 50761
+oid sha256:cc019394ff3e5069ab84d55a68e8e4126bbcd50605da5bdddb13136214de2749
+size 50773

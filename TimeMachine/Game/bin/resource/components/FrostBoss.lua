7cebd60f8659a3cca41681556f828cdfad63ea42
Michael Chambers <m.chambers@digipen.edu>
2015-03-20 05:00:20 -0700

    Commented out changing snow color so it stops being black like bomb damage.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db3f2ed6984421c6167ecd60c9aa1a6f44fecb660e414e4cc22ccfb1bc500ef4
-size 79114
+oid sha256:c16f95c2dd8a8c2cc5b38b84188b50b7668e0ce8ce2be2e195ccdd818a168301
+size 79123

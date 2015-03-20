c346d934b6a2c6141c1ee6362c526150970590ac
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-03-20 05:20:31 -0700

    Platform drop further along

--- TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
+++ TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0deec769c7a92c42c15821553b6d47b48512c24854d3470879c0ab67bec6d306
-size 4583
+oid sha256:1f1f72ab180b71f75c8db6739816ad7d25a7fc40d29de14de59984ccc3debf0d
+size 5374

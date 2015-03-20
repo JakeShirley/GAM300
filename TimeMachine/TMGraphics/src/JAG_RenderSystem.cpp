6853a0e178b55fed0677c93f38e674e7cf10ce34
Jake Shirley <navi@odinseye.org>
2015-03-20 05:38:51 -0700

    Added profiling to RenderSystem.

--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c09993a1707d015b511e01f10ebdfbc0fd36359fece7956f00dc43a68292e6d2
-size 85297
+oid sha256:cdb71e949d25de9cdc49f2189ba9c1b8fd80e2f95d006c6d15491ee1a3376e39
+size 86143

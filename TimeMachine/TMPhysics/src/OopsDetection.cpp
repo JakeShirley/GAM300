b76d36ee61a85c29d1200cfd8cfd2cf9f9b15607
Jake Shirley <navi@odinseye.org>
2015-03-19 22:26:50 -0700

    Added extra asserts in OopsDetection.

--- TimeMachine/TMPhysics/src/OopsDetection.cpp
+++ TimeMachine/TMPhysics/src/OopsDetection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d0b7831f55fb44aaff46ac7b27d2ff518d79af3a8b4c96bb530ddf3181d4712
-size 24245
+oid sha256:b7131267ffd6a81327224da84b58d1e06addfa32b54c50831464e4766f61b52f
+size 24283

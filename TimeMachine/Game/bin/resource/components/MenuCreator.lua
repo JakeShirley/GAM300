e35c019191e0d094a2eb9fe4b4a17ca9d4f70ae7
Jake Shirley <navi@odinseye.org>
2015-03-20 03:00:46 -0700

    Fixes for Menus.

--- TimeMachine/Game/bin/resource/components/MenuCreator.lua
+++ TimeMachine/Game/bin/resource/components/MenuCreator.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b90b5e894d169772250cedee76d0abe80db7fafdc5abd1557b72f4fc23c272b
-size 17260
+oid sha256:b5d0c9ac4d2721fe374163e1f6cfe9df7b8f6a93144b91e5493f0b4bc66de101
+size 17741

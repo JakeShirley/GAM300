797cc0d55789ae65e1c1398d7a488159e4e3020f
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-03-20 06:04:43 -0700

    Uncommented half comment.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:140d624584f4fd29627c57eb338ed8dfef1d4d7bc807ddefc19beb80e74b73b3
-size 79121
+oid sha256:0260d5cd187cd96c65c594d41bffaa2dff40f404c53b6a9652ee85797a5ee965
+size 79120

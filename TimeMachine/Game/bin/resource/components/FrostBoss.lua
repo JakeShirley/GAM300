2dbf960a7009fd97a1e56ffd8e801dd7784d88f7
Michael Chambers <m.chambers@digipen.edu>
2015-03-19 20:33:15 -0700

    Fix for Blizzard and the functionality of GetExtents.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c07ce7e4de22cc01e83dbb373d58dae87d8bcde96f536ea5b3136d0cc010da1
-size 78108
+oid sha256:c788ec2e79b84f36b69618c72ab306aa41c7d202e60dac1b61cf0eb7d9c4bb78
+size 78109

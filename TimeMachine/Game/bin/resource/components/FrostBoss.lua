07e09557a5e1ab0d7654e44a361bf308efaaf1b9
Michael Chambers <m.chambers@digipen.edu>
2015-03-19 21:43:56 -0700

    Reduced frost nova shaking a bit more.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c788ec2e79b84f36b69618c72ab306aa41c7d202e60dac1b61cf0eb7d9c4bb78
+oid sha256:289c98a121e58e2f244677749227f7d9caf52a51a9365c63dc4c35b91f81f314
 size 78109

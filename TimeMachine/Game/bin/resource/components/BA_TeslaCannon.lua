33b417ffcaa2a12c839c03f758ce9722d3d06ecc
garrett.allen <allengar22@gmail.com>
2015-03-20 04:42:12 -0700

    new visual stuff for tesla cannon and added a stop strobe to volumetric strobe light.

--- TimeMachine/Game/bin/resource/components/BA_TeslaCannon.lua
+++ TimeMachine/Game/bin/resource/components/BA_TeslaCannon.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4aacafca492ab910fa1a9e717c0fe42c2331d0a5e2ac6e4ffbdbe6b8ad639807
-size 14647
+oid sha256:7adac9274da42560522af4d63b2efe17c2b7dbeeb5f9ab9977534bd6724739ed
+size 17270

f3a3158f6d51f51e311be84c945a36e99258f674
Jake Shirley <navi@odinseye.org>
2015-03-24 16:53:03 -0700

    Fix splash screen not showing in the right build configs.

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9570fb7d7e84a6859fbc93fcc08c01facb23eab36d5ee9853446d9610768e2d
-size 19096
+oid sha256:7a93289a2512e263c3064ed547a38f740d364e778a49fc4d16ad238dec6b14f7
+size 19104

20a15685e9a43e094eee29993e7fe92210d1c10f
Jake Shirley <navi@odinseye.org>
2015-03-24 16:53:32 -0700

    Added opacity to Sprite component.

--- TimeMachine/Game/src/Sprite.h
+++ TimeMachine/Game/src/Sprite.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57b55490155e669b92530cf0edffe80d156e2f06d34304d1749d79221e75dfd5
-size 2441
+oid sha256:981b09d107a174f94d3bafece70165dd53b6ad27c1f3918bb865f98ff1a8437d
+size 2573

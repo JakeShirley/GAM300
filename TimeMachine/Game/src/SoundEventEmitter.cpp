0d5b6d5a1c81647d71d2a22be41a7d86141cf765
Michael Chambers <m.chambers@digipen.edu>
2015-03-23 22:29:31 -0700

    PlayEvent method that allows you to pass the event that you want to play.

--- TimeMachine/Game/src/SoundEventEmitter.cpp
+++ TimeMachine/Game/src/SoundEventEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a8bc0f4de3789a6b5d11af1679bdeecd8f0ba550a4d56393f1738e2fb425b21
-size 3722
+oid sha256:0727baf7bb3e75bf8709fb3ea51f06c02d2143a82b7026a2e18841dae0fea68c
+size 3847

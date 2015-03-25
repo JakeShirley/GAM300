9b9bf6edbfd48e0ce8558fcb73f929e707e27d6a
Jake Shirley <navi@odinseye.org>
2015-03-25 12:52:28 -0700

    Fixed incorrect logging in fullscreen mode.

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8138bc3cca1703b6a5147a40d6c53274b5adaa22a07f370e3f4536a2726aa97
-size 19115
+oid sha256:7e3a889fd71fb69cc82b249edfae344ec4d5e82f72b5135a05fcd3468d62ad68
+size 19213

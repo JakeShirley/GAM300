0d01cff7772ac885812a3c0773570c407d3feb81
Jake Shirley <navi@odinseye.org>
2015-03-25 16:58:08 -0700

    Changed Space to cancel its event when changing levels.

--- TimeMachine/Game/src/Space.cpp
+++ TimeMachine/Game/src/Space.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1b58685772bf3e6f909ca9698e9f9e73553446d61d1aeb54f9d4bb49b8c4477
-size 23777
+oid sha256:2183249f67a7f6fe9a6dbde75edb87cee0e1bb424938b83436b02b829533c0be
+size 23850

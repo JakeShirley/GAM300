45ec2068fd4cd606d95c407ebcc24d014b6b7b26
Jake Shirley <navi@odinseye.org>
2015-03-24 18:12:14 -0700

    Exposed GetBoneNames and GetBonePosition in SkinnedModel.

---
+++ TimeMachine/Game/src/PrimitiveContainers.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c42c3329e6536e64744325c386ca86d32fdeeb5530f0d03962adc7d19fb5b554
+size 361
--- TimeMachine/Game/src/SkinnedModel.h
+++ TimeMachine/Game/src/SkinnedModel.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b3f5a4a10c4d7822b150891c9ec02cc60c46fe243f83a1fd5ec7dabdac89409
-size 2392
+oid sha256:f6a1fd66346e8bf8e2a7b4bc23d451c574ff92e821d50e31a1bba8eb4c20fd28
+size 2565

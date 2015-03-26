10345cdeb01f1e02e28bdc9ee4d028fb3e7f0e18
Garrett Allen <garrett.allen@digipen.edu>
2015-03-25 17:18:22 -0700

    lightning boss shouldn't teleport when you stop the cinematic anymore

--- TimeMachine/Game/bin/resource/components/LightningBoss.lua
+++ TimeMachine/Game/bin/resource/components/LightningBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc019394ff3e5069ab84d55a68e8e4126bbcd50605da5bdddb13136214de2749
-size 50773
+oid sha256:fde0ce4129a2b6008bf73f18004644e8358d749ede08f415e9f580fca39edbff
+size 51098

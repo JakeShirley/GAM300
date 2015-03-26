0d33e4dc492e3cc4448064d0f6c8450c8e7f5fef
Jake Shirley <navi@odinseye.org>
2015-03-26 16:17:59 -0700

    Splash screen fixes.

--- TimeMachine/Game/bin/resource/components/SplashScreenHandler.lua
+++ TimeMachine/Game/bin/resource/components/SplashScreenHandler.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63052ea91ba9a5061934ec5d87e81ce347f026a0f3a5667f2a973da59d5709db
-size 3005
+oid sha256:2d3e3136642cbc567edc278532c5bfd84bbba758fda2b0bd78dfcced647122e8
+size 3223

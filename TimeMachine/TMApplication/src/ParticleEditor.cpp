20da064c1c3ff47169322eb72c9f750acd316b71
Cody Centers <murtag27@gmail.com>
2015-03-25 18:01:47 -0700

    Added texture functionality to particle editor.

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23bf0daba7bfe3d55a215fb3f3068abcddc34aa71ebc411568b733c31f195b17
-size 64687
+oid sha256:3d2e7123451297db89a9c7cb79647f20120d2f3fca0388e80ce30a5f79dd6f1a
+size 66014
--- TimeMachine/TMApplication/src/ParticleEditor.h
+++ TimeMachine/TMApplication/src/ParticleEditor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4adeb7574aae4c21c16012586fbf19cdeba1f25030d9e6ab8eeb5811be902713
-size 2685
+oid sha256:dd6b0b0681fc71fbad124e869119da9bfa868f41c3f05ce18a8fdca17d50a104
+size 2745

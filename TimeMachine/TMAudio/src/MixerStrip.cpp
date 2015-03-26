31200d099f382966c76869bda7e2081bbde9673f
Michael Chambers <m.chambers@digipen.edu>
2015-03-25 23:48:06 -0700

    Uncommented necessary cleanup code that I thought was causing an error.

--- TimeMachine/TMAudio/src/MixerStrip.cpp
+++ TimeMachine/TMAudio/src/MixerStrip.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97562457fc7881af155ba12838007f96d0910a266c96e8b5111b9a464b1cc2e0
-size 1106
+oid sha256:0a89ce318bad3e92a9a52fa7e3b95045667ccb2599c93daf8b67ca65217523ad
+size 1103

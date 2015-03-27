ce8031be254363f1b5eae603915002d37d6c1d23
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-03-26 22:17:43 -0700

    Made nova projectiles and particles not in the ground, which was causing a weird visual glitch. Also tweaked the particles for the frost nova trail to account for it no longer being half way in the ground.

--- TimeMachine/Game/bin/resource/components/Boss_Shoot.lua
+++ TimeMachine/Game/bin/resource/components/Boss_Shoot.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2b7c6c1d5f0f0d05acf6006d8c637b1ce2c43b80a58546d35cdc3c2a3f99401
-size 2898
+oid sha256:353a6988d6f336798214df9265fe62b37b692ff8690228d5cbe73fe810b521dc
+size 3028

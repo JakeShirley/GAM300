a147b1953cbdff54299769f4ded310306da9c0c2
Garrett Allen <garrett.allen@digipen.edu>
2015-03-27 14:56:51 -0700

    added fades to cinematic canceling.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d97f483ed4699b8fe546e63be66b924001e60707fb39858911a0e5d5c2317a57
-size 79171
+oid sha256:e16de1c31df59592d14b4d81a7f3f159ac33de72439f5a9be229da838db27203
+size 79455

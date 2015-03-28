5146302a814090ab93dd1b72cf4d074e4ee7f928
Cody Centers <murtag27@gmail.com>
2015-03-27 17:18:17 -0700

    particle editor additions, fixed a bug

--- TimeMachine/TMApplication/src/ParticleEditor.cpp
+++ TimeMachine/TMApplication/src/ParticleEditor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5ec2d2b72f94bd885b04c4ed7e8d0bc04284cc8b88f99a0de68b5c525051d81
-size 66996
+oid sha256:744831d5fb0a455ba9932a9967b7044e27d0423f6c10089e38c407fa4e0b65f8
+size 65571

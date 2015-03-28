d3b20bfc190e1c44df42a93563de593e9526d129
Brian Inkrot <binkrot@gmail.com>
2015-03-28 14:49:47 -0700

    Removed some debug text, turned Lightning Arena Puzzle and Cinematic back on.

--- TimeMachine/Game/bin/resource/components/BA_ThunderboltAllAlive.lua
+++ TimeMachine/Game/bin/resource/components/BA_ThunderboltAllAlive.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ea34fd7d706e55ce1bd1255b59b56e96dc3bb826a208e74dd79cdaad68ecf4c
-size 8732
+oid sha256:3c82483987ce3a4858a4ddd94b029d0acc0d170373e651fb08ed7a2e811f7ece
+size 8738

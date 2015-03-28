342233174cbe9c55841169066e5ced38b0d9baad
garrett.allen <allengar22@gmail.com>
2015-03-28 16:35:08 -0700

    added spencers sound effects places. heal isn't in and the roar and nova blast are kinda weaksauce.  heal I didn't put in because it loops.

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ae016394b18863410d03beebda50fca6519f383298ad8d778c3c50a5ec6f77a
-size 79502
+oid sha256:95bdece27ac1ddbbe84a3313b84b6b7f3ebd9e38cb8923c77baef3cec634a591
+size 79619

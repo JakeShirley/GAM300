b111f42253a2af7600ef33bce01334a227973d64
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-03-28 19:17:58 -0700

    Fixed a directx version error.

--- TimeMachine/Game/src/GPUParticleManager.cpp
+++ TimeMachine/Game/src/GPUParticleManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:618e378b5c6c8ae646de7844eb84c1a5f09819c39d00c58773c83554ee2a0c3d
-size 2342
+oid sha256:d1e2da3372790496e4d2acb043220aa0814bb91a3ef15f0098fe843d4f04d019
+size 2330

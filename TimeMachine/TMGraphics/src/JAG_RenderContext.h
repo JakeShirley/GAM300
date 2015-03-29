cb9e7ab2b9de1901179fcfabbf1de23fb69a8719
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-03-28 19:28:21 -0700

    Cleaned Particle.vs shader, added GetFeatureLevel to RenderSystem and RenderContext

--- TimeMachine/TMGraphics/src/JAG_RenderContext.h
+++ TimeMachine/TMGraphics/src/JAG_RenderContext.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a737d03538ace3f8617dcdca9b1fbc7912b00b4133cba27fb5fa21d0b24f3749
-size 10006
+oid sha256:cbd891c2110f3138617a50322cdb43cd8e904337f812170d5db6e6cbcfbbd7da
+size 10054
--- TimeMachine/TMGraphics/src/JAG_Types.h
+++ TimeMachine/TMGraphics/src/JAG_Types.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:047f116f2849e26f87d4b4bbc34b6440176ff9c4d79013665fbe979d27f30de5
-size 4752
+oid sha256:cb9b993c185287780e961189395a5022d18bff5187e6fc8e28f5d93bb3ba7936
+size 4751

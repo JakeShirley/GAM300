e04f8346845ef139137cf66fe24983bc3b109bcc
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-03-28 17:45:45 -0700

    Fixed (maybe) the problem with the shoot speed of the hookshot

--- TimeMachine/Game/bin/resource/components/MeleeLoadout.lua
+++ TimeMachine/Game/bin/resource/components/MeleeLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b9f6ca6449986712f517a57d08e0997349c7dfe9413b1505bc431d7d27daaf6
-size 16167
+oid sha256:04d8fc0c7c71ddd0bbc67fd3b82e112e36c18c1f5e66de8d24e74862fc215b7f
+size 16163

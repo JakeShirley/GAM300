ca703f239c531c70c6c7086b82f962a8efd2b393
Brian Inkrot <binkrot@gmail.com>
2015-03-28 22:26:35 -0700

    MORE Ghost Owls, MORE MothOwl Movement, MORE Golem Health, MORE Lightning Arena Fun!

--- TimeMachine/Game/bin/resource/components/LightningBoss.lua
+++ TimeMachine/Game/bin/resource/components/LightningBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0970053d4c4fb0122cca3caf01dd70b5a7137c5567875a31e227475a63005bc3
-size 51635
+oid sha256:c56bfe19bba85fd7861fb8e87b17534f807632f1dcfb4d28e95a793efc87677c
+size 56216

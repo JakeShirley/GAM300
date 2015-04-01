8fd8f6e39d56c7990bd7a382e53106cd9a1debab
Garrett Allen <garrett.allen@digipen.edu>
2015-03-31 23:23:16 -0700

    ghost owl has feedback on attacking a player. also removed a stupid debug string I was printing. Ma Bad

--- TimeMachine/Game/bin/resource/components/FrostBoss.lua
+++ TimeMachine/Game/bin/resource/components/FrostBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3569677de9bec119b88fa95938dd7c47baae06c384b03676e95d5d90f8ebaeea
-size 80894
+oid sha256:2b31cd0f8520f254b6190e1eccfaa815cc767104e2f516221add534413a7b2ce
+size 80835

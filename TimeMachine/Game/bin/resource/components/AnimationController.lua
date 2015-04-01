07a2684ac6d22b4d4882f9a35493621c684a6fe2
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-04-01 14:21:26 -0700

    Tesla cannon and pylons

--- TimeMachine/Game/bin/resource/components/AnimationController.lua
+++ TimeMachine/Game/bin/resource/components/AnimationController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6c18fa6be7928021468aac2870419f39344c8f72d9af8aa2fc3c6b7e14b0e7d
-size 4059
+oid sha256:02ce5d9017725d5b5b3cc6613354d50a028e6e67e962e4de23f361ff4438ad0d
+size 4451
--- TimeMachine/Game/bin/resource/components/BA_TeslaCannon.lua
+++ TimeMachine/Game/bin/resource/components/BA_TeslaCannon.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b811cc53b4727b699c77f4869ce37e14729e71a2fb49aceacd7662b48f646635
-size 20461
+oid sha256:4c92bf66d388a39be5949ff86a806ac892defe420cae337b3171e74febec95c7
+size 22844

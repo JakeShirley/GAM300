a85b53d934583b98e7dd5afd6174a02ba4a1a82b
Brian Inkrot <binkrot@gmail.com>
2015-04-01 20:56:54 -0700

    Starting the purge of 20+ action sequences.  Joey, if you could do BA_TeslaCannon I'd really appreciate it, specifically the local ones created on the towers / cannon

--- TimeMachine/Game/bin/resource/components/BA_Thunderstorm.lua
+++ TimeMachine/Game/bin/resource/components/BA_Thunderstorm.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7b15f39ee9d96e8cd1b9edee67883627cc8fc2e125739f867b0bca976ce4f02
-size 8378
+oid sha256:1644e76ff6e331af490d3afd3316c60c373efa3b43187931f53336c3b6a27b99
+size 8445

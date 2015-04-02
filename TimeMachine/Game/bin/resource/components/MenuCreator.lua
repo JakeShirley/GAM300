fe0afe77871da0059f7a15d99b2c4e72e103dfe2
Michael Chambers <mike@friendpeople.org>
2015-04-01 22:54:11 -0700

    Beginning of implementing menu sounds.

--- TimeMachine/Game/bin/resource/components/MenuCreator.lua
+++ TimeMachine/Game/bin/resource/components/MenuCreator.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e55373965a90f7b1ca834f79fee56cd779b7477ddd5372c53f414d1e8be51388
-size 25778
+oid sha256:b208c36f48415875fd75f77b6278b26506ebab3927ae20348aef521adc93b852
+size 25816

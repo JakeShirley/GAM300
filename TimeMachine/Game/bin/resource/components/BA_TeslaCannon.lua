33d74983d699519a694c722903d0e598f24a2c40
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-04-01 21:49:59 -0700

    Fixed action lists in BA_TeslaCanon

--- TimeMachine/Game/bin/resource/components/BA_TeslaCannon.lua
+++ TimeMachine/Game/bin/resource/components/BA_TeslaCannon.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c12ca11b54d1e8bdd86d7f7b705d1ecdbcb55ab9ef35be3f16b098ee6d6e372
+oid sha256:4b298f8a747f9415a33810c5ba2e53ba16f9958c296e9465921f0a57f8cbc51f
 size 23680

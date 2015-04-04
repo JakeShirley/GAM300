16809f879875ea33478ac70d55f7ee855aa571d4
Cody Centers <murtag27@gmail.com>
2015-04-04 15:20:18 -0700

    missed a ptr check

--- TimeMachine/Game/src/RibbonEmitter.cpp
+++ TimeMachine/Game/src/RibbonEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ff96c646eb248e4cccc15d563ae2683fbb4371fa9a64390e09df5b8fdcc850a
-size 11094
+oid sha256:1d3108525eb68b10ff3a6e19a6e502a94f3b81434f065dd5fc1caf654b0d76f9
+size 11083

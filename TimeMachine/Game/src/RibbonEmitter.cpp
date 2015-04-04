e8f847262c64a56b6c34e4e0fbbec8738cafc0a9
Cody Centers <murtag27@gmail.com>
2015-04-04 15:58:52 -0700

    changed method of attaching ribbons.

--- TimeMachine/Game/src/RibbonEmitter.cpp
+++ TimeMachine/Game/src/RibbonEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d3108525eb68b10ff3a6e19a6e502a94f3b81434f065dd5fc1caf654b0d76f9
-size 11083
+oid sha256:59124f82765c2b6cc6c6ffb652b4fb3c0143a6d446fbcb2845b72bc100927e1e
+size 10010

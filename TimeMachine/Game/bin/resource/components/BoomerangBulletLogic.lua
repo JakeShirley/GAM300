207d3915594ad85d5f8c013c2d2c45e1eaf3d859
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-04-04 14:36:58 -0700

    Boomerangs now return to the player and refill when caught, increased the auto re-charge time if boomerangs are destroyed by walls. Buffed damage of melee loadout

--- TimeMachine/Game/bin/resource/components/BoomerangBulletLogic.lua
+++ TimeMachine/Game/bin/resource/components/BoomerangBulletLogic.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b0eda3e34d650ba3ecd85f3cefa9e75b570d96a7972d1ddb2760a4bf8cac917
-size 3983
+oid sha256:3083bd64c43e3ce2cbf0955e6e1f0ee29e146c0046ff5cbd80e57caab0ce7782
+size 4630

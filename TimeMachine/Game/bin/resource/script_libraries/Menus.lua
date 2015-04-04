76e2baee8ec2409027fc20809c4a55440d8cdcdd
Jake Shirley <navi@odinseye.org>
2015-04-04 15:41:59 -0700

    Menu sounds + updated sound bank.

--- TimeMachine/Game/bin/resource/script_libraries/Menus.lua
+++ TimeMachine/Game/bin/resource/script_libraries/Menus.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f331f66901ada1437bec11b854f01b3c25a69847f505641a9af5beccade13a98
-size 35263
+oid sha256:491ff68313bf93e0d281617889d36e636ee0489295e964dee83e788bdc2e4f87
+size 35381

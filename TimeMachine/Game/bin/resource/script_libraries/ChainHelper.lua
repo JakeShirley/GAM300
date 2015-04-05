edb6421262bacdfd45d6e9e2fca2972491e889ae
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-04-04 22:29:39 -0700

    Put capsule colliders on chains. Created dynamic chains with new helper which should make adjustments (number of links or slack) easier.

--- TimeMachine/Game/bin/resource/script_libraries/ChainHelper.lua
+++ TimeMachine/Game/bin/resource/script_libraries/ChainHelper.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e9c9edac73f897bfdc4a0a1ef7c6961b190d8052b7f176de0289fc268721ca3
-size 3002
+oid sha256:a19bec775eb2933f05e1f8a42ef45643c3e2ded3a204a70db2536033acf3d164
+size 3161

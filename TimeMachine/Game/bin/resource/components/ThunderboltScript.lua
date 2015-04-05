313d61f48dca4e10c47ad5b495418b830915f3cd
garrett.allen <allengar22@gmail.com>
2015-04-04 18:25:00 -0700

    fixed an emitter being off in lightning bolt and also fixed ghost owl giving attack feedback at inappropriate times.

--- TimeMachine/Game/bin/resource/components/ThunderboltScript.lua
+++ TimeMachine/Game/bin/resource/components/ThunderboltScript.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc3026ddc8a7ed5068d323c58b17069d3d81c5047feb21c3611d336ebf5b5809
-size 3834
+oid sha256:b164b4855d16804e3c2970506b54019e8d61a50b9005622d6538aff0a1c3a83c
+size 3910

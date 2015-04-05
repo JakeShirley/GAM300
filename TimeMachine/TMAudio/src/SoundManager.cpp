ab3c9929672e06f2f82a1a8dc47d2ff9c2876dcd
Michael Chambers <m.chambers@digipen.edu>
2015-04-04 17:41:19 -0700

    VCA wrapped. The old functionality is there until we edit it and get some lua calls or whatever need Jake thing.

--- TimeMachine/TMAudio/src/SoundManager.cpp
+++ TimeMachine/TMAudio/src/SoundManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89ee53eaaf0f2cfa5b91480d8b25ffc802503a9881a2b1c399a43eb614b3db51
-size 9652
+oid sha256:ec9813bb9d4b7fccecdab8bcd4ee4bf83706c78b0d47ee02c93f1beebcbb3bcf
+size 10664

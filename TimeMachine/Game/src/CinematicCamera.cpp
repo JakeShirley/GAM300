dd2a54f07edbeb68d501dd3da8f00a3313a37e1d
Cody Centers <murtag27@gmail.com>
2015-04-04 20:49:48 -0700

    Revert "Added a total track time function on cinematic camera"
    This reverts commit 52e84a2b2998fc1510925ea0b069fba5cdbfa7be.

--- TimeMachine/Game/src/CinematicCamera.cpp
+++ TimeMachine/Game/src/CinematicCamera.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:498a243391ac6c66844cf1869e32e1965202a3fb6ae2e2994aa774d6d466c331
-size 37000
+oid sha256:affc91b63c11bbbffa05dc5ecf08e23c6d26a7c7efd05dee56787c292ecb7f31
+size 36792

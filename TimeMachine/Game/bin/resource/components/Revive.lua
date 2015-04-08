a1125c7fe2810308cb24fcdd14c1ee543fbee2bd
Brian Inkrot <binkrot@gmail.com>
2015-04-07 18:38:47 -0700

    Bug fix with level switching mid revive.

--- TimeMachine/Game/bin/resource/components/Revive.lua
+++ TimeMachine/Game/bin/resource/components/Revive.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d181e21a8490e9228170e71857f28681ca0a9f60cb881a914c23dc3b531fa4e5
-size 17507
+oid sha256:cf070ae23c6b21c2fb2c73f17bab3849a9352bdcf1af28423644d5663319a0ec
+size 17561

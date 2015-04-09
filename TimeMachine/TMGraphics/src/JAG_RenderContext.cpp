d50c124a71301f9c21fc3ff1b01910be1e0a3b78
Kai-Hwa Yao <kaihwayao@gmail.com>
2015-04-08 19:19:37 -0700

    Vertex buffer UAV

--- TimeMachine/TMGraphics/src/JAG_RenderContext.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderContext.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28d37729aa819849b8e2cdb6dafea9f3c3de34691f3738616d3c0ac3e95f3ba9
-size 108320
+oid sha256:ac6e7911bc3baf448ef4e2b20aa80c1a97971ebc3bea26b5f22f1db4cf065e17
+size 109950
--- TimeMachine/TMGraphics/src/JAG_RenderContext.h
+++ TimeMachine/TMGraphics/src/JAG_RenderContext.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc290f7e739dd854e8a03975ef40774ad14ffc2790590b4608ea5f73484e4ab7
-size 10369
+oid sha256:95a188d2212d2a02987452bb4eb161b0551ebff13dbf14d36b3b3ffe45ccca29
+size 10422

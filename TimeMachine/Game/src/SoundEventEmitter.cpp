4e6623cc809cb78f2cd13f01c1619058ca28db5c
Jake Shirley <navi@odinseye.org>
2015-04-10 16:31:20 -0700

    Spiffed up error reporting for SoundEventEmitter.

--- TimeMachine/Game/src/SoundEventEmitter.cpp
+++ TimeMachine/Game/src/SoundEventEmitter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8f5e1693cd0d7a97cac355d722cbe5bc0d4ce3497d3af7a63562312ab5e2be4
-size 3827
+oid sha256:1cc892db6ec1d682f96a909510c30f9c549f2d6d6e4953b488ed79afd8f1c53c
+size 3687

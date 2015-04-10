c19560ce11470a119f407c04c5673b7fd89cc113
Brian Inkrot <binkrot@gmail.com>
2015-04-10 16:35:48 -0700

    bugs

--- TimeMachine/Game/bin/resource/components/BA_Gust.lua
+++ TimeMachine/Game/bin/resource/components/BA_Gust.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31c888c31b9732bbbdc59e3f84d85a770988b28b8698d29b9e5298d4667ecc05
-size 5802
+oid sha256:ccf09416317e0aae759588f0dc0969e95de768855b79ff53c8da39e2ae1b6df5
+size 5799
--- TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
+++ TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dc35f34f6b8fb419378237bdab4414f0f8a791031946d46d563535057ebfd5f
-size 6434
+oid sha256:8cabe02871677638f261cd197abeb6b481665bb56fe0e8133ffcf4478b355d6d
+size 6458

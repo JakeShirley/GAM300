be87c205261576a34be9dd82f13826da542a99fc
Brian Inkrot <binkrot@gmail.com>
2015-04-11 21:19:45 -0700

    Playtest last touched to gust and arena

--- TimeMachine/Game/bin/resource/components/BA_Gust.lua
+++ TimeMachine/Game/bin/resource/components/BA_Gust.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2df0a7472b08fa79347929e99facfd046732166f51be54454b54280b8b5433ac
-size 5902
+oid sha256:162c168b9f8d905ccfed0c386b5855a918ac13a7ed05379a872fe89d0f6fd7a4
+size 5898

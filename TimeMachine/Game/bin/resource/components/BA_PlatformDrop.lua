46977513675849703fbf709c44a6b74140b4dd06
garrett.allen <allengar22@gmail.com>
2015-04-11 22:19:12 -0700

    some bug fixes

--- TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
+++ TimeMachine/Game/bin/resource/components/BA_PlatformDrop.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dc35f34f6b8fb419378237bdab4414f0f8a791031946d46d563535057ebfd5f
-size 6434
+oid sha256:25aff52fc0d3dc39a380d5bdec3d2f00b90c588d22cb8af4ef46f51e5dba3ccd
+size 6569

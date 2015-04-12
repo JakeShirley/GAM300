add7caf8f1185b261f3e1a2cdbcee162e3cc4fe2
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-04-11 23:07:44 -0700

    Some additions to wind effect allowing it to affect chains.

--- TimeMachine/Game/bin/resource/components/WindEffect.lua
+++ TimeMachine/Game/bin/resource/components/WindEffect.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dec598adfcc23e82f43d6bd235d18489f1f58616d68f3408c30c297af39e0a00
-size 3569
+oid sha256:72fffe4af1413ce556d8dd852c9d576c8f77ea1a4eccd784b636c33be9e1fd94
+size 4245

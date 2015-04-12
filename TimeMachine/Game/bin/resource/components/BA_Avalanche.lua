fa8d069b39dc7c99e1fd386485fd9e3bd8957efa
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-04-11 23:21:59 -0700

    Set volume on avalanche to 1

--- TimeMachine/Game/bin/resource/components/BA_Avalanche.lua
+++ TimeMachine/Game/bin/resource/components/BA_Avalanche.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fab175055dbc72f72294aeca90ce6c56b2c07a08b53c1ff3757e38be51493bab
-size 7152
+oid sha256:cb955bb231429fd7eca8bdffa826775674fc17521fcd931c8f89ed0620dc1d43
+size 7190

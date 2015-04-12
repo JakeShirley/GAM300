6bfe4db35cb2ad3b30b841d76f40fb4b0037098b
Jake Shirley <navi@odinseye.org>
2015-04-12 00:15:36 -0700

    Removed logging in stat board.

--- TimeMachine/Game/bin/resource/components/StatBoard.lua
+++ TimeMachine/Game/bin/resource/components/StatBoard.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5970b308a413637dfcaa600b628df4ec9a00a2c0f0abe259e7454e0e16a1b735
-size 11854
+oid sha256:e0f7d06621dc09d268cab6a2536a3bac99d4ff2e2e33d6f7e3c7018b9204fe1c
+size 11715

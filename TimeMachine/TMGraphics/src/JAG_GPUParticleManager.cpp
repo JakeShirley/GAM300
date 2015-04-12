0f434b7114e845db5ffc174c2d3d917065684148
Cody Centers <murtag27@gmail.com>
2015-04-11 21:42:28 -0700

    Ribbons can now be textured, no component way yet. fixed spaces for ribbons/beams, fixed particle editor crashing on startup

--- TimeMachine/TMGraphics/src/JAG_GPUParticleManager.cpp
+++ TimeMachine/TMGraphics/src/JAG_GPUParticleManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a3afa0c8d1e9489b8a3d810fe8c83880a78d51de92c5f111719bd3d8084ea7b
-size 6244
+oid sha256:2d2a3836537b44c9f79628fad627b1d3489077b9b026b23e28c6ff8f3dd834ed
+size 7269
--- TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
+++ TimeMachine/TMGraphics/src/JAG_RenderSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de013ef4ec6f5bdf24c5dc8cb3cbf9d44b1e19b5b6888014b6e04dd2e646e24f
-size 88550
+oid sha256:0db111b1297fac0839e74fc09e4ef47438fe612e21f5f252012f4abc165ad3ef
+size 88589

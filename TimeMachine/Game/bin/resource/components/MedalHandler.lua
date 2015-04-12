7e2264f6344c9d35b29fb339ffd202965a3dcd3d
Jake Shirley <navi@odinseye.org>
2015-04-11 22:18:32 -0700

    Stats board update

--- TimeMachine/Game/bin/resource/components/MedalHandler.lua
+++ TimeMachine/Game/bin/resource/components/MedalHandler.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f59d548da222811abd16df287fd0bc312bf50d0b97193250deae8ceb2eace21
-size 7093
+oid sha256:94d69ea81648e2ef128867dd57c876d5c489449d042dc6f089cf5d21e1897a6b
+size 7845

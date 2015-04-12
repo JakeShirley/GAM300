29e49539cd384650728b2cff04ef346f57b9920b
garrett.allen <allengar22@gmail.com>
2015-04-11 23:34:15 -0700

    turning ribbons off and on in cinematic

--- TimeMachine/Game/bin/resource/components/LightningBoss.lua
+++ TimeMachine/Game/bin/resource/components/LightningBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bbed052c5c897a9b1143a36ad93650a65ac05f7317921af6aa9742c044aef3c
-size 60113
+oid sha256:2754ed6bc76aac142f3b8f92669371eb9c1e7bbe6aff46f9be7503e623343abe
+size 60644

62965b0be251abc6e67d347b7348664351e8632f
Jake Shirley <navi@odinseye.org>
2015-04-12 20:47:10 -0700

    Check for animations before getting bones.

--- TimeMachine/TMGraphics/src/JAG_AnimationController.cpp
+++ TimeMachine/TMGraphics/src/JAG_AnimationController.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:357541a0bff325b329030d32df08633748abeeac750829823b67e3c08f02ee01
-size 7111
+oid sha256:45618acd2120dfd9c1c02e7041c0b82872bc57cce090542e51fca7b34f851ab0
+size 7159

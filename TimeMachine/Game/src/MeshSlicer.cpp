63338e447ee5d97b30a135c294f647cf73ac34d5
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-04-13 15:43:40 -0700

    Forgot to return a value. Weird that it built in release.

--- TimeMachine/Game/src/MeshSlicer.cpp
+++ TimeMachine/Game/src/MeshSlicer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2db30bd1da506279ecf319af1fb1a891c73e9609e6c60acf02a08384f238205
-size 10412
+oid sha256:112f387329da876c8d0f446de1ec49bbef6f6a2b806e3000a186fefa7bdf3bd2
+size 10429

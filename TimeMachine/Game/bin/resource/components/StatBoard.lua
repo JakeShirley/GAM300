d5356014171764f587d7a71a0f38db804c90b9c2
Brian Inkrot <binkrot@gmail.com>
2015-04-14 14:47:18 -0700

    Nerfed Baby phase Implemented NewHubWorld level change option Swapped the particle on playerbullet explosion Fixed a bug in Object Spawner

--- TimeMachine/Game/bin/resource/components/StatBoard.lua
+++ TimeMachine/Game/bin/resource/components/StatBoard.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43ef6058c2d20a8c54bb7e5d64a97b9fd7c6e38cbc9908ef1e8ff1a0e9775002
-size 12541
+oid sha256:81d6d858612886365aea5676bb425ac547e3221f7d52c7a244867596b9a1d194
+size 12553

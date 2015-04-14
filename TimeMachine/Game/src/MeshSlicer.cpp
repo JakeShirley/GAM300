a3c4a9569a6e351a4c99b884fbffa15179bd1a35
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-04-13 22:13:17 -0700

    Sliced meshes use disposable graphics models.

--- TimeMachine/Game/src/MeshSlicer.cpp
+++ TimeMachine/Game/src/MeshSlicer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bd927bfc01621352cdcdad0f68bdb752a0cb88392cd961d6b7c3b477b379d91
-size 11153
+oid sha256:a468fc5e63cf6d2fd8d290a769395d7cef89c90f15f69a51a727e2d5726e8142
+size 11252
--- TimeMachine/Game/src/Model.cpp
+++ TimeMachine/Game/src/Model.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0750295cdc2cf225704e9e7e3a55deae492bf9a6954266fb7894c1a9e382ad6
-size 9595
+oid sha256:4b35b930ca0f89f3a01a2a31e930e457fb2696e5dcae2540276ebcd48422a9cd
+size 9711

9bec53d602488793277f1cf5efdca34e56f1bbde
Brian Inkrot <binkrot@gmail.com>
2015-04-14 15:01:56 -0700

    Working on hooking everything up, cleaning up any merge stuff.

--- TimeMachine/Game/bin/resource/components/HubGateController.lua
+++ TimeMachine/Game/bin/resource/components/HubGateController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d84a7cfb4982e88c3f7eec16c15435657e782b696f84846a54040888da4cfa99
-size 2852
+oid sha256:be3f7ac0cc4f6b5fedc173900719c7960291ac3d54e8ad8dd5108c9b84acfb9b
+size 2925

a2cc5a33111300d82b1bc3a5ebf2a85a49a225d2
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-04-14 22:24:13 -0700

    Slicing for icicle projectiles.

--- TimeMachine/Game/bin/resource/components/PhysicsDeath.lua
+++ TimeMachine/Game/bin/resource/components/PhysicsDeath.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79de55c1990f296160e01e5c2c3411079f5cd5cbbeccaa8dc94dcf28b7d5fbcf
-size 7513
+oid sha256:0e863758388ddd971b0af7adf04e1f72ea695ec87495a2506b9ab231c1feae6e
+size 7512

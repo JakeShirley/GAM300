be21e6b3c7fa4c4818f6ccc120b5cfcd2212abe4
Michael Chambers <m.chambers@digipen.edu>
2015-04-14 23:35:19 -0700

    Fixed owl screech to use the event.

--- TimeMachine/Game/bin/resource/components/LightningBoss.lua
+++ TimeMachine/Game/bin/resource/components/LightningBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd57a124e7b1baf3018dff737acaf41e1badbe1578d849301727861ed94d6684
-size 63514
+oid sha256:e2f4620c38f1c9480a62d8e49212fe126380cb9ff751e83d1b68aef7f6b61b61
+size 63510

0bdb016770d3a1585e47e59cf25676827506620d
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-04-14 23:48:48 -0700

    Shattering boulders in level 2.

--- TimeMachine/Game/bin/resource/components/PhysicsDeath.lua
+++ TimeMachine/Game/bin/resource/components/PhysicsDeath.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:944dcc888f925fea837c4270e1bc40f45dabd42ba62f967090f90652b9f756db
-size 7756
+oid sha256:da4c184dbb330b58e92f74bc5db6635a71a73b5633c2f9005e8f7e54bd610999
+size 7715

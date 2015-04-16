a17d0cc62939248ee2e38a4898b2f8e088365248
Jake Shirley <navi@odinseye.org>
2015-04-15 23:18:31 -0700

    Turnin

--- Installer/InstallScript.iss
+++ Installer/InstallScript.iss
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0497af9fb4193bf8b6bb55aa2ad20288fffb46564d8b1eb19247e46d82de38be
-size 8935
+oid sha256:7532bddc75b9d77be805b3baaf50217c4737df721903290ce082685e3db9c648
+size 8982
--- TimeMachine/TMTools/src/LuaObject.cpp
+++ TimeMachine/TMTools/src/LuaObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dd7e1e14cd2c048803fd3a253f5717a203dd14ef003e8bdf3863d38525abf35
-size 4512
+oid sha256:12344a5e9c8b8c6efc1b6cd8d597ac517794c8b7fc67edd4991bf88fd0ebd927
+size 4535

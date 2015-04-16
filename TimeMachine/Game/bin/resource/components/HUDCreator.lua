271b1c005c230b78ac319abab9127f05410e11f1
Garrett Allen <garrett.allen@digipen.edu>
2015-04-15 17:26:03 -0700

    cinematic bars are now in a seperate space from the hud. hud goes inactive on boss death.  fixed pointers disappearing. lightning sound when frost boss dies changed to sound event.

--- TimeMachine/Game/bin/resource/components/HUDCreator.lua
+++ TimeMachine/Game/bin/resource/components/HUDCreator.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:445afd29fd885958b741c42bf83d593bcf3f8eb7aa24e883413e01a5d528b34d
-size 2736
+oid sha256:3d161c42a6941af3d6413b663e77f98d35ccbffda4d3c76ace67da11f67493f2
+size 2796
--- TimeMachine/Game/bin/resource/components/Health.lua
+++ TimeMachine/Game/bin/resource/components/Health.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c9b228e71e64f643c032bd35f1899e829d7fcec7428c1ddb6952211c413be1d
-size 9912
+oid sha256:fc9eb45a07d5534f966f2810171fbaf0dab5d5ab8fff263cec290578f4ac28de
+size 9760

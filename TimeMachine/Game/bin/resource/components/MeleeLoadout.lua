e603d063ae5dee04a6bd9e79a35b198ee762138b
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-04-15 19:58:25 -0700

    Fixed melee loadout "running out of swords"

--- TimeMachine/Game/bin/resource/components/MeleeLoadout.lua
+++ TimeMachine/Game/bin/resource/components/MeleeLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:886eadff1fc0b6a2e9dbf9416f114136c74e6985179acf47e03b65a132ed60b0
-size 19279
+oid sha256:2372869b65875db9fa06e5dc3d5af214a3719e5cf88f1e3995f60d25dab7b609
+size 19281

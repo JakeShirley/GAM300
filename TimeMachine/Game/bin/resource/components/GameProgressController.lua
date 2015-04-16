ec7a16f6a59be03e6ae4a196d77e154b3b6e4991
Garrett Allen <garrett.allen@digipen.edu>
2015-04-15 21:40:24 -0700

    hub world not starting in victory mode

--- TimeMachine/Game/bin/resource/components/GameProgressController.lua
+++ TimeMachine/Game/bin/resource/components/GameProgressController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88989ea6f48f73e9445cc3a397e64f407076fb139fcfc7b6f69cbac09b99ceed
-size 1489
+oid sha256:ad778f92481c9ec037f9847f108bb9261daff46e7cc4713509a508dddb8bf0cd
+size 1490

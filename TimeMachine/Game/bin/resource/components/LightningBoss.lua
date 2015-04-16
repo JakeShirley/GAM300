e7ad6c9c814677b8cb28e2110adc41b88cc263d0
Garrett Allen <garrett.allen@digipen.edu>
2015-04-15 21:55:17 -0700

    turning off boss trail at end of cinematic

--- TimeMachine/Game/bin/resource/components/LightningBoss.lua
+++ TimeMachine/Game/bin/resource/components/LightningBoss.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de243e80a8028f6845ee3f0555b1721ae41b71d0e184fdba7ab03a809b6ddcd5
-size 65442
+oid sha256:08c9bcf297982b66b099193d59d5ea8fc190369172c38f7781c6ac38bd3ca31f
+size 65509

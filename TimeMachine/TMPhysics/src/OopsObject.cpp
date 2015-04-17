94c931b0c71205566e7fa7e79d7f583774cd2bfb
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-04-16 22:57:10 -0700

    Fixed a bug causing rigidbodies not to be at their center of mass when switching from and to certain model types. Fixed a crash caused by switching model of a collider from an environment mesh to something else.

--- TimeMachine/TMPhysics/src/OopsObject.cpp
+++ TimeMachine/TMPhysics/src/OopsObject.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dab22147e751513e8c887d13f4ebd51c8fe1f3a3c1d20c1bca81514477f49b34
-size 19112
+oid sha256:8c57bde2b9e5c5d82185d6b52bbc9603bb4ba67815ac7253b861a82452cea949
+size 19181

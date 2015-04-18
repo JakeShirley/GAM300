5bd4405a7c5b7d695bbc5418fb976c548f92cd46
Jake Shirley <navi@odinseye.org>
2015-04-18 16:18:10 -0700

    Turned off OopsFile logger in redist. mode

--- TimeMachine/TMPhysics/src/OopsFileLogger.h
+++ TimeMachine/TMPhysics/src/OopsFileLogger.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f8c47160df34e6594e73b05c9426a7632184a1cd2e786665ffdf1b927cba4d6
-size 1550
+oid sha256:921806948391fdb9468ae59e6647b18fb48bfbb6e105d3e3372385a72a0b368c
+size 1624

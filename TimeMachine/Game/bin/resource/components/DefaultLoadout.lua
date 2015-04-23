5c478ec8789d7e4528624eb35054a0dabda3f0d2
Brian Inkrot <binkrot@gmail.com>
2015-04-22 19:08:12 -0700

    Fixed charge show colliding with the ground.

--- TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
+++ TimeMachine/Game/bin/resource/components/DefaultLoadout.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b36628e609beafab066bf9a54f817c6c355f0d9f2d2cd2eee63b8c021a0ea751
-size 20521
+oid sha256:ad66893c0f0dfe5ccfc0bf4c8688816a5b5892af5ea5274d656d53ffd4b22df6
+size 20520

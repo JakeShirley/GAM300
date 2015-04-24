fc5b5f30fbde46f0713eab71b800e3819858e3d6
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-04-23 22:12:31 -0700

    Added a push to intrusive containers that optimizes for cache coherency and some testing functions for it. Object list now uses this.

--- TimeMachine/TMPhysics/src/OopsIntrusive.h
+++ TimeMachine/TMPhysics/src/OopsIntrusive.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be71fe1c36f9fdefbcb07f8c9e4c3d2a95139b333054cf0acf3c8306e383cb3f
-size 9244
+oid sha256:da915e34a38a256ce239b0e2a96b57e4c0597a6c06f8c10f97a8a42c071d99f6
+size 14671
--- TimeMachine/TMPhysics/src/OopsSpace.h
+++ TimeMachine/TMPhysics/src/OopsSpace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f461f9f97dc7540e1b2d639909fbdb3c0bbb1a0f831dfab0ed854d94151099f8
-size 7377
+oid sha256:bea8b3b3d393517b63f1141ce9a86f82fba6f08f806cfab54dadaabecc5e1af5
+size 7351

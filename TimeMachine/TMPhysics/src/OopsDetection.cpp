af1012844144f9865caafb94c0222b3274ac65b9
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-04-24 20:32:37 -0700

    Containers in collision detection now use the more efficient push. Added traversal tracking for debugging efficiency of intrusive containers.

--- TimeMachine/TMPhysics/src/OopsDetection.cpp
+++ TimeMachine/TMPhysics/src/OopsDetection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38ceedc604fb120533d7b91da032b2cd0864cf1413ceaafe02afebccbd0bf2c7
-size 24929
+oid sha256:19277a21ea174732772dc30a37ef40e619273858968a25435f3f99294f9da354
+size 24570

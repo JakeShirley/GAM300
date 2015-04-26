8d05792495386195642eb9600d826a58d3b6970f
Jake Shirley <navi@odinseye.org>
2015-04-25 22:22:03 -0700

    Fix for working directory of game in redist. mode.

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cea638342bbeb7a43166c4d5e31ffa2c647a8d29cefa8deb12368d72133c300
-size 19236
+oid sha256:4b180947953c83ad1c4ab225f641d6f209c9fe86dfe896b61bd469fe996d5b56
+size 19296

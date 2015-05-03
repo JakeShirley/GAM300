1a6bd32ae45de7ffd4313b26fac5bb4b46747458
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-05-03 14:07:43 -0700

    No more unused variable nonsense.

--- TimeMachine/TMGraphics/src/JAG_GraphicsSpace.cpp
+++ TimeMachine/TMGraphics/src/JAG_GraphicsSpace.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b994d1bf94b0d527d07927554ac14bba24db77078378566f9495b8d5f1158b3
-size 5039
+oid sha256:953c1b93c0fc08aec2203be1f120d41ee776dcaf6f6c0a6ddee8a371a3f5f61a
+size 5043

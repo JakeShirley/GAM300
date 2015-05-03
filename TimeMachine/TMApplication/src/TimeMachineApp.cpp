63ae2619452c74f9147ec90a3a3c4eb358fa02dc
Jake Shirley <navi@odinseye.org>
2015-05-03 12:09:33 -0700

    Refactored and thread-safe'd Logging

--- TimeMachine/TMApplication/src/TimeMachineApp.cpp
+++ TimeMachine/TMApplication/src/TimeMachineApp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b180947953c83ad1c4ab225f641d6f209c9fe86dfe896b61bd469fe996d5b56
-size 19296
+oid sha256:1798125f85837c580f79db41235eb9a04f6a2fdcbe8b9c5a84c42b228e63640e
+size 19179

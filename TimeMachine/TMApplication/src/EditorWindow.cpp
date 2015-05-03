81f8a1eba40125791c4cdb1ef29cca352e754572
Jake Shirley <navi@odinseye.org>
2015-05-03 12:35:18 -0700

    Attempt at updating color logging.

--- TimeMachine/TMApplication/src/EditorWindow.cpp
+++ TimeMachine/TMApplication/src/EditorWindow.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6071626291410b67630e63bce29b4aed2eca3643ce9aff4bc49b6640a3b637e
-size 10580
+oid sha256:c1668ad494feb111512fe1ee3979d48dc70b38f7f9e8195bff60c5e7ae79e6f7
+size 11055

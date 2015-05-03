07dbd34d7a12a724993b8252ad42705fe7d495f6
Jake Shirley <navi@odinseye.org>
2015-05-03 12:11:36 -0700

    Changed how LuaComponents are registered with auto complete.

--- TimeMachine/Game/src/GameManager.h
+++ TimeMachine/Game/src/GameManager.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdefbaf6594d35f2351cad8f822da28e02f6dd30b8810cafac0dd5104515971e
-size 5608
+oid sha256:7594780309e4cd247aa2e37589911d0b2601478a57bb0d87f8228e47a61e8d6e
+size 5649
--- TimeMachine/Game/src/LuaComponent.h
+++ TimeMachine/Game/src/LuaComponent.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd566a539fd19f3b7b1a9c3a8e119c64b19d2e3564418794b482008750464aef
-size 2375
+oid sha256:a0185378b0f0ab0e68f2eaca1eda5103f20118f75964e58ea216923deca64b61
+size 2433

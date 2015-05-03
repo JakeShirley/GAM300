d761d5bde6e389582ea1b87bb1c9488cdef7c862
Jake Shirley <navi@odinseye.org>
2015-05-03 12:11:00 -0700

    Optimized color output for EditorWindow logging.

--- TimeMachine/TMApplication/src/EditorWindow.cpp
+++ TimeMachine/TMApplication/src/EditorWindow.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d77277597ca0d907cf3b6c48a12d46d7689c56e4aa005bbbc5ffcde666ab573
-size 10350
+oid sha256:a6071626291410b67630e63bce29b4aed2eca3643ce9aff4bc49b6640a3b637e
+size 10580

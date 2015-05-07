3255c4c00b61ea4a8fbc55b2cbfbe541aa306aa4
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-05-07 15:27:14 -0700

    Took out minimal load check in GameManager, as it isn't particularly faster anymore.

--- TimeMachine/Game/src/GameManager.cpp
+++ TimeMachine/Game/src/GameManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a37629c93eec8a3c8ad0d11d8764bf7db8e1377f8a8d9da168b695e86d4da0ad
-size 60908
+oid sha256:db83de8ac1d9ca49e87e05f97be295614c65829acce2a7f05316bf7da824d7e5
+size 59538

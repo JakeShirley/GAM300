91c051375c6b9004815a08197c831e62cf92c08e
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-05-06 22:47:04 -0700

    resource.json opened once instead of for every Archetype and such.

--- TimeMachine/TMTools/src/IResource.cpp
+++ TimeMachine/TMTools/src/IResource.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff95519077d2470b98d0a33087f0a6e4dc619d183be01334fb69ff47d3072668
-size 9704
+oid sha256:d5bce55b4feabdf5e3d8e5d8bbf10b409ad927e091f3b4d555d4f678cc6261fd
+size 9924

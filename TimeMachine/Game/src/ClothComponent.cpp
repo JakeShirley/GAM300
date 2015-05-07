087fe36620eec22fa183e28c58576b38f9f6c9e1
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-05-07 02:11:57 -0700

    I guess .h and .cpp files are important.

---
+++ TimeMachine/Game/src/ClothComponent.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:03ede69a64b1a497576f081220f65139c0af933547ddd5422ed202bfbff998b4
+size 6054
---
+++ TimeMachine/Game/src/ClothComponent.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:55ad41288eb7672da3810fa906a5b27c75ac8fdeafb087152f543ee9c8a6d6d8
+size 1567

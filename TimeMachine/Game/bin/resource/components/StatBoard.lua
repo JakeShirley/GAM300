d3d856cbf888cc6ead97683cd1acc03a064517f6
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-05-09 15:29:24 -0400

    Random tips on accolade screen

--- TimeMachine/Game/bin/resource/components/StatBoard.lua
+++ TimeMachine/Game/bin/resource/components/StatBoard.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3a5050bc1f3aa49787bc99d4fe555bbf4299850b27bc75ac6a72dfada0df925
-size 13207
+oid sha256:7572b7edd6ad6d4040b563c17595500264fb53c7529d1420f9e0992d2b3a6b00
+size 14101

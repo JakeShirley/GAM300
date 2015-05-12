cd7e861e754488d11f66ebedff7b6e71f0c29cba
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-05-11 18:12:07 -0700

    Debug drawing meshes only draws edges, not twins or normals.

--- TimeMachine/TMPhysics/src/OopsModelAdjInfo.cpp
+++ TimeMachine/TMPhysics/src/OopsModelAdjInfo.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d66c41dfa47576f0c9847b6ebf74d748c7dfad8794822d189c272b7bd523ff2
-size 10601
+oid sha256:2905bcf33fa4a1b09c289c2e41903e608577998fbe22481a09c92ef8481343a2
+size 10605

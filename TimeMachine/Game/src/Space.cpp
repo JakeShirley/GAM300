d140916c3fbcf726e022f8e2657c7b7c78aa9b36
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-05-14 17:00:51 -0700

    Game logs error instead of crashing when trying to create an archetype that doesn't exist.

--- TimeMachine/Game/src/Space.cpp
+++ TimeMachine/Game/src/Space.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:469f3cd3b1c128ea53fbd0830aeb069d02ff2ea7002383f502cda557d0eb73ad
-size 23525
+oid sha256:0f3c1549a4bcfe3620254eed9e5162b760b69c0605adfae6db95f8950e85cbfe
+size 23589

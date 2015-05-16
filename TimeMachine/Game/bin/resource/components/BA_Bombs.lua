763abbb20d34747d55a027cce4c95f61e61f4432
Brian Inkrot <binkrot@gmail.com>
2015-05-16 12:31:30 -0700

    Basic reveal effect

--- TimeMachine/Game/bin/resource/components/BA_Bombs.lua
+++ TimeMachine/Game/bin/resource/components/BA_Bombs.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e130806e199b3a9a11697b3357421140b26c0675bd09032dfe6752ff0003591f
+oid sha256:b54ff800ecaacdbce9f01f46ec69254e1005f4b42e7c70f4b529728c383d62be
 size 4137

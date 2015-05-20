1e03c9a9de251d4f930574bd06f179a4d73a5ed3
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-05-19 22:36:42 -0700

    Made expensive matrix calculation happen per emitter instead of per particle, and perhaps optimized ParticleEngine's rotation matrix builder, depending on how far compiler magic goes.

--- TimeMachine/TMParticles/src/ParticleEngine.cpp
+++ TimeMachine/TMParticles/src/ParticleEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2ead77baaae35fad5e4d2a92c7aa657c15bf599b1aeb791e01a835de204f78d
-size 43280
+oid sha256:af19058a6f7c9ae96e4f9a4a0727f99a922b30ca5834d14f9e58453590596246
+size 43217
--- TimeMachine/TMParticles/src/ParticleEngine.h
+++ TimeMachine/TMParticles/src/ParticleEngine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3279930b99c968ff03070b8f58fee41e39e0308ba6bea77f6117447c58e4d4fa
-size 11831
+oid sha256:aa4b33b18bdd8332a8d524eca823125b668336acca05cbb6ea5d18d8c69ed377
+size 11976

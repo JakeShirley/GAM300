782b94a3c6d6571f0a736f8d743060ad4c7837f2
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-05-19 17:35:20 -0700

    Fixed the remaining rock shadow bug.

--- TimeMachine/Game/bin/resource/components/AvalancheSpawnerScript.lua
+++ TimeMachine/Game/bin/resource/components/AvalancheSpawnerScript.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db3cc04ac58dae9e69ab33df56f01be950da61ea29ee8ce4bf7bf39b91e98c04
-size 4809
+oid sha256:2abbcbcc18ba397637cb4ded9459724e6fc04e5fda081ef6241943d71fb18f73
+size 4810

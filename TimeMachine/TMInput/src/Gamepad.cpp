ae41f77cca25aa6ccecacb0427ec70df058e724f
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-05-27 21:21:39 -0700

    Threaded gamepad update that takes 2+ milliseconds off of my update at the cost of input being one frame delayed. If this offends anyone, it can be disabled by uncommenting a define at the top of Gamepad.cpp.

--- TimeMachine/TMInput/src/Gamepad.cpp
+++ TimeMachine/TMInput/src/Gamepad.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c55609eec603362f4a5c9d70ab31bbc454cd0c084b6e88a3e39d272ba2d14b5d
-size 12710
+oid sha256:7257f99ee9dab5eb5f6f18f86f2e056893ab1acd319186b709db4d9306a4e67b
+size 14215
--- TimeMachine/TMInput/src/Gamepad.h
+++ TimeMachine/TMInput/src/Gamepad.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e77543ca33a6593aaf6add3bfb5b03b0c318ee0a202cbcd937f9d687cd9c4c04
-size 1319
+oid sha256:80bfa1c5e84394f7cc5774c7e76d0434ee8c953a3f34c37a2565dd37b60b2e10
+size 1874

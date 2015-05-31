0d99dc13b0936c8f072f1cde9c814f5ddaa4586f
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-05-31 12:07:10 -0400

    Players now face the direction they are moving while rightstick input is not being given

--- TimeMachine/Game/bin/resource/components/WizardMovementController.lua
+++ TimeMachine/Game/bin/resource/components/WizardMovementController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6c87ba4f07816bb26942611f848637723b6f22b8feff9699d29dde480c37a87
-size 15854
+oid sha256:880def630f95556beaab9c970a7778014df69d16d8dfc0b3273cd302b0582f78
+size 16062

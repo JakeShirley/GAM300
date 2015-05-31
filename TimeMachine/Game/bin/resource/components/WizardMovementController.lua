f19c85cb24b2e83124ce8d846b279df432f6f660
Joe Bonaccorsi <joseph.bonaccorsi@digipen.edu>
2015-05-31 15:00:15 -0400

    Added bobbing effect to players that changes based on player speed.

--- TimeMachine/Game/bin/resource/components/WizardMovementController.lua
+++ TimeMachine/Game/bin/resource/components/WizardMovementController.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:880def630f95556beaab9c970a7778014df69d16d8dfc0b3273cd302b0582f78
-size 16062
+oid sha256:151377b2dc8a341ec9fe8c331d105938bae0d1a4ea6d585fe0328790324ccc01
+size 16956

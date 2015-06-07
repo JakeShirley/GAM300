4f7e55dcd970b2f97ad33055ae3e070c3cc7d323
Jake Shirley <navi@odinseye.org>
2015-06-07 12:52:23 -0700

    Removed unused "SpaceUninitializeEvent" class.

--- TimeMachine/Game/src/GameEvents.h
+++ TimeMachine/Game/src/GameEvents.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcc4da9cffc7cd84e873c628680adaa481a4823ee803701ffdc74e1cae0395f2
-size 3435
+oid sha256:ec4fa296c570fa2ba69ae3bb9cfc013184be801a118b6585292a5dddf78f4505
+size 3170

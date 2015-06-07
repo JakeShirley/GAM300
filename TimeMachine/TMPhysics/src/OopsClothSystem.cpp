15ccf468fdb6701b61c62a661e17e0a096c53586
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-06-06 20:18:11 -0700

    Fast square root thing for solving cloth constraints.

--- TimeMachine/TMPhysics/src/OopsClothSystem.cpp
+++ TimeMachine/TMPhysics/src/OopsClothSystem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca381cd72d7d077b57c96147a98d137afdcaee7f989c64d00812e5eaaca067de
-size 18924
+oid sha256:50fd86a97d66ee61acfa50846e2a14c8d8804a7f1565e72501d0ecc2e9f0e485
+size 19406

46619ff697d13f9dc53464267cef11b2c75a5156
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-06-09 13:08:18 -0700

    Get/Set Local X/Y/Z for Joezzfff.

--- TimeMachine/Game/src/Transform.cpp
+++ TimeMachine/Game/src/Transform.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec897b4ce18087cfcc6151ec78aa5cb772c6ab48b6d25c2da8ceef3b08333b0c
-size 22990
+oid sha256:3ca4fcbf7c0a46ab06b85d43bbf10030e7c288c0c1c83dd6799660d1b9ae1e70
+size 24007
--- TimeMachine/Game/src/Transform.h
+++ TimeMachine/Game/src/Transform.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae7dfaca24ba39d7f3822ba4ce901d6f1db4a0b330ab1bd1cbdc0afb3bc39351
-size 5003
+oid sha256:95c58663f0c179616bbaae9516a3726e09ff07d157e90695795c29ebf5498acb
+size 5147

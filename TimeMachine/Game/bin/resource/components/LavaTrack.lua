a23664ec82260f61f33717f5a087359c9f67ff72
Justin Merrill-Boyet <ecureuil21@gmail.com>
2015-06-20 12:43:44 -0700

    Minor adjustments to lava track, chains get detached when track starts

--- TimeMachine/Game/bin/resource/components/LavaTrack.lua
+++ TimeMachine/Game/bin/resource/components/LavaTrack.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:678fde6dda4be77cf3ab42f729bd31b50113775c34f018844dcc3078e2bd7b6a
-size 1457
+oid sha256:843f597bdb5be240bdc0395345e33962c3ef7f8b62be373b59f231cf1d44561b
+size 1871

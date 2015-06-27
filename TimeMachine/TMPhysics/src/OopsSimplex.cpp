090e9832e39b158fbc820a961a2e0c8a6ea04c2f
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-06-26 20:03:29 -0700

    Corrected edge cases in simplex operations, such as adding a fourth coplanar point and some solving issues.

--- TimeMachine/TMPhysics/src/OopsSimplex.cpp
+++ TimeMachine/TMPhysics/src/OopsSimplex.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf6b868ca0710194d42fa962ebdb8585372de3a30352483279dac7736c9ed084
-size 18122
+oid sha256:c9723e373b5d9cf512385f41e84fcda4943ea7e3baf9c0a068ddb4f760d7dd59
+size 17625
--- TimeMachine/TMPhysics/src/OopsSimplex.h
+++ TimeMachine/TMPhysics/src/OopsSimplex.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ac9db6aefdd045c539ef7cd10e1004106b03140408974e198a94f033d3b5682
-size 3006
+oid sha256:f16594843face5b68932ba4c734f55cf6f5b92faff6bdbf8540e1850d4049d1c
+size 2899

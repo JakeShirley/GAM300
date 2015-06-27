251b7729902ad1ca061a419814ffb0534e34e3b0
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-06-26 20:14:05 -0700

    Fixed bugs in initial simplex creation for convex hull causing suboptimal or even coplanar simplexes.

--- TimeMachine/TMPhysics/src/OopsQuickHull.cpp
+++ TimeMachine/TMPhysics/src/OopsQuickHull.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46460e2745b323ae83f7a039d479ec0afc4d1c97bb5b9b26809283bc2c506232
-size 6749
+oid sha256:ed345f40226ca40eadd9765e64f2dfc0186e29f46b7480c13b8cca032208f032
+size 5005
--- TimeMachine/TMPhysics/src/OopsQuickHull.h
+++ TimeMachine/TMPhysics/src/OopsQuickHull.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e23611d9e253035c81ac0f695ab40c4c59a7778e5ba936db8dbcb7b75ba9a2b6
-size 1388
+oid sha256:555c2b81b933ab68f9fd6d431e62e211d550a46a43aaab0743a3c4f0b7a58851
+size 1457

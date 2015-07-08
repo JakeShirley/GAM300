5227a97f33956a8904d4dcdfacbb60fb5fbd14a0
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-07-07 21:59:42 -0700

    Changed the way buoyancy is tracked to reduce bugs.

--- TimeMachine/TMPhysics/src/OopsCollider.h
+++ TimeMachine/TMPhysics/src/OopsCollider.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:daa98de2d19fba725cd41fbc91e83592d001c19995508ce4835ddde8c2efd88a
-size 6677
+oid sha256:9f75803cf61e16a1eeb9ad124f039394bbd84af0c0fc730c763627dbc4912cf3
+size 6819

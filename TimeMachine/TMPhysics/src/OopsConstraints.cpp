55a69668e6c3796857fdaf6bfe35a7389486375b
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-08-05 21:29:17 -0700

    Fixed an uninitialized value bug in ball and socket joint causing massive instability.

--- TimeMachine/TMPhysics/src/OopsConstraints.cpp
+++ TimeMachine/TMPhysics/src/OopsConstraints.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63023bfe565678301627598fe28fa60e8435ea636dd7489c92fc12edd6d0f366
-size 15481
+oid sha256:739affa9755757d0cfe80c0a5f62dcab14372888fb1d2f71a508ef69aaaa1054
+size 15412
--- TimeMachine/TMPhysics/src/OopsSphericalJoint.cpp
+++ TimeMachine/TMPhysics/src/OopsSphericalJoint.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4312f755891c1ef977eae5ba78bebe70bf99921b640e52dd2ced2fae2baed6a6
-size 3917
+oid sha256:4c0ed5b78eae321752997ec43c049b2c9186e1cfcd48f7a8fd14c54bb9ccb05e
+size 4166

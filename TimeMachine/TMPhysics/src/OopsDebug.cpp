d0b43141524ca21bf928eec2e349098efd1e0221
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-08-10 19:58:19 -0700

    Fixed right handed to left handed rotation matrix conversion so spheres drawn in physics rotate properly.

--- TimeMachine/TMPhysics/src/OopsDebug.cpp
+++ TimeMachine/TMPhysics/src/OopsDebug.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd1af5d6180dec8776320563ddfbf39f89f482ef93abe2ce352a8bf0e37eeabf
-size 3683
+oid sha256:5cf59f686d8f2b7b35a4d1fa7521f2551a4d6c5a087a08dc9692688527c915d4
+size 3725

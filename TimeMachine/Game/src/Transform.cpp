a140c621c23b6426b9780bf6c2c9ae48fa594cda
Tristan Schneider <forgottenplanet22@yahoo.com>
2015-08-22 15:24:41 -0700

    Fixed a bug causing hierarchies containing  non-physics and physics objects not to update collider positions when moved.

--- TimeMachine/Game/src/Transform.cpp
+++ TimeMachine/Game/src/Transform.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ca4fcbf7c0a46ab06b85d43bbf10030e7c288c0c1c83dd6799660d1b9ae1e70
-size 24007
+oid sha256:1b1f0744bf68367ab78e183ad8503df40e240094668e1a7bb51aa42d4b02b6a5
+size 24255
